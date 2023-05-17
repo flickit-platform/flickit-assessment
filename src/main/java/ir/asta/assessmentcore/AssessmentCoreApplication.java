57b3f57ba08c07b129dc8f3518b93677a46440e2
m.koohestani <angelmoonstar77@gmail.com>
2023-05-17 16:02:18 +0330

    Create project root and necessary files

---
+++ src/main/java/ir/asta/assessmentcore/AssessmentCoreApplication.java
@@ -0,0 +1,13 @@
+package ir.asta.assessmentcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/asta/assessmentcore/AssessmentCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.asta.assessmentcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
