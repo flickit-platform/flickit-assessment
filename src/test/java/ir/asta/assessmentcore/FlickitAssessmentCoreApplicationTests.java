9d5638da43e6779e6e325fba60a60850c2e32928
m.koohestani <angelmoonstar77@gmail.com>
2023-05-21 10:55:01 +0330

    Rename Project and root package

--- src/test/java/ir/asta/assessmentcore/FlickitAssessmentCoreApplicationTests.java
+++ src/test/java/ir/asta/assessmentcore/FlickitAssessmentCoreApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AssessmentCoreApplicationTests {
+class FlickitAssessmentCoreApplicationTests {
 
 	@Test
 	void contextLoads() {
