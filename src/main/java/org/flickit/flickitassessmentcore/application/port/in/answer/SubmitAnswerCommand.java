6dfb13274bb26d320a67c22b520bca9dbd08e377
mahdirezazadeh <riza.rs2000@gmail.com>
2023-06-24 16:36:47 +0330

    OTAT-521:feature:Add error messages for SubmitAnswerCommand

--- src/main/java/org/flickit/flickitassessmentcore/application/port/in/answer/SubmitAnswerCommand.java
+++ src/main/java/org/flickit/flickitassessmentcore/application/port/in/answer/SubmitAnswerCommand.java
@@ -6,16 +6,18 @@ import org.flickit.flickitassessmentcore.common.SelfValidating;
 
 import java.util.UUID;
 
+import static org.flickit.flickitassessmentcore.common.ErrorMessageKey.*;
+
 @Value
 public class SubmitAnswerCommand extends SelfValidating<SubmitAnswerCommand> {
 
-    @NotNull
+    @NotNull(message = SUBMIT_ANSWER_ASSESSMENT_RESULT_ID_NOT_NULL)
     UUID assessmentResultId;
 
-    @NotNull
+    @NotNull(message = SUBMIT_ANSWER_QUESTION_ID_NOT_NULL)
     Long questionId;
 
-    @NotNull
+    @NotNull(message = SUBMIT_ANSWER_ANSWER_OPTION_ID_NOT_NULL)
     Long answerOptionId;
 
     public SubmitAnswerCommand(UUID assessmentResultId, Long questionId, Long answerOptionId) {
--- src/main/java/org/flickit/flickitassessmentcore/common/ErrorMessageKey.java
+++ src/main/java/org/flickit/flickitassessmentcore/common/ErrorMessageKey.java
@@ -9,4 +9,7 @@ public class ErrorMessageKey {
     public static final String CREATE_ASSESSMENT_SPACE_ID_NOT_NULL = "create-assessment.spaceId.notNull";
     public static final String CREATE_ASSESSMENT_COLOR_ID_NOT_FOUND_MESSAGE = "create-assessment.colorId.notFound.message";
     public static final String SUBMIT_ANSWER_ASSESSMENT_RESULT_ID_NOT_FOUND_MESSAGE = "submit-answer.assessmentResultId.notFound.message";
+    public static final String SUBMIT_ANSWER_ASSESSMENT_RESULT_ID_NOT_NULL = "submit-answer.assessmentResultId.notNull";
+    public static final String SUBMIT_ANSWER_QUESTION_ID_NOT_NULL = "submit-answer.questionId.notNull";
+    public static final String SUBMIT_ANSWER_ANSWER_OPTION_ID_NOT_NULL = "submit-answer.answerOptionId.notNull";
 }
