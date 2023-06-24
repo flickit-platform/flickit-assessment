2da7ff4e185516112517baacca35f74128c23ebb
mahdirezazadeh <riza.rs2000@gmail.com>
2023-06-24 16:26:25 +0330

    OTAT-521:feature:Add javadoc to SaveAnswerPort and UpdateAnswerPort

--- src/main/java/org/flickit/flickitassessmentcore/application/port/out/answer/UpdateAnswerPort.java
+++ src/main/java/org/flickit/flickitassessmentcore/application/port/out/answer/UpdateAnswerPort.java
@@ -1,9 +1,14 @@
 package org.flickit.flickitassessmentcore.application.port.out.answer;
 
+import org.flickit.flickitassessmentcore.application.service.exception.ResourceNotFoundException;
+
 import java.util.UUID;
 
 public interface UpdateAnswerPort {
 
+    /**
+     * @throws ResourceNotFoundException if no assessmentResult found by the given id
+     */
     UUID update(Param param);
 
     record Param(UUID id,
