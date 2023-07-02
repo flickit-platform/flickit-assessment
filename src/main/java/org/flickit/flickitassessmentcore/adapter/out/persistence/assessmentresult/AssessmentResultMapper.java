bafe5766813f7e782abac0ceb0a579687aa15f6f
mahdirezazadeh <riza.rs2000@gmail.com>
2023-07-02 09:41:31 +0330

    OTAT-521:feature:Remove unused method

--- src/main/java/org/flickit/flickitassessmentcore/adapter/out/persistence/assessmentresult/AssessmentResultMapper.java
+++ src/main/java/org/flickit/flickitassessmentcore/adapter/out/persistence/assessmentresult/AssessmentResultMapper.java
@@ -1,19 +1,8 @@
 package org.flickit.flickitassessmentcore.adapter.out.persistence.assessmentresult;
 
-import org.flickit.flickitassessmentcore.adapter.out.persistence.assessment.AssessmentMapper;
 import org.flickit.flickitassessmentcore.application.port.out.assessmentresult.CreateAssessmentResultPort;
-import org.flickit.flickitassessmentcore.domain.AssessmentResult;
 
 public class AssessmentResultMapper {
-
-    public static AssessmentResult mapToDomainModel(AssessmentResultJpaEntity assessmentResultEntity) {
-        return new AssessmentResult(
-            assessmentResultEntity.getId(),
-            AssessmentMapper.mapToDomainModel(assessmentResultEntity.getAssessment()),
-            null,
-            assessmentResultEntity.getIsValid()
-        );
-    }
 
     public static AssessmentResultJpaEntity mapToJpaEntity(CreateAssessmentResultPort.Param param) {
         return new AssessmentResultJpaEntity(
