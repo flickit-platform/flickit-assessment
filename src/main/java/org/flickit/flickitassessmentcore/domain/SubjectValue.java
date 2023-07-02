4ac52a29ae3fcf62b663679af7afe8cfaec86451
mahdirezazadeh <riza.rs2000@gmail.com>
2023-07-02 11:41:54 +0330

    OTAT-521:feature:Rename AssessmentSubjectValue to SubjectValue

--- src/main/java/org/flickit/flickitassessmentcore/domain/SubjectValue.java
+++ src/main/java/org/flickit/flickitassessmentcore/domain/SubjectValue.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class AssessmentSubjectValue {
+public class SubjectValue {
     private UUID id;
     private AssessmentResult assessmentResult;
     private Long assessmentSubjectId;
