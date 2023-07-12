daeaba0cbd91fb459ddbe3e4928a0e8e20fabf01
m.khorsandi <khorsandi.mahnaz@gmail.com>
2023-07-12 23:30:22 +0330

    OTAT-553: Import LocaleDateTime

--- src/main/java/org/flickit/flickitassessmentcore/application/service/evidence/AddEvidenceService.java
+++ src/main/java/org/flickit/flickitassessmentcore/application/service/evidence/AddEvidenceService.java
@@ -6,6 +6,7 @@ import org.flickit.flickitassessmentcore.application.port.out.evidence.CreateEvi
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
