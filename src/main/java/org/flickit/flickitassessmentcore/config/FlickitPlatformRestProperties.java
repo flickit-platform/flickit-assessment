09964cd2fd9d8965494b3870362b904e93bb8cea
m.khorsandi <khorsandi.mahnaz@gmail.com>
2023-07-29 12:19:04 +0330

    OTAT-464: Refactor filikit platform rest urls

--- src/main/java/org/flickit/flickitassessmentcore/config/FlickitPlatformRestProperties.java
+++ src/main/java/org/flickit/flickitassessmentcore/config/FlickitPlatformRestProperties.java
@@ -17,25 +17,25 @@ public class FlickitPlatformRestProperties {
     private String baseUrl;
 
     @NotBlank
-    private String getSubjectIdsUrl = "/api/internal/assessment-kit/%d/assessment-subjects/";
+    private String getSubjectIdsUrl = "/api/internal/assessment-kits/%d/assessment-subjects/";
 
     @NotBlank
-    private String getQuestionImpactsUrl = "/api/internal/questionimpact/%d/";
+    private String getQuestionImpactByIdUrl = "/api/internal/question-impacts/%d/";
 
     @NotBlank
-    private String getQuestionsUrl = "/api/internal/quality-attribute/%d/question/";
+    private String getQuestionsUrl = "/api/internal/quality-attributes/%d/question/";
 
     @NotBlank
-    private String getQualityAttributesUrl = "/api/internal/assessment-subject/%d/quality-attributes/";
+    private String getQualityAttributesUrl = "/api/internal/assessment-subjects/%d/quality-attributes/";
 
     @NotBlank
-    private String getMaturityLevelsUrl = "/api/internal/assessment-kit/%d/maturity-levels/";
+    private String getMaturityLevelsUrl = "/api/internal/assessment-kits/%d/maturity-levels/";
 
     @NotBlank
-    private String getLevelCompetencesUrl = "/api/internal/maturity-level/%d/level-competences/";
+    private String getLevelCompetencesUrl = "/api/internal/maturity-levels/%d/level-competences/";
 
     @NotBlank
-    private String getAnswerOptionImpactsUrl = "/api/internal/answer-template/%d/%d/option-values/";
+    private String getAnswerOptionImpactsUrl = "/api/internal/answer-options/%d/%d/option-values/";
 
     @Valid
     private RestClientProps restClient = new RestClientProps();
