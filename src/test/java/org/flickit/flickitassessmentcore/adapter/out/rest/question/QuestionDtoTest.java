da72f19d7c56945a4fc9f65ab2fc4f9870727e6a
m.khorsandi <khorsandi.mahnaz@gmail.com>
2023-08-07 22:47:50 +0330

    FAC-1: Add tests for adapters dto classes

---
+++ src/test/java/org/flickit/flickitassessmentcore/adapter/out/rest/question/QuestionDtoTest.java
@@ -0,0 +1,34 @@
+package org.flickit.flickitassessmentcore.adapter.out.rest.question;
+
+import org.flickit.flickitassessmentcore.adapter.out.rest.questionImpact.QuestionImpactDto;
+import org.flickit.flickitassessmentcore.domain.calculate.Question;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class QuestionDtoTest {
+
+    @Test
+    void dtoToDomain() {
+        List<QuestionImpactDto> questionImpactDtoList = List.of(
+            new QuestionImpactDto(100L, 1, 110L, 120L),
+            new QuestionImpactDto(200L, 2, 210L, 220L)
+        );
+        QuestionDto dto = new QuestionDto(123L, questionImpactDtoList);
+
+        Question domain = dto.dtoToDomain();
+
+        assertNotNull(domain);
+        assertEquals(dto.id(), domain.getId());
+        assertNotNull(domain.getImpacts());
+        assertEquals(questionImpactDtoList.size(), domain.getImpacts().size());
+
+        for (int i = 0; i < questionImpactDtoList.size(); i++) {
+            assertEquals(questionImpactDtoList.get(i).weight(), domain.getImpacts().get(i).getWeight());
+            assertEquals(questionImpactDtoList.get(i).maturityLevelId(), domain.getImpacts().get(i).getMaturityLevelId());
+        }
+    }
+}
---
+++ src/test/java/org/flickit/flickitassessmentcore/adapter/out/rest/questionImpact/QuestionImpactDtoTest.java
@@ -0,0 +1,20 @@
+package org.flickit.flickitassessmentcore.adapter.out.rest.questionImpact;
+
+import org.flickit.flickitassessmentcore.domain.calculate.QuestionImpact;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionImpactDtoTest {
+
+    @Test
+    void dtoToDomain() {
+        QuestionImpactDto dto = new QuestionImpactDto(123L, 2, 234L, 345L);
+
+        QuestionImpact domain = dto.dtoToDomain();
+
+        assertNotNull(domain);
+        assertEquals(dto.weight(), domain.getWeight());
+        assertEquals(dto.maturityLevelId(), domain.getMaturityLevelId());
+    }
+}
