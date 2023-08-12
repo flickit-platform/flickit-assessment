bb6b924f6400e72714f607ec14afe74541a0300a
m.khorsandi <khorsandi.mahnaz@gmail.com>
2023-08-12 15:50:44 +0330

    FAC-1: Add test for Question

--- src/test/java/org/flickit/flickitassessmentcore/domain/calculate/QuestionTest.java
+++ src/test/java/org/flickit/flickitassessmentcore/domain/calculate/QuestionTest.java
@@ -1,5 +1,9 @@
 package org.flickit.flickitassessmentcore.domain.calculate;
 
+import org.flickit.flickitassessmentcore.domain.Question;
+import org.flickit.flickitassessmentcore.domain.QuestionImpact;
+import org.flickit.flickitassessmentcore.domain.calculate.mother.MaturityLevelMother;
+import org.flickit.flickitassessmentcore.domain.calculate.mother.QuestionMother;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,5 +12,11 @@ class QuestionTest {
 
     @Test
     void findImpactByMaturityLevel() {
+        Question question = QuestionMother.withImpactsOnLevel24();
+
+        QuestionImpact impact = question.findImpactByMaturityLevel(MaturityLevelMother.levelTwo());
+
+        assertNotNull(impact);
+        assertEquals(MaturityLevelMother.levelTwo().getId(), impact.getMaturityLevelId());
     }
 }
