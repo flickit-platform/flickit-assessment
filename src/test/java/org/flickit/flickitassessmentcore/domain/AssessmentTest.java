309466188ff15b9a2dffa23c2a2bf4adefe5b780
Mahnaz Khorsandi <khorsandi.mahnaz@gmail.com>
2023-08-25 11:59:19 +0330

    FAC-22: Refactor tests

--- src/test/java/org/flickit/flickitassessmentcore/domain/AssessmentTest.java
+++ src/test/java/org/flickit/flickitassessmentcore/domain/AssessmentTest.java
@@ -2,31 +2,21 @@ package org.flickit.flickitassessmentcore.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.flickit.flickitassessmentcore.domain.Assessment.generateSlugCode;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AssessmentTest {
+class AssessmentTest {
 
     @Test
     void generateSlugCode_NoWhitespace_ReturnsLowerCaseCode() {
-        String title = "ExampleTitle";
-        String code = Assessment.generateSlugCode(title);
+        assertEquals("exampletitle",
+            generateSlugCode("ExampleTitle"));
 
-        assertEquals("exampletitle", code);
+        assertEquals("with-whitespace",
+            generateSlugCode("With Whitespace"));
+
+        assertEquals("with-leading-and-trailing-whitespace",
+            generateSlugCode("  With   Leading and Trailing   Whitespace  "));
     }
 
-    @Test
-    void generateSlugCode_WithWhitespace_ReturnsLowerCaseCodeWithHyphens() {
-        String title = "Example Title with Whitespace";
-        String code = Assessment.generateSlugCode(title);
-
-        assertEquals("example-title-with-whitespace", code);
-    }
-
-    @Test
-    void generateSlugCode_WithLeadingAndTrailingWhitespace_ReturnsLowerCaseCodeWithHyphens() {
-        String title = "  Example Title with   Leading and Trailing   Whitespace  ";
-        String code = Assessment.generateSlugCode(title);
-
-        assertEquals("example-title-with-leading-and-trailing-whitespace", code);
-    }
 }
