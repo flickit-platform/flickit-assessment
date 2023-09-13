a25295b7ccc3efede19d218cad7a624df7a68eaf
m.koohestani <angelmoonstar77@gmail.com>
2023-09-13 15:11:02 +0330

    FAC-43: Remove unused import

--- src/main/java/org/flickit/flickitassessmentcore/application/port/out/question/LoadImpactfulQuestionsBySubjectPort.java
+++ src/main/java/org/flickit/flickitassessmentcore/application/port/out/question/LoadImpactfulQuestionsBySubjectPort.java
@@ -1,7 +1,6 @@
 package org.flickit.flickitassessmentcore.application.port.out.question;
 
 import org.flickit.flickitassessmentcore.adapter.out.rest.question.ImpactfulQuestionDto;
-import org.flickit.flickitassessmentcore.adapter.out.rest.question.QuestionDto;
 
 import java.util.List;
 
