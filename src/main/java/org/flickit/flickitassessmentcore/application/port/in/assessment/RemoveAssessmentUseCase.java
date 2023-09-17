428782bfae8d0522108f6be504325e4a06227ec2
mahdirezazadeh <riza.rs2000@gmail.com>
2023-09-17 14:20:58 +0330

    FAC-24:feature:Add EqualsAndHashCode annotation

--- src/main/java/org/flickit/flickitassessmentcore/application/port/in/assessment/RemoveAssessmentUseCase.java
+++ src/main/java/org/flickit/flickitassessmentcore/application/port/in/assessment/RemoveAssessmentUseCase.java
@@ -1,6 +1,7 @@
 package org.flickit.flickitassessmentcore.application.port.in.assessment;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.flickit.flickitassessmentcore.common.SelfValidating;
 
@@ -13,6 +14,7 @@ public interface RemoveAssessmentUseCase {
     void removeAssessment(Param param);
 
     @Value
+    @EqualsAndHashCode(callSuper = false)
     class Param extends SelfValidating<Param> {
 
         @NotNull(message = REMOVE_ASSESSMENT_ID_NOT_NULL)
