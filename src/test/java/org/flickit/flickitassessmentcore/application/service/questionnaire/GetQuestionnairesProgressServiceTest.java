5d42264c652ad8075482e6ed86a87cc085af1286
mahdirezazadeh <riza.rs2000@gmail.com>
2023-09-17 15:12:31 +0330

    FAC-37:feature:Add test assertion for result

--- src/test/java/org/flickit/flickitassessmentcore/application/service/questionnaire/GetQuestionnairesProgressServiceTest.java
+++ src/test/java/org/flickit/flickitassessmentcore/application/service/questionnaire/GetQuestionnairesProgressServiceTest.java
@@ -1,6 +1,8 @@
 package org.flickit.flickitassessmentcore.application.service.questionnaire;
 
 import org.flickit.flickitassessmentcore.application.port.in.questionnaire.GetQuestionnairesProgressUseCase.Param;
+import org.flickit.flickitassessmentcore.application.port.in.questionnaire.GetQuestionnairesProgressUseCase.QuestionnaireProgress;
+import org.flickit.flickitassessmentcore.application.port.in.questionnaire.GetQuestionnairesProgressUseCase.Result;
 import org.flickit.flickitassessmentcore.application.port.out.questionnaire.GetQuestionnairesProgressPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,12 +11,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class GetQuestionnairesProgressServiceTest {
@@ -30,11 +33,22 @@ class GetQuestionnairesProgressServiceTest {
         UUID assessmentId = UUID.randomUUID();
         Param useCaseParam = new Param(assessmentId);
 
-        service.getQuestionnairesProgress(useCaseParam);
+        List<QuestionnaireProgress> expectedQProgresses = Arrays.asList(new QuestionnaireProgress(1L, 15),
+            new QuestionnaireProgress(2L, 17));
+        when(getQuestionnairesProgressPort.getQuestionnairesProgressByAssessmentId(assessmentId)).thenReturn(expectedQProgresses);
+
+        Result questionnairesProgress = service.getQuestionnairesProgress(useCaseParam);
 
         ArgumentCaptor<UUID> portArgAssessmentId = ArgumentCaptor.forClass(UUID.class);
         verify(getQuestionnairesProgressPort).getQuestionnairesProgressByAssessmentId(portArgAssessmentId.capture());
 
+        assertEquals(expectedQProgresses.size(), questionnairesProgress.questionnairesProgress().size());
+        for (int progressIndex = 0; progressIndex < expectedQProgresses.size(); progressIndex++) {
+            QuestionnaireProgress expected = expectedQProgresses.get(progressIndex);
+            QuestionnaireProgress result = questionnairesProgress.questionnairesProgress().get(progressIndex);
+            assertEquals(expected.id(), result.id());
+            assertEquals(expected.answersCount(), result.answersCount());
+        }
         assertEquals(assessmentId, portArgAssessmentId.getValue());
 
         verify(getQuestionnairesProgressPort, times(1)).getQuestionnairesProgressByAssessmentId(any());
