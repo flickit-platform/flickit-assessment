268d83623a592c6ee7d48d71a2391267571c810f
m.koohestani <angelmoonstar77@gmail.com>
2023-10-14 13:30:56 +0330

    FAC-67: Create Regular coding rules arch unit test

---
+++ src/test/java/org/flickit/flickitassessmentcore/architecture/RegularCodingRulesArchUnitTest.java
@@ -0,0 +1,6 @@
+package org.flickit.flickitassessmentcore.architecture;
+
+public class RegularCodingRulesArchUnitTest {
+
+    public static final String PROJECT_ARTIFACT_ID = "org.flickit.flickitassessmentcore";
+}
