567670a42f55e2c62637ba1df7339ed06cc097b3
m.koohestani <angelmoonstar77@gmail.com>
2023-10-15 14:02:25 +0330

    FAC-69: Check cyclic dependency between classes

---
+++ src/test/java/org/flickit/flickitassessmentcore/architecture/constants/ArchUnitTestConstants.java
@@ -0,0 +1,8 @@
+package org.flickit.flickitassessmentcore.architecture.constants;
+
+public class ArchUnitTestConstants {
+
+    public static final String PROJECT_ARTIFACT_ID = "org.flickit.flickitassessmentcore";
+    public static final String ROOT_SLICE = "..flickitassessmentcore.(*)..";
+
+}
