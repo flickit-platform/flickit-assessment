d1ba4e2cfeb514136fc5ed5e3d204c94e35922ef
m.khorsandi <khorsandi.mahnaz@gmail.com>
2023-10-30 20:33:26 +0330

    FAC-92 Add an AutoConfig to each module

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/config/AssessmentKitAutoConfig.java
@@ -0,0 +1,9 @@
+package org.flickit.assessment.kit.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.flickit.assessment.kit")
+public class AssessmentKitAutoConfig {
+}
