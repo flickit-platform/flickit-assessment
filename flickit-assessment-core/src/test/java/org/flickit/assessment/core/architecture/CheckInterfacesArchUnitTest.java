4c301c1b80fb40eb437307e3feb5ffee501c7f4b
m.koohestani <angelmoonstar77@gmail.com>
2023-10-31 14:11:23 +0330

    FAC-74: Complete interface arch tests

---
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/architecture/CheckInterfacesArchUnitTest.java
@@ -0,0 +1,76 @@
+package org.flickit.assessment.core.architecture;
+
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
+
+import java.util.Set;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static java.util.stream.Collectors.joining;
+import static org.flickit.assessment.core.architecture.constants.ArchUnitTestConstants.*;
+
+@AnalyzeClasses(packages = PROJECT_ARTIFACT_ID, importOptions = ImportOption.DoNotIncludeTests.class)
+public class CheckInterfacesArchUnitTest {
+
+    @ArchTest
+    static final ArchRule ports_and_useCases_must_be_interface =
+        classes()
+            .that()
+            .areTopLevelClasses()
+            .and()
+            .resideInAnyPackage(APPLICATION_PORT_IN, APPLICATION_PORT_OUT)
+            .should()
+            .beInterfaces();
+
+    @ArchTest
+    static final ArchRule interfaces_must_not_be_placed_in_implementation_packages =
+        classes()
+            .that()
+            .resideInAnyPackage(APPLICATION_SERVICE, ADAPTER_OUT_PERSISTENCE)
+            .and()
+            .haveNameNotMatching(VIEW_OR_REPOSITORY_SUFFIX)
+            .should()
+            .notBeInterfaces();
+
+    @ArchTest
+    static final ArchRule ports_and_useCases_must_have_one_implementation =
+        classes()
+            .that()
+            .areTopLevelClasses()
+            .and()
+            .resideInAnyPackage(APPLICATION_PORT_IN, APPLICATION_PORT_OUT)
+            .should(haveAUniqueImplementation());
+
+
+    private static ArchCondition<JavaClass> haveAUniqueImplementation() {
+        return new ArchCondition<JavaClass>("have a unique implementation") {
+            @Override
+            public void check(JavaClass port, ConditionEvents events) {
+                events.add(new SimpleConditionEvent(port,
+                    port.getAllSubclasses().size() <= 1,
+                    describe(port)));
+            }
+
+            private String describe(JavaClass port) {
+                return String.format("%s is implemented by %s",
+                    port.getSimpleName(), joinNamesOf(port.getAllSubclasses()));
+            }
+
+            private String joinNamesOf(Set<JavaClass> implementations) {
+                if (implementations.isEmpty()) {
+                    return "";
+                }
+
+                return implementations.stream().map(JavaClass::getSimpleName).collect(joining(", "));
+            }
+        };
+    }
+
+
+}
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/architecture/constants/ArchUnitTestConstants.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/architecture/constants/ArchUnitTestConstants.java
@@ -4,4 +4,13 @@ public class ArchUnitTestConstants {
 
     public static final String PROJECT_ARTIFACT_ID = "org.flickit.assessment.core";
 
+    // Package
+    public final static String ADAPTER_OUT_PERSISTENCE = "..adapter.out.persistence..";
+    public final static String APPLICATION_SERVICE = "..application.service..";
+    public final static String APPLICATION_PORT_IN = "..application.port.in..";
+    public final static String APPLICATION_PORT_OUT = "..application.port.out..";
+
+    // Suffix
+    public final static String VIEW_OR_REPOSITORY_SUFFIX = "(.*)(View|Repository)";
+
 }
