a53e290e67bf69d2801cca0211663f10852c9f46
m.koohestani <angelmoonstar77@gmail.com>
2023-10-31 13:15:27 +0330

    FAC-71: Resolve conflicts

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/architecture/MethodOutputArchUnitTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/architecture/MethodOutputArchUnitTest.java
@@ -1,19 +1,19 @@
-package org.flickit.flickitassessmentcore.architecture;
+package org.flickit.assessment.core.architecture;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import org.flickit.flickitassessmentcore.application.domain.crud.PaginatedResponse;
+import org.flickit.assessment.core.application.domain.crud.PaginatedResponse;
 import org.springframework.http.ResponseEntity;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
-import static org.flickit.flickitassessmentcore.architecture.constants.ArchUnitTestConstants.*;
+import static org.flickit.assessment.core.architecture.constants.ArchUnitTestConstants.*;
 
 @AnalyzeClasses(packages = {APPLICATION_PORT_IN_FULL_PACKAGE, ADAPTER_IN_REST_FULL_PACKAGE})
 public class MethodOutputArchUnitTest {
 
     @ArchTest
-    static final ArchRule list_usecases_methods_should_have_paginated_response_output =
+    static final ArchRule list_useCases_methods_should_have_paginated_response_output =
         methods()
             .that()
             .areDeclaredInClassesThat()
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/architecture/constants/ArchUnitTestConstants.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/architecture/constants/ArchUnitTestConstants.java
@@ -4,4 +4,12 @@ public class ArchUnitTestConstants {
 
     public static final String PROJECT_ARTIFACT_ID = "org.flickit.assessment.core";
 
+    // Full Package
+    public final static String ADAPTER_IN_REST_FULL_PACKAGE = "org.flickit.assessment.core.adapter.in.rest";
+    public final static String APPLICATION_PORT_IN_FULL_PACKAGE = "org.flickit.assessment.core.application.port.in";
+
+    // Suffix
+    public final static String REST_CONTROLLER_SUFFIX = "RestController";
+    public final static String LIST_USE_CASE_SUFFIX = "ListUseCase";
+
 }
