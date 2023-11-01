882ff429bf04fc26b9951b0eaa412afb16c74dd1
m.koohestani <angelmoonstar77@gmail.com>
2023-11-01 12:44:14 +0330

    FAC-85: Separate and rename tests to their packages

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/architecture/constants/ArchUnitTestConstants.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/architecture/constants/ArchUnitTestConstants.java
@@ -9,8 +9,10 @@ public class ArchUnitTestConstants {
     public final static String ADAPTER_FULL_PACKAGE = "org.flickit.assessment.core.adapter";
     public final static String APPLICATION_PORT_IN_FULL_PACKAGE = "org.flickit.assessment.core.application.port.in";
     public final static String APPLICATION_PORT_OUT_FULL_PACKAGE = "org.flickit.assessment.core.application.port.out";
+    public final static String APPLICATION_PORT_FULL_PACKAGE = "org.flickit.assessment.core.application.port";
     public final static String APPLICATION_SERVICE_FULL_PACKAGE = "org.flickit.assessment.core.application.service";
     public final static String APPLICATION_DOMAIN_FULL_PACKAGE = "org.flickit.assessment.core.application.domain";
+    public final static String ADAPTER_IN_REST_FULL_PACKAGE = "org.flickit.assessment.core.adapter.in.rest";
 
     // Package
     public final static String ADAPTER_IN_REST = "..adapter.in.rest..";
@@ -32,33 +34,12 @@ public class ArchUnitTestConstants {
     public final static String ADAPTER_SUFFIX = "Adapter";
     public final static String REST_ADAPTER_SUFFIX = "RestAdapter";
     public final static String EXCEPTION_HANDLER_SUFFIX = "ExceptionHandler";
-
-    // Full Package
-    public final static String ADAPTER_IN_REST_FULL_PACKAGE = "org.flickit.assessment.core.adapter.in.rest";
-    public final static String APPLICATION_PORT_IN_FULL_PACKAGE = "org.flickit.assessment.core.application.port.in";
-
-    // Suffix
-    public final static String REST_CONTROLLER_SUFFIX = "RestController";
     public final static String LIST_USE_CASE_SUFFIX = "ListUseCase";
-
-    // Package
-    public final static String ADAPTER_IN_REST = "..adapter.in.rest..";
-    public final static String ADAPTER_OUT_PERSISTENCE = "..adapter.out.persistence..";
-    public final static String APPLICATION_SERVICE = "..application.service..";
-    public final static String ADAPTER_OUT_CALCULATE = "..adapter.out.calculate..";
-    public final static String ADAPTER_OUT_REPORT = "..adapter.out.report..";
-    public final static String ADAPTER_OUT_REST = "..adapter.out.rest..";
-    public final static String APPLICATION_PORT_IN = "..application.port.in..";
-
-    // Suffix
-    public final static String REST_CONTROLLER_SUFFIX = "RestController";
-    public final static String PERSISTENCE_JPA_ADAPTER_SUFFIX = "PersistenceJpaAdapter";
     public final static String JPA_ENTITY_SUFFIX = "JpaEntity";
-    public final static String REST_ADAPTER_SUFFIX = "RestAdapter";
     public final static String USE_CASE_PARAM_TEST_SUFFIX = "UseCaseParamTest";
     public final static String SERVICE_TEST = "ServiceTest";
     public final static String SERVICE_SUFFIX = "Service";
-    public final static String ADAPTER_SUFFIX = "Adapter";
     public final static String COLOR_SERVICE = "GetAssessmentColorsService";
+
 
 }
