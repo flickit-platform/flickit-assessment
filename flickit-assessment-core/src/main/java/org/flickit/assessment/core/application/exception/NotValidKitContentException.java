bbe89e98315f12d5fa8e066f84515a067540d0a4
m.koohestani <angelmoonstar77@gmail.com>
2023-11-11 15:31:52 +0330

    FAC-99: Create edit kit service and models and read from json

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/exception/NotValidKitContentException.java
@@ -0,0 +1,8 @@
+package org.flickit.assessment.core.application.exception;
+
+public class NotValidKitContentException extends RuntimeException {
+
+    public NotValidKitContentException(String message) {
+        super(message);
+    }
+}
