2e0ba2339ad326430fba6fb6823fc29876575fc1
m.koohestani <angelmoonstar77@gmail.com>
2023-11-13 14:01:22 +0330

    FAC-98: Rename

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/common/ErrorMessageKey.java
@@ -0,0 +1,7 @@
+package org.flickit.assessment.kit.common;
+
+public class ErrorMessageKey {
+
+    public static final String UPDATE_KIT_BY_DSL_KIT_ID_NOT_NULL = "update-kit-by-dsl.kitId.notNull";
+    public static final String UPDATE_KIT_BY_DSL_DSL_CONTENT_NOT_NULL = "update-kit-by-dsl.dsl-content.notNull";
+}
