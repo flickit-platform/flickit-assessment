06ccf9c6ad8ae511a49538844a3d08489d333cb7
m.khorsandi <khorsandi.mahnaz@gmail.com>
2023-11-20 22:35:11 +0330

    FAC-106: Remove unused exception classes

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/exception/NotValidKitChangesException.java
+++
@@ -1,8 +0,0 @@
-package org.flickit.assessment.kit.application.exception;
-
-public class NotValidKitChangesException extends RuntimeException {
-
-    public NotValidKitChangesException(String message) {
-        super(message);
-    }
-}
