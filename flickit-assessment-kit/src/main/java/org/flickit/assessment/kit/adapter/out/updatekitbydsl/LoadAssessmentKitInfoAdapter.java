16bc8638d077be2930fe042814a021d30eeb5cd0
m.koohestani <angelmoonstar77@gmail.com>
2023-11-25 14:24:42 +0330

    FAC-108: Refactor

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/updatekitbydsl/LoadAssessmentKitInfoAdapter.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/updatekitbydsl/LoadAssessmentKitInfoAdapter.java
@@ -1,46 +1,46 @@
 package org.flickit.assessment.kit.adapter.out.updatekitbydsl;
 
 import lombok.AllArgsConstructor;
+import org.flickit.assessment.data.jpa.kit.assessmentkit.AssessmentKitJpaEntity;
+import org.flickit.assessment.data.jpa.kit.assessmentkit.AssessmentKitJpaRepository;
 import org.flickit.assessment.kit.application.domain.AssessmentKit;
 import org.flickit.assessment.kit.application.domain.MaturityLevel;
 import org.flickit.assessment.kit.application.exception.ResourceNotFoundException;
 import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitInfoPort;
-import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitPort;
 import org.flickit.assessment.kit.application.port.out.levelcomptenece.LoadLevelCompetencesByMaturityLevelPort;
 import org.flickit.assessment.kit.application.port.out.maturitylevel.LoadMaturityLevelByKitPort;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
-import static org.flickit.assessment.kit.common.ErrorMessageKey.UPDATE_KIT_BY_DSL_KIT_NOT_FOUND;
+import static org.flickit.assessment.kit.common.ErrorMessageKey.FIND_KIT_ID_NOT_FOUND;
 
 @Component
 @AllArgsConstructor
 public class LoadAssessmentKitInfoAdapter implements LoadAssessmentKitInfoPort {
 
-    private final LoadAssessmentKitPort loadAssessmentKitPort;
+    private final AssessmentKitJpaRepository repository;
     private final LoadMaturityLevelByKitPort loadMaturityLevelByKitPort;
     private final LoadLevelCompetencesByMaturityLevelPort loadLevelCompetencesByMaturityLevelPort;
 
     @Override
     public AssessmentKit load(Long kitId) {
-        AssessmentKit assessmentKit = loadAssessmentKitPort.load(kitId).orElseThrow(
-            () -> new ResourceNotFoundException(UPDATE_KIT_BY_DSL_KIT_NOT_FOUND));
+        AssessmentKitJpaEntity entity = repository.findById(kitId).orElseThrow(
+            () -> new ResourceNotFoundException(FIND_KIT_ID_NOT_FOUND));
         List<MaturityLevel> levels = new ArrayList<>(loadMaturityLevelByKitPort.loadByKitId(kitId));
         setLevelCompetences(levels);
 
         return new AssessmentKit(
             kitId,
-            assessmentKit.getCode(),
-            assessmentKit.getTitle(),
-            assessmentKit.getSummary(),
-            assessmentKit.getAbout(),
-            assessmentKit.getCreationTime(),
-            assessmentKit.getLastModificationTime(),
-            assessmentKit.isPublished(),
-            assessmentKit.getExpertGroupId(),
+            entity.getCode(),
+            entity.getTitle(),
+            entity.getSummary(),
+            entity.getAbout(),
+            entity.getCreationTime(),
+            entity.getLastModificationTime(),
+            entity.getIsActive(),
+            entity.getExpertGroupId(),
             null,
             levels,
             null
--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/common/ErrorMessageKey.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/common/ErrorMessageKey.java
@@ -9,9 +9,8 @@ public class ErrorMessageKey {
     public static final String TRANSLATE_KIT_DSL_UNABLE_TO_PARSE_JSON = "translate-kit-dsl.unable.to.parse.json";
 
     public static final String FIND_MATURITY_LEVEL_ID_NOT_FOUND = "find-maturity-level.id.notFound";
+    public static final String FIND_KIT_ID_NOT_FOUND = "find-kit.id.notFound";
 
     public static final String UPDATE_KIT_BY_DSL_KIT_ID_NOT_NULL = "update-kit-by-dsl.kitId.notNull";
     public static final String UPDATE_KIT_BY_DSL_DSL_CONTENT_NOT_NULL = "update-kit-by-dsl.dsl-content.notNull";
-    public static final String UPDATE_KIT_BY_DSL_KIT_NOT_FOUND = "update-kit-by-dsl.kit.notFound";
-    public static final String UPDATE_KIT_BY_DSL_KIT_CHANGE_NOT_VALID = "update-kit-by-dsl.kit-change.notValid";
 }
