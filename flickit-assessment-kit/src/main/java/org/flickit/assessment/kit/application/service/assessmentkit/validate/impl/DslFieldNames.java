b11161f78efc5f451802f65617fafa2dba72f70e
m.khorsandi <khorsandi.mahnaz@gmail.com>
2023-11-28 19:14:04 +0330

    FAC-107: Add private NoArgsConstructor to DslFieldNames class

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/validate/impl/DslFieldNames.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/validate/impl/DslFieldNames.java
@@ -1,5 +1,9 @@
 package org.flickit.assessment.kit.application.service.assessmentkit.validate.impl;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DslFieldNames {
 
     public static final String SUBJECT = "subject";
