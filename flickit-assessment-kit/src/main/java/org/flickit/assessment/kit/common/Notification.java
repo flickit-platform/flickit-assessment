bcb232d2356cc81f53dfbc0c3c9a094b74541bed
mahdirezazadeh <riza.rs2000@gmail.com>
2023-11-28 09:52:46 +0330

    FAC-100:feature:Add equals, hashCode and toString to Notification.Error

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/common/Notification.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/common/Notification.java
@@ -2,7 +2,9 @@ package org.flickit.assessment.kit.common;
 
 import lombok.Getter;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Getter
@@ -25,5 +27,28 @@ public class Notification {
     }
 
     public record Error(String errorMessage, String... values) {
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Error error = (Error) o;
+            return Objects.equals(errorMessage, error.errorMessage) && Arrays.equals(values, error.values);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Objects.hash(errorMessage);
+            result = 31 * result + Arrays.hashCode(values);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Error{" +
+                "values=" + Arrays.toString(values) +
+                ", errorMessage=" + errorMessage +
+                '}';
+        }
     }
 }
