3c1cf03d15a89fdd72db7ecfedb1e7bec8b0aadc
Ali Sedaghat <a.sedaghat1372@gmail.com>
2023-12-05 23:32:38 +0330

    FAC-109 Refactor AttributeUpdateKitPersister

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/update/impl/AttributeUpdateKitPersister.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/update/impl/AttributeUpdateKitPersister.java
@@ -12,14 +12,10 @@ import org.flickit.assessment.kit.application.service.assessmentkit.update.Updat
 import org.flickit.assessment.kit.application.service.assessmentkit.update.UpdateKitPersisterContext;
 import org.flickit.assessment.kit.application.service.assessmentkit.update.UpdateKitPersisterResult;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -27,64 +23,71 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(propagation = Propagation.MANDATORY)
 public class AttributeUpdateKitPersister implements UpdateKitPersister {
 
     private final UpdateAttributePort updateAttributePort;
-
-    @Override
-    public UpdateKitPersisterResult persist(UpdateKitPersisterContext ctx, AssessmentKit savedKit, AssessmentKitDslModel dslKit) {
-
-        Map<String, Long> collect = savedKit.getSubjects().stream()
-            .collect(Collectors.toMap(Subject::getCode, Subject::getId));
-
-        Map<String, Attribute> codeToAttribute = savedKit.getSubjects().stream()
-            .map(Subject::getAttributes)
-            .flatMap(Collection::stream)
-            .collect(Collectors.toMap(Attribute::getCode, Function.identity()));
-
-
-        Map<String, Long> codeToSubjectId = new HashMap<>();
-
-        savedKit.getSubjects().forEach(e -> {
-            e.getAttributes().forEach(o -> codeToSubjectId.put(o.getCode(), e.getId()));
-        });
-
-        boolean shouldInvalidated = false;
-        for (AttributeDslModel e : dslKit.getAttributes()) {
-            String subjectCode = e.getSubjectCode();
-            Long subjectId = collect.get(subjectCode);
-            Long oldSubjectId = codeToSubjectId.get(e.getCode());
-            if (!Objects.equals(e.getWeight(), codeToAttribute.get(e.getCode()).getWeight()) || !Objects.equals(subjectId, oldSubjectId))
-                shouldInvalidated = true;
-
-            updateAttributePort.update(toUpdatePram(codeToAttribute.get(e.getCode()).getId(), subjectId, e));
-
-        }
-
-        Map<String, Long> codeToAttributeId = savedKit.getSubjects().stream()
-            .map(Subject::getAttributes)
-            .flatMap(Collection::stream)
-            .collect(Collectors.toMap(Attribute::getCode, Attribute::getId));
-
-        ctx.put(UpdateKitPersisterContext.KEY_ATTRIBUTES, codeToAttributeId);
-
-        return new UpdateKitPersisterResult(shouldInvalidated);
-    }
-
-    private UpdateAttributePort.Param toUpdatePram(long id, Long subjectId, AttributeDslModel e) {
-        return new UpdateAttributePort.Param(id,
-            e.getTitle(),
-            e.getIndex(),
-            e.getDescription(),
-            e.getWeight(),
-            LocalDateTime.now(),
-            subjectId);
-    }
 
     @Override
     public int order() {
         return 4;
     }
 
+    @Override
+    public UpdateKitPersisterResult persist(UpdateKitPersisterContext ctx, AssessmentKit savedKit, AssessmentKitDslModel dslKit) {
+
+        boolean shouldInvalidated = false;
+
+        Map<String, Long> subjectCodeToSubjectId = savedKit.getSubjects().stream()
+            .collect(Collectors.toMap(Subject::getCode, Subject::getId));
+
+        Map<String, AttributeDslModel> attrCodeToAttrDslModel = dslKit.getAttributes().stream()
+            .collect(Collectors.toMap(AttributeDslModel::getCode, Function.identity()));
+
+        for (Subject subject : savedKit.getSubjects()) {
+            String subjectCode = subject.getCode();
+            for (Attribute attribute : subject.getAttributes()) {
+
+                AttributeDslModel attributeDslModel = attrCodeToAttrDslModel.get(attribute.getCode());
+
+
+                if (!attribute.getTitle().equals(attributeDslModel.getTitle()) ||
+                    !attribute.getDescription().equals(attributeDslModel.getDescription()) ||
+                    !subjectCode.equals(attributeDslModel.getSubjectCode()) ||
+                    attribute.getIndex() != attributeDslModel.getIndex() ||
+                    attribute.getWeight() != attributeDslModel.getWeight()
+                    ) {
+
+                    updateAttributePort.update(toUpdatePram(attribute.getId(),
+                        subjectCodeToSubjectId.get(attributeDslModel.getSubjectCode()),
+                        attributeDslModel));
+                    log.debug("Attribute[id = {}, code = {}] updated!", attribute.getId(), attribute.getCode());
+                }
+
+                if (!subjectCode.equals(attributeDslModel.getSubjectCode()) ||
+                    attribute.getWeight() != attributeDslModel.getWeight()) {
+                    shouldInvalidated = true;
+                }
+            }
+        }
+
+
+        Map<String, Long> attrCodeToAttrId = savedKit.getSubjects().stream()
+            .map(Subject::getAttributes)
+            .flatMap(Collection::stream)
+            .collect(Collectors.toMap(Attribute::getCode, Attribute::getId));
+        ctx.put(UpdateKitPersisterContext.KEY_ATTRIBUTES, attrCodeToAttrId);
+        log.debug("Final attributes: {}", attrCodeToAttrId);
+
+        return new UpdateKitPersisterResult(shouldInvalidated);
+    }
+
+    private UpdateAttributePort.Param toUpdatePram(long id, Long subjectId, AttributeDslModel attributeDslModel) {
+        return new UpdateAttributePort.Param(id,
+            attributeDslModel.getTitle(),
+            attributeDslModel.getIndex(),
+            attributeDslModel.getDescription(),
+            attributeDslModel.getWeight(),
+            LocalDateTime.now(),
+            subjectId);
+    }
 }
