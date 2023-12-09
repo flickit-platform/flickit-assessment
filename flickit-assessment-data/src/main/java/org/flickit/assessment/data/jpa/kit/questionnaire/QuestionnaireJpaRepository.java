fe71757866ef74ec030f9d42bbc6a00e00b2b4cd
m.koohestani <angelmoonstar77@gmail.com>
2023-12-09 16:01:36 +0330

    FAC-144: Remove unused imports

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/questionnaire/QuestionnaireJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/questionnaire/QuestionnaireJpaRepository.java
@@ -1,11 +1,7 @@
 package org.flickit.assessment.data.jpa.kit.questionnaire;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface QuestionnaireJpaRepository extends JpaRepository<QuestionnaireJpaEntity, Long> {
