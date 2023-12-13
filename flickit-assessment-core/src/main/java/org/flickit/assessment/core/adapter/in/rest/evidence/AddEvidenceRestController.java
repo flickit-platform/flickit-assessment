f7e956517462c5ecafe6b7929d4b9fbb3a19247a
mahdirezazadeh <riza.rs2000@gmail.com>
2023-12-13 17:40:56 +0330

    TEMP:Test FAC-149 functionality

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/evidence/AddEvidenceRestController.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/evidence/AddEvidenceRestController.java
@@ -1,6 +1,9 @@
 package org.flickit.assessment.core.adapter.in.rest.evidence;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.flickit.assessment.common.config.jwt.UserDetail;
 import org.flickit.assessment.core.application.port.in.evidence.AddEvidenceUseCase;
 import org.flickit.assessment.core.application.port.in.evidence.AddEvidenceUseCase.Param;
 import org.springframework.http.HttpStatus;
@@ -9,14 +12,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AddEvidenceRestController {
 
     private final AddEvidenceUseCase useCase;
+    private final UserContext userContext;
 
     @PostMapping("/evidences")
     public ResponseEntity<AddEvidenceResponseDto> addEvidence(@RequestBody AddEvidenceRequestDto requestDto) {
+        UserDetail user = userContext.getUser();
+        log.info("User [{}] send request to add evidence", user.email());
         AddEvidenceUseCase.Result result = useCase.addEvidence(toParam(requestDto));
         return new ResponseEntity<>(toResponseDto(result), HttpStatus.CREATED);
     }
