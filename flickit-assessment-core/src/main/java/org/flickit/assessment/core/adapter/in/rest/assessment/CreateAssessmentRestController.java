c5e8ac7656a99557fb5f73ae9d50af2f70a6b705
m.koohestani <angelmoonstar77@gmail.com>
2023-12-18 16:13:42 +0330

    FAC-150: Remove test code

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessment/CreateAssessmentRestController.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessment/CreateAssessmentRestController.java
@@ -2,7 +2,6 @@ package org.flickit.assessment.core.adapter.in.rest.assessment;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.config.jwt.UserContext;
-import org.flickit.assessment.common.config.jwt.UserDetail;
 import org.flickit.assessment.core.application.port.in.assessment.CreateAssessmentUseCase;
 import org.flickit.assessment.core.application.port.in.assessment.CreateAssessmentUseCase.Param;
 import org.springframework.http.HttpStatus;
@@ -22,7 +21,6 @@ public class CreateAssessmentRestController {
 
     @PostMapping("/assessments")
     public ResponseEntity<CreateAssessmentResponseDto> createAssessment(@RequestBody CreateAssessmentRequestDto request) {
-        userContext.setUser(new UserDetail(UUID.fromString("00000000-0000-0000-0000-0000000003e8")));
         UUID currentUserId = userContext.getUser().id();
         CreateAssessmentResponseDto response = toResponseDto(useCase.createAssessment(toParam(request, currentUserId)));
         return new ResponseEntity<>(response, HttpStatus.CREATED);
