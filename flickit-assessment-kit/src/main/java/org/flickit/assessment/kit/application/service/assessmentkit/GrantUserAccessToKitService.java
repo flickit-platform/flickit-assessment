c95034b57e50a0d6f25433223f16739845beebb5
mahdirezazadeh <riza.rs2000@gmail.com>
2023-12-18 10:34:35 +0330

    FAC-146:feature:Move kit.application.exception.InvalidActionException to common.exception.AccessDeniedException

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/GrantUserAccessToKitService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/GrantUserAccessToKitService.java
@@ -2,8 +2,8 @@ package org.flickit.assessment.kit.application.service.assessmentkit;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.kit.application.domain.User;
-import org.flickit.assessment.kit.application.exception.InvalidActionException;
 import org.flickit.assessment.kit.application.port.in.assessmentkit.GrantUserAccessToKitUseCase;
 import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitOwnerPort;
 import org.flickit.assessment.kit.application.port.out.useraccess.GrantUserAccessToKitPort;
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Objects;
 
 import static org.flickit.assessment.kit.common.ErrorMessageKey.GRANT_USER_ACCESS_TO_KIT_CURRENT_USER_NOT_KIT_OWNER;
-import static org.flickit.assessment.kit.common.ErrorMessageKey.GRANT_USER_ACCESS_TO_KIT_USER_ALREADY_EXIST;
 
 @Slf4j
 @Service
@@ -29,15 +28,12 @@ public class GrantUserAccessToKitService implements GrantUserAccessToKitUseCase 
         validateCurrentUser(param);
         boolean isAccessUpdated = grantUserAccessToKitPort.grantUserAccessToKitByUserEmail(param.getKitId(), param.getUserEmail());
         log.debug("User [{}] granted access to kit [{}]", param.getUserEmail(), param.getKitId());
-        if (!isAccessUpdated) {
-            throw new InvalidActionException(GRANT_USER_ACCESS_TO_KIT_USER_ALREADY_EXIST);
-        }
     }
 
     private void validateCurrentUser(Param param) {
         User user = loadKitOwnerPort.loadKitOwnerById(param.getKitId());
         if (!Objects.equals(user.getId(), param.getCurrentUserId())) {
-            throw new InvalidActionException(GRANT_USER_ACCESS_TO_KIT_CURRENT_USER_NOT_KIT_OWNER);
+            throw new AccessDeniedException(GRANT_USER_ACCESS_TO_KIT_CURRENT_USER_NOT_KIT_OWNER);
         }
     }
 }
--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/assessmentkit/GrantUserAccessToKitServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/assessmentkit/GrantUserAccessToKitServiceTest.java
@@ -1,7 +1,7 @@
 package org.flickit.assessment.kit.application.service.assessmentkit;
 
+import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.kit.application.domain.User;
-import org.flickit.assessment.kit.application.exception.InvalidActionException;
 import org.flickit.assessment.kit.application.port.in.assessmentkit.GrantUserAccessToKitUseCase;
 import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitOwnerPort;
 import org.flickit.assessment.kit.application.port.out.useraccess.GrantUserAccessToKitPort;
@@ -16,7 +16,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.UUID;
 
 import static org.flickit.assessment.kit.common.ErrorMessageKey.GRANT_USER_ACCESS_TO_KIT_CURRENT_USER_NOT_KIT_OWNER;
-import static org.flickit.assessment.kit.common.ErrorMessageKey.GRANT_USER_ACCESS_TO_KIT_USER_ALREADY_EXIST;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -69,7 +68,7 @@ class GrantUserAccessToKitServiceTest {
         User kitOwner = UserMother.userWithId(UUID.randomUUID());
         when(loadKitOwnerPort.loadKitOwnerById(param.getKitId())).thenReturn(kitOwner);
 
-        var exception = assertThrows(InvalidActionException.class, () -> service.grantUserAccessToKit(param));
+        var exception = assertThrows(AccessDeniedException.class, () -> service.grantUserAccessToKit(param));
 
         assertEquals(GRANT_USER_ACCESS_TO_KIT_CURRENT_USER_NOT_KIT_OWNER, exception.getMessage());
         verify(loadKitOwnerPort, times(1)).loadKitOwnerById(any());
@@ -77,7 +76,7 @@ class GrantUserAccessToKitServiceTest {
     }
 
     @Test
-    void testGrantUserAccessToKit_UserAlreadyHasAccess_ThrowsException() {
+    void testGrantUserAccessToKit_UserAlreadyHasAccess_AddUserSuccessfully() {
         GrantUserAccessToKitUseCase.Param param = new GrantUserAccessToKitUseCase.Param(
             1L,
             "user@email.com",
@@ -88,10 +87,17 @@ class GrantUserAccessToKitServiceTest {
         when(grantUserAccessToKitPort.grantUserAccessToKitByUserEmail(param.getKitId(), param.getUserEmail()))
             .thenReturn(false);
 
-        var exception = assertThrows(InvalidActionException.class, () -> service.grantUserAccessToKit(param));
+        service.grantUserAccessToKit(param);
 
-        assertEquals(GRANT_USER_ACCESS_TO_KIT_USER_ALREADY_EXIST, exception.getMessage());
-        verify(loadKitOwnerPort, times(1)).loadKitOwnerById(any());
-        verify(grantUserAccessToKitPort, times(1)).grantUserAccessToKitByUserEmail(any(), any());
+        var loadKitOwnerParam = ArgumentCaptor.forClass(Long.class);
+        verify(loadKitOwnerPort, times(1)).loadKitOwnerById(loadKitOwnerParam.capture());
+        assertEquals(param.getKitId(), loadKitOwnerParam.getValue());
+
+        var grantAccessKitIdParam = ArgumentCaptor.forClass(Long.class);
+        var grantAccessUserEmailParam = ArgumentCaptor.forClass(String.class);
+        verify(grantUserAccessToKitPort, times(1))
+            .grantUserAccessToKitByUserEmail(grantAccessKitIdParam.capture(), grantAccessUserEmailParam.capture());
+        assertEquals(param.getKitId(), grantAccessKitIdParam.getValue());
+        assertEquals(param.getUserEmail(), grantAccessUserEmailParam.getValue());
     }
 }
