1ff9a4b0373ffed5fb8bbae89791ab5d87145577
m.koohestani <angelmoonstar77@gmail.com>
2023-12-18 12:24:30 +0330

    FAC-151: Refactor

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/answer/SubmitAnswerRequestDto.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/answer/SubmitAnswerRequestDto.java
@@ -1,6 +1,4 @@
 package org.flickit.assessment.core.adapter.in.rest.answer;
 
-import java.util.UUID;
-
-public record SubmitAnswerRequestDto(Long questionnaireId, Long questionId, Long answerOptionId, Integer confidenceLevelId, Boolean isNotApplicable, UUID createdBy) {
+public record SubmitAnswerRequestDto(Long questionnaireId, Long questionId, Long answerOptionId, Integer confidenceLevelId, Boolean isNotApplicable) {
 }
--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/answer/SubmitAnswerRestController.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/answer/SubmitAnswerRestController.java
@@ -1,6 +1,7 @@
 package org.flickit.assessment.core.adapter.in.rest.answer;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.config.jwt.UserContext;
 import org.flickit.assessment.core.application.port.in.answer.SubmitAnswerUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,16 +18,17 @@ public class SubmitAnswerRestController {
 
 
     private final SubmitAnswerUseCase useCase;
+    private final UserContext userContext;
 
     @PutMapping("/assessments/{assessmentId}/answer-question")
     public ResponseEntity<SubmitAnswerResponseDto> submitAnswer(@PathVariable("assessmentId") UUID assessmentId,
                                                                 @RequestBody SubmitAnswerRequestDto requestDto) {
-        // TODO: get created by id from jwt
-        SubmitAnswerResponseDto responseDto = toResponseDto(useCase.submitAnswer(toParam(assessmentId, requestDto)));
+        UUID currentUserId = userContext.getUser().id();
+        SubmitAnswerResponseDto responseDto = toResponseDto(useCase.submitAnswer(toParam(assessmentId, requestDto, currentUserId)));
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
-    private SubmitAnswerUseCase.Param toParam(UUID assessmentId, SubmitAnswerRequestDto requestDto) {
+    private SubmitAnswerUseCase.Param toParam(UUID assessmentId, SubmitAnswerRequestDto requestDto, UUID currentUserId) {
         return new SubmitAnswerUseCase.Param(
             assessmentId,
             requestDto.questionnaireId(),
@@ -34,7 +36,7 @@ public class SubmitAnswerRestController {
             requestDto.answerOptionId(),
             requestDto.confidenceLevelId(),
             requestDto.isNotApplicable(),
-            requestDto.createdBy()
+            currentUserId
         );
     }
 
