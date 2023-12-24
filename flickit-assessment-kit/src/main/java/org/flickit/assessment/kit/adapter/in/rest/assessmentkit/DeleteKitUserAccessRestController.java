7cf2a8615f4d8b04a2ee14f7be2c204ca796a6d7
m.koohestani <angelmoonstar77@gmail.com>
2023-12-24 11:42:35 +0330

    FAC-162: Change kit user access input from email to id

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/assessmentkit/DeleteKitUserAccessRestController.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/assessmentkit/DeleteKitUserAccessRestController.java
@@ -18,13 +18,13 @@ public class DeleteKitUserAccessRestController {
 
     @DeleteMapping("assessment-kits/{kitId}/users")
     public ResponseEntity<Void> deleteKitUserAccess(@PathVariable("kitId") Long kitId,
-                                                 @RequestParam("email") String email) {
+                                                 @RequestParam("userId") UUID userId) {
         UUID currentUserId = userContext.getUser().id();
-        useCase.delete(toParam(kitId, email, currentUserId));
+        useCase.delete(toParam(kitId, userId, currentUserId));
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    private DeleteKitUserAccessUseCase.Param toParam(Long kitId, String email, UUID currentUserId) {
-        return new DeleteKitUserAccessUseCase.Param(kitId, email, currentUserId);
+    private DeleteKitUserAccessUseCase.Param toParam(Long kitId, UUID userId, UUID currentUserId) {
+        return new DeleteKitUserAccessUseCase.Param(kitId, userId, currentUserId);
     }
 }
