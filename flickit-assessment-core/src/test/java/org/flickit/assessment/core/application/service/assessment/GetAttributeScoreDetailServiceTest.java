4e4af411eaa392c06f96f52ef74d857863217221
m.khorsandi <khorsandi.mahnaz@gmail.com>
2023-12-25 21:18:58 +0330

    FAC-126: Add service test

---
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/GetAttributeScoreDetailServiceTest.java
@@ -0,0 +1,129 @@
+package org.flickit.assessment.core.application.service.assessment;
+
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.core.application.port.in.assessment.GetAttributeScoreDetailUseCase;
+import org.flickit.assessment.core.application.port.out.assessment.CheckUserAssessmentAccessPort;
+import org.flickit.assessment.core.application.port.out.question.LoadAttributeScoreDetailPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.core.application.port.in.assessment.GetAttributeScoreDetailUseCase.Param;
+import static org.flickit.assessment.core.application.port.in.assessment.GetAttributeScoreDetailUseCase.QuestionScore;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class GetAttributeScoreDetailServiceTest {
+
+    @InjectMocks
+    private GetAttributeScoreDetailService service;
+
+    @Mock
+    private LoadAttributeScoreDetailPort loadAttributeScoreDetailPort;
+
+    @Mock
+    private CheckUserAssessmentAccessPort checkUserAssessmentAccessPort;
+
+    @Test
+    void testGetAttributeScoreDetail_ValidParam() {
+        UUID assessmentId = UUID.randomUUID();
+        long attributeId = 123L;
+        long maturityLevelId = 124L;
+        UUID currentUserId = UUID.randomUUID();
+        Param param = new Param(
+            assessmentId,
+            attributeId,
+            maturityLevelId,
+            currentUserId);
+
+        List<QuestionScore> scores = List.of(
+            new QuestionScore(
+                "DevOps",
+                1,
+                "Do you have CI/CD?",
+                2,
+                2,
+                "Yes",
+                0.5,
+                1.0),
+            new QuestionScore(
+                "DevOps",
+                3,
+                "Do you have Test?",
+                1,
+                1,
+                "NO",
+                0.0,
+                0.0),
+            new QuestionScore(
+                "Technology",
+                10,
+                "Do you have Technology?",
+                4,
+                null,
+                null,
+                null,
+                0.0)
+        );
+        when(loadAttributeScoreDetailPort.load(attributeId, maturityLevelId, assessmentId)).thenReturn(scores);
+        when(checkUserAssessmentAccessPort.hasAccess(assessmentId, currentUserId)).thenReturn(true);
+
+        GetAttributeScoreDetailUseCase.Result result = service.getAttributeScoreDetail(param);
+
+        assertNotNull(result);
+        assertEquals(1, result.gainedScore());
+        assertEquals(7, result.totalScore());
+        assertEquals(3, result.questionScores().size());
+    }
+
+    @Test
+    void testGetAttributeScoreDetail_ValidParam_NoQuestionScore() {
+        UUID assessmentId = UUID.randomUUID();
+        long attributeId = 123L;
+        long maturityLevelId = 124L;
+        UUID currentUserId = UUID.randomUUID();
+        Param param = new Param(
+            assessmentId,
+            attributeId,
+            maturityLevelId,
+            currentUserId);
+
+        List<QuestionScore> scores = List.of();
+        when(loadAttributeScoreDetailPort.load(attributeId, maturityLevelId, assessmentId)).thenReturn(scores);
+        when(checkUserAssessmentAccessPort.hasAccess(assessmentId, currentUserId)).thenReturn(true);
+
+        GetAttributeScoreDetailUseCase.Result result = service.getAttributeScoreDetail(param);
+
+        assertNotNull(result);
+        assertEquals(0, result.gainedScore());
+        assertEquals(0, result.totalScore());
+        assertEquals(0, result.questionScores().size());
+    }
+
+    @Test
+    void testGetAttributeScoreDetail_InvalidCurrentUser_ThrowsException() {
+        UUID assessmentId = UUID.randomUUID();
+        long attributeId = 123L;
+        long maturityLevelId = 124L;
+        UUID currentUserId = UUID.randomUUID();
+        Param param = new Param(
+            assessmentId,
+            attributeId,
+            maturityLevelId,
+            currentUserId);
+
+        when(checkUserAssessmentAccessPort.hasAccess(assessmentId, currentUserId)).thenReturn(false);
+
+        var throwable = assertThrows(AccessDeniedException.class,
+            () -> service.getAttributeScoreDetail(param));
+        assertThat(throwable).hasMessage(COMMON_CURRENT_USER_NOT_ALLOWED);
+    }
+}
