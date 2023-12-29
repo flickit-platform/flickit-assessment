fe144279d0342d2913f02cead7b7bba99b40538d
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2023-12-29 14:07:02 +0330

    Domain model for ExpertGroup refined

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/domain/ExpertGroup.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/domain/ExpertGroup.java
@@ -1,8 +1,22 @@
 package org.flickit.assessment.kit.application.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.flickit.assessment.kit.application.port.in.assessmentkit.GetKitUserListUseCase;
+import org.springframework.boot.actuate.endpoint.web.Link;
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExpertGroup {
 
     long id;
+    String name;
+    String bio;
+    Link picture;
+    GetKitUserListUseCase.UserListItem members;
+    int membersCount;
+    int publishedKitsCount;
+    boolean editable;
     public ExpertGroup(Long id) {
         this.id = id;
     }
