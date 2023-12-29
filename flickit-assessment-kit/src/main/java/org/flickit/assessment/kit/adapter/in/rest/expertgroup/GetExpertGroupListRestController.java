ce94b6e5d807a07834b116e2a1c6d986c1e06718
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2023-12-29 14:06:00 +0330

    Correcting some small sonar issues

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/expertgroup/GetExpertGroupListRestController.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/expertgroup/GetExpertGroupListRestController.java
@@ -9,7 +9,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.flickit.assessment.kit.application.port.in.expertgroup.GetExpertGroupListUseCase;
 
 @Validated
 @RestController
--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/expertgroup/LoadExpertGroupListPort.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/expertgroup/LoadExpertGroupListPort.java
@@ -3,9 +3,6 @@ package org.flickit.assessment.kit.application.port.out.expertgroup;
 import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
 import org.flickit.assessment.kit.application.port.in.expertgroup.GetExpertGroupListUseCase;
 
-import java.util.Optional;
-import java.util.UUID;
-
 public interface LoadExpertGroupListPort {
     PaginatedResponse<GetExpertGroupListUseCase.ExpertGroupListItem> loadExpertGroupList(Param param);
     record Param(int page, int size) {
