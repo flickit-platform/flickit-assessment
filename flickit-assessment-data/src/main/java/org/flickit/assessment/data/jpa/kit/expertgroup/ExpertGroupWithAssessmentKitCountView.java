9b5615c90e4105f1e9e4ef1ac2f4b4131352858d
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2023-12-30 14:31:54 +0330

    New changes 2 (temp)

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/expertgroup/ExpertGroupWithAssessmentKitCountView.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/expertgroup/ExpertGroupWithAssessmentKitCountView.java
@@ -1,11 +1,25 @@
 package org.flickit.assessment.data.jpa.kit.expertgroup;
 
+import jakarta.persistence.Column;
 import org.flickit.assessment.data.jpa.core.assessment.AssessmentJpaEntity;
+
+import java.util.UUID;
 
 // 1) To incorporate a new field named 'publishedKitsCount' into the JPA Entity, I have created the following interface:
 
 public interface ExpertGroupWithAssessmentKitCountView {
-    ExpertGroupJpaEntity getExpertGroup();
+    Long getId();
 
+    String getName();
+
+    String getAbout();
+
+    String getPicture();
+
+    String getWebsite();
+
+    String getBio();
+
+    UUID getOwnerId();
     Long getPublishedKitsCount();
 }
