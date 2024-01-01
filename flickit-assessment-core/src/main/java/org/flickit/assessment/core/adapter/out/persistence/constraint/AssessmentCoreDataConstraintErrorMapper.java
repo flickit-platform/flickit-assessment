2c3d48a6a6f1091f70f7b969356d75108f8f4cc5
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-01-01 19:03:30 +0330

    FAC-152: Add foreign keys to ConstraintErrorMapper

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/constraint/AssessmentCoreDataConstraintErrorMapper.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/constraint/AssessmentCoreDataConstraintErrorMapper.java
@@ -5,23 +5,26 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+import static java.util.Map.entry;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_FOUND;
 import static org.flickit.assessment.core.common.ErrorMessageKey.*;
 
 @Component
 public class AssessmentCoreDataConstraintErrorMapper implements DataConstraintErrorMapper {
 
-    Map<String, String> constraintToErrorMsg = Map.of(
-        "uq_fac_assessment_spaceid_code_deletion_time", CREATE_ASSESSMENT_DUPLICATE_TITLE_AND_SPACE_ID,
-        "fk_fac_assessmentresult_assessment", CREATE_ASSESSMENT_RESULT_ASSESSMENT_ID_NOT_FOUND,
-        "fk_fac_qualityattributevalue_assessmentresult", CREATE_QUALITY_ATTRIBUTE_VALUE_ASSESSMENT_RESULT_ID_NOT_FOUND,
-        "fk_fac_subjectvalue_assessmentresult", CREATE_SUBJECT_VALUE_ASSESSMENT_RESULT_ID_NOT_FOUND,
-        "fk_fac_answer_assessmentresult", SUBMIT_ANSWER_ASSESSMENT_RESULT_NOT_FOUND,
-        "fk_fac_evidence_assessment", ADD_EVIDENCE_ASSESSMENT_ID_NOT_FOUND,
-        "fk_fac_assessment_account_user_created_by", COMMON_CURRENT_USER_NOT_FOUND,
-        "fk_fac_assessment_account_user_last_modified_by", COMMON_CURRENT_USER_NOT_FOUND);
-        "fk_fac_evidence_account_user_created_by", COMMON_CURRENT_USER_NOT_FOUND,
-        "fk_fac_evidence_account_user_last_modified_by_id", COMMON_CURRENT_USER_NOT_FOUND);
+    Map<String, String> constraintToErrorMsg = Map.ofEntries(
+        entry("uq_fac_assessment_spaceid_code_deletion_time", CREATE_ASSESSMENT_DUPLICATE_TITLE_AND_SPACE_ID),
+        entry("fk_fac_assessmentresult_assessment", CREATE_ASSESSMENT_RESULT_ASSESSMENT_ID_NOT_FOUND),
+        entry("fk_fac_qualityattributevalue_assessmentresult", CREATE_QUALITY_ATTRIBUTE_VALUE_ASSESSMENT_RESULT_ID_NOT_FOUND),
+        entry("fk_fac_subjectvalue_assessmentresult", CREATE_SUBJECT_VALUE_ASSESSMENT_RESULT_ID_NOT_FOUND),
+        entry("fk_fac_answer_assessmentresult", SUBMIT_ANSWER_ASSESSMENT_RESULT_NOT_FOUND),
+        entry("fk_fac_answer_account_user_created_by", COMMON_CURRENT_USER_NOT_FOUND),
+        entry("fk_fac_answer_account_user_last_modified_by", COMMON_CURRENT_USER_NOT_FOUND),
+        entry("fk_fac_evidence_assessment", ADD_EVIDENCE_ASSESSMENT_ID_NOT_FOUND),
+        entry("fk_fac_assessment_account_user_created_by", COMMON_CURRENT_USER_NOT_FOUND),
+        entry("fk_fac_assessment_account_user_last_modified_by", COMMON_CURRENT_USER_NOT_FOUND),
+        entry("fk_fac_evidence_account_user_created_by", COMMON_CURRENT_USER_NOT_FOUND),
+        entry("fk_fac_evidence_account_user_last_modified_by_id", COMMON_CURRENT_USER_NOT_FOUND));
 
     @Override
     public boolean contains(String constraintName) {
