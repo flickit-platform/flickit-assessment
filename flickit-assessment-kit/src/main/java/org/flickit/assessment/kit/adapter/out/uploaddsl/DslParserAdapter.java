dd5d2c79a6f082aa0b3351b720d107f5190a38b0
m.koohestani <angelmoonstar77@gmail.com>
2024-01-07 14:43:03 +0330

    FAC-179: Resolve zip bomb attack

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/uploaddsl/DslParserAdapter.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/uploaddsl/DslParserAdapter.java
@@ -2,7 +2,6 @@ package org.flickit.assessment.kit.adapter.out.uploaddsl;
 
 import lombok.AllArgsConstructor;
 import org.flickit.assessment.kit.adapter.out.uploaddsl.exception.DSLHasSyntaxErrorException;
-import org.flickit.assessment.kit.adapter.out.uploaddsl.exception.ZipBombException;
 import org.flickit.assessment.kit.application.domain.dsl.AssessmentKitDslModel;
 import org.flickit.assessment.kit.application.port.out.assessmentkit.GetDslContentPort;
 import org.flickit.assessment.kit.config.DslParserRestProperties;
@@ -16,6 +15,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -38,10 +38,10 @@ public class DslParserAdapter implements GetDslContentPort {
         HttpEntity<AssessmentKitRequest> requestEntity = new HttpEntity<>(new AssessmentKitRequest(dslContent), headers);
         try {
             var responseEntity = dslParserRestTemplate.exchange(
-                    properties.getUrl(),
-                    HttpMethod.POST,
-                    requestEntity,
-                    AssessmentKitDslModel.class
+                properties.getUrl(),
+                HttpMethod.POST,
+                requestEntity,
+                AssessmentKitDslModel.class
             );
             return responseEntity.getBody();
         } catch (HttpClientErrorException e) {
@@ -49,43 +49,89 @@ public class DslParserAdapter implements GetDslContentPort {
         }
     }
 
-    private String uniteDslFiles(MultipartFile dslFile) {
+/*    private String uniteDslFiles(MultipartFile dslFile) {
         try (InputStream dslFileStream = dslFile.getInputStream();
              ZipInputStream zipInputStream = new ZipInputStream(dslFileStream)) {
             StringBuilder allContent = new StringBuilder();
-            ZipEntry entry = checkZipBombAttack(zipInputStream);
-            while (entry != null) {
-                long decompressedSize = entry.getSize();
-                if (decompressedSize > 100_000_000) {
-                    throw new ZipBombException();
-                }
-
+            ZipEntry entry;
+            while ((entry = zipInputStream.getNextEntry()) != null) {
                 String name = entry.getName();
                 if (name.endsWith(".ak")) {
                     String fileBaseName = name.substring(name.lastIndexOf('/') + 1);
                     String content = StreamUtils.copyToString(zipInputStream, StandardCharsets.UTF_8);
                     String trimContent = trimContent(content);
                     allContent.append("\n")
-                            .append("// BEGIN FILE ").append(fileBaseName)
-                            .append(trimContent);
+                        .append("// BEGIN FILE ").append(fileBaseName)
+                        .append(trimContent);
                 }
-
-                entry = checkZipBombAttack(zipInputStream);
             }
             zipInputStream.closeEntry();
             return allContent.toString();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }*/
+
+    private String uniteDslFiles(MultipartFile dslFile) {
+        try (InputStream dslFileStream = dslFile.getInputStream();
+             ZipInputStream zipInputStream = new ZipInputStream(dslFileStream)) {
+
+            StringBuilder allContent = new StringBuilder();
+            ZipEntry entry;
+
+            while ((entry = zipInputStream.getNextEntry()) != null) {
+                String name = entry.getName();
+
+                if (name.endsWith(".ak")) {
+                    String fileBaseName = name.substring(name.lastIndexOf('/') + 1);
+
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[2048];
+                    int bytesRead;
+
+                    while ((bytesRead = zipInputStream.read(buffer)) > 0) {
+                        byteArrayOutputStream.write(buffer, 0, bytesRead);
+                    }
+
+                    byte[] contentBytes = byteArrayOutputStream.toByteArray();
+                    byteArrayOutputStream.close();
+
+                    if (isZipBombAttack(contentBytes)) {
+                        throw new SecurityException("Detected Zip Bomb Attack.");
+                    }
+
+                    String content = new String(contentBytes, StandardCharsets.UTF_8);
+                    String trimContent = trimContent(content);
+
+                    allContent.append("\n")
+                        .append("// BEGIN FILE ").append(fileBaseName)
+                        .append(trimContent);
+                }
+            }
+
+            return allContent.toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private ZipEntry checkZipBombAttack(ZipInputStream zipInputStream) throws IOException {
-        ZipEntry entry;
-        if ((entry = zipInputStream.getNextEntry()) != null && entry.getSize() > 100_000_000) {
-            throw new ZipBombException();
+    private boolean isZipBombAttack(byte[] contentBytes) {
+        int thresholdSize = 1000000000; // 1 GB
+        double thresholdRatio = 10;
+        int totalSizeEntry = contentBytes.length;
+        double compressionRatio = totalSizeEntry / (double) totalSizeEntry; // No compression in this case
+        if (compressionRatio > thresholdRatio) {
+            // Ratio between compressed and uncompressed data is highly suspicious, looks like a Zip Bomb Attack
+            return true;
         }
-        return entry;
+        if (totalSizeEntry > thresholdSize) {
+            // The uncompressed data size is too much for the application resource capacity
+            return true;
+        }
+        return false;
     }
+
 
     private String trimContent(String content) {
         StringBuilder newContent = new StringBuilder();
