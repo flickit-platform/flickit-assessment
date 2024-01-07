cceb32b761ce2f2e3b2881d79d30cbd4455ec77c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-01-07 18:22:05 +0330

    Api implemented

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/expertgroup/CreateExpertGroupService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/expertgroup/CreateExpertGroupService.java
@@ -1,12 +1,31 @@
 package org.flickit.assessment.kit.application.service.expertgroup;
 
+import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.kit.application.port.in.expertgroup.CreateExpertGroupUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroup.CreateExpertGroupPort;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CreateExpertGroupService implements CreateExpertGroupUseCase {
+
+    private final CreateExpertGroupPort createExpertGroupPort;
     @Override
     public Result createExpertGroup(Param param) {
-        return null;
+        CreateExpertGroupPort.Param portParam = toParam(param);
+        long id = createExpertGroupPort.persist(portParam);
+        return new Result(id);
+    }
+
+    private CreateExpertGroupPort.Param toParam(Param param) {
+
+        return new CreateExpertGroupPort.Param(
+            param.getName(),
+            param.getAbout(),
+            param.getPicture(),
+            param.getWebsite(),
+            param.getBio(),
+            param.getCurrentUserId()
+        );
     }
 }
---
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroup/CreateExpertGroupServiceTest.java
@@ -0,0 +1,40 @@
+package org.flickit.assessment.kit.application.service.expertgroup;
+
+import org.flickit.assessment.kit.application.port.in.expertgroup.CreateExpertGroupUseCase;
+import org.flickit.assessment.kit.application.port.in.expertgroup.CreateExpertGroupUseCase.Param;
+import org.flickit.assessment.kit.application.port.out.expertgroup.CreateExpertGroupPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Random;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class CreateExpertGroupServiceTest {
+
+    @InjectMocks
+    private CreateExpertGroupService service;
+    @Mock
+    private CreateExpertGroupPort createExpertGroupPort;
+
+    @Test
+    void testCreateExpertGroup_validParams_persistResult() {
+        UUID currentUserId = UUID.randomUUID();
+        Param param = new Param("Expert Group Name",
+            "Expert Group Bio",
+            "Expert Group About",
+            "Expert Group Website",
+            "Expert Group About picture",
+            currentUserId);
+        long expectedId = new  Random().nextLong();
+        when(createExpertGroupPort.persist(any(CreateExpertGroupPort.Param.class))).thenReturn(expectedId);
+
+        CreateExpertGroupUseCase.Result result = service.createExpertGroup(param);
+        assertEquals(expectedId, result.id(), "The result should be long ID");
+    }
+}
