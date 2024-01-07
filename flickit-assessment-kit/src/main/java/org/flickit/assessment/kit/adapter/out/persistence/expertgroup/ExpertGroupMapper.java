6f249bc7535277fde77b608c232cd8fb2b25ab25
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-01-07 18:31:58 +0330

    Minor sonar related issue fixed

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/expertgroup/ExpertGroupMapper.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/expertgroup/ExpertGroupMapper.java
@@ -4,6 +4,8 @@ import org.flickit.assessment.data.jpa.kit.expertgroup.ExpertGroupJpaEntity;
 import org.flickit.assessment.kit.application.port.out.expertgroup.CreateExpertGroupPort;
 
 public class ExpertGroupMapper {
+
+    private ExpertGroupMapper(){}
     static ExpertGroupJpaEntity mapCreateParamToJpaEntity(CreateExpertGroupPort.Param param) {
         return new ExpertGroupJpaEntity(
             null,
