90dd6f62270ea5de21d0eb3c9efa52eefec18e23
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-01-13 08:39:39 +0330

    FAC-177: Update service added

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/expertgroup/UpdateExpertGroupService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/expertgroup/UpdateExpertGroupService.java
@@ -2,6 +2,7 @@ package org.flickit.assessment.kit.application.service.expertgroup;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.kit.application.port.in.expertgroup.UpdateExpertGroupUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroup.UpdateExpertGroupPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,8 +10,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UpdateExpertGroupService implements UpdateExpertGroupUseCase {
+
+    private  final UpdateExpertGroupPort updateExpertGroupPort;
     @Override
     public void updateExpertGroup(Param param) {
+        updateExpertGroupPort.update(toExpertGroupParam(param));
+    }
 
+    private UpdateExpertGroupPort.Param toExpertGroupParam(Param param) {
+        return new UpdateExpertGroupPort.Param(
+            param.getId(),
+            param.getName(),
+            param.getAbout(),
+            param.getPicture(),
+            param.getWebsite(),
+            param.getBio(),
+            param.getCurrentUserId()
+        );
     }
 }
