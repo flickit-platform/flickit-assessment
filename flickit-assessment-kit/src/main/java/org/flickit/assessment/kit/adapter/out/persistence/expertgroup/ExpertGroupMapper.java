a5e809d8cb48ce135dedfd928d7e30cd660e90d7
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-01-14 15:30:58 +0330

    FAC-176: Minor reformatting (Ready)

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/expertgroup/ExpertGroupMapper.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/expertgroup/ExpertGroupMapper.java
@@ -3,12 +3,12 @@ package org.flickit.assessment.kit.adapter.out.persistence.expertgroup;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.flickit.assessment.data.jpa.kit.expertgroup.ExpertGroupJpaEntity;
-import org.flickit.assessment.kit.application.port.out.expertgroup.CreateExpertGroupPort;
+import org.flickit.assessment.kit.application.port.out.expertgroup.CreateExpertGroupPort.Param;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExpertGroupMapper {
 
-    static ExpertGroupJpaEntity mapCreateParamToJpaEntity(CreateExpertGroupPort.Param param) {
+    static ExpertGroupJpaEntity mapCreateParamToJpaEntity(Param param) {
         return new ExpertGroupJpaEntity(
             null,
             param.name(),
--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/expertgroupaccess/ExpertGroupAccessMapper.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/expertgroupaccess/ExpertGroupAccessMapper.java
@@ -3,12 +3,12 @@ package org.flickit.assessment.kit.adapter.out.persistence.expertgroupaccess;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.flickit.assessment.data.jpa.kit.expertgroupaccess.ExpertGroupAccessJpaEntity;
-import org.flickit.assessment.kit.application.port.out.expertgroupaccess.CreateExpertGroupAccessPort;
+import org.flickit.assessment.kit.application.port.out.expertgroupaccess.CreateExpertGroupAccessPort.Param;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExpertGroupAccessMapper {
 
-    static ExpertGroupAccessJpaEntity mapCreateParamToJpaEntity(CreateExpertGroupAccessPort.Param param) {
+    static ExpertGroupAccessJpaEntity mapCreateParamToJpaEntity(Param param) {
         return new ExpertGroupAccessJpaEntity(
             null,
             param.expertGroupId(),
