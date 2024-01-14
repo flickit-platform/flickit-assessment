dd206cff26ca8699b284059a22b3ba49b2ef9946
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-01-14 11:20:07 +0330

    FAC-202: Domain model refactored

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/domain/ExpertGroup.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/domain/ExpertGroup.java
@@ -2,23 +2,16 @@ package org.flickit.assessment.kit.application.domain;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.flickit.assessment.kit.application.port.in.expertgroup.GetExpertGroupUseCase.Member;
-
-import java.util.List;
-import java.util.UUID;
 
 @Getter
 @RequiredArgsConstructor
 public class ExpertGroup {
 
     private final long id;
-    private final String title;
+    private final String name;
     private final String bio;
     private final String about;
     private final String picture;
     private final String website;
-    private final int membersCount;
-    private final int publishedKitCount;
-    private final List<Member> members;
-    private final UUID ownerId;
+    private final boolean isOwner;
 }
