26afc4cc9020749c5942aad9e69d06b401e7b87b
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-01-14 09:59:17 +0330

    FAC-202: Test refactored

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroup/GetExpertGroupServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroup/GetExpertGroupServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,16 +31,17 @@ class GetExpertGroupServiceTest {
 
     @Test
     void testGetExpertGroup_ValidInputs_ValidResults() {
+        long expertGroupId = new Random().nextLong();
         UUID currentUserId = UUID.randomUUID();
+
         var portResult = createPortResult(expertGroupId, currentUserId);
         var useCaseResul = portToUseCaseResult(portResult);
-
-        var param = new GetExpertGroupUseCase.Param(expertGroupId);
-        var result = service.getExpertGroup(param);
 
         when(loadExpertGroupPort.loadExpertGroup(any(LoadExpertGroupPort.Param.class)))
             .thenReturn(portResult);
 
+        var param = new GetExpertGroupUseCase.Param(expertGroupId);
+        var result = service.getExpertGroup(param);
 
         ArgumentCaptor<LoadExpertGroupPort.Param> loadPortParam = ArgumentCaptor.forClass(LoadExpertGroupPort.Param.class);
         verify(loadExpertGroupPort).loadExpertGroup(loadPortParam.capture());
@@ -48,8 +50,6 @@ class GetExpertGroupServiceTest {
         assertEquals(useCaseResul.getId(),result.getId());
         assertEquals(useCaseResul.getOwnerId(),result.getOwnerId());
     }
-
-    private static long expertGroupId = 123;
 
     private static LoadExpertGroupPort.Result createPortResult(long id,UUID ownerId) {
         return new LoadExpertGroupPort.Result(id,
@@ -79,3 +79,4 @@ class GetExpertGroupServiceTest {
         );
     }
 }
+
