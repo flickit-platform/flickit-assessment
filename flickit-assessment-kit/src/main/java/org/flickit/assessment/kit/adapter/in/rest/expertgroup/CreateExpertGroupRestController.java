e3b3536b472e4e5ebb128013f1e99ee0fb5c9901
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-01-18 14:14:07 +0330

    FAC-176: Sonar minor issue fixed

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/expertgroup/CreateExpertGroupRestController.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/expertgroup/CreateExpertGroupRestController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
@@ -31,14 +32,17 @@ public class CreateExpertGroupRestController {
     }
 
     private Param toParam(CreateExpertGroupRequestDto request, UUID currentUserId) {
-        String website = request.website().trim();
+        String website = (request.website() != null) ? request.website().trim() : null;
+
+        MultipartFile picture = request.picture();
+        picture = (picture != null && picture.getOriginalFilename() != null && !picture.getOriginalFilename().isEmpty()) ? picture : null;
 
         return new CreateExpertGroupUseCase.Param(
             request.title(),
             request.bio(),
             request.about(),
-            request.picture().getOriginalFilename().isEmpty() ? null : request.picture(),
-            website.isEmpty() ? null : website,
+            picture,
+            website,
             currentUserId
         );
     }
