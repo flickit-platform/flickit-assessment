cc9da0cde246f20173ac3fdf50219e998cd59872
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-01-23 23:08:07 +0330

    FAC-217: Rename upload ports methods

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/kitdsl/UploadKitDslToFileStoragePort.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/kitdsl/UploadKitDslToFileStoragePort.java
@@ -4,7 +4,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface UploadKitDslToFileStoragePort {
 
-    Result upload(MultipartFile dslZipFile, String dslJsonFile);
+    Result uploadKitDsl(MultipartFile dslZipFile, String dslJsonFile);
 
     record Result(String dslFilePath, String jsonFilePath) {}
 }
