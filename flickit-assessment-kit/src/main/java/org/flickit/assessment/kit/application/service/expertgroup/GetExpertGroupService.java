aac46c8c1937a7690c635855dadcc61a256bfa78
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-01-29 22:34:45 +0330

    FAC-202: Service refactored to get picture from LoadExpertGroupPort

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/expertgroup/GetExpertGroupService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/expertgroup/GetExpertGroupService.java
@@ -3,26 +3,32 @@ package org.flickit.assessment.kit.application.service.expertgroup;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.kit.application.domain.ExpertGroup;
 import org.flickit.assessment.kit.application.port.in.expertgroup.GetExpertGroupUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupPictureLinkPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class GetExpertGroupService implements GetExpertGroupUseCase {
 
+    private static final Duration EXPIRY_DURATION = Duration.ofHours(1);
     private final LoadExpertGroupPort loadExpertGroupPort;
+    private final LoadExpertGroupPictureLinkPort loadExpertGroupPictureLinkPort;
 
     @Override
     public ExpertGroup getExpertGroup(Param param) {
         var portResult = loadExpertGroupPort.loadExpertGroup(toParam(param.getId()));
+        var picturePath = loadExpertGroupPictureLinkPort.loadPictureLink(portResult.picture(), EXPIRY_DURATION);
 
         return new ExpertGroup(portResult.id(),
             portResult.title(),
             portResult.bio(),
             portResult.about(),
-            portResult.picture(),
+            picturePath,
             portResult.website(),
             portResult.ownerId().equals(param.getCurrentUserId()));
     }
