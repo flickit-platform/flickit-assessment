177cc7a9ef4209f6e04e658a7fb9e1a9d1a775b9
m.koohestani <angelmoonstar77@gmail.com>
2024-02-03 12:42:19 +0330

    FAC-219: Add confidence value calculation related pre initialize

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessment/CalculateConfidenceService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessment/CalculateConfidenceService.java
@@ -1,15 +1,26 @@
 package org.flickit.assessment.core.application.service.assessment;
 
 import lombok.RequiredArgsConstructor;
-import org.flickit.assessment.core.application.domain.AssessmentResult;
+import org.flickit.assessment.core.application.domain.*;
 import org.flickit.assessment.core.application.port.in.assessment.CalculateConfidenceUseCase;
 import org.flickit.assessment.core.application.port.out.assessment.UpdateAssessmentPort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.UpdateCalculatedConfidencePort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.LoadConfidenceLevelCalculateInfoPort;
+import org.flickit.assessment.core.application.port.out.qualityattributevalue.CreateQualityAttributeValuePort;
+import org.flickit.assessment.core.application.port.out.subject.LoadSubjectPort;
+import org.flickit.assessment.core.application.port.out.subjectvalue.CreateSubjectValuePort;
+import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadKitLastEffectiveModificationTimePort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
 
 @Service
 @Transactional
@@ -19,10 +30,16 @@ public class CalculateConfidenceService implements CalculateConfidenceUseCase {
     private final LoadConfidenceLevelCalculateInfoPort loadConfidenceLevelCalculateInfoPort;
     private final UpdateCalculatedConfidencePort updateCalculatedConfidenceLevelResultPort;
     private final UpdateAssessmentPort updateAssessmentPort;
+    private final LoadKitLastEffectiveModificationTimePort loadKitLastEffectiveModificationTimePort;
+    private final LoadSubjectPort loadSubjectPort;
+    private final CreateSubjectValuePort createSubjectValuePort;
+    private final CreateQualityAttributeValuePort createQualityAttributeValuePort;
 
     @Override
     public Result calculate(Param param) {
         AssessmentResult assessmentResult = loadConfidenceLevelCalculateInfoPort.load(param.getAssessmentId());
+
+        initializeBasedOnKitChanges(assessmentResult);
 
         Double confidenceValue = assessmentResult.calculateConfidenceValue();
 
@@ -36,5 +53,76 @@ public class CalculateConfidenceService implements CalculateConfidenceUseCase {
         updateAssessmentPort.updateLastModificationTime(param.getAssessmentId(), assessmentResult.getLastModificationTime());
 
         return new Result(confidenceValue);
+    }
+
+    private void initializeBasedOnKitChanges(AssessmentResult assessmentResult) {
+        Long kitId = assessmentResult.getAssessment().getAssessmentKit().getId();
+        LocalDateTime kitLastEffectiveModificationTime = loadKitLastEffectiveModificationTimePort.load(kitId);
+        if (assessmentResult.getLastConfidenceCalculationTime().isBefore(kitLastEffectiveModificationTime)) {
+            var subjects = loadSubjectPort.loadByKitIdWithAttributes(kitId);
+            Map<Long, List<QualityAttributeValue>> subjectIdToAttributeValueMap = createNewAttributeValues(subjects, assessmentResult);
+            createNewSubjectValues(subjects, assessmentResult, subjectIdToAttributeValueMap);
+        }
+    }
+
+    private Map<Long, List<QualityAttributeValue>> createNewAttributeValues(List<Subject> subjects, AssessmentResult assessmentResult) {
+        var attributesWithValue = assessmentResult.getSubjectValues().stream()
+            .flatMap(s -> s.getQualityAttributeValues().stream())
+            .distinct()
+            .map(q -> q.getQualityAttribute().getId())
+            .collect(Collectors.toSet());
+
+        var attributes = subjects.stream()
+            .flatMap(s -> s.getQualityAttributes().stream())
+            .distinct()
+            .toList();
+
+        var attributeIdsWithNoValue = attributes.stream()
+            .map(QualityAttribute::getId)
+            .filter(a -> !attributesWithValue.contains(a))
+            .toList();
+
+        var newAttributeValues = createQualityAttributeValuePort.persistAll(attributeIdsWithNoValue, assessmentResult.getId());
+
+        assessmentResult.getSubjectValues().forEach(s -> {
+            List<QualityAttributeValue> newAttributeValueList = new ArrayList<>();
+            newAttributeValueList.addAll(s.getQualityAttributeValues());
+            newAttributeValueList.addAll(newAttributeValues);
+            s.setQualityAttributeValues(newAttributeValueList);
+        });
+
+        var attributeIdToAttributeValueMap = newAttributeValues.stream()
+            .collect(groupingBy(qav -> qav.getQualityAttribute().getId()));
+
+        return subjects.stream()
+            .collect(toMap(Subject::getId,
+                s -> s.getQualityAttributes().stream()
+                    .filter(q -> !attributesWithValue.contains(q.getId()))
+                    .flatMap(qa -> attributeIdToAttributeValueMap.get(qa.getId()).stream()).toList()));
+    }
+
+    private void createNewSubjectValues(List<Subject> subjects, AssessmentResult assessmentResult, Map<Long, List<QualityAttributeValue>> subjectIdToAttributeValueMap) {
+        var subjectsWithValue = assessmentResult.getSubjectValues().stream()
+            .map(s -> s.getSubject().getId())
+            .collect(Collectors.toSet());
+
+        var subjectIdsWithNoValue = subjects.stream()
+            .map(Subject::getId)
+            .filter(s -> !subjectsWithValue.contains(s))
+            .toList();
+
+        var newSubjectValues = createSubjectValuePort.persistAll(subjectIdsWithNoValue, assessmentResult.getId());
+
+        newSubjectValues.forEach(sv -> {
+            List<QualityAttributeValue> newAttributeValueList = new ArrayList<>();
+            newAttributeValueList.addAll(sv.getQualityAttributeValues());
+            newAttributeValueList.addAll(subjectIdToAttributeValueMap.get(sv.getSubject().getId()));
+            sv.setQualityAttributeValues(newAttributeValueList);
+        });
+
+        List<SubjectValue> newSubjectValueList = new ArrayList<>();
+        newSubjectValueList.addAll(assessmentResult.getSubjectValues());
+        newSubjectValueList.addAll(newSubjectValues);
+        assessmentResult.setSubjectValues(newSubjectValueList);
     }
 }
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/CalculateConfidenceServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/CalculateConfidenceServiceTest.java
@@ -1,23 +1,27 @@
 package org.flickit.assessment.core.application.service.assessment;
 
-import org.flickit.assessment.core.application.domain.AssessmentResult;
-import org.flickit.assessment.core.application.domain.ConfidenceLevel;
-import org.flickit.assessment.core.application.domain.QualityAttributeValue;
-import org.flickit.assessment.core.application.domain.SubjectValue;
+import org.flickit.assessment.core.application.domain.*;
+import org.flickit.assessment.core.application.port.in.assessment.CalculateConfidenceUseCase;
 import org.flickit.assessment.core.application.port.in.assessment.CalculateConfidenceUseCase.Result;
 import org.flickit.assessment.core.application.port.in.assessment.CalculateConfidenceUseCase.Param;
 import org.flickit.assessment.core.application.port.out.assessment.UpdateAssessmentPort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.UpdateCalculatedConfidencePort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.LoadConfidenceLevelCalculateInfoPort;
+import org.flickit.assessment.core.application.port.out.qualityattributevalue.CreateQualityAttributeValuePort;
+import org.flickit.assessment.core.application.port.out.subject.LoadSubjectPort;
+import org.flickit.assessment.core.application.port.out.subjectvalue.CreateSubjectValuePort;
 import org.flickit.assessment.core.test.fixture.application.AssessmentResultMother;
 import org.flickit.assessment.core.test.fixture.application.QualityAttributeValueMother;
 import org.flickit.assessment.core.test.fixture.application.SubjectValueMother;
+import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadKitLastEffectiveModificationTimePort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -40,8 +44,22 @@ class CalculateConfidenceServiceTest {
     @Mock
     private UpdateAssessmentPort updateAssessmentPort;
 
+    @Mock
+    private LoadKitLastEffectiveModificationTimePort loadKitLastEffectiveModificationTimePort;
+
+    @Mock
+    private LoadSubjectPort loadSubjectPort;
+
+    @Mock
+    private CreateSubjectValuePort createSubjectValuePort;
+
+    @Mock
+    private CreateQualityAttributeValuePort createQualityAttributeValuePort;
+
     @Test
     void testCalculateConfidenceLevel_ValidInput_ValidResults() {
+        LocalDateTime kitLastEffectiveModificationTime = LocalDateTime.now();
+
         List<QualityAttributeValue> s1QualityAttributeValues = List.of(
             QualityAttributeValueMother.toBeCalcAsConfidenceLevelWithWeight(2, ConfidenceLevel.COMPLETELY_UNSURE.getId()),
             QualityAttributeValueMother.toBeCalcAsConfidenceLevelWithWeight(2, ConfidenceLevel.COMPLETELY_SURE.getId()),
@@ -65,6 +83,7 @@ class CalculateConfidenceServiceTest {
         Param param = new Param(assessmentResult.getAssessment().getId());
 
         when(loadConfidenceLevelCalculateInfoPort.load(assessmentResult.getAssessment().getId())).thenReturn(assessmentResult);
+        when(loadKitLastEffectiveModificationTimePort.load(any())).thenReturn(kitLastEffectiveModificationTime);
 
         Result result = service.calculate(param);
         verify(updateCalculatedConfidenceLevelResultPort, times(1)).updateCalculatedConfidence(any(AssessmentResult.class));
@@ -72,5 +91,49 @@ class CalculateConfidenceServiceTest {
 
         assertNotNull(result);
         assertEquals(70.666666666666666, result.confidenceValue());
+    }
+
+    @Test
+    void testCalculateMaturityLevel_KitChanged_CreatesNewAttributeAnSubjectValuesAndCalculates() {
+        List<QualityAttributeValue> s1QualityAttributeValues = List.of(
+            QualityAttributeValueMother.toBeCalcAsConfidenceLevelWithWeight(2, ConfidenceLevel.COMPLETELY_UNSURE.getId()),
+            QualityAttributeValueMother.toBeCalcAsConfidenceLevelWithWeight(2, ConfidenceLevel.COMPLETELY_SURE.getId()),
+            QualityAttributeValueMother.toBeCalcAsConfidenceLevelWithWeight(3, ConfidenceLevel.SOMEWHAT_UNSURE.getId()),
+            QualityAttributeValueMother.toBeCalcAsConfidenceLevelWithWeight(3, ConfidenceLevel.COMPLETELY_SURE.getId())
+        );
+
+        List<QualityAttributeValue> s2QualityAttributeValues = List.of(
+            QualityAttributeValueMother.toBeCalcAsConfidenceLevelWithWeight(4, ConfidenceLevel.SOMEWHAT_UNSURE.getId()),
+            QualityAttributeValueMother.toBeCalcAsConfidenceLevelWithWeight(1, ConfidenceLevel.COMPLETELY_SURE.getId())
+        );
+
+        List<SubjectValue> subjectValues = List.of(
+            SubjectValueMother.withQAValuesAndSubjectWithQAs(s1QualityAttributeValues, s1QualityAttributeValues.stream().map(QualityAttributeValue::getQualityAttribute).toList()),
+            SubjectValueMother.withQAValuesAndSubjectWithQAs(s2QualityAttributeValues, s2QualityAttributeValues.stream().map(QualityAttributeValue::getQualityAttribute).toList())
+        );
+
+        List<Subject> subjects = new ArrayList<>(subjectValues.stream().map(SubjectValue::getSubject).toList());
+        var newAttributeValue = QualityAttributeValueMother.toBeCalcAsConfidenceLevelWithWeight(4, ConfidenceLevel.SOMEWHAT_UNSURE.getId());
+        var newSubjectValue = SubjectValueMother.withQAValuesAndSubjectWithQAs(List.of(newAttributeValue), List.of(newAttributeValue.getQualityAttribute()));
+        subjects.add(newSubjectValue.getSubject());
+
+        AssessmentResult assessmentResult = AssessmentResultMother.invalidResultWithSubjectValues(subjectValues);
+        assessmentResult.setLastCalculationTime(LocalDateTime.now());
+
+        CalculateConfidenceUseCase.Param param = new CalculateConfidenceUseCase.Param(assessmentResult.getAssessment().getId());
+
+        when(loadConfidenceLevelCalculateInfoPort.load(assessmentResult.getAssessment().getId())).thenReturn(assessmentResult);
+        when(loadKitLastEffectiveModificationTimePort.load(any())).thenReturn(LocalDateTime.now());
+        when(loadSubjectPort.loadByKitIdWithAttributes(any())).thenReturn(subjects);
+        when(createSubjectValuePort.persistAll(anyList(), any())).thenReturn(List.of(newSubjectValue));
+        when(createQualityAttributeValuePort.persistAll(anyList(), any())).thenReturn(List.of(newAttributeValue));
+
+        CalculateConfidenceUseCase.Result result = service.calculate(param);
+        verify(updateCalculatedConfidenceLevelResultPort, times(1)).updateCalculatedConfidence(any(AssessmentResult.class));
+        verify(updateAssessmentPort, times(1)).updateLastModificationTime(any(), any());
+
+        assertNotNull(result);
+        assertNotNull(result.confidenceValue());
+        assertEquals(65.16129032258064, result.confidenceValue());
     }
 }
