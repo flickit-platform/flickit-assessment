26a9aaedfc45fee6dca8da39e9cef78705bfe08b
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-02-05 17:11:28 +0330

    FAC-209: Send email added to service

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/expertgroupaccess/InviteExpertGroupMemberService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/expertgroupaccess/InviteExpertGroupMemberService.java
@@ -5,6 +5,7 @@ import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.data.jpa.kit.expertgroupaccess.ExpertGroupAccessStatus;
 import org.flickit.assessment.kit.application.port.in.expertgroupaccess.InviteExpertGroupMemberUseCase;
 import org.flickit.assessment.kit.application.port.out.expertgroupaccess.InviteExpertGroupMemberPort;
+import org.flickit.assessment.kit.application.port.out.mail.SendExpertGroupInvitationMailPort;
 import org.flickit.assessment.kit.application.port.out.user.LoadUserEmailByUserIdPort;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +17,14 @@ public class InviteExpertGroupMemberService implements InviteExpertGroupMemberUs
 
     private final InviteExpertGroupMemberPort inviteExpertGroupMemberPort;
     private final LoadUserEmailByUserIdPort loadUserEmailByUserIdPort;
+    private final SendExpertGroupInvitationMailPort sendExpertGroupInvitationMailPort;
 
     @Override
     public void addMember(Param param) {
         String email = loadUserEmailByUserIdPort.loadEmail(param.getUserId()).orElseThrow(() ->
             new ResourceNotFoundException(INVITE_EXPERT_GROUP_MEMBER_EMAIL_NOT_FOUND));
-
         inviteExpertGroupMemberPort.invite(toParam(param));
+        sendExpertGroupInvitationMailPort.sendEmail(email);
     }
 
     private InviteExpertGroupMemberPort.Param toParam(Param param) {
