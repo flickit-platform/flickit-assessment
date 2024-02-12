ec5dc31fb9fea7fce3b62f37ed61f2b320145c7a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-02-12 14:54:03 +0330

    FAC-208: temp test added

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroup/GetExpertGroupMembersServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroup/GetExpertGroupMembersServiceTest.java
@@ -1,83 +1,57 @@
 package org.flickit.assessment.kit.application.service.expertgroup;
 
 import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
-import org.flickit.assessment.kit.application.port.in.expertgroup.CreateExpertGroupUseCase;
-import org.flickit.assessment.kit.application.port.in.expertgroup.GetExpertGroupListUseCase;
-import org.flickit.assessment.kit.application.port.in.expertgroup.GetExpertGroupMembersUseCase;
-import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupListPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupMembersPort;
 import org.flickit.assessment.kit.application.port.out.minio.CreateFileDownloadLinkPort;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.Duration;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
-
+@ExtendWith(MockitoExtension.class)
 public class GetExpertGroupMembersServiceTest {
 
     @InjectMocks
     private GetExpertGroupMembersService service;
     @Mock
     private LoadExpertGroupMembersPort loadExpertGroupMembersPort;
-
     @Mock
     private CreateFileDownloadLinkPort createFileDownloadLinkPort;
 
     @Test
-    void testGetExpertGroupMembers_Success() {
-        int page = 1;
-        int size = 10;
+    void testGetExpertGroupMembers_ValidInputs_ValidResults() {
+        int page = 0;
+        int size = 1;
         long id = 123;
 
-        PaginatedResponse<GetExpertGroupMembersUseCase.Member> expectedResult = new PaginatedResponse<>(
-            Collections.singletonList(member),
+        var member1 = createMember(UUID.randomUUID());
+        var member2 = createMember(UUID.randomUUID());
+
+        List<LoadExpertGroupMembersPort.Result> members = List.of(member1, member2);
+
+        PaginatedResponse<LoadExpertGroupMembersPort.Result> portResult = new PaginatedResponse<>(
+            members,
             page,
             size,
             null,
             null,
-            1
+            2
         );
 
-        // Mock behavior
-        when(loadExpertGroupMembersPort.loadExpertGroupMembers(any())).thenReturn(new PaginatedResponse<>(List.of(Mem),0,10) );
-        when(createFileDownloadLinkPort.createDownloadLink(anyString(), any(Duration.class))).thenReturn("test-download-link");
-
-        // Call the service method
-        GetExpertGroupMembersUseCase.Param param = new GetExpertGroupMembersUseCase.Param(page, size, id);
-        PaginatedResponse<GetExpertGroupMembersUseCase.Member> actualResult = service.getExpertGroupMembers(param);
-
-        // Verify interactions
-
-        ArgumentCaptor<LoadExpertGroupMembersPort.Param> loadPortParam = ArgumentCaptor.forClass(LoadExpertGroupMembersPort.Param.class);
-
-        verify(loadExpertGroupMembersPort).loadExpertGroupMembers(loadPortParam.capture());
-        assertEquals(page, loadPortParam.getValue().page());
-        assertEquals(size, loadPortParam.getValue().size());
-        assertEquals(id, loadPortParam.getValue().expertGroupId());
-        verify(createFileDownloadLinkPort).createDownloadLink(mockResult.picture(), Duration.ofHours(1));
-
-        // Verify the result
-        assertNotNull(actualResult);
-        assertEquals(expectedResult, actualResult);
     }
 
-    private final UUID currentUserId = UUID.randomUUID();
-    private final GetExpertGroupMembersUseCase.Member member = new GetExpertGroupMembersUseCase.Member(
-        UUID.randomUUID(),
-        "Expert Group Bio",
-        "Expert Group About",
-        null,
-        "http://www.example.com/sample.png",
-        "http://www.example.com");
+    private LoadExpertGroupMembersPort.Result createMember(UUID memberId) {
+
+        return new LoadExpertGroupMembersPort.Result(
+            memberId,
+            "Expert Group Bio",
+            "Expert Group About",
+            null,
+            null,
+            "http://www.example.com");
+    }
 }
