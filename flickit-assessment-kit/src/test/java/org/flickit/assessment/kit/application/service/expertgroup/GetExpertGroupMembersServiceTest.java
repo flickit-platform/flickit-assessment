703162a6c0884f46a2c42c6c78eeb6177c27a728
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-02-13 04:53:54 +0330

    FAC-208: minor sonnar issue fixed

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroup/GetExpertGroupMembersServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroup/GetExpertGroupMembersServiceTest.java
@@ -21,7 +21,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class GetExpertGroupMembersServiceTest {
+class GetExpertGroupMembersServiceTest {
 
     @InjectMocks
     private GetExpertGroupMembersService service;
