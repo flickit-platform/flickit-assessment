c8327db495f343417af884923005784706fe9414
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-02-13 11:08:14 +0330

    FAC-202: LoadExpertGroupPictureLinkPort removed

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/expertgroup/LoadExpertGroupPictureLinkPort.java
+++
@@ -1,8 +0,0 @@
-package org.flickit.assessment.kit.application.port.out.expertgroup;
-
-import java.time.Duration;
-
-public interface LoadExpertGroupPictureLinkPort {
-
-    String loadPictureLink(String filePath, Duration expiryDuration);
-}
