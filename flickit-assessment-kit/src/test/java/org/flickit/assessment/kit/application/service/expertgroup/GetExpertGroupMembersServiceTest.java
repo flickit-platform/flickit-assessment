d7d6aa2c434873b90d4c2dc6fb68b1f307ed7ff1
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-02-13 04:19:14 +0330

    FAC-208: tests refactored

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroup/GetExpertGroupMembersServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroup/GetExpertGroupMembersServiceTest.java
@@ -17,7 +17,6 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -30,56 +29,68 @@ public class GetExpertGroupMembersServiceTest {
     private LoadExpertGroupMembersPort loadExpertGroupMembersPort;
     @Mock
     private CreateFileDownloadLinkPort createFileDownloadLinkPort;
+    int count = 0;
+    String expectedDownloadLink = "downloadLink";
 
 
     @Test
     void testGetExpertGroupMembers_ValidInputs_ValidResults() {
 
-
-        // Test data
         int page = 0;
         int size = 10;
-        UUID member1id = UUID.randomUUID();
-        UUID member2id = UUID.randomUUID();
-        long groupId = 123L;
+        var member1 = createPortResult(UUID.randomUUID());
+        var member2 = createPortResult(UUID.randomUUID());
+        long expertGroupId = 123L;
 
-        List<LoadExpertGroupMembersPort.Result> portResults = List.of(
-            new LoadExpertGroupMembersPort.Result(member1id, "email1@example.com", "John Doe", "Bio 1", "picture1.jpg", "linkedin1"),
-            new LoadExpertGroupMembersPort.Result(member2id, "email2@example.com", "Jane Smith", "Bio 2", "picture2.jpg", "linkedin2")
-        );
+        List<LoadExpertGroupMembersPort.Result> portResults = List.of(member1, member2);
 
         List<GetExpertGroupMembersUseCase.Member> expectedMembers = List.of(
-            new GetExpertGroupMembersUseCase.Member(member1id, "email1@example.com", "John Doe", "Bio 1", "downloadLink1", "linkedin1"),
-            new GetExpertGroupMembersUseCase.Member(member2id, "email2@example.com", "Jane Smith", "Bio 2", "downloadLink2", "linkedin2")
+            portToUseCaseResult(member1),
+            portToUseCaseResult(member2)
         );
 
-        PaginatedResponse<LoadExpertGroupMembersPort.Result> paginatedResult = new PaginatedResponse<>(portResults, page, size, "name", "asc", 2);
-        PaginatedResponse<GetExpertGroupMembersUseCase.Member> paginatedMember = new PaginatedResponse<>(expectedMembers, page, size, "name", "asc", 2);
+        PaginatedResponse<LoadExpertGroupMembersPort.Result> paginatedResult = new PaginatedResponse<>(portResults, page, size, "title", "asc", 2);
 
-
-        // Mock behavior
         when(loadExpertGroupMembersPort.loadExpertGroupMembers(any())).thenReturn(paginatedResult);
-        when(createFileDownloadLinkPort.createDownloadLink(eq("picture1.jpg"), any(Duration.class))).thenReturn("downloadLink1");
-        when(createFileDownloadLinkPort.createDownloadLink(eq("picture2.jpg"), any(Duration.class))).thenReturn("downloadLink2");
+        when(createFileDownloadLinkPort.createDownloadLink(any(String.class), any(Duration.class))).thenReturn(expectedDownloadLink);
 
-        // Execute the service method
-        GetExpertGroupMembersUseCase.Param param = new GetExpertGroupMembersUseCase.Param(size, page, groupId);
+        GetExpertGroupMembersUseCase.Param param = new GetExpertGroupMembersUseCase.Param(size, page, expertGroupId);
         PaginatedResponse<GetExpertGroupMembersUseCase.Member> result = service.getExpertGroupMembers(param);
 
-        // Verify the interactions and assertions
         ArgumentCaptor<LoadExpertGroupMembersPort.Param> captor = ArgumentCaptor.forClass(LoadExpertGroupMembersPort.Param.class);
         verify(loadExpertGroupMembersPort).loadExpertGroupMembers(captor.capture());
 
         assertEquals(page, captor.getValue().page());
         assertEquals(size, captor.getValue().size());
-        assertEquals(groupId, captor.getValue().expertGroupId());
+        assertEquals(expertGroupId, captor.getValue().expertGroupId());
 
         assertEquals(expectedMembers, result.getItems());
         assertEquals(page, result.getPage());
         assertEquals(size, result.getSize());
-        assertEquals("name", result.getSort());
+        assertEquals("title", result.getSort());
         assertEquals("asc", result.getOrder());
         assertEquals(portResults.size(), result.getTotal());
-        assertEquals(paginatedMember, service.getExpertGroupMembers(new GetExpertGroupMembersUseCase.Param(size, page, groupId)));
+    }
+
+    private LoadExpertGroupMembersPort.Result createPortResult(UUID memberId) {
+
+        return new LoadExpertGroupMembersPort.Result(
+            memberId,
+            "email" + count + "@example.com",
+            "Name" + count,
+            "Bio" + count,
+            "picture" + count + ".png",
+            "http://www.example" + count + ".com");
+    }
+
+    private GetExpertGroupMembersUseCase.Member portToUseCaseResult(LoadExpertGroupMembersPort.Result portResult) {
+        return new GetExpertGroupMembersUseCase.Member(
+            portResult.id(),
+            portResult.email(),
+            portResult.displayNme(),
+            portResult.bio(),
+            expectedDownloadLink,
+            portResult.linkedin()
+        );
     }
 }
