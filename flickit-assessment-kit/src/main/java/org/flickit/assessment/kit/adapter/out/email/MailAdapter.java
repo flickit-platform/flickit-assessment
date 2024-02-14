83fa929b3c36f67c3fd1daf2814dd3e82dc26ef3
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-02-14 13:26:07 +0330

    FAC-209: MimmeMessage added for a html message

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/email/MailAdapter.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/email/MailAdapter.java
@@ -1,10 +1,11 @@
 package org.flickit.assessment.kit.adapter.out.email;
 
+import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.flickit.assessment.kit.application.port.out.mail.SendExpertGroupInvitationMailPort;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.annotation.Retryable;
@@ -25,10 +26,17 @@ public class MailAdapter implements
     @Retryable(retryFor = Exception.class, maxAttempts = 10, backoff = @Backoff(delay = 10000))
     @Override
     public void sendInviteExpertGroupMemberEmail(String to, UUID inviteToken) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("invite to join");
-        message.setText("/expert-groups/invite/confirm/{invite_token}/"+inviteToken.toString());
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true); // Enable HTML content and multipart support
+
+        helper.setTo(to);
+        helper.setSubject("Invite to join");
+
+        String htmlContent = "<p>Dear recipient,</p>"
+            + "<p>You have been invited to join our expert group. Please click the following link to confirm your invitation:</p>"
+            + "<p><a href=\"http://www.example.com/expert-groups/invite/confirm/" + inviteToken.toString() + "\">Confirm Invitation</a></p>";
+
+        helper.setText(htmlContent, true);
         mailSender.send(message);
     }
 }
