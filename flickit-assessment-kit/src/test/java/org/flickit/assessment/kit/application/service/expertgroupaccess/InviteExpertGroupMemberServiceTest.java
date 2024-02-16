0db4c964834cd4114eca6948035f9ea51598a041
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-02-16 08:39:26 +0330

    FAC-209: Minor test restyling

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroupaccess/InviteExpertGroupMemberServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/expertgroupaccess/InviteExpertGroupMemberServiceTest.java
@@ -6,7 +6,6 @@ import org.flickit.assessment.kit.application.port.out.expertgroup.CheckExpertGr
 import org.flickit.assessment.kit.application.port.out.expertgroup.CheckExpertGroupOwnerPort;
 import org.flickit.assessment.kit.application.port.out.expertgroupaccess.InviteExpertGroupMemberPort;
 import org.flickit.assessment.kit.application.port.out.expertgroupaccess.InviteTokenCheckPort;
-import org.flickit.assessment.kit.application.port.out.mail.SendExpertGroupInvitationMailPort;
 import org.flickit.assessment.kit.application.port.out.user.LoadUserEmailByUserIdPort;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -23,7 +22,8 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class InviteExpertGroupMemberServiceTest {
 
-
+    @InjectMocks
+    private InviteExpertGroupMemberService service;
     @Mock
     private LoadUserEmailByUserIdPort loadUserEmailByUserIdPort;
     @Mock
@@ -32,19 +32,8 @@ class InviteExpertGroupMemberServiceTest {
     private CheckExpertGroupOwnerPort checkExpertGroupOwnerPort;
     @Mock
     private InviteTokenCheckPort inviteTokenCheckPort;
-
     @Mock
     private InviteExpertGroupMemberPort inviteExpertGroupMemberPort;
-    @Mock
-    private SendExpertGroupInvitationMailPort sendExpertGroupInvitationMailPort;
-    @InjectMocks
-    private InviteExpertGroupMemberService service;
-
-    UUID userId = UUID.randomUUID();
-    long expertGroupId = 0L;
-    UUID currentUserId = UUID.randomUUID();
-    InviteExpertGroupMemberUseCase.Param param = new InviteExpertGroupMemberUseCase.Param(expertGroupId, userId, currentUserId);
-    String email = "test@example.com";
 
     @Test
     void inviteMember_validParameters_success() {
@@ -54,7 +43,6 @@ class InviteExpertGroupMemberServiceTest {
         when(checkExpertGroupOwnerPort.checkIsOwner(any(Long.class), any(UUID.class))).thenReturn(true);
         when(inviteTokenCheckPort.checkInviteToken(any(UUID.class))).thenReturn(true);
         doNothing().when(inviteExpertGroupMemberPort).persist(isA(InviteExpertGroupMemberPort.Param.class));
-        doNothing().when(sendExpertGroupInvitationMailPort).sendInviteExpertGroupMemberEmail(isA(String.class), isA(UUID.class));
 
         service.inviteMember(param);
 
@@ -63,39 +51,29 @@ class InviteExpertGroupMemberServiceTest {
         verify(checkExpertGroupOwnerPort).checkIsOwner(any(Long.class), any(UUID.class));
         verify(inviteExpertGroupMemberPort).persist(any());
         verify(inviteTokenCheckPort).checkInviteToken(any(UUID.class));
-        verify(sendExpertGroupInvitationMailPort).sendInviteExpertGroupMemberEmail(any(String.class), any(UUID.class));
     }
 
     @Test
     void inviteMember_expertGroupNotExist_fail() {
-        UUID userId = UUID.randomUUID();
-        long expertGroupId = 0L;
-        UUID currentUserId = UUID.randomUUID();
-        InviteExpertGroupMemberUseCase.Param param = new InviteExpertGroupMemberUseCase.Param(expertGroupId, userId, currentUserId);
-
-        String email = "test@example.com";
-
         when(loadUserEmailByUserIdPort.loadEmail(userId)).thenReturn(email);
         when(checkExpertGroupExistsPort.existsById(any(Long.class))).thenReturn(false);
 
         Assertions.assertThrows(ResourceNotFoundException.class, () -> service.inviteMember(param));
-
     }
 
     @Test
     void inviteMember_expertGroupInviterNotOwner_fail() {
-        UUID userId = UUID.randomUUID();
-        long expertGroupId = 0L;
-        UUID currentUserId = UUID.randomUUID();
-        InviteExpertGroupMemberUseCase.Param param = new InviteExpertGroupMemberUseCase.Param(expertGroupId, userId, currentUserId);
-
-        String email = "test@example.com";
-
         when(loadUserEmailByUserIdPort.loadEmail(userId)).thenReturn(email);
         when(checkExpertGroupExistsPort.existsById(any(Long.class))).thenReturn(true);
         when(checkExpertGroupOwnerPort.checkIsOwner(any(Long.class), any(UUID.class))).thenReturn(false);
 
         Assertions.assertThrows(AccessDeniedException.class, () -> service.inviteMember(param));
-
     }
+
+    UUID userId = UUID.randomUUID();
+    long expertGroupId = 0L;
+    UUID currentUserId = UUID.randomUUID();
+    InviteExpertGroupMemberUseCase.Param param = new InviteExpertGroupMemberUseCase.Param(expertGroupId, userId, currentUserId);
+    String email = "test@example.com";
+
 }
