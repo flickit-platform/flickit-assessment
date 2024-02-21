71cbbdb710fd2ce6b83b0d071480910c3bfbf9ab
mahdirezazadeh <riza.rs2000@gmail.com>
2024-02-21 15:52:39 +0330

    FAC-249:feature:Impl CreateAdviceRestController

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/in/rest/advice/CreateAdviceRequestDto.java
@@ -0,0 +1,10 @@
+package org.flickit.assessment.advice.adapter.in.rest.advice;
+
+import org.flickit.assessment.advice.application.port.in.advice.CalculateAdviceUseCase.AttributeLevelTarget;
+import org.flickit.assessment.advice.application.port.in.advice.CreateAdviceUseCase.AdviceQuestion;
+
+import java.util.List;
+
+public record CreateAdviceRequestDto(List<AttributeLevelTarget> attributeLevelTargets,
+                                     List<AdviceQuestion> adviceQuestions) {
+}
---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/in/rest/advice/CreateAdviceResponseDto.java
@@ -0,0 +1,6 @@
+package org.flickit.assessment.advice.adapter.in.rest.advice;
+
+import java.util.UUID;
+
+public record CreateAdviceResponseDto(UUID id) {
+}
