2cc0da51dbb9a29af2a793a50dc089a599689c1f
mahdirezazadeh <riza.rs2000@gmail.com>
2024-02-21 15:51:11 +0330

    FAC-249:feature:Impl CreateAdviceUseCase

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/port/in/advice/CreateAdviceUseCase.java
@@ -0,0 +1,54 @@
+package org.flickit.assessment.advice.application.port.in.advice;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.flickit.assessment.common.application.SelfValidating;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.flickit.assessment.advice.common.ErrorMessageKey.*;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
+
+public interface CreateAdviceUseCase {
+
+    Result createAdvice(Param param);
+
+    @Value
+    @EqualsAndHashCode(callSuper = false)
+    class Param extends SelfValidating<Param> {
+
+        @NotNull(message = CREATE_ADVICE_ASSESSMENT_ID_NOT_NULL)
+        UUID assessmentId;
+
+        @NotNull(message = CREATE_ADVICE_ATTRIBUTE_LEVEL_TARGETS_NOT_NULL)
+        @Size(min = 1, message = CREATE_ADVICE_ATTRIBUTE_LEVEL_TARGETS_SIZE_MIN)
+        List<CalculateAdviceUseCase.AttributeLevelTarget> attributeLevelTargets;
+
+        @NotNull(message = CREATE_ADVICE_ADVICE_QUESTIONS_NOT_NULL)
+        @Size(min = 1, message = CREATE_ADVICE_ADVICE_QUESTIONS_SIZE_MIN)
+        List<AdviceQuestion> adviceQuestions;
+
+        @NotNull(message = COMMON_CURRENT_USER_ID_NOT_NULL)
+        UUID currentUserId;
+
+        public Param(UUID assessmentId,
+                     List<CalculateAdviceUseCase.AttributeLevelTarget> attributeLevelTargets,
+                     List<AdviceQuestion> adviceQuestion,
+                     UUID currentUserId) {
+            this.assessmentId = assessmentId;
+            this.attributeLevelTargets = attributeLevelTargets;
+            this.adviceQuestions = adviceQuestion;
+            this.currentUserId = currentUserId;
+            this.validateSelf();
+        }
+    }
+
+    record AdviceQuestion(Long questionId, Integer recommendedOptionIndex, Double benefit) {
+    }
+
+    record Result(UUID id){
+    }
+}
--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/common/ErrorMessageKey.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/common/ErrorMessageKey.java
@@ -15,4 +15,10 @@ public class ErrorMessageKey {
     public static final String CALCULATE_ADVICE_FINDING_BEST_SOLUTION_EXCEPTION = "calculate-advice.finding-best-solution.execution";
     public static final String CALCULATE_ADVICE_ASSESSMENT_ATTRIBUTE_RELATION_NOT_FOUND = "calculate-advice.assessmentAttributeRelation.notFound";
     public static final String CALCULATE_ADVICE_ASSESSMENT_LEVEL_RELATION_NOT_FOUND = "calculate-advice.assessmentLevelRelation.notFound";
+
+    public static final String CREATE_ADVICE_ASSESSMENT_ID_NOT_NULL = "create-advice.assessmentId.notNull";
+    public static final String CREATE_ADVICE_ATTRIBUTE_LEVEL_TARGETS_NOT_NULL = "create-advice.attributeLevelTargets.notNull";
+    public static final String CREATE_ADVICE_ATTRIBUTE_LEVEL_TARGETS_SIZE_MIN = "create-advice.attributeLevelTargets.size.min";
+    public static final String CREATE_ADVICE_ADVICE_QUESTIONS_NOT_NULL = "create-advice.adviceQuestions.notNull";
+    public static final String CREATE_ADVICE_ADVICE_QUESTIONS_SIZE_MIN = "create-advice.adviceQuestions.size.min";
 }
