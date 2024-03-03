b8a057873c92c2cf5ad818be6e11421c93d83b83
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-03-03 21:37:36 +0330

    FAC-262 Check current user has access to assessment
    When a user add an evidence, access of the user to assessment should check. A test has added to check the logic is work correctly, and other tests have changed, corresponding to the new logic.

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/evidence/AddEvidenceService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/evidence/AddEvidenceService.java
@@ -1,10 +1,12 @@
 package org.flickit.assessment.core.application.service.evidence;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.core.application.domain.EvidenceType;
 import org.flickit.assessment.core.application.port.in.evidence.AddEvidenceUseCase;
 import org.flickit.assessment.core.application.port.out.assessment.CheckAssessmentExistencePort;
+import org.flickit.assessment.core.application.port.out.assessment.CheckUserAssessmentAccessPort;
 import org.flickit.assessment.core.application.port.out.evidence.CreateEvidencePort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.flickit.assessment.core.common.ErrorMessageKey.ADD_EVIDENCE_ASSESSMENT_ID_NOT_FOUND;
 
 @Service
@@ -21,11 +24,16 @@ public class AddEvidenceService implements AddEvidenceUseCase {
 
     private final CreateEvidencePort createEvidencePort;
     private final CheckAssessmentExistencePort checkAssessmentExistencePort;
+    private final CheckUserAssessmentAccessPort checkUserAssessmentAccessPort;
 
     @Override
     public Result addEvidence(Param param) {
         if (!checkAssessmentExistencePort.existsById(param.getAssessmentId()))
             throw new ResourceNotFoundException(ADD_EVIDENCE_ASSESSMENT_ID_NOT_FOUND);
+
+        if (!checkUserAssessmentAccessPort.hasAccess(param.getAssessmentId(), param.getCreatedById()))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+
         var createPortParam = toCreatePortParam(param);
         UUID id = createEvidencePort.persist(createPortParam);
         return new AddEvidenceUseCase.Result(id);
