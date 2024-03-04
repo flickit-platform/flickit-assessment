3fbd26e80bd95ed82920602f04ec9dc463ae5026
m.koohestani <angelmoonstar77@gmail.com>
2024-03-04 13:56:49 +0330

    FAC-255: Fix find by kit id query for subject based on new changes

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/subject/SubjectJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/subject/SubjectJpaRepository.java
@@ -41,6 +41,11 @@ public interface SubjectJpaRepository extends JpaRepository<SubjectJpaEntity, Lo
         """)
     List<SubjectJpaEntity> loadByKitIdWithAttributes(Long kitId);
 
+    @Query("""
+            SELECT s as subject
+            FROM SubjectJpaEntity s
+            WHERE s.id = :id AND s.kitVersionId = (SELECT k.kitVersion.id FROM AssessmentKitJpaEntity k WHERE k.id = :kitId)
+        """)
     Optional<SubjectJpaEntity> findByIdAndKitId(@Param(value = "id") long id, @Param(value = "kitId") long kitId);
 
     @Query("""
