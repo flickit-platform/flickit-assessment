64df17ca5d82e908c8f80334927672eb50126fbf
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-03-05 14:16:45 +0330

    FAC-255: Use kitVersionId instead of kitId in load SubjectQuestionnaire

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/subjectquestionnaire/SubjectQuestionnaireJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/subjectquestionnaire/SubjectQuestionnaireJpaRepository.java
@@ -2,13 +2,16 @@ package org.flickit.assessment.data.jpa.kit.subjectquestionnaire;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface SubjectQuestionnaireJpaRepository extends JpaRepository<SubjectQuestionnaireJpaEntity, Long> {
 
-    @Query("FROM SubjectQuestionnaireJpaEntity sq " +
-        "where sq.subjectId in " +
-        "(SELECT s.id FROM SubjectJpaEntity s where s.kitVersionId = (SELECT k.kitVersionId FROM AssessmentKitJpaEntity k WHERE k.id = :assessmentKitId))")
-    List<SubjectQuestionnaireJpaEntity> findAllByAssessmentKitId(Long assessmentKitId);
+    @Query("""
+            FROM SubjectQuestionnaireJpaEntity sq
+            where sq.subjectId in
+                (SELECT s.id FROM SubjectJpaEntity s WHERE s.kitVersionId = :kitVersionId)
+        """)
+    List<SubjectQuestionnaireJpaEntity> findAllByKitVersionId(@Param(value = "kitVersionId") Long kitVersionId);
 }
