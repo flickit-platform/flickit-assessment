62593edfdb440dfeaa57e7bf5099666b61df75d4
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-09 13:38:45 +0330

    FAC-268: Checking, if previously handled, added

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/exception/handler/RuntimeExceptionHandler.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/exception/handler/RuntimeExceptionHandler.java
@@ -7,17 +7,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.flickit.assessment.common.exception.api.ErrorCodes.INTERNAL_ERROR;
 
 @Slf4j
-//@RestControllerAdvice
+@RestControllerAdvice
 public class RuntimeExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     ErrorResponseDto handle(RuntimeException ex) {
+        if (ex.getMessage() != null) throw ex;
         log.error("An unexpected error occurred", ex);
         return new ErrorResponseDto(INTERNAL_ERROR, MessageBundle.message("common.internal.error"));
     }
