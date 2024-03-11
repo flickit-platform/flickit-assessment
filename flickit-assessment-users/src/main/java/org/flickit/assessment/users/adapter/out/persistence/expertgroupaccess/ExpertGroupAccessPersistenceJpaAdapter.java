7c5bb564d477fb74bae87b3828e52b9f12cfb941
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-11 12:47:08 +0330

    FAC-286:CheckExpertGroupAccessPort removed

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/expertgroupaccess/ExpertGroupAccessPersistenceJpaAdapter.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/expertgroupaccess/ExpertGroupAccessPersistenceJpaAdapter.java
@@ -1,18 +1,14 @@
 package org.flickit.assessment.users.adapter.out.persistence.expertgroupaccess;
 
-import org.flickit.assessment.users.application.port.out.expertgroupaccess.CheckExpertGroupAccessPort;
 import org.flickit.assessment.users.application.port.out.expertgroupaccess.CreateExpertGroupAccessPort;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.data.jpa.kit.expertgroupaccess.*;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component("usersExpertGroupAccessPersistenceJpaAdapter")
 @RequiredArgsConstructor
 public class ExpertGroupAccessPersistenceJpaAdapter implements
-    CreateExpertGroupAccessPort,
-    CheckExpertGroupAccessPort {
+    CreateExpertGroupAccessPort{
 
     private final ExpertGroupAccessJpaRepository repository;
 
@@ -21,10 +17,5 @@ public class ExpertGroupAccessPersistenceJpaAdapter implements
         ExpertGroupAccessJpaEntity unsavedEntity = ExpertGroupAccessMapper.mapCreateParamToJpaEntity(param);
         ExpertGroupAccessJpaEntity savedEntity = repository.save(unsavedEntity);
         return savedEntity.getId();
-    }
-
-    @Override
-    public boolean checkIsMember(long expertGroupId, UUID userId) {
-        return repository.existsByExpertGroupIdAndUserId(expertGroupId, userId);
     }
 }
