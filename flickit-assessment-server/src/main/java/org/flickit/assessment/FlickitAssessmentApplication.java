d3fb7d47321d6b28632711b929b87e384d21b0c9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-12 16:59:45 +0330

    FAC-268: advice component added for scanning

--- flickit-assessment-server/src/main/java/org/flickit/assessment/FlickitAssessmentApplication.java
+++ flickit-assessment-server/src/main/java/org/flickit/assessment/FlickitAssessmentApplication.java
@@ -2,8 +2,10 @@ package org.flickit.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.flickit.assessment.advice","org.flickit.assessment.kit"})
 public class FlickitAssessmentApplication {
 
     public static void main(String[] args) {
