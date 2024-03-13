71c1ebcd13faf33ee3dc0ff341755254d6ffbb99
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-13 14:06:22 +0330

    FAC-268: Dependency order changed

--- flickit-assessment-server/src/main/java/org/flickit/assessment/FlickitAssessmentApplication.java
+++ flickit-assessment-server/src/main/java/org/flickit/assessment/FlickitAssessmentApplication.java
@@ -2,10 +2,8 @@ package org.flickit.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.flickit.assessment.advice","org.flickit.assessment.kit"})
 public class FlickitAssessmentApplication {
 
     public static void main(String[] args) {
