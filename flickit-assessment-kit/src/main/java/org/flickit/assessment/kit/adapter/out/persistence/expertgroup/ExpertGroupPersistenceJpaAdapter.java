2a56550c7d78b41343783208f3167a392482f9cb
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-13 15:30:43 +0330

    286: Adapters renamed

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/expertgroup/ExpertGroupPersistenceJpaAdapter.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/expertgroup/ExpertGroupPersistenceJpaAdapter.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@Component
+@Component("kitExpertGroupPersistenceJpaAdapter")
 @RequiredArgsConstructor
 public class ExpertGroupPersistenceJpaAdapter implements
     LoadExpertGroupOwnerPort,
--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/expertgroup/ExpertGroupPersistenceJpaAdapter.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/expertgroup/ExpertGroupPersistenceJpaAdapter.java
@@ -22,7 +22,7 @@ import static org.flickit.assessment.users.adapter.out.persistence.expertgroup.E
 import static org.flickit.assessment.users.adapter.out.persistence.expertgroup.ExpertGroupMapper.mapToPortResult;
 import static org.flickit.assessment.users.common.ErrorMessageKey.GET_EXPERT_GROUP_EXPERT_GROUP_NOT_FOUND;
 
-@Component("usersExpertGroupJpaRepository")
+@Component
 @RequiredArgsConstructor
 public class ExpertGroupPersistenceJpaAdapter implements
     LoadExpertGroupListPort,
