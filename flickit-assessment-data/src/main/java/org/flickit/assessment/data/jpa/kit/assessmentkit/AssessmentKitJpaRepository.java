7862f3c3f2feae816804ed939a074331c412a116
m.koohestani <angelmoonstar77@gmail.com>
2024-03-17 11:44:33 +0330

    FAC-293: Remove unused import

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/assessmentkit/AssessmentKitJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/assessmentkit/AssessmentKitJpaRepository.java
@@ -1,7 +1,6 @@
 package org.flickit.assessment.data.jpa.kit.assessmentkit;
 
 import org.flickit.assessment.data.jpa.kit.subject.SubjectJpaEntity;
-import org.flickit.assessment.data.jpa.kit.user.UserJpaEntity;
 import org.flickit.assessment.data.jpa.users.user.UserJpaEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
