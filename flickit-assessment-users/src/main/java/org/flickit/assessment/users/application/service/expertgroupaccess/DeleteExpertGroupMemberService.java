ec27a1912e81ce7cfaf586f4157464032c6b7cdf
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-23 20:17:57 +0330

    210: Usecase Service and test added for checking user and expertGroup validation

---
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/expertgroupaccess/DeleteExpertGroupMemberService.java
@@ -0,0 +1,36 @@
+package org.flickit.assessment.users.application.service.expertgroupaccess;
+
+import lombok.AllArgsConstructor;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.users.application.port.in.expertgroupaccess.DeleteExpertGroupMemberUseCase;
+import org.flickit.assessment.users.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.users.common.ErrorMessageKey.EXPERT_GROUP_ID_NOT_FOUND;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class DeleteExpertGroupMemberService implements DeleteExpertGroupMemberUseCase {
+
+    private final LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
+
+    @Override
+    public void deleteMember(Param param) {
+        validateCurrentUser(param.getExpertGroupId(), param.getCurrentUserId());
+
+    }
+
+    private void validateCurrentUser(Long expertGroupId, UUID currentUserId) {
+        UUID expertGroupOwnerId = loadExpertGroupOwnerPort.loadOwnerId(expertGroupId)
+            .orElseThrow(() -> new ResourceNotFoundException(EXPERT_GROUP_ID_NOT_FOUND));
+        if (!Objects.equals(expertGroupOwnerId, currentUserId))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+    }
+}
---
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroupaccess/DeleteExpertGroupMemberServiceTest.java
@@ -0,0 +1,52 @@
+package org.flickit.assessment.users.application.service.expertgroupaccess;
+
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.users.application.port.in.expertgroupaccess.DeleteExpertGroupMemberUseCase;
+import org.flickit.assessment.users.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteExpertGroupMemberServiceTest {
+
+    @InjectMocks
+    private DeleteExpertGroupMemberService service;
+    @Mock
+    private LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
+
+    @Test
+    void inviteMember_expertGroupNotExist_fail() {
+        UUID userId = UUID.randomUUID();
+        long expertGroupId = 0L;
+        UUID currentUserId = UUID.randomUUID();
+        DeleteExpertGroupMemberUseCase.Param param =
+            new DeleteExpertGroupMemberUseCase.Param(expertGroupId,userId,currentUserId);
+
+        when(loadExpertGroupOwnerPort.loadOwnerId(expertGroupId)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> service.deleteMember(param));
+    }
+
+    @Test
+    void inviteMember_expertGroupInviterNotOwner_fail() {
+        UUID userId = UUID.randomUUID();
+        long expertGroupId = 0L;
+        UUID currentUserId = UUID.randomUUID();
+        DeleteExpertGroupMemberUseCase.Param param =
+            new DeleteExpertGroupMemberUseCase.Param(expertGroupId,userId,currentUserId);
+
+        when(loadExpertGroupOwnerPort.loadOwnerId(expertGroupId)).thenReturn(Optional.of(UUID.randomUUID()));
+
+        Assertions.assertThrows(AccessDeniedException.class, () -> service.deleteMember(param));
+    }
+}
