323367e945888f0ca10731fd4279064911879bd9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-23 18:08:31 +0330

    209: Minor fix

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/port/out/expertgroupaccess/CreateExpertGroupAccessPort.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/port/out/expertgroupaccess/CreateExpertGroupAccessPort.java
@@ -2,7 +2,6 @@ package org.flickit.assessment.users.application.port.out.expertgroupaccess;
 
 import org.flickit.assessment.users.application.domain.ExpertGroupAccessStatus;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface CreateExpertGroupAccessPort {
