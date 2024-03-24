fd8d1ae7a059489cb688611fe315a8b3f10b4c14
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-24 11:13:36 +0330

    209: ConstantMessageKey renamed to MessageKey

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/common/MessageKey.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/common/MessageKey.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ConstantMessageKey {
+public class MessageKey {
 
     public static final String INVITE_EXPERT_GROUP_MEMBER_MAIL_SUBJECT = "invite-expert-group-member.mail.subject";
     public static final String INVITE_EXPERT_GROUP_MEMBER_MAIL_BODY = "invite-expert-group-member.mail.body";
