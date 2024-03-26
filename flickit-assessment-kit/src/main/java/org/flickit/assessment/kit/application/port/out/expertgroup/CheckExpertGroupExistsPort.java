1902b366a9d0e8cf2f355d5b6b42dddff585948f
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-26 12:03:54 +0330

    265: extra files removed

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/expertgroup/CheckExpertGroupExistsPort.java
+++
@@ -1,5 +0,0 @@
-package org.flickit.assessment.kit.application.port.out.expertgroup;
-
-public interface CheckExpertGroupExistsPort {
-    boolean existsById(long id);
-}
--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/expertgroup/CheckExpertGroupOwnerPort.java
+++
@@ -1,8 +0,0 @@
-package org.flickit.assessment.kit.application.port.out.expertgroup;
-
-import java.util.UUID;
-
-public interface CheckExpertGroupOwnerPort {
-
-    boolean checkIsOwner(long expertGroupId, UUID userId);
-}
