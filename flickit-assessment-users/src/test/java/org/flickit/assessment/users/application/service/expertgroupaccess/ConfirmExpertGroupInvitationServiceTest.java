d0e51b21c7ee87554768771e487b5383e22a470c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-30 09:25:41 +0330

    FAC-301: Tests refactored

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroupaccess/ConfirmExpertGroupInvitationServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroupaccess/ConfirmExpertGroupInvitationServiceTest.java
@@ -1,6 +1,7 @@
 package org.flickit.assessment.users.application.service.expertgroupaccess;
 
-import org.flickit.assessment.users.application.port.out.expertgroupaccess.CheckInviteInputDataValidationPort;
+import org.flickit.assessment.users.application.port.out.expertgroupaccess.CheckInviteInputDataValidityPort;
+import org.flickit.assessment.users.application.port.out.expertgroupaccess.CheckInviteTokenExpiryPort;
 import org.flickit.assessment.users.application.port.out.expertgroupaccess.ConfirmExpertGroupInvitationPort;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -23,7 +23,9 @@ class ConfirmExpertGroupInvitationServiceTest {
     @InjectMocks
     private ConfirmExpertGroupInvitationService service;
     @Mock
-    private CheckInviteInputDataValidationPort checkInviteInputDataValidationPort;
+    private CheckInviteInputDataValidityPort checkInviteInputDataValidityPort;
+    @Mock
+    private CheckInviteTokenExpiryPort checkInviteTokenExpiryPort;
     @Mock
     private ConfirmExpertGroupInvitationPort confirmExpertGroupInvitationPort;
 
@@ -33,12 +35,13 @@ class ConfirmExpertGroupInvitationServiceTest {
         UUID inviteToken = UUID.randomUUID();
         long expertGroupId = 0L;
 
-        doNothing().when(checkInviteInputDataValidationPort).checkToken(isA(Long.class), isA(UUID.class), isA(UUID.class));
+        when(checkInviteInputDataValidityPort.checkInputData(expertGroupId,userId, inviteToken)).thenReturn(true);
+        when(checkInviteTokenExpiryPort.isInviteTokenValid(inviteToken)).thenReturn(true);
         doNothing().when(confirmExpertGroupInvitationPort).confirmInvitation(isA(UUID.class));
 
         Assertions.assertDoesNotThrow(() -> service.confirmInvitation(expertGroupId, userId, inviteToken));
 
-        verify(checkInviteInputDataValidationPort).checkToken(anyLong(), any(UUID.class), any(UUID.class));
+        verify(checkInviteInputDataValidityPort).checkInputData(anyLong(), any(UUID.class), any(UUID.class));
         verify(confirmExpertGroupInvitationPort).confirmInvitation(any(UUID.class));
     }
 }
