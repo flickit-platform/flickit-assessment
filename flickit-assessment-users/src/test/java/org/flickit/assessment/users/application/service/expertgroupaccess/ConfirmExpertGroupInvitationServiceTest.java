3cca646b525fd069682b3a9d44417d5f52d7b9e5
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-03-30 11:25:21 +0330

    FAC-301: new tests added

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroupaccess/ConfirmExpertGroupInvitationServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroupaccess/ConfirmExpertGroupInvitationServiceTest.java
@@ -1,9 +1,11 @@
 package org.flickit.assessment.users.application.service.expertgroupaccess;
 
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.users.application.port.out.expertgroupaccess.CheckInviteInputDataValidityPort;
 import org.flickit.assessment.users.application.port.out.expertgroupaccess.CheckInviteTokenExpiryPort;
 import org.flickit.assessment.users.application.port.out.expertgroupaccess.ConfirmExpertGroupInvitationPort;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -30,7 +32,8 @@ class ConfirmExpertGroupInvitationServiceTest {
     private ConfirmExpertGroupInvitationPort confirmExpertGroupInvitationPort;
 
     @Test
-    void testInviteMember_validParameters_success() {
+    @DisplayName("Confirm invite member invitation with valid parameters causes success confirmation")
+    void testInviteMemberConfirmation_validParameters_success() {
         UUID userId = UUID.randomUUID();
         UUID inviteToken = UUID.randomUUID();
         long expertGroupId = 0L;
@@ -43,5 +46,41 @@ class ConfirmExpertGroupInvitationServiceTest {
 
         verify(checkInviteInputDataValidityPort).checkInputData(anyLong(), any(UUID.class), any(UUID.class));
         verify(confirmExpertGroupInvitationPort).confirmInvitation(any(UUID.class));
+    }
+
+    @Test
+    @DisplayName("Confirm invite member invitation with invalid parameters causes ResourceNotFoundException")
+    void testConfirmInviteMember_invalidInputData_fail() {
+        UUID userId = UUID.randomUUID();
+        UUID inviteToken = UUID.randomUUID();
+        long expertGroupId = 0L;
+
+        when(checkInviteInputDataValidityPort.checkInputData(anyLong(),any(UUID.class), any(UUID.class))).thenReturn(false);
+
+        Assertions.assertThrows(ResourceNotFoundException.class,
+            () -> service.confirmInvitation(expertGroupId, userId, inviteToken));
+
+        verify(checkInviteInputDataValidityPort).checkInputData(anyLong(), any(UUID.class), any(UUID.class));
+        verifyNoInteractions(checkInviteTokenExpiryPort);
+        verifyNoInteractions(confirmExpertGroupInvitationPort);
+    }
+
+    @Test
+    @DisplayName("Confirm invite member invitation with invalid parameters causes ResourceNotFoundException")
+    void testConfirmInviteMember_expiredInviteToken_fail() {
+        UUID userId = UUID.randomUUID();
+        UUID inviteToken = UUID.randomUUID();
+        long expertGroupId = 0L;
+
+        when(checkInviteInputDataValidityPort.checkInputData(anyLong(),any(UUID.class), any(UUID.class))).thenReturn(true);
+        when(checkInviteTokenExpiryPort.isInviteTokenValid(inviteToken)).thenReturn(false);
+
+
+        Assertions.assertThrows(ResourceNotFoundException.class,
+            () -> service.confirmInvitation(expertGroupId, userId, inviteToken));
+
+        verify(checkInviteInputDataValidityPort).checkInputData(anyLong(), any(UUID.class), any(UUID.class));
+        verify(checkInviteTokenExpiryPort).isInviteTokenValid(any(UUID.class));
+        verifyNoInteractions(confirmExpertGroupInvitationPort);
     }
 }
