996e269b2ccd34ec6e1cf3ea9bb69cedd3eb2de4
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-04-02 15:17:30 +0330

    FAC-209: Set from in EmailAdapter

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/email/EmailAdapter.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/email/EmailAdapter.java
@@ -1,23 +1,21 @@
 package org.flickit.assessment.users.adapter.out.email;
 
 import jakarta.mail.internet.MimeMessage;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.flickit.assessment.common.application.MessageBundle;
 import org.flickit.assessment.common.config.AppSpecProperties;
 import org.flickit.assessment.users.application.port.out.mail.SendExpertGroupInviteMailPort;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 import static org.flickit.assessment.common.config.EmailConfig.EMAIL_SENDER_THREAD_EXECUTOR;
 import static org.flickit.assessment.users.common.MessageKey.INVITE_EXPERT_GROUP_MEMBER_MAIL_BODY;
@@ -25,22 +23,23 @@ import static org.flickit.assessment.users.common.MessageKey.INVITE_EXPERT_GROUP
 
 @Slf4j
 @Component
-@EnableRetry
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class EmailAdapter implements SendExpertGroupInviteMailPort {
 
     private final JavaMailSender mailSender;
     private final AppSpecProperties appSpecProperties;
+    private final MailProperties springMailProperties;
 
     @Override
     @SneakyThrows
     @Async(EMAIL_SENDER_THREAD_EXECUTOR)
-    @Retryable(retryFor = Exception.class, maxAttempts = 10, backoff = @Backoff(delay = 10000))
+    @Retryable(retryFor = Exception.class, maxAttempts = 5, backoff = @Backoff(delay = 10000))
     public void sendInvite(String to, long expertGroupId, UUID inviteToken) {
         MimeMessage message = mailSender.createMimeMessage();
 
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setTo(to);
+        helper.setFrom(springMailProperties.getUsername());
         helper.setSubject(MessageBundle.message(INVITE_EXPERT_GROUP_MEMBER_MAIL_SUBJECT));
         helper.setText(createText(expertGroupId, inviteToken), true);
 
