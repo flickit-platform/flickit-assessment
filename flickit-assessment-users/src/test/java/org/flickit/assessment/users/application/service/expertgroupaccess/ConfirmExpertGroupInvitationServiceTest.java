5bd639e616e67ef8e40a5edb35fafb5fafccb68c
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-04-03 12:59:10 +0330

    FAC-301: Refactor unit tests

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroupaccess/ConfirmExpertGroupInvitationServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroupaccess/ConfirmExpertGroupInvitationServiceTest.java
@@ -1,13 +1,14 @@
 package org.flickit.assessment.users.application.service.expertgroupaccess;
 
+import org.assertj.core.api.Assertions;
 import org.flickit.assessment.common.exception.ResourceAlreadyExistsException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.common.exception.ValidationException;
 import org.flickit.assessment.users.application.domain.ExpertGroupAccess;
 import org.flickit.assessment.users.application.domain.ExpertGroupAccessStatus;
 import org.flickit.assessment.users.application.port.in.expertgroupaccess.ConfirmExpertGroupInvitationUseCase.Param;
-import org.flickit.assessment.users.application.port.out.expertgroupaccess.LoadExpertGroupAccessPort;
 import org.flickit.assessment.users.application.port.out.expertgroupaccess.ConfirmExpertGroupInvitationPort;
+import org.flickit.assessment.users.application.port.out.expertgroupaccess.LoadExpertGroupAccessPort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,13 +20,12 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.flickit.assessment.users.common.ErrorMessageKey.CONFIRM_EXPERT_GROUP_INVITATION_LINK_INVALID;
+import static org.flickit.assessment.users.common.ErrorMessageKey.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class ConfirmExpertGroupInvitationServiceTest {
@@ -43,6 +43,7 @@ class ConfirmExpertGroupInvitationServiceTest {
         UUID currentUserId = UUID.randomUUID();
         UUID inviteToken = UUID.randomUUID();
         long expertGroupId = 0L;
+
         Param param = new Param(expertGroupId, inviteToken, currentUserId);
         LocalDateTime expirationDate = LocalDateTime.now().plusDays(7);
         ExpertGroupAccess expertGroupAccess =
@@ -50,7 +51,7 @@ class ConfirmExpertGroupInvitationServiceTest {
 
         when(loadExpertGroupAccessPort.loadExpertGroupAccess(expertGroupId, currentUserId))
             .thenReturn(Optional.of(expertGroupAccess));
-        doNothing().when(confirmExpertGroupInvitationPort).confirmInvitation(isA(Long.class), isA(UUID.class) );
+        doNothing().when(confirmExpertGroupInvitationPort).confirmInvitation(expertGroupId, currentUserId);
 
         assertDoesNotThrow(() -> service.confirmInvitation(param));
 
@@ -64,12 +65,14 @@ class ConfirmExpertGroupInvitationServiceTest {
         UUID currentUserId = UUID.randomUUID();
         UUID inviteToken = UUID.randomUUID();
         long expertGroupId = 0L;
+
         Param param = new Param(expertGroupId, inviteToken, currentUserId);
 
-        when(loadExpertGroupAccessPort.loadExpertGroupAccess(anyLong(), any(UUID.class))).
-            thenThrow(new ResourceNotFoundException(CONFIRM_EXPERT_GROUP_INVITATION_LINK_INVALID));
+        when(loadExpertGroupAccessPort.loadExpertGroupAccess(expertGroupId, currentUserId))
+            .thenThrow(new ResourceNotFoundException(CONFIRM_EXPERT_GROUP_INVITATION_LINK_INVALID));
 
-        assertThrows(ResourceNotFoundException.class, () -> service.confirmInvitation(param));
+        var throwable = assertThrows(ResourceNotFoundException.class, () -> service.confirmInvitation(param));
+        Assertions.assertThat(throwable).hasMessage(CONFIRM_EXPERT_GROUP_INVITATION_LINK_INVALID);
 
         verify(loadExpertGroupAccessPort).loadExpertGroupAccess(anyLong(), any(UUID.class));
         verifyNoInteractions(confirmExpertGroupInvitationPort);
@@ -81,15 +84,37 @@ class ConfirmExpertGroupInvitationServiceTest {
         UUID currentUserId = UUID.randomUUID();
         UUID inviteToken = UUID.randomUUID();
         long expertGroupId = 0L;
+
         Param param = new Param(expertGroupId, inviteToken, currentUserId);
         LocalDateTime expirationDate = LocalDateTime.now().minusDays(1);
-        ExpertGroupAccess expertGroupAccess =
-            new ExpertGroupAccess(expirationDate, inviteToken, ExpertGroupAccessStatus.PENDING.ordinal());
+        var expertGroupAccess = new ExpertGroupAccess(expirationDate, inviteToken, ExpertGroupAccessStatus.PENDING.ordinal());
 
-        when(loadExpertGroupAccessPort.loadExpertGroupAccess(anyLong(), any(UUID.class))).
-            thenReturn(Optional.of(expertGroupAccess));
+        when(loadExpertGroupAccessPort.loadExpertGroupAccess(expertGroupId, currentUserId))
+            .thenReturn(Optional.of(expertGroupAccess));
 
-        assertThrows(ValidationException.class, () -> service.confirmInvitation(param));
+        var throwable = assertThrows(ValidationException.class, () -> service.confirmInvitation(param));
+        Assertions.assertThat(throwable).hasMessage(CONFIRM_EXPERT_GROUP_INVITATION_INVITE_TOKEN_EXPIRED);
+
+        verify(loadExpertGroupAccessPort).loadExpertGroupAccess(anyLong(), any(UUID.class));
+        verifyNoInteractions(confirmExpertGroupInvitationPort);
+    }
+
+    @Test
+    @DisplayName("Confirm invite member invitation with invalid invite token causes validationException")
+    void testConfirmInviteMember_invalidInviteToken_validationException() {
+        UUID currentUserId = UUID.randomUUID();
+        UUID inviteToken = UUID.randomUUID();
+        long expertGroupId = 0L;
+
+        Param param = new Param(expertGroupId, inviteToken, currentUserId);
+        LocalDateTime expirationDate = LocalDateTime.now().minusDays(1);
+        var expertGroupAccess = new ExpertGroupAccess(expirationDate, UUID.randomUUID(), ExpertGroupAccessStatus.PENDING.ordinal());
+
+        when(loadExpertGroupAccessPort.loadExpertGroupAccess(expertGroupId, currentUserId))
+            .thenReturn(Optional.of(expertGroupAccess));
+
+        var throwable = assertThrows(ValidationException.class, () -> service.confirmInvitation(param));
+        Assertions.assertThat(throwable).hasMessage(CONFIRM_EXPERT_GROUP_INVITATION_INVITE_TOKEN_INVALID);
 
         verify(loadExpertGroupAccessPort).loadExpertGroupAccess(anyLong(), any(UUID.class));
         verifyNoInteractions(confirmExpertGroupInvitationPort);
@@ -101,15 +126,16 @@ class ConfirmExpertGroupInvitationServiceTest {
         UUID currentUserId = UUID.randomUUID();
         UUID inviteToken = UUID.randomUUID();
         long expertGroupId = 0L;
+
         Param param = new Param(expertGroupId, inviteToken, currentUserId);
         LocalDateTime expirationDate = LocalDateTime.now().plusDays(1);
-        ExpertGroupAccess expertGroupAccess =
-            new ExpertGroupAccess(expirationDate, inviteToken, ExpertGroupAccessStatus.ACTIVE.ordinal());
+        var expertGroupAccess = new ExpertGroupAccess(expirationDate, inviteToken, ExpertGroupAccessStatus.ACTIVE.ordinal());
 
-        when(loadExpertGroupAccessPort.loadExpertGroupAccess(anyLong(), any(UUID.class))).
-            thenReturn(Optional.of(expertGroupAccess));
+        when(loadExpertGroupAccessPort.loadExpertGroupAccess(expertGroupId, currentUserId))
+            .thenReturn(Optional.of(expertGroupAccess));
 
-        assertThrows(ResourceAlreadyExistsException.class, () -> service.confirmInvitation(param));
+        var throwable = assertThrows(ResourceAlreadyExistsException.class, () -> service.confirmInvitation(param));
+        Assertions.assertThat(throwable).hasMessage(CONFIRM_EXPERT_GROUP_INVITATION_USER_ID_DUPLICATE);
 
         verify(loadExpertGroupAccessPort).loadExpertGroupAccess(anyLong(), any(UUID.class));
         verifyNoInteractions(confirmExpertGroupInvitationPort);
