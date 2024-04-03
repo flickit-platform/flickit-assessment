f3983be43dc59ce1bd8384acd7b83ab30de56d50
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-04-03 14:30:22 +0330

    FAC-210: Minor reformatting

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/expertgroupaccess/DeleteExpertGroupMemberService.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/expertgroupaccess/DeleteExpertGroupMemberService.java
@@ -2,7 +2,6 @@ package org.flickit.assessment.users.application.service.expertgroupaccess;
 
 import lombok.AllArgsConstructor;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.users.application.port.in.expertgroupaccess.DeleteExpertGroupMemberUseCase;
 import org.flickit.assessment.users.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.users.application.port.out.expertgroupaccess.DeleteExpertGroupMemberPort;
@@ -13,7 +12,6 @@ import java.util.Objects;
 import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
-import static org.flickit.assessment.users.common.ErrorMessageKey.EXPERT_GROUP_ID_NOT_FOUND;
 
 @Service
 @Transactional
