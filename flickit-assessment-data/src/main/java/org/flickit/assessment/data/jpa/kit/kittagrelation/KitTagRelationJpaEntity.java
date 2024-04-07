b3ef09246ad3795ada5195c3cac9a2423964783e
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-04-07 16:46:27 +0330

    FAC-326 Change  baseinfo_assessmentkittag_assessmentkits  table
    The Name of baseinfo_assessmentkittag_assessmentkits  has changed to fak_kit_tag_relation, constraint names have changed too. The id and sequence have been removed. A composite primary key has been established from tagId and kitId. The corresponding jpa entity which named KitTagRelationJpaEntity has been changed to conform to table changes.

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/kittagrelation/KitTagRelationJpaEntity.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/kittagrelation/KitTagRelationJpaEntity.java
@@ -3,25 +3,32 @@ package org.flickit.assessment.data.jpa.kit.kittagrelation;
 import jakarta.persistence.*;
 import lombok.*;
 
+
 @Entity
-@Table(name = "baseinfo_assessmentkittag_assessmentkits")
+@Table(name = "fak_kit_tag_relation")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@IdClass(KitTagRelationJpaEntity.KitTagRelationKey.class)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class KitTagRelationJpaEntity {
 
     @Id
     @EqualsAndHashCode.Include
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "baseinfo_profiletag_profiles_id_seq")
-    @SequenceGenerator(name = "baseinfo_profiletag_profiles_id_seq", sequenceName = "baseinfo_profiletag_profiles_id_seq", allocationSize = 1)
-    @Column(name = "id", updatable = false, nullable = false)
-    private Long id;
-
-    @Column(name = "assessmentkittag_id")
+    @Column(name = "kittag_id", nullable = false)
     private Long tagId;
 
-    @Column(name = "assessmentkit_id")
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "kit_id", nullable = false)
     private Long kitId;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class KitTagRelationKey {
+        private Long tagId;
+        private Long kitId;
+    }
 }
