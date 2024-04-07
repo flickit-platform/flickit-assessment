923f2d550b4bd0902f30eb8b66cff717492ff619
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-04-07 09:08:49 +0330

    FAC-299: Simply handled for users

---
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/config/FileProperties.java
@@ -0,0 +1,23 @@
+package org.flickit.assessment.common.config;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.validation.annotation.Validated;
+
+@Getter
+@Setter
+@Validated
+@Configuration
+@RequiredArgsConstructor
+public class FileProperties {
+
+    @NotBlank
+    DataSize pictureMaxSize = DataSize.ofBytes(1);
+
+    @NotBlank
+    DataSize kitMaxSize = DataSize.ofBytes(1);
+}
--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/common/ErrorMessageKey.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/common/ErrorMessageKey.java
@@ -53,4 +53,6 @@ public class ErrorMessageKey {
     public static final String CONFIRM_EXPERT_GROUP_INVITATION_INVITE_TOKEN_EXPIRED = "confirm-expert-group-invitation.invite-token.expired";
     public static final String CONFIRM_EXPERT_GROUP_INVITATION_INVITE_TOKEN_INVALID = "confirm-expert-group-invitation.invite-token.notValid";
     public static final String CONFIRM_EXPERT_GROUP_INVITATION_USER_ID_DUPLICATE = "confirm-expert-group-invitation.userId.duplicate";
+
+    public static final String FILE_PROPERTIES_MAX_FILE_SIZE_EXCEEDED = "file-properties.max-file-size.exceeded";
 }
