9ec892d77c71e15350c5cd42fc3792feb9a471da
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-04-07 12:45:54 +0330

    FAC-325 Change  baseinfo_assessmentkittag table
    Name of baseinfo_assessmentkittag has changed to fak_kit_tag, the sequence and constraint names have changed too. The corresponding jpa entity which named KitTagJpaEntity, and also its repository interface has added too.

---
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/kittag/KitTagJpaEntity.java
@@ -0,0 +1,27 @@
+package org.flickit.assessment.data.jpa.kit.kittag;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "fak_kit_tag")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class KitTagJpaEntity {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fak_kit_tag_id_seq")
+    @SequenceGenerator(name = "fak_kit_tag_id_seq", sequenceName = "fak_kit_tag_id_seq", allocationSize = 1)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false, unique = true)
+    private String title;
+
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+}
