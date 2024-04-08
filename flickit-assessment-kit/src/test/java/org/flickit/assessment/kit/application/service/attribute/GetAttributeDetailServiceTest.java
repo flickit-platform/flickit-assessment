cdb437a6bac095661c00c16ea42c5227e9260381
mahdirezazadeh <riza.rs2000@gmail.com>
2024-04-09 01:53:16 +0330

    FAC-333:feature:Refactor GetAttributeDetailServiceTest

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/attribute/GetAttributeDetailServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/attribute/GetAttributeDetailServiceTest.java
@@ -2,21 +2,29 @@ package org.flickit.assessment.kit.application.service.attribute;
 
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.kit.application.domain.Attribute;
+import org.flickit.assessment.kit.application.port.in.attribute.GetAttributeDetailUseCase.MaturityLevel;
 import org.flickit.assessment.kit.application.port.in.attribute.GetAttributeDetailUseCase.Param;
+import org.flickit.assessment.kit.application.port.in.attribute.GetAttributeDetailUseCase.Result;
 import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadKitExpertGroupPort;
+import org.flickit.assessment.kit.application.port.out.attribute.LoadAttributePort;
 import org.flickit.assessment.kit.application.port.out.expertgroupaccess.CheckExpertGroupAccessPort;
+import org.flickit.assessment.kit.application.port.out.maturitylevel.LoadAttributeMaturityLevelPort;
+import org.flickit.assessment.kit.application.port.out.question.LoadAttributeQuestionCountPort;
+import org.flickit.assessment.kit.test.fixture.application.AttributeMother;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.flickit.assessment.kit.common.ErrorMessageKey.KIT_ID_NOT_FOUND;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,38 +39,45 @@ class GetAttributeDetailServiceTest {
     @Mock
     private CheckExpertGroupAccessPort checkExpertGroupAccessPort;
 
-//    @Mock
-//    private LoadSubjectDetailPort loadSubjectDetailPort;
-/*
+    @Mock
+    private LoadAttributePort loadAttributePort;
+
+    @Mock
+    private LoadAttributeQuestionCountPort loadAttributeQuestionCountPort;
+
+    @Mock
+    private LoadAttributeMaturityLevelPort loadAttributeMaturityLevelPort;
+
     @Test
-    void testGetSubjectDetail_WhenKitExist_shouldReturnKitDetails() {
+    void testGetAttributeDetail_WhenAttributeExist_shouldReturnAttributeDetails() {
         Param param = new Param(2000L, 2L, UUID.randomUUID());
         var expertGroupId = 14L;
-
+        var expectedQuestionCount = 14;
+        Attribute expectedAttribute = AttributeMother.attributeWithTitle("EgAttribute");
+        List<MaturityLevel> expectedMaturityLevels = List.of(new MaturityLevel(1L, "MaturityLevelEg", 1, 15));
         when(loadKitExpertGroupPort.loadKitExpertGroupId(param.getKitId())).thenReturn(expertGroupId);
         when(checkExpertGroupAccessPort.checkIsMember(expertGroupId, param.getCurrentUserId())).thenReturn(true);
 
-        GetSubjectDetailUseCase.Result expectedResult = new GetSubjectDetailUseCase.Result(
-            123,
-            "subject description",
-            List.of(
-                new GetSubjectDetailUseCase.Attribute(1L, 1, "attribute1"),
-                new GetSubjectDetailUseCase.Attribute(2L, 2, "attribute2"),
-                new GetSubjectDetailUseCase.Attribute(3L, 3, "attribute3")
-            )
-        );
+        when(loadAttributePort.loadByIdAndKitId(param.getAttributeId(), param.getKitId()))
+            .thenReturn(Optional.of(expectedAttribute));
+        when(loadAttributeQuestionCountPort.loadByAttributeId(param.getAttributeId()))
+            .thenReturn(expectedQuestionCount);
+        when(loadAttributeMaturityLevelPort.loadByAttributeId(param.getAttributeId()))
+            .thenReturn(expectedMaturityLevels);
 
-        when(loadSubjectDetailPort.loadByIdAndKitId(param.getSubjectId(), param.getKitId())).thenReturn(expectedResult);
+        Result result = service.getAttributeDetail(param);
 
-        GetSubjectDetailUseCase.Result result = service.getSubjectDetail(param);
-
-        assertEquals(expectedResult.questionCount(), result.questionCount());
-        assertEquals(expectedResult.description(), result.description());
-        assertIterableEquals(expectedResult.attributes(), result.attributes());
-    }*/
+        assertEquals(expectedAttribute.getId(), result.id());
+        assertEquals(expectedAttribute.getIndex(), result.index());
+        assertEquals(expectedAttribute.getTitle(), result.title());
+        assertEquals(expectedQuestionCount, result.questionCount());
+        assertEquals(expectedAttribute.getWeight(), result.weight());
+        assertEquals(expectedAttribute.getDescription(), result.description());
+        assertIterableEquals(expectedMaturityLevels, result.maturityLevels());
+    }
 
     @Test
-    void testGetSubjectDetail_WhenKitDoesNotExist_ThrowsException() {
+    void testGetAttributeDetail_WhenKitDoesNotExist_ThrowsException() {
         Param param = new Param(2000L, 2L, UUID.randomUUID());
 
         when(loadKitExpertGroupPort.loadKitExpertGroupId(param.getKitId()))
@@ -73,7 +88,7 @@ class GetAttributeDetailServiceTest {
     }
 
     @Test
-    void testGetSubjectDetail_WhenUserIsNotMember_ThrowsException() {
+    void testGetAttributeDetail_WhenUserIsNotMember_ThrowsException() {
         Param param = new Param(2000L, 2L, UUID.randomUUID());
         var expertGroupId = 14L;
 
