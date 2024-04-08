20538b7dc7d92a578f4ff1cbad17b46a3cd9a7f8
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-04-08 17:20:40 +0330

    FAC-299: Remove FileConfig and add FileProperties to CommonAutoConfig

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/config/FileConfig.java
+++
@@ -1,9 +0,0 @@
-package org.flickit.assessment.common.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(FileProperties.class)
-public class FileConfig {
-}
