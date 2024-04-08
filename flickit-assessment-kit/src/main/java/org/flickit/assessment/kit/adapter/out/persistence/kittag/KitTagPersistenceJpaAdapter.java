9506ab97282269f4bc590b594d1f8402835da1fc
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-04-08 23:10:55 +0330

    FAC-294: Create LoadKitTagsListPort

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kittag/KitTagPersistenceJpaAdapter.java
@@ -0,0 +1,25 @@
+package org.flickit.assessment.kit.adapter.out.persistence.kittag;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.data.jpa.kit.kittag.KitTagJpaEntity;
+import org.flickit.assessment.data.jpa.kit.kittag.KitTagJpaRepository;
+import org.flickit.assessment.kit.application.domain.KitTag;
+import org.flickit.assessment.kit.application.port.out.kittag.LoadKitTagsListPort;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class KitTagPersistenceJpaAdapter implements LoadKitTagsListPort {
+
+    private final KitTagJpaRepository repository;
+
+    @Override
+    public List<KitTag> load(long kitId) {
+        List<KitTagJpaEntity> entities = repository.findAllByKitId(kitId);
+        return entities.stream()
+            .map(entity -> new KitTag(entity.getId(), entity.getTitle()))
+            .toList();
+    }
+}
