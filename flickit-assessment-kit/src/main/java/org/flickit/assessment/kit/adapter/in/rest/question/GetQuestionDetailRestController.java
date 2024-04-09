be7ac6666d7cdf8d07e8fea661a936b66d3836ff
mahdirezazadeh <riza.rs2000@gmail.com>
2024-04-09 14:06:27 +0330

    FAC-335:feature:Impl GetQuestionDetailRestController

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/question/GetQuestionDetailRestController.java
@@ -0,0 +1,38 @@
+package org.flickit.assessment.kit.adapter.in.rest.question;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.flickit.assessment.kit.application.port.in.question.GetQuestionDetailUseCase;
+import org.flickit.assessment.kit.application.port.in.question.GetQuestionDetailUseCase.Param;
+import org.flickit.assessment.kit.application.port.in.question.GetQuestionDetailUseCase.Result;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class GetQuestionDetailRestController {
+
+    private final GetQuestionDetailUseCase useCase;
+    private final UserContext userContext;
+
+    @GetMapping("assessment-kits/{kitId}/details/questions/{questionId}")
+    public ResponseEntity<GetQuestionDetailResponseDto> getAttributeDetail(@PathVariable("kitId") Long kitId,
+                                                                           @PathVariable("questionId") Long questionId) {
+        var currentUserId = userContext.getUser().id();
+        var response = useCase.getQuestionDetail(toParam(kitId, questionId, currentUserId));
+        return new ResponseEntity<>(toResponseDto(response), HttpStatus.OK);
+    }
+
+    private Param toParam(Long kitId, Long subjectId, UUID currentUserId) {
+        return new Param(kitId, subjectId, currentUserId);
+    }
+
+    private GetQuestionDetailResponseDto toResponseDto(Result result) {
+        return new GetQuestionDetailResponseDto(result.options(), result.attributeImpacts());
+    }
+}
