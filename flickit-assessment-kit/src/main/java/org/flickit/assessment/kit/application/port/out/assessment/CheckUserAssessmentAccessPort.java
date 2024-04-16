1a0b884762cf3a904174fcde21fa96922e0d9be2
Mahnaz Khorsandi <khorsandi.mahnaz@gmail.com>
2024-04-16 17:03:12 +0330

    FAC-303: Remove unused port from kit module

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/assessment/CheckUserAssessmentAccessPort.java
+++
@@ -1,8 +0,0 @@
-package org.flickit.assessment.kit.application.port.out.assessment;
-
-import java.util.UUID;
-
-public interface CheckUserAssessmentAccessPort {
-
-    boolean hasAccess(UUID assessmentId, UUID userId);
-}
