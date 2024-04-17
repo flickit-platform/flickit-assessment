089213adc38416b6a48c2b66b9e04ca649810c79
mahdirezazadeh <riza.rs2000@gmail.com>
2024-04-17 12:29:37 +0330

    FAC-346:feature:Impl ToggleKitLikeService

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/ToggleKitLikeService.java
@@ -0,0 +1,53 @@
+package org.flickit.assessment.kit.application.service.assessmentkit;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.kit.application.port.in.assessmentkit.ToggleKitLikeUseCase;
+import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitPort;
+import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadKitExpertGroupPort;
+import org.flickit.assessment.kit.application.port.out.expertgroupaccess.CheckExpertGroupAccessPort;
+import org.flickit.assessment.kit.application.port.out.kitlike.CheckKitLikeExistencePort;
+import org.flickit.assessment.kit.application.port.out.kitlike.CountKitLikePort;
+import org.flickit.assessment.kit.application.port.out.kitlike.CreateKitLikePort;
+import org.flickit.assessment.kit.application.port.out.kitlike.DeleteKitLikePort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ToggleKitLikeService implements ToggleKitLikeUseCase {
+
+    private final LoadAssessmentKitPort loadKitPort;
+    private final LoadKitExpertGroupPort loadKitExpertGroupPort;
+    private final CheckExpertGroupAccessPort checkExpertGroupAccessPort;
+    private final CheckKitLikeExistencePort checkKitLikeExistencePort;
+    private final CreateKitLikePort createKitLikePort;
+    private final DeleteKitLikePort deleteKitLikePort;
+    private final CountKitLikePort countKitLikePort;
+
+    @Override
+    public Result toggleKitLike(Param param) {
+        var kit = loadKitPort.load(param.getKitId());
+        if (kit.isPrivate())
+            validateCurrentUserIsExpertGroupMember(param.getKitId(), param.getCurrentUserId());
+
+        if (!checkKitLikeExistencePort.exist(param.getKitId(), param.getCurrentUserId())) {
+            createKitLikePort.create(param.getKitId(), param.getCurrentUserId());
+        } else {
+            deleteKitLikePort.delete(param.getKitId(), param.getCurrentUserId());
+        }
+        int likes = countKitLikePort.countByKitId(param.getKitId());
+        return new Result(likes);
+    }
+
+    private void validateCurrentUserIsExpertGroupMember(Long kitId, UUID currentUserId) {
+        var expertGroup = loadKitExpertGroupPort.loadKitExpertGroup(kitId);
+        if (!checkExpertGroupAccessPort.checkIsMember(expertGroup.getId(), currentUserId))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+    }
+}
