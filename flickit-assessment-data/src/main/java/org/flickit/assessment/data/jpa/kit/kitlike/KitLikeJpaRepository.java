6f11fd3d17a9c17eda99d87ff4e8ac5c9d6b70b3
mahdirezazadeh <riza.rs2000@gmail.com>
2024-04-17 15:05:05 +0330

    FAC-346:feature:Impl ToggleKitLikeService out ports

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/kitlike/KitLikeJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/kitlike/KitLikeJpaRepository.java
@@ -2,6 +2,14 @@ package org.flickit.assessment.data.jpa.kit.kitlike;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface KitLikeJpaRepository extends JpaRepository<KitLikeJpaEntity, KitLikeJpaEntity.KitLikeKey> {
 
+    Optional<KitLikeJpaEntity> findByKitIdAndUserId(Long kitId, UUID userId);
+
+    void deleteByKitIdAndUserId(Long kitId, UUID userId);
+
+    Integer countAllByKitId(Long kitId);
 }
