f4d345bdde3033b97746bc8862f67f0b13f38ad2
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-04-17 16:36:01 +0330

    FAC-339: RequestDto added

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/in/rest/spaceuseraccess/InviteSpaceMemberRestController.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/in/rest/spaceuseraccess/InviteSpaceMemberRestController.java
@@ -1,13 +1,10 @@
-package org.flickit.assessment.users.adapter.in.rest.spaceaccess;
+package org.flickit.assessment.users.adapter.in.rest.spaceuseraccess;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.config.jwt.UserContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.flickit.assessment.users.application.port.in.spaceaccess.InviteSpaceMemberUseCase;
 
 import java.util.UUID;
@@ -21,9 +18,14 @@ public class InviteSpaceMemberRestController {
 
     @PostMapping("/spaces/{id}/invite")
     public ResponseEntity<Void> inviteSpaceMember(@PathVariable("id") Long id,
-                                                  @RequestParam("email") String email) {
-        UUID currentUserId = userContext.getUser().id();
+                                                  @RequestBody InviteSpaceMemberRequestDto request) {
+        var currentUserId = userContext.getUser().id();
+        useCase.inviteMember(toParam(id, request, currentUserId));
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    InviteSpaceMemberUseCase.Param toParam(long id, InviteSpaceMemberRequestDto requestDto, UUID currentUserId) {
+        return new InviteSpaceMemberUseCase.Param(id, requestDto.email(), currentUserId);
     }
 }
