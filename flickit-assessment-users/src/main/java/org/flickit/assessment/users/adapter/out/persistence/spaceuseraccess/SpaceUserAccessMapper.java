51166e0a029bcfe75b8abd8f90280bf9de4827f9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-04-22 11:16:54 +0330

    FAC-340: SpaceUserAccessAdapter with its Mapper class added

---
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/spaceuseraccess/SpaceUserAccessMapper.java
@@ -0,0 +1,13 @@
+package org.flickit.assessment.users.adapter.out.persistence.spaceuseraccess;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.flickit.assessment.data.jpa.users.spaceuseraccess.SpaceUserAccessJpaEntity;
+import org.flickit.assessment.users.application.port.out.spaceuseraccess.CreateSpaceUserAccessPort;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SpaceUserAccessMapper {
+    public static SpaceUserAccessJpaEntity paramsToEntity(CreateSpaceUserAccessPort.Param param) {
+        return new SpaceUserAccessJpaEntity(param.spaceId(), param.userid(), param.creationTime(), param.createdBy());
+    }
+}
---
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/spaceuseraccess/SpaceUserAccessPersistenceAdapter.java
@@ -0,0 +1,26 @@
+package org.flickit.assessment.users.adapter.out.persistence.spaceuseraccess;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.data.jpa.users.spaceuseraccess.SpaceUserAccessJpaEntity;
+import org.flickit.assessment.data.jpa.users.spaceuseraccess.SpaceUserAccessJpaRepository;
+import org.flickit.assessment.users.application.port.out.spaceuseraccess.CreateSpaceUserAccessPort;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class SpaceUserAccessPersistenceAdapter implements
+    CreateSpaceUserAccessPort {
+
+    private SpaceUserAccessJpaRepository repository;
+
+    @Override
+    public void createAccess(List<Param> param) {
+        List<SpaceUserAccessJpaEntity> entities = param
+            .stream()
+            .map(SpaceUserAccessMapper::paramsToEntity).toList();
+
+        repository.saveAll(entities);
+    }
+}
