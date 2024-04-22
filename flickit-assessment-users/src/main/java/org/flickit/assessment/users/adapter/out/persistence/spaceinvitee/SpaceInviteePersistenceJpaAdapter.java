7f8287cabce201f36b8abb3e237f2c8d55546bc7
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-04-22 09:34:48 +0330

    FAC-340: SpaceInviteePersistenceJpaAdapter with  two ports implementation, added

---
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/spaceinvitee/SpaceInviteePersistenceJpaAdapter.java
@@ -0,0 +1,38 @@
+package org.flickit.assessment.users.adapter.out.persistence.spaceinvitee;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.data.jpa.users.spaceinvitee.SpaceInviteeJpaEntity;
+import org.flickit.assessment.data.jpa.users.spaceinvitee.SpaceInviteeJpaRepository;
+import org.flickit.assessment.users.application.port.in.spaceinvitee.LoadUserInvitedSpacesPort;
+import org.flickit.assessment.users.application.port.out.spaceinvitee.DeleteSpaceUserInvitations;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+@RequiredArgsConstructor
+public class SpaceInviteePersistenceJpaAdapter implements
+    LoadUserInvitedSpacesPort,
+    DeleteSpaceUserInvitations {
+
+    private final SpaceInviteeJpaRepository repository;
+
+    @Override
+    public void delete(String email) {
+        repository.deleteByEmail(email);
+    }
+
+    @Override
+    public List<Result> loadSpaces(String email) {
+        var invitee = repository.findByEmail(email);
+        return invitee
+            .stream()
+            .map(SpaceInviteePersistenceJpaAdapter::mapInviteeEntityToPortResult)
+            .toList();
+    }
+
+    private static Result mapInviteeEntityToPortResult(SpaceInviteeJpaEntity entities) {
+        return new Result(entities.getSpaceId(), entities.getCreatedBy());
+    }
+}
