f823cf85d1527928545b88f80f931b70194d0030
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-04-22 16:16:38 +0330

    FAC-340: Minor fix to respond top multi rows in invitee table

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/users/spaceinvitee/SpaceInviteeJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/users/spaceinvitee/SpaceInviteeJpaRepository.java
@@ -2,12 +2,12 @@ package org.flickit.assessment.data.jpa.users.spaceinvitee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface SpaceInviteeJpaRepository extends JpaRepository<SpaceInviteeJpaEntity, UUID> {
 
     void deleteByEmail(String email);
 
-    Optional<SpaceInviteeJpaEntity> findByEmail(String email);
+    List<SpaceInviteeJpaEntity> findByEmail(String email);
 }
