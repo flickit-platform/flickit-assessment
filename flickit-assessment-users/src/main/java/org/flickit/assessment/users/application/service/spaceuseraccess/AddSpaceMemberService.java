5af2403bce342518a59e507225b603f257a2dfc3
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-04-22 18:43:17 +0330

    FAC-329: Refactor service

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/spaceuseraccess/AddSpaceMemberService.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/spaceuseraccess/AddSpaceMemberService.java
@@ -3,19 +3,18 @@ package org.flickit.assessment.users.application.service.spaceuseraccess;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceAlreadyExistsException;
-import org.flickit.assessment.common.exception.ValidationException;
 import org.flickit.assessment.users.application.port.in.spaceuseraccess.AddSpaceMemberUseCase;
 import org.flickit.assessment.users.application.port.out.spaceuseraccess.AddSpaceMemberPort;
-import org.flickit.assessment.users.application.port.out.spaceuseraccess.CheckSpaceMemberAccessPort;
-import org.flickit.assessment.users.application.port.out.space.CheckSpaceExistencePort;
-import org.flickit.assessment.users.application.port.out.user.LoadUserIdByEmailPort;
+import org.flickit.assessment.users.application.port.out.spaceuseraccess.CheckSpaceAccessPort;
+import org.flickit.assessment.users.application.port.out.user.LoadUserPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static org.flickit.assessment.users.common.ErrorMessageKey.*;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.users.common.ErrorMessageKey.ADD_SPACE_MEMBER_SPACE_USER_DUPLICATE;
 
 @Service
 @Transactional
@@ -33,13 +32,13 @@ public class AddSpaceMemberService implements AddSpaceMemberUseCase {
 
         boolean inviterHasAccess = checkSpaceAccessPort.checkIsMember(spaceId, currentUserId);
         if (!inviterHasAccess)
-            throw new AccessDeniedException(ADD_SPACE_MEMBER_INVITER_ACCESS_NOT_FOUND);
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
-        UUID userId = loadUserIdByEmailPort.loadByEmail(param.getEmail());
+        UUID userId = loadUserPort.loadUserIdByEmail(param.getEmail());
 
         boolean inviteeHasAccess = checkSpaceAccessPort.checkIsMember(spaceId, userId);
         if (inviteeHasAccess)
-            throw new ResourceAlreadyExistsException(ADD_SPACE_MEMBER_INVITEE_ACCESS_DUPLICATE);
+            throw new ResourceAlreadyExistsException(ADD_SPACE_MEMBER_SPACE_USER_DUPLICATE);
 
         addSpaceMemberPort.persist(toParam(spaceId, userId, currentUserId, LocalDateTime.now()));
     }
