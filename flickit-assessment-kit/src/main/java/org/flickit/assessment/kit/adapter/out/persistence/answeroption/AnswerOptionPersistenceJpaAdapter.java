39b1b0034c63831dfaf7313b2793439e554b1bf3
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-04-23 17:41:08 +0330

    FAC-335: Remove LoadAnswerOptionsByQuestionAndKitPort

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/answeroption/AnswerOptionPersistenceJpaAdapter.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/answeroption/AnswerOptionPersistenceJpaAdapter.java
@@ -1,30 +1,23 @@
 package org.flickit.assessment.kit.adapter.out.persistence.answeroption;
 
 import lombok.RequiredArgsConstructor;
-import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.data.jpa.kit.answeroption.AnswerOptionJpaRepository;
-import org.flickit.assessment.data.jpa.kit.assessmentkit.AssessmentKitJpaRepository;
 import org.flickit.assessment.kit.application.domain.AnswerOption;
 import org.flickit.assessment.kit.application.port.out.answeroption.CreateAnswerOptionPort;
-import org.flickit.assessment.kit.application.port.out.answeroption.LoadAnswerOptionsByQuestionAndKitPort;
 import org.flickit.assessment.kit.application.port.out.answeroption.LoadAnswerOptionsByQuestionPort;
 import org.flickit.assessment.kit.application.port.out.answeroption.UpdateAnswerOptionPort;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-import static org.flickit.assessment.kit.common.ErrorMessageKey.KIT_ID_NOT_FOUND;
-
 @Component
 @RequiredArgsConstructor
 public class AnswerOptionPersistenceJpaAdapter implements
     UpdateAnswerOptionPort,
     LoadAnswerOptionsByQuestionPort,
-    CreateAnswerOptionPort,
-    LoadAnswerOptionsByQuestionAndKitPort {
+    CreateAnswerOptionPort {
 
     private final AnswerOptionJpaRepository repository;
-    private final AssessmentKitJpaRepository kitRepository;
 
     @Override
     public void update(UpdateAnswerOptionPort.Param param) {
@@ -44,16 +37,5 @@ public class AnswerOptionPersistenceJpaAdapter implements
     @Override
     public Long persist(CreateAnswerOptionPort.Param param) {
         return repository.save(AnswerOptionMapper.mapToJpaEntity(param)).getId();
-    }
-
-    @Override
-    public List<AnswerOption> loadByQuestionIdAndKitId(Long questionId, Long kitId) {
-        Long kitVersionId = kitRepository.findById(kitId)
-            .orElseThrow(() -> new ResourceNotFoundException(KIT_ID_NOT_FOUND))
-            .getKitVersionId();
-
-        return repository.findByQuestionIdAndKitVersionId(questionId, kitVersionId).stream()
-            .map(AnswerOptionMapper::mapToDomainModel)
-            .toList();
     }
 }
