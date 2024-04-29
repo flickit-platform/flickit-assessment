75cab5f694257a3669ff88fea7122945049590ee
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-04-29 06:22:42 +0330

    FAC-361: Controller, Usecase and UsecaseParamTest added

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/common/ErrorMessageKey.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/common/ErrorMessageKey.java
@@ -68,4 +68,8 @@ public class ErrorMessageKey {
     public static final String INVITE_SPACE_MEMBER_SPACE_ID_NOT_NULL = "invite-space-member.spaceId.notUll";
     public static final String INVITE_SPACE_MEMBER_EMAIL_NOT_NULL = "invite-space-member.email.notNull";
     public static final String INVITE_SPACE_MEMBER_SPACE_USER_DUPLICATE = "invite-space-member.spaceId.userId.duplicate";
+
+    public static final String DELETE_SPACE_ASSESSMENT_EXIST = "delete-space.kits.exist";
+    public static final String DELETE_SPACE_SPACE_ID_NOT_NULL = "delete-space.space-id.notNull";
+
 }
