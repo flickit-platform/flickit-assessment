221ee2c1979ae8b65a14ca285dd871da50945d64
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-04-29 16:38:11 +0330

    FAC-340: Merge SpaceUserAccessPersistenceAdapter into SpaceUserAccessPersistenceJpaAdapter

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/spaceuseraccess/SpaceUserAccessPersistenceJpaAdapter.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/spaceuseraccess/SpaceUserAccessPersistenceJpaAdapter.java
@@ -5,10 +5,11 @@ import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.data.jpa.users.space.SpaceJpaRepository;
 import org.flickit.assessment.data.jpa.users.spaceuseraccess.SpaceUserAccessJpaEntity;
 import org.flickit.assessment.data.jpa.users.spaceuseraccess.SpaceUserAccessJpaRepository;
-import org.flickit.assessment.users.application.port.out.spaceuseraccess.AddSpaceMemberPort;
 import org.flickit.assessment.users.application.port.out.spaceuseraccess.CheckSpaceAccessPort;
+import org.flickit.assessment.users.application.port.out.spaceuseraccess.CreateSpaceUserAccessPort;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.flickit.assessment.users.common.ErrorMessageKey.SPACE_ID_NOT_FOUND;
@@ -16,22 +17,31 @@ import static org.flickit.assessment.users.common.ErrorMessageKey.SPACE_ID_NOT_F
 @Component
 @RequiredArgsConstructor
 public class SpaceUserAccessPersistenceJpaAdapter implements
-    AddSpaceMemberPort,
+    CreateSpaceUserAccessPort,
     CheckSpaceAccessPort {
 
     private final SpaceUserAccessJpaRepository repository;
     private final SpaceJpaRepository spaceRepository;
 
     @Override
-    public void persist(Param param) {
-        SpaceUserAccessJpaEntity unsavedEntity = new SpaceUserAccessJpaEntity(param.spaceId(), param.invitee(),
-            param.inviter(), param.creationTime());
+    public void persist(CreateSpaceUserAccessPort.Param param) {
+        SpaceUserAccessJpaEntity unsavedEntity = new SpaceUserAccessJpaEntity(param.spaceId(), param.userId(),
+            param.createdBy(), param.creationTime());
         repository.save(unsavedEntity);
     }
 
     @Override
+    public void persistAll(List<CreateSpaceUserAccessPort.Param> param) {
+        List<SpaceUserAccessJpaEntity> entities = param
+            .stream()
+            .map(SpaceUserAccessMapper::paramsToEntity).toList();
+
+        repository.saveAll(entities);
+    }
+
+    @Override
     public boolean checkIsMember(long spaceId, UUID userId) {
-        if(!spaceRepository.existsById(spaceId))
+        if (!spaceRepository.existsById(spaceId))
             throw new ResourceNotFoundException(SPACE_ID_NOT_FOUND);
 
         return repository.existsByUserIdAndSpaceId(userId, spaceId);
