04a5b3e86d7c9fdf79387bd3e2a20f1873b8fd91
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-04-29 18:10:29 +0330

    FAC-363: CheckSpaceAccessPort added to service with test

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/spaceuseraccess/DeleteSpaceMemberService.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/spaceuseraccess/DeleteSpaceMemberService.java
@@ -2,8 +2,10 @@ package org.flickit.assessment.users.application.service.spaceuseraccess;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.users.application.port.in.spaceuseraccess.DeleteSpaceMemberUseCase;
 import org.flickit.assessment.users.application.port.out.space.LoadSpaceOwnerPort;
+import org.flickit.assessment.users.application.port.out.spaceuseraccess.CheckSpaceAccessPort;
 import org.flickit.assessment.users.application.port.out.spaceuseraccess.DeleteSpaceMemberPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +14,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.users.common.ErrorMessageKey.DELETE_SPACE_MEMBER_USER_ID_NOT_FOUND;
 
 @Service
 @Transactional
@@ -19,16 +22,21 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT
 public class DeleteSpaceMemberService implements DeleteSpaceMemberUseCase {
 
     private final LoadSpaceOwnerPort loadSpaceOwnerPort;
+    private final CheckSpaceAccessPort checkSpaceAccessPort;
     private final DeleteSpaceMemberPort deleteSpaceMemberPort;
 
     @Override
     public void deleteMember(Param param) {
         validateCurrentUser(param.getSpaceId(), param.getCurrentUserId());
+        var access = checkSpaceAccessPort.checkIsMember(param.getSpaceId(), param.getUserId());
+        if (!access)
+            throw new ResourceNotFoundException(DELETE_SPACE_MEMBER_USER_ID_NOT_FOUND);
         deleteSpaceMemberPort.delete(param.getSpaceId(), param.getUserId());
     }
 
     private void validateCurrentUser(Long spaceId, UUID currentUserId) {
         UUID spaceOwnerId = loadSpaceOwnerPort.loadOwnerId(spaceId);
+
         if (!Objects.equals(spaceOwnerId, currentUserId))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
     }
--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/spaceuseraccess/DeleteSpaceMemberServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/spaceuseraccess/DeleteSpaceMemberServiceTest.java
@@ -1,8 +1,10 @@
 package org.flickit.assessment.users.application.service.spaceuseraccess;
 
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.users.application.port.in.spaceuseraccess.DeleteSpaceMemberUseCase.Param;
 import org.flickit.assessment.users.application.port.out.space.LoadSpaceOwnerPort;
+import org.flickit.assessment.users.application.port.out.spaceuseraccess.CheckSpaceAccessPort;
 import org.flickit.assessment.users.application.port.out.spaceuseraccess.DeleteSpaceMemberPort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.users.common.ErrorMessageKey.DELETE_SPACE_MEMBER_USER_ID_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -25,6 +28,8 @@ class DeleteSpaceMemberServiceTest {
 
     @Mock
     LoadSpaceOwnerPort loadSpaceOwnerPort;
+    @Mock
+    CheckSpaceAccessPort checkSpaceAccessPort;
     @Mock
     DeleteSpaceMemberPort deleteSpaceMemberPort;
 
@@ -40,6 +45,24 @@ class DeleteSpaceMemberServiceTest {
 
         assertThrows(AccessDeniedException.class, ()-> service.deleteMember(param), COMMON_CURRENT_USER_NOT_ALLOWED);
         verify(loadSpaceOwnerPort).loadOwnerId(spaceId);
+        verifyNoInteractions(checkSpaceAccessPort);
+        verifyNoInteractions(deleteSpaceMemberPort);
+    }
+
+    @Test
+    @DisplayName("Deleting a member from space, should be done on a member")
+    void testDeleteSpaceMember_invalidUserId_resourceNotFound(){
+        long spaceId = 0L;
+        var userId = UUID.randomUUID();
+        var currentUserId = UUID.randomUUID();
+        Param param = new Param(spaceId, userId, currentUserId);
+
+        when(loadSpaceOwnerPort.loadOwnerId(spaceId)).thenReturn(currentUserId);
+        when(checkSpaceAccessPort.checkIsMember(spaceId, userId)).thenReturn(false);
+
+        assertThrows(ResourceNotFoundException.class, ()-> service.deleteMember(param), DELETE_SPACE_MEMBER_USER_ID_NOT_FOUND);
+        verify(loadSpaceOwnerPort).loadOwnerId(spaceId);
+        verify(checkSpaceAccessPort).checkIsMember(spaceId, userId);
         verifyNoInteractions(deleteSpaceMemberPort);
     }
 
@@ -52,10 +75,12 @@ class DeleteSpaceMemberServiceTest {
         Param param = new Param(spaceId, userId, currentUserId);
 
         when(loadSpaceOwnerPort.loadOwnerId(spaceId)).thenReturn(currentUserId);
+        when(checkSpaceAccessPort.checkIsMember(spaceId, userId)).thenReturn(true);
         doNothing().when(deleteSpaceMemberPort).delete(spaceId, userId);
 
         assertDoesNotThrow(() -> service.deleteMember(param));
         verify(loadSpaceOwnerPort).loadOwnerId(spaceId);
+        verify(checkSpaceAccessPort).checkIsMember(spaceId, userId);
         verify(deleteSpaceMemberPort).delete(spaceId, userId);
     }
 }
