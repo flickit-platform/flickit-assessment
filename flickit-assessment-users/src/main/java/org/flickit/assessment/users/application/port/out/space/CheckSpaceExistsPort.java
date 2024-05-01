5818cdaf85059d033c2b6c64581a8f6f3d76cfe6
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-05-01 16:05:21 +0330

    FAC-369: One TODO added

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/port/out/space/CheckSpaceExistsPort.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/port/out/space/CheckSpaceExistsPort.java
@@ -2,5 +2,5 @@ package org.flickit.assessment.users.application.port.out.space;
 
 public interface CheckSpaceExistsPort {
 
-    boolean existById(long spaceId);
+    boolean existById(long spaceId); //TODO: Change to existByIdDeletedFalse after merging Delete Space
 }
