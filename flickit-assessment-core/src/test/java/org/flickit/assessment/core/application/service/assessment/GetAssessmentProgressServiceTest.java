f68df8266f775b252e01754eabde0a66fc6d3022
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-05-02 13:59:22 +0330

    FAC-379 Add user assessment access validation to service
    Add new tests due to the new validation. Fix identifier duplicate numbering issue in changelog-0.18.xml.

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/GetAssessmentProgressServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/GetAssessmentProgressServiceTest.java
@@ -1,8 +1,10 @@
 package org.flickit.assessment.core.application.service.assessment;
 
 import org.assertj.core.api.Assertions;
+import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.core.application.port.in.assessment.GetAssessmentProgressUseCase.Param;
+import org.flickit.assessment.core.application.port.out.assessment.CheckUserAssessmentAccessPort;
 import org.flickit.assessment.core.application.port.out.assessment.GetAssessmentProgressPort;
 import org.flickit.assessment.core.test.fixture.application.AssessmentMother;
 import org.junit.jupiter.api.Test;
@@ -14,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.flickit.assessment.core.common.ErrorMessageKey.GET_ASSESSMENT_PROGRESS_ASSESSMENT_RESULT_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -29,12 +32,17 @@ class GetAssessmentProgressServiceTest {
     @Mock
     private GetAssessmentProgressPort getAssessmentProgressPort;
 
+    @Mock
+    private CheckUserAssessmentAccessPort checkUserAssessmentAccessPort;
+
     @Test
     void testGetAssessmentProgress_ValidResult() {
         var assessment = AssessmentMother.assessment();
         UUID assessmentId = assessment.getId();
-        Param param = new Param(assessmentId);
+        UUID currentUserId = UUID.randomUUID();
+        Param param = new Param(assessmentId, currentUserId);
 
+        when(checkUserAssessmentAccessPort.hasAccess(assessmentId, currentUserId)).thenReturn(true);
         when(getAssessmentProgressPort.getAssessmentProgressById(assessmentId))
             .thenReturn(new GetAssessmentProgressPort.Result(assessmentId, 5, 10));
 
@@ -55,12 +63,27 @@ class GetAssessmentProgressServiceTest {
     void testGetAssessmentProgress_InValidAssessmentId() {
         var assessment = AssessmentMother.assessment();
         UUID assessmentId = assessment.getId();
-        Param param = new Param(assessmentId);
+        UUID currentUserId = UUID.randomUUID();
+        Param param = new Param(assessmentId, currentUserId);
 
+        when(checkUserAssessmentAccessPort.hasAccess(assessmentId, currentUserId)).thenReturn(true);
         when(getAssessmentProgressPort.getAssessmentProgressById(assessmentId))
             .thenThrow(new ResourceNotFoundException(GET_ASSESSMENT_PROGRESS_ASSESSMENT_RESULT_NOT_FOUND));
 
         var throwable = assertThrows(ResourceNotFoundException.class, () -> service.getAssessmentProgress(param));
         Assertions.assertThat(throwable).hasMessage(GET_ASSESSMENT_PROGRESS_ASSESSMENT_RESULT_NOT_FOUND);
+    }
+
+    @Test
+    void testGetAssessmentProgress_WhenCurrentUserDoesntHaveAssessmentAccess_ThenThrowsAccessDeniedException() {
+        var assessment = AssessmentMother.assessment();
+        UUID assessmentId = assessment.getId();
+        UUID currentUserId = UUID.randomUUID();
+        Param param = new Param(assessmentId, currentUserId);
+
+        when(checkUserAssessmentAccessPort.hasAccess(assessmentId, currentUserId)).thenReturn(false);
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.getAssessmentProgress(param));
+        Assertions.assertThat(throwable).hasMessage(COMMON_CURRENT_USER_NOT_ALLOWED);
     }
 }
