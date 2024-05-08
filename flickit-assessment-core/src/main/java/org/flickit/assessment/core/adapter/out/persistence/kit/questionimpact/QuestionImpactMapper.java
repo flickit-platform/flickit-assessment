4c7e5bd11480873ef599dbdab2deb9c92bd63ebd
mahdirezazadeh <riza.rs2000@gmail.com>
2024-05-08 15:13:32 +0330

    FAC-385:feature:Load AnswerOptions with persistence in AssessmentCalculateInfoLoadAdapter

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/kit/questionimpact/QuestionImpactMapper.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/kit/questionimpact/QuestionImpactMapper.java
@@ -6,7 +6,7 @@ import org.flickit.assessment.core.application.domain.QuestionImpact;
 import org.flickit.assessment.data.jpa.kit.questionimpact.QuestionImpactJpaEntity;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class QuestionImpactMother {
+public class QuestionImpactMapper {
 
     public static QuestionImpact mapToDomainModel(QuestionImpactJpaEntity entity) {
         return new QuestionImpact(
