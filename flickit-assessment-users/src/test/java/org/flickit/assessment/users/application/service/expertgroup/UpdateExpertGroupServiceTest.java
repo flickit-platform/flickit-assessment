05f637412775559e1a1607d2b4f5b0582c6d28c0
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-05-09 18:42:45 +0330

    FAC-177: new test added to check expert group existence

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroup/UpdateExpertGroupServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroup/UpdateExpertGroupServiceTest.java
@@ -1,6 +1,7 @@
 package org.flickit.assessment.users.application.service.expertgroup;
 
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.users.application.port.in.expertgroup.UpdateExpertGroupUseCase;
 import org.flickit.assessment.users.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.users.application.port.out.expertgroup.UpdateExpertGroupPort;
@@ -15,6 +16,8 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.flickit.assessment.users.common.ErrorMessageKey.EXPERT_GROUP_ID_NOT_FOUND;
+
 
 
 @ExtendWith(MockitoExtension.class)
@@ -30,7 +33,7 @@ class UpdateExpertGroupServiceTest {
     UpdateExpertGroupPort updateExpertGroupPort;
 
     @Test
-    @DisplayName("If expert group not exists or the CurrentUser is not owner, service should throw AccessDenied")
+    @DisplayName("If the currentUser is not owner, service should throw AccessDenied")
     void testUpdateExpertGroup_invalidParameters_AccessDeniedException(){
         var expertGroupId = 1L;
         var currentUserId = UUID.randomUUID();
@@ -40,6 +43,22 @@ class UpdateExpertGroupServiceTest {
         when(loadExpertGroupOwnerPort.loadOwnerId(expertGroupId)).thenReturn(UUID.randomUUID());
 
         assertThrows(AccessDeniedException.class, ()-> service.updateExpertGroup(param));
+
+        verify(loadExpertGroupOwnerPort).loadOwnerId(expertGroupId);
+        verifyNoInteractions(updateExpertGroupPort);
+    }
+
+    @Test
+    @DisplayName("If the expert group not exists, service should throw ResourceNotFound")
+    void testUpdateExpertGroup_expertGroupNotExists_ResourceNotFoundException(){
+        var expertGroupId = 1L;
+        var currentUserId = UUID.randomUUID();
+        UpdateExpertGroupUseCase.Param param = new UpdateExpertGroupUseCase.Param(expertGroupId, "title", "bio",
+            "about", "https://www.google.com", currentUserId);
+
+        when(loadExpertGroupOwnerPort.loadOwnerId(expertGroupId)).thenThrow(new ResourceNotFoundException(EXPERT_GROUP_ID_NOT_FOUND));
+
+        assertThrows(ResourceNotFoundException.class, ()-> service.updateExpertGroup(param), EXPERT_GROUP_ID_NOT_FOUND);
 
         verify(loadExpertGroupOwnerPort).loadOwnerId(expertGroupId);
         verifyNoInteractions(updateExpertGroupPort);
@@ -57,8 +76,8 @@ class UpdateExpertGroupServiceTest {
         doNothing().when(updateExpertGroupPort).update(any());
 
         assertDoesNotThrow(()-> service.updateExpertGroup(param));
-        verify(loadExpertGroupOwnerPort).loadOwnerId(expertGroupId);
 
+        verify(loadExpertGroupOwnerPort).loadOwnerId(expertGroupId);
         verify(updateExpertGroupPort).update(any());
     }
 }
