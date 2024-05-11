bf7695c51f99755352a78fe4d410ade59ac927eb
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-05-11 21:30:23 +0330

    FAC-389 Impl update user service

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/user/UpdateUserService.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/user/UpdateUserService.java
@@ -1,16 +1,28 @@
 package org.flickit.assessment.users.application.service.user;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.users.application.domain.User;
 import org.flickit.assessment.users.application.port.in.user.UpdateUserUseCase;
+import org.flickit.assessment.users.application.port.out.user.UpdateUserPort;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class UpdateUserService implements UpdateUserUseCase {
+
+    private final UpdateUserPort port;
 
     @Override
     public User updateUser(Param param) {
-        return null;
+        return port.updateUser(toParam(param));
+    }
+
+    private UpdateUserPort.Param toParam(Param param) {
+        return new UpdateUserPort.Param(param.getUserId(),
+            param.getDisplayName(),
+            param.getBio(),
+            param.getLinkedin());
     }
 }
