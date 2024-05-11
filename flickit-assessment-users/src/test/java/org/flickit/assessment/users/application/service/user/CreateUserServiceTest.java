df70f55b755a1a2b21e73cdff86ca869dfb2b607
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-05-11 16:12:12 +0330

    FAC-395 Add unit tests to CreateUserServiceTest

---
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/user/CreateUserServiceTest.java
@@ -0,0 +1,42 @@
+package org.flickit.assessment.users.application.service.user;
+
+import org.flickit.assessment.users.application.domain.User;
+import org.flickit.assessment.users.application.port.in.user.CreateUserUseCase;
+import org.flickit.assessment.users.application.port.out.user.CreateUserPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CreateUserServiceTest {
+
+    @InjectMocks
+    private CreateUserService service;
+
+    @Mock
+    CreateUserPort port;
+
+    @Test
+    void testCreateUserService_ValidInput_ValidResult() {
+        UUID userId = UUID.randomUUID();
+        String email = "admin@flickit.org";
+        String displayName = "Flickit Admin";
+        CreateUserUseCase.Param param = new CreateUserUseCase.Param(userId,
+            email,
+            displayName);
+        when(port.createUser(any(User.class))).thenReturn(userId);
+
+        CreateUserUseCase.Result result = service.createUser(param);
+
+        assertNotNull(result);
+        assertEquals(userId, result.userId());
+    }
+}
