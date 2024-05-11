0df9a7f650e7866cd6eccada24f339142c0ae1b4
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-05-11 15:39:54 +0330

    FAC-395 Use @Pattern instead of @Email
    As @Email validation doesn't work properly, a custom regex for email validation has been used.

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/port/in/user/CreateUserUseCase.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/port/in/user/CreateUserUseCase.java
@@ -1,8 +1,8 @@
 package org.flickit.assessment.users.application.port.in.user;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.flickit.assessment.common.application.SelfValidating;
@@ -24,7 +24,7 @@ public interface CreateUserUseCase {
         @NotNull(message = COMMON_CURRENT_USER_ID_NOT_NULL)
         UUID userId;
 
-        @Email(message = CREATE_USER_EMAIL_NOT_VALID)
+        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = CREATE_USER_EMAIL_NOT_VALID)
         String email;
 
         @NotBlank(message = CREATE_USER_DISPLAY_NAME_NOT_BLANK)
