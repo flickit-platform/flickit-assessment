39e17c096535ade6022cca87e17773f300691144
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-05-11 15:20:18 +0330

    FAC-395 Impl create user service

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/user/CreateUserService.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/user/CreateUserService.java
@@ -1,15 +1,31 @@
 package org.flickit.assessment.users.application.service.user;
 
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.users.application.domain.User;
 import org.flickit.assessment.users.application.port.in.user.CreateUserUseCase;
+import org.flickit.assessment.users.application.port.out.user.CreateUserPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class CreateUserService implements CreateUserUseCase {
+
+    private final CreateUserPort port;
 
     @Override
     public Result createUser(Param param) {
-        return null;
+        User user = new User(param.getUserId(),
+            param.getEmail(),
+            param.getDisplayName(),
+            null,
+            null,
+            null);
+
+        UUID userId = port.createUser(user);
+        return new Result(userId);
     }
 }
