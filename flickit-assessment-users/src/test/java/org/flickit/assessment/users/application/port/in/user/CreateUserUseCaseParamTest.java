e57fb8493c36991761fa28c9b64cdef2d5140039
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-05-11 16:01:17 +0330

    FAC-395 Add unit tests to CreateUserUseCaseParamTest

---
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/port/in/user/CreateUserUseCaseParamTest.java
@@ -0,0 +1,50 @@
+package org.flickit.assessment.users.application.port.in.user;
+
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
+import static org.flickit.assessment.users.common.ErrorMessageKey.CREATE_USER_DISPLAY_NAME_NOT_BLANK;
+import static org.flickit.assessment.users.common.ErrorMessageKey.CREATE_USER_EMAIL_NOT_VALID;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateUserUseCaseParamTest {
+
+    @Test
+    void testCreateUser_UserIdIsNull_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> new CreateUserUseCase.Param(null, "admin@flickit.org", "Flickit Admin"));
+        assertThat(throwable).hasMessage("userId: " + COMMON_CURRENT_USER_ID_NOT_NULL);
+    }
+
+    @Test
+    void testCreateUser_NotValidEmail_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> new CreateUserUseCase.Param(UUID.randomUUID(), "admin@flickit", "Flickit Admin"));
+        assertThat(throwable).hasMessage("email: " + CREATE_USER_EMAIL_NOT_VALID);
+    }
+
+    @Test
+    void testCreateUser_NullDisplayName_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> new CreateUserUseCase.Param(UUID.randomUUID(), "admin@flickit.com", null));
+        assertThat(throwable).hasMessage("displayName: " + CREATE_USER_DISPLAY_NAME_NOT_BLANK);
+    }
+
+    @Test
+    void testCreateUser_EmptyDisplayName_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> new CreateUserUseCase.Param(UUID.randomUUID(), "admin@flickit.com", ""));
+        assertThat(throwable).hasMessage("displayName: " + CREATE_USER_DISPLAY_NAME_NOT_BLANK);
+    }
+
+    @Test
+    void testCreateUser_BlankDisplayName_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> new CreateUserUseCase.Param(UUID.randomUUID(), "admin@flickit.com", "  "));
+        assertThat(throwable).hasMessage("displayName: " + CREATE_USER_DISPLAY_NAME_NOT_BLANK);
+    }
+}
