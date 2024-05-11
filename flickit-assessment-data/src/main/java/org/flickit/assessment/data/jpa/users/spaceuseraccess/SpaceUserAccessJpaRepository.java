008e011f652677421084dfa7e1ac82b76e416921
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-05-11 18:32:25 +0330

    FAC-363: Change order of query parameters

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/users/spaceuseraccess/SpaceUserAccessJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/users/spaceuseraccess/SpaceUserAccessJpaRepository.java
@@ -3,17 +3,18 @@ package org.flickit.assessment.data.jpa.users.spaceuseraccess;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface SpaceUserAccessJpaRepository extends JpaRepository<SpaceUserAccessJpaEntity, Long> {
 
-    boolean existsByUserIdAndSpaceId(UUID userId, Long spaceId);
+    boolean existsBySpaceIdAndUserId(@Param("spaceId") Long spaceId, @Param("userId") UUID userId);
 
-    void deleteByUserIdAndSpaceId(UUID userId, long spaceId);
+    void deleteBySpaceIdAndUserId(@Param("spaceId") long spaceId, @Param("userId") UUID userId);
 
     @Query("""
             SELECT u.id as id,
