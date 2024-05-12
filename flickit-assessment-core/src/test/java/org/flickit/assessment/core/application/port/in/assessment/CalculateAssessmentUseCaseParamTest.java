088d3880ec81e681d08d97169bae233f21d0b357
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-05-12 17:14:43 +0330

    FAC-374: Rename use case param tests

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/port/in/assessment/CalculateAssessmentUseCaseParamTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/port/in/assessment/CalculateAssessmentUseCaseParamTest.java
@@ -1,7 +1,6 @@
 package org.flickit.assessment.core.application.port.in.assessment;
 
 import jakarta.validation.ConstraintViolationException;
-import org.flickit.assessment.core.application.domain.AssessmentColor;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
@@ -9,23 +8,21 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
 import static org.flickit.assessment.core.common.ErrorMessageKey.CALCULATE_ASSESSMENT_ID_NOT_NULL;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CalculateAssessmentUseCaseParamTest {
 
     @Test
-    void testUpdateAssessmentParam_IdIsNull_ErrorMessage() {
+    void testCalculateAssessmentParam_IdIsNull_ErrorMessage() {
         UUID currentUserId = UUID.randomUUID();
-
         var throwable = assertThrows(ConstraintViolationException.class,
             () -> new CalculateAssessmentUseCase.Param(null, currentUserId));
         assertThat(throwable).hasMessage("assessmentId: " + CALCULATE_ASSESSMENT_ID_NOT_NULL);
     }
 
     @Test
-    void testUpdateAssessmentParam_lastModifiedByIdIsNull_ErrorMessage() {
+    void testCalculateAssessmentParam_CurrentUserIdIsNull_ErrorMessage() {
         UUID assessmentId = UUID.randomUUID();
-
         var throwable = assertThrows(ConstraintViolationException.class,
             () -> new CalculateAssessmentUseCase.Param(assessmentId, null));
         assertThat(throwable).hasMessage("currentUserId: " + COMMON_CURRENT_USER_ID_NOT_NULL);
