c81faa6e8178e7748a0b479abdef70464b0da14f
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-05-12 09:06:09 +0330

    FAC-355: Minor renaming

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/common/ErrorMessageKey.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/common/ErrorMessageKey.java
@@ -81,5 +81,5 @@ public class ErrorMessageKey {
     public static final String CREATE_SPACE_TITLE_SIZE_MIN = "create-space.title.size.min";
     public static final String CREATE_SPACE_TITLE_SIZE_MAX = "create-space.title.size.max";
 
-    public static final String GET_SPACE_SPACE_ID_NOT_NULL = "get-space.spaceId.notUll";
+    public static final String GET_SPACE_SPACE_ID_NOT_NULL = "get-space.spaceId.notNull";
 }
