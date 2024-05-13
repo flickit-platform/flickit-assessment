2903e66ab3f19d9f2eb1d0df148d897725ed4acf
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-05-13 12:57:02 +0330

    FAC-391: pictureIsEmpty test added to useCaseParamTest

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/port/in/expertgroup/UpdateExpertGroupPictureUseCaseParamTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/port/in/expertgroup/UpdateExpertGroupPictureUseCaseParamTest.java
@@ -8,6 +8,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.UUID;
 
+import static io.jsonwebtoken.lang.Classes.getResourceAsStream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
 import static org.flickit.assessment.users.common.ErrorMessageKey.*;
@@ -31,6 +32,17 @@ class UpdateExpertGroupPictureUseCaseParamTest {
         UUID currentUserId = UUID.randomUUID();
         var throwable = assertThrows(ConstraintViolationException.class,
             () -> new UpdateExpertGroupPictureUseCase.Param(expertGroupId, null , currentUserId));
+        assertThat(throwable).hasMessage("picture: " + UPDATE_EXPERT_GROUP_PICTURE_PICTURE_NOT_NULL);
+    }
+
+    @Test
+    void testUpdateExpertGroupPictureParam_pictureIsEmpty_ErrorMessage() throws IOException {
+        long expertGroupId = 0L;
+        UUID currentUserId = UUID.randomUUID();
+        MockMultipartFile picture = new MockMultipartFile("images", "image1",
+            "image/png", getResourceAsStream("/no-where/nothing.png"));
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> new UpdateExpertGroupPictureUseCase.Param(expertGroupId, picture , currentUserId));
         assertThat(throwable).hasMessage("picture: " + UPDATE_EXPERT_GROUP_PICTURE_PICTURE_NOT_NULL);
     }
 
