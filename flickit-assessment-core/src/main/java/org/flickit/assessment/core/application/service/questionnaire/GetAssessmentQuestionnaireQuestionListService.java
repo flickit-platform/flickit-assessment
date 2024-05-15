dbf489c455357ed5bf34fda9a43d3a0578bd2357
mahdirezazadeh <riza.rs2000@gmail.com>
2024-05-15 13:45:49 +0330

    FAC-304:feature:Change use case result structure

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/questionnaire/GetAssessmentQuestionnaireQuestionListService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/questionnaire/GetAssessmentQuestionnaireQuestionListService.java
@@ -3,7 +3,7 @@ package org.flickit.assessment.core.application.service.questionnaire;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.core.application.domain.Answer;
+import org.flickit.assessment.core.application.domain.AnswerOption;
 import org.flickit.assessment.core.application.domain.ConfidenceLevel;
 import org.flickit.assessment.core.application.domain.Question;
 import org.flickit.assessment.core.application.port.in.questionnaire.GetAssessmentQuestionnaireQuestionListUseCase;
@@ -38,11 +38,11 @@ public class GetAssessmentQuestionnaireQuestionListService implements GetAssessm
             param.getSize(),
             param.getPage());
 
-        Map<Long, Answer> questionIdToAnswerMap = loadAssessmentQuestionnaireAnswerListPort.loadQuestionnaireAnswers(param.getAssessmentId(), param.getQuestionnaireId(),
+        Map<Long, org.flickit.assessment.core.application.domain.Answer> questionIdToAnswerMap = loadAssessmentQuestionnaireAnswerListPort.loadQuestionnaireAnswers(param.getAssessmentId(), param.getQuestionnaireId(),
                 param.getSize(),
                 param.getPage())
             .stream()
-            .collect(toMap(Answer::getQuestionId, Function.identity()));
+            .collect(toMap(org.flickit.assessment.core.application.domain.Answer::getQuestionId, Function.identity()));
 
         var items = pageResult.getItems().stream()
             .map((Question q) -> mapToResult(q, questionIdToAnswerMap.get(q.getId()))).toList();
@@ -56,39 +56,35 @@ public class GetAssessmentQuestionnaireQuestionListService implements GetAssessm
             pageResult.getTotal());
     }
 
-    private Result mapToResult(Question question, Answer answer) {
-        ConfidenceLevel confidenceLevel = null;
-        AnswerOption selectedOption = null;
-        Boolean isNotApplicable = null;
+    private Result mapToResult(Question question, org.flickit.assessment.core.application.domain.Answer answer) {
+        QuestionAnswer selectedOption = null;
         if (answer != null) {
-            confidenceLevel = valueOfById(answer.getConfidenceLevelId());
             var answerOption = question.getOptions().stream()
                 .filter(x -> Objects.equals(x.getId(), answer.getSelectedOption().getId()))
                 .findAny()
                 .orElse(null);
             if (answerOption != null)
-                selectedOption = mapToAnswer(answerOption);
-            isNotApplicable = answer.getIsNotApplicable();
+                selectedOption = mapToAnswer(answerOption,
+                    valueOfById(answer.getConfidenceLevelId()),
+                    answer.getIsNotApplicable());
         }
         return new Result(
             question.getId(),
-            question.getTitle(),
             question.getIndex(),
+            question.getTitle(),
             question.getHint(),
             question.getMayNotBeApplicable(),
             question.getOptions().stream()
-                .map(this::mapToOptionListItem)
+                .map(this::mapToOption)
                 .toList(),
-            selectedOption,
-            confidenceLevel,
-            isNotApplicable);
+            selectedOption);
     }
 
-    private OptionListItem mapToOptionListItem(org.flickit.assessment.core.application.domain.AnswerOption ao) {
-        return new OptionListItem(ao.getId(), ao.getTitle(), ao.getIndex());
+    private Option mapToOption(org.flickit.assessment.core.application.domain.AnswerOption ao) {
+        return new Option(ao.getId(), ao.getTitle(), ao.getIndex());
     }
 
-    private AnswerOption mapToAnswer(org.flickit.assessment.core.application.domain.AnswerOption selectedOption) {
-        return new AnswerOption(selectedOption.getId(), selectedOption.getTitle(), selectedOption.getIndex());
+    private QuestionAnswer mapToAnswer(AnswerOption selectedOption, ConfidenceLevel confidenceLevel, Boolean isNotApplicable) {
+        return new QuestionAnswer(mapToOption(selectedOption), confidenceLevel, isNotApplicable);
     }
 }
