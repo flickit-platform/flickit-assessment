6847b8a880a37306a51696f2306a9940409a8ba3
mahdirezazadeh <riza.rs2000@gmail.com>
2024-05-15 11:15:28 +0330

    FAC-400:feature:Use AssessmentPermissionChecker in GrantUserAssessmentRoleService

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/GrantUserAssessmentRoleService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/GrantUserAssessmentRoleService.java
@@ -3,15 +3,15 @@ package org.flickit.assessment.core.application.service.assessmentuserrole;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.common.permission.AssessmentPermissionChecker;
 import org.flickit.assessment.core.application.port.in.assessmentuserrole.GrantUserAssessmentRoleUseCase;
 import org.flickit.assessment.core.application.port.out.assessment.CheckUserAssessmentAccessPort;
 import org.flickit.assessment.core.application.port.out.assessmentuserrole.GrantUserAssessmentRolePort;
-import org.flickit.assessment.core.application.port.out.assessmentuserrole.LoadUserRoleForAssessmentPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
-import static org.flickit.assessment.core.common.AssessmentPermission.GRANT_USER_ASSESSMENT_ROLE;
+import static org.flickit.assessment.common.permission.AssessmentPermission.GRANT_USER_ASSESSMENT_ROLE;
 import static org.flickit.assessment.core.common.ErrorMessageKey.GRANT_ASSESSMENT_USER_ROLE_USER_ID_NOT_MEMBER;
 
 @Service
@@ -19,14 +19,13 @@ import static org.flickit.assessment.core.common.ErrorMessageKey.GRANT_ASSESSMEN
 @RequiredArgsConstructor
 public class GrantUserAssessmentRoleService implements GrantUserAssessmentRoleUseCase {
 
-    private final LoadUserRoleForAssessmentPort loadUserRoleForAssessmentPort;
+    private final AssessmentPermissionChecker permissionChecker;
     private final CheckUserAssessmentAccessPort checkUserAssessmentAccessPort;
     private final GrantUserAssessmentRolePort grantUserAssessmentRolePort;
 
     @Override
     public void grantAssessmentUserRole(Param param) {
-        var currentUserRole = loadUserRoleForAssessmentPort.load(param.getAssessmentId(), param.getCurrentUserId());
-        if (currentUserRole == null || !currentUserRole.hasAccess(GRANT_USER_ASSESSMENT_ROLE))
+        if (!permissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), GRANT_USER_ASSESSMENT_ROLE))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
         if (!checkUserAssessmentAccessPort.hasAccess(param.getAssessmentId(), param.getUserId()))
