8e8ded9bcedefd498c94bfd078a388ba24623d7b
mahdirezazadeh <riza.rs2000@gmail.com>
2024-05-19 12:45:12 +0330

    FAC-413:feature:Add notApplicable validation to service

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/question/CheckQuestionMayNotBeApplicablePort.java
@@ -0,0 +1,6 @@
+package org.flickit.assessment.core.application.port.out.question;
+
+public interface CheckQuestionMayNotBeApplicablePort {
+
+    boolean checkQuestionMayNotBeApplicable(Long questionId);
+}
--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
@@ -27,6 +27,7 @@ public class ErrorMessageKey {
     public static final String SUBMIT_ANSWER_QUESTIONNAIRE_ID_NOT_NULL = "submit-answer.questionnaireId.notNull";
     public static final String SUBMIT_ANSWER_QUESTION_ID_NOT_NULL = "submit-answer.questionId.notNull";
     public static final String SUBMIT_ANSWER_QUESTION_ID_NOT_FOUND = "submit-answer.questionId.notFound";
+    public static final String SUBMIT_ANSWER_QUESTION_ID_NOT_MAY_NOT_BE_APPLICABLE = "submit-answer.questionId.notMayNotBeApplicable";
     public static final String SUBMIT_ANSWER_ANSWER_OPTION_ID_NOT_FOUND = "submit-answer.answerOptionId.notFound";
     public static final String SUBMIT_ANSWER_ANSWER_ID_NOT_FOUND = "submit-answer.answerId.notFound";
 
