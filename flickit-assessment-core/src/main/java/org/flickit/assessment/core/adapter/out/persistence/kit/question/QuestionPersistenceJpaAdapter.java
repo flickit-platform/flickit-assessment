0a05c257f2f48644fef1894fd0f40d58e3f40093
mahdirezazadeh <riza.rs2000@gmail.com>
2024-05-19 12:46:25 +0330

    FAC-413:feature:Impl CheckQuestionMayNotBeApplicablePort in QuestionPersistenceJpaAdapter

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/kit/question/QuestionPersistenceJpaAdapter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/kit/question/QuestionPersistenceJpaAdapter.java
@@ -1,16 +1,22 @@
 package org.flickit.assessment.core.adapter.out.persistence.kit.question;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.core.application.domain.Question;
+import org.flickit.assessment.core.application.port.out.question.CheckQuestionMayNotBeApplicablePort;
 import org.flickit.assessment.core.application.port.out.question.LoadQuestionsBySubjectPort;
 import org.flickit.assessment.data.jpa.kit.question.QuestionJpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+import static org.flickit.assessment.core.common.ErrorMessageKey.SUBMIT_ANSWER_QUESTION_ID_NOT_FOUND;
+
 @Component("coreQuestionPersistenceJpaAdapter")
 @RequiredArgsConstructor
-public class QuestionPersistenceJpaAdapter implements LoadQuestionsBySubjectPort {
+public class QuestionPersistenceJpaAdapter implements
+    LoadQuestionsBySubjectPort,
+    CheckQuestionMayNotBeApplicablePort {
 
     private final QuestionJpaRepository repository;
 
@@ -19,5 +25,12 @@ public class QuestionPersistenceJpaAdapter implements LoadQuestionsBySubjectPort
         return repository.findBySubjectId(subjectId).stream()
             .map(q -> QuestionMapper.mapToDomainModel(q.getId(), null))
             .toList();
+    }
+
+    @Override
+    public boolean checkQuestionMayNotBeApplicable(Long questionId) {
+        return repository.findById(questionId)
+            .orElseThrow(() -> new ResourceNotFoundException(SUBMIT_ANSWER_QUESTION_ID_NOT_FOUND))
+            .getMayNotBeApplicable();
     }
 }
