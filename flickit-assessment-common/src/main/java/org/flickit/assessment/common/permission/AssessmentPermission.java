0c282f33c85ba48f2ec97b4d462d087487a1be40
mahdirezazadeh <riza.rs2000@gmail.com>
2024-05-19 16:47:50 +0330

    FAC-384:feature:Remove code from AssessmentPermission

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/permission/AssessmentPermission.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/permission/AssessmentPermission.java
@@ -1,36 +1,29 @@
 package org.flickit.assessment.common.permission;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum AssessmentPermission {
 
-    VIEW_ANSWER("view-answer"),
-    ANSWER_QUESTION("answer-question"),
-    VIEW_REPORT_ASSESSMENT("view-report-assessment"),
-    CALCULATE_ASSESSMENT("calculate-assessment"),
-    CALCULATE_CONFIDENCE("calculate-confidence"),
-    CREATE_ASSESSMENT("create-assessment"),
-    DELETE_ASSESSMENT("delete-assessment"),
-    VIEW_ASSESSMENT_LIST("view-assessment-list"),
-    VIEW_ASSESSMENT_PROGRESS("view-assessment-progress"),
-    VIEW_ASSESSMENT("view-assessment"),
-    UPDATE_ASSESSMENT("update-assessment"),
-    VIEW_ATTRIBUTE_SCORE_DETAIL("view-attribute-score-detail"),
-    ADD_EVIDENCE("add-evidence"),
-    DELETE_EVIDENCE("delete-evidence"),
-    VIEW_EVIDENCE("view-evidence"),
-    VIEW_ATTRIBUTE_EVIDENCE_LIST("view-attribute-evidence-list"),
-    VIEW_EVIDENCE_LIST("view-evidence-list"),
-    UPDATE_EVIDENCE("update-evidence"),
-    VIEW_ASSESSMENT_QUESTIONNAIRE_LIST("view-assessment-questionnaire-list"),
-    VIEW_QUESTIONNAIRES_PROGRESS("view-questionnaires-progress"),
-    VIEW_QUESTIONNAIRE_QUESTIONS("view-questionnaire-questions"),
-    VIEW_SUBJECT_PROGRESS("view-subject-progress"),
-    VIEW_SUBJECT_REPORT("view-subject-report"),
-    CREATE_ADVICE("create-advice");
-
-    private final String code;
+    VIEW_ANSWER,
+    ANSWER_QUESTION,
+    VIEW_REPORT_ASSESSMENT,
+    CALCULATE_ASSESSMENT,
+    CALCULATE_CONFIDENCE,
+    CREATE_ASSESSMENT,
+    DELETE_ASSESSMENT,
+    VIEW_ASSESSMENT_LIST,
+    VIEW_ASSESSMENT_PROGRESS,
+    VIEW_ASSESSMENT,
+    UPDATE_ASSESSMENT,
+    VIEW_ATTRIBUTE_SCORE_DETAIL,
+    ADD_EVIDENCE,
+    DELETE_EVIDENCE,
+    VIEW_EVIDENCE,
+    VIEW_ATTRIBUTE_EVIDENCE_LIST,
+    VIEW_EVIDENCE_LIST,
+    UPDATE_EVIDENCE,
+    VIEW_ASSESSMENT_QUESTIONNAIRE_LIST,
+    VIEW_QUESTIONNAIRES_PROGRESS,
+    VIEW_QUESTIONNAIRE_QUESTIONS,
+    VIEW_SUBJECT_PROGRESS,
+    VIEW_SUBJECT_REPORT,
+    CREATE_ADVICE
 }
