6a4b4cd26631f84fbd58b4f718df769fa807c366
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-05-20 09:39:38 +0330

    FAC-376: Remove LoadSpacePort and use LoadSpaceOwnerPort instead

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/port/out/space/LoadSpacePort.java
+++
@@ -1,8 +0,0 @@
-package org.flickit.assessment.users.application.port.out.space;
-
-import org.flickit.assessment.users.application.domain.Space;
-
-public interface LoadSpacePort {
-
-    Space loadSpace(long id);
-}
--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/UpdateSpaceServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/UpdateSpaceServiceTest.java
@@ -4,7 +4,7 @@ import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.users.application.domain.Space;
 import org.flickit.assessment.users.application.port.in.space.UpdateSpaceUseCase;
-import org.flickit.assessment.users.application.port.out.space.LoadSpacePort;
+import org.flickit.assessment.users.application.port.out.space.LoadSpaceOwnerPort;
 import org.flickit.assessment.users.application.port.out.space.UpdateSpacePort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,8 +16,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static org.flickit.assessment.users.common.ErrorMessageKey.SPACE_ID_NOT_FOUND;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.users.common.ErrorMessageKey.SPACE_ID_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -26,24 +26,26 @@ class UpdateSpaceServiceTest {
 
     @InjectMocks
     UpdateSpaceService service;
+
     @Mock
-    LoadSpacePort loadSpacePort;
+    LoadSpaceOwnerPort loadSpaceOwnerPort;
+
     @Mock
     UpdateSpacePort updateSpacePort;
 
     @Test
-    @DisplayName("If the space does not exist, updating space service should return ResourceNotFound")
+    @DisplayName("If the space does not exist, updating space should return ResourceNotFound")
     void testUpdateSpace_spaceNotExist_resourceNotFound() {
         long spaceId = 0L;
         String title = "Test";
         UUID currentUserId = UUID.randomUUID();
         UpdateSpaceUseCase.Param param = new UpdateSpaceUseCase.Param(spaceId, title, currentUserId);
 
-        when(loadSpacePort.loadSpace(spaceId)).thenThrow(new ResourceNotFoundException(SPACE_ID_NOT_FOUND));
+        when(loadSpaceOwnerPort.loadOwnerId(spaceId)).thenThrow(new ResourceNotFoundException(SPACE_ID_NOT_FOUND));
         var throwable = assertThrows(ResourceNotFoundException.class, () -> service.updateSpace(param));
         assertEquals(SPACE_ID_NOT_FOUND, throwable.getMessage());
 
-        verify(loadSpacePort).loadSpace(anyLong());
+        verify(loadSpaceOwnerPort).loadOwnerId(anyLong());
         verifyNoInteractions(updateSpacePort);
     }
 
@@ -54,19 +56,17 @@ class UpdateSpaceServiceTest {
         String title = "Test";
         UUID currentUserId = UUID.randomUUID();
         UpdateSpaceUseCase.Param param = new UpdateSpaceUseCase.Param(spaceId, title, currentUserId);
-        Space space = new Space(spaceId, "title", "Title", UUID.randomUUID(),
-            LocalDateTime.now(),LocalDateTime.now(), UUID.randomUUID(), UUID.randomUUID());
 
-        when(loadSpacePort.loadSpace(spaceId)).thenReturn(space);
+        when(loadSpaceOwnerPort.loadOwnerId(spaceId)).thenReturn(UUID.randomUUID());
         var throwable = assertThrows(AccessDeniedException.class, ()-> service.updateSpace(param));
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
 
-        verify(loadSpacePort).loadSpace(anyLong());
+        verify(loadSpaceOwnerPort).loadOwnerId(anyLong());
         verifyNoInteractions(updateSpacePort);
     }
 
     @Test
-    @DisplayName("Updatin s dpsce with valid parametrs should be successful")
+    @DisplayName("Updating a space with valid parameters should be successful")
     void testUpdateSpace_validParameters_successful(){
         long spaceId = 0L;
         String title = "Test";
@@ -75,12 +75,12 @@ class UpdateSpaceServiceTest {
         Space space = new Space(spaceId, "title", "Title", currentUserId,
                 LocalDateTime.now(),LocalDateTime.now(), UUID.randomUUID(), currentUserId);
 
-        when(loadSpacePort.loadSpace(spaceId)).thenReturn(space);
+        when(loadSpaceOwnerPort.loadOwnerId(spaceId)).thenReturn(currentUserId);
         doNothing().when(updateSpacePort).updateSpace(any(UpdateSpacePort.Param.class));
 
         assertDoesNotThrow(()-> service.updateSpace(param));
 
-        verify(loadSpacePort).loadSpace(anyLong());
+        verify(loadSpaceOwnerPort).loadOwnerId(anyLong());
         verify(updateSpacePort).updateSpace(any(UpdateSpacePort.Param.class));
     }
 }
