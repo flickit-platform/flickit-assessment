f14429977aa4e9da25fa4077f6ed55f2ff9c8640
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-05-21 16:19:02 +0330

    FAC-400: Check user space membership

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/GrantUserAssessmentRoleService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/GrantUserAssessmentRoleService.java
@@ -3,6 +3,7 @@ package org.flickit.assessment.core.application.service.assessmentuserrole;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentPermissionChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ValidationException;
 import org.flickit.assessment.core.application.port.in.assessmentuserrole.GrantUserAssessmentRoleUseCase;
 import org.flickit.assessment.core.application.port.out.assessment.CheckUserAssessmentAccessPort;
 import org.flickit.assessment.core.application.port.out.assessmentuserrole.GrantUserAssessmentRolePort;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.GRANT_USER_ASSESSMENT_ROLE;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.core.common.ErrorMessageKey.GRANT_ASSESSMENT_USER_ROLE_USER_ID_NOT_MEMBER;
 
 @Service
 @Transactional
@@ -26,8 +28,11 @@ public class GrantUserAssessmentRoleService implements GrantUserAssessmentRoleUs
         if (!permissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), GRANT_USER_ASSESSMENT_ROLE))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
-        if (!checkUserAssessmentAccessPort.hasAccess(param.getAssessmentId(), param.getUserId()))
+        if (!checkUserAssessmentAccessPort.hasAccess(param.getAssessmentId(), param.getCurrentUserId()))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+
+        if (!checkUserAssessmentAccessPort.hasAccess(param.getAssessmentId(), param.getUserId()))
+            throw new ValidationException(GRANT_ASSESSMENT_USER_ROLE_USER_ID_NOT_MEMBER);
 
         grantUserAssessmentRolePort.persist(param.getAssessmentId(), param.getUserId(), param.getRoleId());
     }
