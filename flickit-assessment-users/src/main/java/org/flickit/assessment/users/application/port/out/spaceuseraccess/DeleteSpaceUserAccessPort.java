93139da83be6558bdfef8d95d41f8a12375eca63
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-05-22 17:29:57 +0330

    FAC-375: Extra port DeleteSpaceUserAccessPort removed

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/port/out/spaceuseraccess/DeleteSpaceUserAccessPort.java
+++
@@ -1,8 +0,0 @@
-package org.flickit.assessment.users.application.port.out.spaceuseraccess;
-
-import java.util.UUID;
-
-public interface DeleteSpaceUserAccessPort {
-
-    void deleteAccess(long spaceId, UUID userId);
-}
