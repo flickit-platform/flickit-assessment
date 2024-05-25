31f79a3f606ecbe7dd4ac80489dcbedabc86086e
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-05-25 19:10:53 +0330

    FAC-414: Move userId from request body to path param due to designing restful api

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessmentuserrole/DeleteUserAssessmentRoleRequestDto.java
+++
@@ -1,6 +0,0 @@
-package org.flickit.assessment.core.adapter.in.rest.assessmentuserrole;
-
-import java.util.UUID;
-
-public record DeleteUserAssessmentRoleRequestDto(UUID userId) {
-}
--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessmentuserrole/DeleteUserAssessmentRoleRestController.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessmentuserrole/DeleteUserAssessmentRoleRestController.java
@@ -6,7 +6,9 @@ import org.flickit.assessment.core.application.port.in.assessmentuserrole.Delete
 import org.flickit.assessment.core.application.port.in.assessmentuserrole.DeleteUserAssessmentRoleUseCase.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
@@ -17,15 +19,15 @@ public class DeleteUserAssessmentRoleRestController {
     private final DeleteUserAssessmentRoleUseCase useCase;
     private final UserContext userContext;
 
-    @DeleteMapping("/assessments/{assessmentId}/assessment-user-roles")
+    @DeleteMapping("/assessments/{assessmentId}/assessment-user-roles/{userId}")
     public ResponseEntity<Void> deleteAssessmentUserRole(@PathVariable("assessmentId") UUID assessmentId,
-                                                        @RequestBody DeleteUserAssessmentRoleRequestDto requestDto) {
+                                                         @PathVariable("assessmentId") UUID userId) {
         UUID currentUserId = userContext.getUser().id();
-        useCase.deleteAssessmentUserRole(toParam(assessmentId, requestDto, currentUserId));
+        useCase.deleteAssessmentUserRole(toParam(assessmentId, userId, currentUserId));
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    private Param toParam(UUID assessmentId, DeleteUserAssessmentRoleRequestDto requestDto, UUID currentUserId) {
-        return new Param(assessmentId, requestDto.userId(), currentUserId);
+    private Param toParam(UUID assessmentId, UUID userId, UUID currentUserId) {
+        return new Param(assessmentId, userId, currentUserId);
     }
 }
