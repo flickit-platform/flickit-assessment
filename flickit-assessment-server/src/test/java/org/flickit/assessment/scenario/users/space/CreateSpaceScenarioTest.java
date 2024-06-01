eaa2f1011e58bc917d94e583ac2229476249a4a1
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-06-01 18:41:46 +0330

    FAC-48: JwtTokenTestUtils added and tests refined

--- flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/users/space/CreateSpaceScenarioTest.java
+++ flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/users/space/CreateSpaceScenarioTest.java
@@ -1,5 +1,9 @@
 package org.flickit.assessment.scenario.users.space;
 
+import org.flickit.assessment.helper.auth.JwtTokenTestUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,13 +15,17 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CreateSpaceScenarioTest {
-    String Auth = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJWRmtJYmU0V1U4SGV3Wml0aHdFbmZqU0xUcjJJdk1tRnNKYWRndmh1WU13In0.eyJleHAiOjE3MDU3NTEzMTIsImlhdCI6MTcwNTc1MTAxMiwiYXV0aF90aW1lIjoxNzA1NzUxMDExLCJqdGkiOiJlZjk0NjA5NC1mNDIwLTQwNDktYjEyYy1mNTVkNmZjNzQwODAiLCJpc3MiOiJodHRwczovL3Rlc3QuZmxpY2tpdC5vcmcvYWNjb3VudHMvcmVhbG1zL2ZsaWNraXQiLCJhdWQiOlsiZmxpY2tpdC1iZmYiLCJhY2NvdW50Il0sInN1YiI6ImVkYzJlMjk1LTI5NjQtNGNmYi1hMDZhLTVlYjIyMTU4NmQyOSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImZsaWNraXQtZnJvbnRlbmQiLCJub25jZSI6IjI3ODA0MDFiLWM2YzYtNDI0MS04MmU1LTZmN2IwMTFiMzQ2YSIsInNlc3Npb25fc3RhdGUiOiJjNTU2NDUwMy04ODRmLTQyNzAtODFhNi1jZjBiOTk4YmQ4NGUiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vdGVzdC5mbGlja2l0Lm9yZyIsImh0dHA6Ly9sb2NhbGhvc3Q6MzAwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1mbGlja2l0Iiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiJjNTU2NDUwMy04ODRmLTQyNzAtODFhNi1jZjBiOTk4YmQ4NGUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6IkFkbWluIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYWRtaW5AYXN0YS5pciIsImdpdmVuX25hbWUiOiJBZG1pbiIsImVtYWlsIjoiYWRtaW5AYXN0YS5pciJ9.Zfl_zcjvAcyG5QxesPz1IXRDLE-3vbk3fYvNSEYraagD3CeRpNyBNm62nV65inWATNDqRtaegefBG_9LvbEPNc1I2Qi0Rp6YYVC4xysV28_GUHOfRgtoCFndhgM4lWBQl6JfcUwzLOOMIyP4a_lqw6ESixK14yRByo-vY3ejIDktMcgGkixuYVkXtS5VCbccH7i_jyxcXdanvFGi94j_OaxzxwoiY39cQHjv5WkEVIkVC0VxIuBDS4Z2LjPkcPdjuo-j2vW_HKXyT5q9FE0TXMOeXAUu3rYxDseMfSYbF719ZmjcFHAM-1IDtMfZcIHzVCT8IQbnCpLPID0xHjowKA";
 
     @Autowired
     private TestRestTemplate testRestTemplate;
@@ -36,31 +44,68 @@ class CreateSpaceScenarioTest {
         registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
         registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
         registry.add("spring.liquibase.enabled", () -> false);
-
     }
 
     @Test
+    @DisplayName("The application should connect to testContainers")
     void testDatabaseConnection() {
         String jdbcUrl = postgreSQLContainer.getJdbcUrl();
-        assertEquals("jdbc:postgresql://localhost:" + postgreSQLContainer.getMappedPort(5432) + "/flickit?loggerLevel=OFF", jdbcUrl);
+        assertEquals("jdbc:postgresql://localhost:" + postgreSQLContainer.getMappedPort
+            (5432) + "/"+postgreSQLContainer.getDatabaseName()+"?loggerLevel=OFF", jdbcUrl);
     }
 
     @Test
-    void createSpace() {
+    @DisplayName("Creating space with valid parameters in the database should be done successfully.")
+    void testCreateSpace_validParameters_successful() throws JSONException, SQLException {
         String title = "test";
+        String space = new JSONObject().put("title", title).toString();
+        String authenticationToken = JwtTokenTestUtils.generateJwtToken(UUID.randomUUID());
 
-        ResponseEntity<String> response = whenCreateSpace();
+        ResponseEntity<String> response = whenCreateSpace(space, authenticationToken);
+
+        var conn = DriverManager
+            .getConnection(postgreSQLContainer.getJdbcUrl(), postgreSQLContainer.getUsername(), postgreSQLContainer.getPassword());
+        ResultSet resultSet =
+            conn.createStatement().executeQuery("SELECT * from public.fau_space");
 
         then(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        int rowCount = resultSet.getRow();
+        then(rowCount).isEqualTo(0);
+        whenCreateSpace(space, authenticationToken);
+        resultSet.next();
+        rowCount = resultSet.getRow();
+        then(rowCount).isEqualTo(1);
+        then(resultSet.next()).isEqualTo(false);
     }
 
-    private ResponseEntity<String> whenCreateSpace() {
-        String json = "{\"title\":\"test\"}";
+    @Test
+    @DisplayName("Creating a space with an empty request body, should cause a BadRequest error.")
+    void testCreateSpace_emptyBody_BadRequest() {
+        String space = new JSONObject().toString();
+        String authenticationToken = JwtTokenTestUtils.generateJwtToken(UUID.randomUUID());
 
+        ResponseEntity<String> response = whenCreateSpace(space, authenticationToken);
+
+        then(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    @DisplayName("Creating a space with invalid authentication token, should cause an UNAUTHORIZED error.")
+    void testCreateSpace_incorrectAuthenticationToken_BadRequest() throws JSONException {
+        String title = "test";
+        String space = new JSONObject().put("title", title).toString();
+        String authenticationToken = "blah blah";
+
+        ResponseEntity<String> response = whenCreateSpace(space, authenticationToken);
+
+        then(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    private ResponseEntity<String> whenCreateSpace(String space, String authenticationToken) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("Authorization", Auth);
-        HttpEntity<String> request = new HttpEntity<>(json, headers);
+        headers.add("Authorization", authenticationToken);
+        HttpEntity<String> request = new HttpEntity<>(space, headers);
 
         return testRestTemplate.exchange(
             ENDPOINT,
@@ -69,5 +114,4 @@ class CreateSpaceScenarioTest {
             String.class
         );
     }
-
 }
