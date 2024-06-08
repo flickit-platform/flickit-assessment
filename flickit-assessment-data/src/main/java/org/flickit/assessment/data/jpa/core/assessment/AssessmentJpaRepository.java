3625bc42b4f5866954d6c515661eb5009f23c5c0
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-06-08 15:42:55 +0330

    FAC-359: Refactor find assessments by user query

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/assessment/AssessmentJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/assessment/AssessmentJpaRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
@@ -17,16 +16,25 @@ import java.util.UUID;
 public interface AssessmentJpaRepository extends JpaRepository<AssessmentJpaEntity, UUID>, JpaSpecificationExecutor<AssessmentJpaEntity> {
 
     @Query("""
-            SELECT a as assessment, r.maturityLevelId as maturityLevelId, r.isCalculateValid as isCalculateValid , r.isConfidenceValid as isConfidenceValid
+            SELECT
+                a as assessment,
+                r as assessmentResult,
+                k as assessmentKit,
+                s as space
             FROM AssessmentJpaEntity a
             LEFT JOIN AssessmentResultJpaEntity r ON a.id = r.assessment.id
-            WHERE a.spaceId IN :spaceIds
-                AND a.deleted=false
-                AND (a.assessmentKitId=:kitId OR :kitId IS NULL)
+            LEFT JOIN AssessmentKitJpaEntity k ON k.id = a.assessmentKitId
+            LEFT JOIN SpaceUserAccessJpaEntity sua ON sua.spaceId = a.spaceId
+            LEFT JOIN SpaceJpaEntity s ON s.id = a.spaceId
+            WHERE sua.userId = :userId
+                AND (a.assessmentKitId = :kitId OR :kitId IS NULL)
+                AND a.deleted = FALSE
                 AND r.lastModificationTime = (SELECT MAX(ar.lastModificationTime) FROM AssessmentResultJpaEntity ar WHERE ar.assessment.id = a.id)
             ORDER BY a.lastModificationTime DESC
         """)
-    Page<AssessmentListItemView> findBySpaceIdAndDeletedFalseOrderByLastModificationTimeDesc(List<Long> spaceIds, Long kitId, Pageable pageable);
+    Page<UserAssessmentListItemView> findByUserId(@Param("kitId") Long kitId,
+                                                  @Param("userId") UUID userId,
+                                                  Pageable pageable);
 
     @Query("""
             SELECT
