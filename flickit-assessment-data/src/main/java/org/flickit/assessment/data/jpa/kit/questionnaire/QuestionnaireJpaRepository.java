32078665da37fdbfcf7a4fcd1a8120b87088488f
mahdirezazadeh <riza.rs2000@gmail.com>
2024-06-12 14:51:12 +0330

    FAC-444:feature:Add EntityId to QuestionnaireJpaEntity

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/questionnaire/QuestionnaireJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/questionnaire/QuestionnaireJpaRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface QuestionnaireJpaRepository extends JpaRepository<QuestionnaireJpaEntity, Long> {
+public interface QuestionnaireJpaRepository extends JpaRepository<QuestionnaireJpaEntity, QuestionnaireJpaEntity.EntityId> {
 
     List<QuestionnaireJpaEntity> findAllByKitVersionIdOrderByIndex(Long kitVersionId);
 
