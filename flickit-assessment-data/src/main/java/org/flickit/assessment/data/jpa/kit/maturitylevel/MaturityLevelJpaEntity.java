1c6e37d29a3725fba3469be3319550cdf2831a7b
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-06-19 11:20:27 +0330

    FAC-459: refactor : Remove refNum from MaturityLevel

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/maturitylevel/MaturityLevelJpaEntity.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/maturitylevel/MaturityLevelJpaEntity.java
@@ -29,9 +29,6 @@ public class MaturityLevelJpaEntity {
     @Column(name = "kit_version_id", nullable = false)
     private Long kitVersionId;
 
-    @Column(name = "ref_num", nullable = false)
-    private UUID refNum;
-
     @Column(name = "code", length = 50)
     private String code;
 
