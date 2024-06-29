e53749e0037ffad37ae385ca4ebca29830e9bc28
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-06-29 13:24:38 +0330

    FAC-483: feature : Minor spacing

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/evidenceattachment/EvidenceAttachmentJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/evidenceattachment/EvidenceAttachmentJpaRepository.java
@@ -6,5 +6,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface EvidenceAttachmentJpaRepository extends JpaRepository<EvidenceAttachmentJpaEntity, Long> {
+
     List<EvidenceAttachmentJpaEntity> findByEvidenceId(UUID evidenceId);
 }
