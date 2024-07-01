83e2b0f4a4261c28bcb52238865544c54fbac074
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-07-01 14:20:41 +0330

    FAC-476: feature : Add evidence check to EvidenceAttachmentPersistenceJpaAdapter

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/evidenceattachment/EvidenceAttachmentPersistenceJpaAdapter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/evidenceattachment/EvidenceAttachmentPersistenceJpaAdapter.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.core.application.port.out.evidenceattachment.CountEvidenceAttachmentsPort;
 import org.flickit.assessment.core.application.port.out.evidenceattachment.CreateEvidenceAttachmentPort;
+import org.flickit.assessment.data.jpa.core.evidence.EvidenceJpaRepository;
 import org.flickit.assessment.data.jpa.core.evidenceattachment.EvidenceAttachmentJpaEntity;
 import org.flickit.assessment.data.jpa.core.evidenceattachment.EvidenceAttachmentJpaRepository;
 import org.springframework.stereotype.Component;
@@ -18,10 +19,11 @@ import static org.flickit.assessment.core.common.ErrorMessageKey.EVIDENCE_ID_NOT
 public class EvidenceAttachmentPersistenceJpaAdapter implements CreateEvidenceAttachmentPort, CountEvidenceAttachmentsPort {
 
     private final EvidenceAttachmentJpaRepository repository;
+    private final EvidenceJpaRepository evidenceRepository;
 
     @Override
     public UUID persist(UUID evidenceId, String filePath, String description, UUID currentUserId, LocalDateTime now) {
-        if (repository.existsById(evidenceId))
+        if (!evidenceRepository.existsByIdAndDeletedFalse(evidenceId))
             throw new ResourceNotFoundException(EVIDENCE_ID_NOT_FOUND);
         var unsavedEntity = new EvidenceAttachmentJpaEntity(null, evidenceId, filePath, description, currentUserId, now);
         var savedEntity = repository.save(unsavedEntity);
