77d21d5d7b1fb1af115749e0dc91309bad444a17
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-07-02 18:00:32 +0330

    FAC-403: Fix merge conflicts

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/in/rest/user/GetUsersMeResponseDto.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/in/rest/user/GetUsersMeResponseDto.java
@@ -2,4 +2,5 @@ package org.flickit.assessment.users.adapter.in.rest.user;
 
 import java.util.UUID;
 
-public record GetUsersMeResponseDto(UUID id, String displayName, String pictureLink) {}
+public record GetUsersMeResponseDto(UUID id, String displayName, String pictureLink) {
+}
--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/in/rest/user/GetUsersMeRestController.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/in/rest/user/GetUsersMeRestController.java
@@ -2,7 +2,6 @@ package org.flickit.assessment.users.adapter.in.rest.user;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.config.jwt.UserContext;
-import org.flickit.assessment.users.application.domain.User;
 import org.flickit.assessment.users.application.port.in.user.GetUserProfileUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +20,11 @@ public class GetUsersMeRestController {
     @GetMapping("/users/me")
     public ResponseEntity<GetUsersMeResponseDto> getUsersMe() {
         UUID currentUserId = userContext.getUser().id();
-        User user = useCase.getUserProfile(new GetUserProfileUseCase.Param(currentUserId));
+        GetUserProfileUseCase.UserProfile user = useCase.getUserProfile(new GetUserProfileUseCase.Param(currentUserId));
         return new ResponseEntity<>(toResponseDto(user), HttpStatus.OK);
     }
 
-    private GetUsersMeResponseDto toResponseDto(User user) {
-        return new GetUsersMeResponseDto(user.getId(), user.getDisplayName(), user.getPicture());
+    private GetUsersMeResponseDto toResponseDto(GetUserProfileUseCase.UserProfile user) {
+        return new GetUsersMeResponseDto(user.id(), user.displayName(), user.pictureLink());
     }
 }
