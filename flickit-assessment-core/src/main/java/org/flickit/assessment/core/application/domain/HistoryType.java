fd484c0dcb04b74896b39eb15984a8b0ce6677ec
mahdirezazadeh <riza.rs2000@gmail.com>
2024-07-02 10:01:09 +0330

    FAC-478:feature:Persist AnswerHistory in SubmitAnswerService

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/HistoryType.java
@@ -0,0 +1,19 @@
+package org.flickit.assessment.core.application.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum HistoryType {
+
+    PERSIST,
+    UPDATE,
+    DELETE;
+
+    public String getCode() {
+        return name();
+    }
+}
---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/answerhistory/CreateAnswerHistoryPort.java
@@ -0,0 +1,26 @@
+package org.flickit.assessment.core.application.port.out.answerhistory;
+
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public interface CreateAnswerHistoryPort {
+
+    /**
+     * @throws ResourceNotFoundException if no assessmentResult found by the given id
+     */
+    UUID persist(Param param);
+
+    record Param(UUID answerId,
+                 UUID assessmentResultId,
+                 Long questionnaireId,
+                 Long questionId,
+                 Long answerOptionId,
+                 Integer confidenceLevelId,
+                 Boolean isNotApplicable,
+                 UUID currentUserId,
+                 LocalDateTime lastModificationTime,
+                 int historyTypeId) {
+    }
+}
