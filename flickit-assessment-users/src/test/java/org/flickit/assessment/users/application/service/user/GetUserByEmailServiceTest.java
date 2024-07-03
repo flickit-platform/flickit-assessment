644fb2e95063fbd8916dcb992feaf14348fbdd5c
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-07-03 19:16:41 +0330

    FAC-500 Impl load user by email service unit test

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/user/GetUserByEmailServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/user/GetUserByEmailServiceTest.java
@@ -1,56 +1,58 @@
 package org.flickit.assessment.users.application.service.user;
 
-import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.flickit.assessment.users.application.domain.User;
 import org.flickit.assessment.users.application.port.in.user.GetUserByEmailUseCase;
 import org.flickit.assessment.users.application.port.out.user.LoadUserPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static org.flickit.assessment.users.common.ErrorMessageKey.USER_BY_EMAIL_NOT_FOUND;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class GetUserByEmailServiceTest {
 
     @InjectMocks
-    private GetUserByEmailService service;
+    private GetUserByEmailService getUserByEmailService;
 
     @Mock
     private LoadUserPort loadUserPort;
 
     @Test
-    void testGetUserByEmail_ValidParams_ReturnUserIdSuccessfully() {
-        GetUserByEmailUseCase.Param param = new GetUserByEmailUseCase.Param("user@email.com");
-        UUID expectedUserId = UUID.randomUUID();
-        when(loadUserPort.loadUserIdByEmail(param.getEmail())).thenReturn(Optional.of(expectedUserId));
+    void testGetUserByEmail_WhenEmailIsValid_ReturnsUser() {
+        String mail = "admin@flickit.org";
+        User user = new User(UUID.randomUUID(),
+            mail,
+            "Flickit Admin",
+            "admin",
+            "linkedin.com/in/flickit",
+            "media/flickit.svg");
+        LocalDateTime lastLogin = LocalDateTime.now();
+        LoadUserPort.Result result = new LoadUserPort.Result(user,
+            lastLogin,
+            true,
+            true,
+            true,
+            RandomStringUtils.randomAlphanumeric(50));
 
-        UUID id = service.getUserByEmail(param);
+        when(loadUserPort.loadUserByEmail(mail)).thenReturn(result);
+        GetUserByEmailUseCase.Param param = new GetUserByEmailUseCase.Param(mail);
+        GetUserByEmailUseCase.Result actual = getUserByEmailService.getUserByEmail(param);
 
-        var emailParam = ArgumentCaptor.forClass(String.class);
-        verify(loadUserPort, times(1)).loadUserIdByEmail(emailParam.capture());
-        assertEquals(param.getEmail(), emailParam.getValue());
-
-        assertEquals(expectedUserId, id);
+        assertNotNull(actual);
+        assertEquals(result.user(), actual.user());
+        assertEquals(result.lastLogin(), actual.lastLogin());
+        assertTrue(actual.isSuperUser());
+        assertTrue(actual.isStaff());
+        assertTrue(actual.isActive());
+        assertEquals(result.password(), actual.password());
     }
 
-    @Test
-    void testGetUserByEmail_InvalidEmail_ThrowsException() {
-        GetUserByEmailUseCase.Param param = new GetUserByEmailUseCase.Param("user@email.com");
-        when(loadUserPort.loadUserIdByEmail(param.getEmail()))
-            .thenThrow(new ResourceNotFoundException(USER_BY_EMAIL_NOT_FOUND));
-
-        var throwable = assertThrows(ResourceNotFoundException.class,
-            () -> service.getUserByEmail(param));
-
-        assertEquals(USER_BY_EMAIL_NOT_FOUND, throwable.getMessage());
-    }
 }
