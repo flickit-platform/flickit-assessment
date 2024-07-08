349cad72b7ead2ac076291c0c4d7f21f2d9961aa
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-07-08 15:07:43 +0330

    FAC-483: Assert service result in test

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/evidenceattachment/GetEvidenceAttachmentsServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/evidenceattachment/GetEvidenceAttachmentsServiceTest.java
@@ -3,7 +3,7 @@ package org.flickit.assessment.core.application.service.evidenceattachment;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
-import org.flickit.assessment.core.application.domain.User;
+import org.flickit.assessment.core.application.port.in.evidenceattachment.GetEvidenceAttachmentsUseCase.Param;
 import org.flickit.assessment.core.application.port.out.evidence.LoadEvidencePort;
 import org.flickit.assessment.core.application.port.out.evidenceattachment.LoadEvidenceAttachmentsPort;
 import org.flickit.assessment.core.application.port.out.minio.CreateFileDownloadLinkPort;
@@ -14,15 +14,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.flickit.assessment.core.application.port.in.evidenceattachment.GetEvidenceAttachmentsUseCase.Param;
 
 import java.time.Duration;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.flickit.assessment.core.common.ErrorMessageKey.EVIDENCE_ID_NOT_FOUND;
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.VIEW_EVIDENCE_ATTACHMENT;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.core.common.ErrorMessageKey.EVIDENCE_ID_NOT_FOUND;
+import static org.flickit.assessment.core.test.fixture.application.UserMother.createUser;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -66,16 +68,28 @@ class GetEvidenceAttachmentsServiceTest {
         var currentUserId = UUID.randomUUID();
         var evidence = EvidenceMother.simpleEvidenceWithId(evidenceId);
         var param = new Param(evidenceId, currentUserId);
-        var attachment1 = new LoadEvidenceAttachmentsPort.Result(UUID.randomUUID(), "path/to/file", "des", new User(UUID.randomUUID(), "Name1"));
-        var attachment2 = new LoadEvidenceAttachmentsPort.Result(UUID.randomUUID(), "path/to/file", "des", new User(UUID.randomUUID(), "Name2"));
+        var attachment1 = new LoadEvidenceAttachmentsPort.Result(UUID.randomUUID(), "path/to/file", "des", createUser());
+        var attachment2 = new LoadEvidenceAttachmentsPort.Result(UUID.randomUUID(), "path/to/file", "des", createUser());
         var attachments = List.of(attachment1, attachment2);
 
         when(loadEvidencePort.loadNotDeletedEvidence(evidenceId)).thenReturn(evidence);
-        when(assessmentAccessChecker.isAuthorized(eq(evidence.getAssessmentId()), eq(currentUserId), any())).thenReturn(true);
+        when(assessmentAccessChecker.isAuthorized(evidence.getAssessmentId(), currentUserId, VIEW_EVIDENCE_ATTACHMENT)).thenReturn(true);
         when(loadEvidenceAttachmentsPort.loadEvidenceAttachments(evidenceId)).thenReturn(attachments);
         when(createFileDownloadLinkPort.createDownloadLink(anyString(), any(Duration.class))).thenReturn("link");
 
-        assertDoesNotThrow(() -> service.getEvidenceAttachments(param));
+        var result = service.getEvidenceAttachments(param);
+
+        assertEquals(2, result.size());
+        assertEquals(attachment1.id(), result.get(0).id());
+        assertEquals(attachment1.description(), result.get(0).description());
+        assertEquals(attachment1.createdBy().getId(), result.get(0).createdBy().getId());
+        assertEquals(attachment1.createdBy().getDisplayName(), result.get(0).createdBy().getDisplayName());
+        assertEquals("link", result.get(0).link());
+        assertEquals(attachment2.id(), result.get(1).id());
+        assertEquals(attachment2.description(), result.get(1).description());
+        assertEquals(attachment2.createdBy().getId(), result.get(1).createdBy().getId());
+        assertEquals(attachment2.createdBy().getDisplayName(), result.get(1).createdBy().getDisplayName());
+        assertEquals("link", result.get(1).link());
 
         verify(loadEvidencePort).loadNotDeletedEvidence(evidenceId);
         verify(assessmentAccessChecker).isAuthorized(eq(evidence.getAssessmentId()), eq(currentUserId), any());
@@ -92,10 +106,9 @@ class GetEvidenceAttachmentsServiceTest {
         var param = new Param(evidenceId, currentUserId);
 
         when(loadEvidencePort.loadNotDeletedEvidence(evidenceId)).thenReturn(evidence);
-        when(assessmentAccessChecker.isAuthorized(eq(evidence.getAssessmentId()), eq(currentUserId), any())).thenReturn(false);
+        when(assessmentAccessChecker.isAuthorized(evidence.getAssessmentId(), currentUserId, VIEW_EVIDENCE_ATTACHMENT)).thenReturn(false);
 
         var throwable = assertThrows(AccessDeniedException.class, () -> service.getEvidenceAttachments(param));
-
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
 
         verify(loadEvidencePort).loadNotDeletedEvidence(evidenceId);
