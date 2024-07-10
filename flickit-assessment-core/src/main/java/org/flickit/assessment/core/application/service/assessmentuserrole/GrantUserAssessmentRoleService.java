4049ce9f50d2ff7cad50f933112af3aa541ff28c
mahdirezazadeh <riza.rs2000@gmail.com>
2024-07-10 14:28:47 +0330

    FAC-511:feature:Add sendNotificationPort to GrantUserAssessmentRoleService

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/GrantUserAssessmentRoleService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/GrantUserAssessmentRoleService.java
@@ -2,16 +2,25 @@ package org.flickit.assessment.core.application.service.assessmentuserrole;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.application.domain.assessment.NotificationType;
 import org.flickit.assessment.common.application.domain.assessment.SpaceAccessChecker;
+import org.flickit.assessment.common.error.ErrorMessageKey;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.common.exception.ValidationException;
+import org.flickit.assessment.core.application.domain.AssessmentUserRole;
+import org.flickit.assessment.core.application.domain.notification.GrantAssessmentUserRolePayLoad;
 import org.flickit.assessment.core.application.port.in.assessmentuserrole.GrantUserAssessmentRoleUseCase;
+import org.flickit.assessment.core.application.port.out.assessment.GetAssessmentPort;
 import org.flickit.assessment.core.application.port.out.assessmentuserrole.GrantUserAssessmentRolePort;
+import org.flickit.assessment.core.application.port.out.notification.SendNotificationPort;
+import org.flickit.assessment.core.application.port.out.user.LoadUserPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.GRANT_USER_ASSESSMENT_ROLE;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.core.common.ErrorMessageKey.ASSESSMENT_ID_NOT_FOUND;
 import static org.flickit.assessment.core.common.ErrorMessageKey.GRANT_ASSESSMENT_USER_ROLE_USER_ID_NOT_MEMBER;
 
 @Service
@@ -22,6 +31,9 @@ public class GrantUserAssessmentRoleService implements GrantUserAssessmentRoleUs
     private final GrantUserAssessmentRolePort grantUserAssessmentRolePort;
     private final AssessmentAccessChecker assessmentAccessChecker;
     private final SpaceAccessChecker spaceAccessChecker;
+    private final GetAssessmentPort getAssessmentPort;
+    private final LoadUserPort loadUserPort;
+    private final SendNotificationPort sendNotificationPort;
 
     @Override
     public void grantAssessmentUserRole(Param param) {
@@ -32,5 +44,23 @@ public class GrantUserAssessmentRoleService implements GrantUserAssessmentRoleUs
             throw new ValidationException(GRANT_ASSESSMENT_USER_ROLE_USER_ID_NOT_MEMBER);
 
         grantUserAssessmentRolePort.persist(param.getAssessmentId(), param.getUserId(), param.getRoleId());
+
+        sendNotificationPort.sendNotification(param.getUserId(),
+            NotificationType.GRANT_USER_ASSESSMENT_ROLE,
+            createNotificationData(param));
+    }
+
+    private GrantAssessmentUserRolePayLoad createNotificationData(Param param) {
+        var assessment = getAssessmentPort.getAssessmentById(param.getAssessmentId())
+            .orElseThrow(() -> new ResourceNotFoundException(ASSESSMENT_ID_NOT_FOUND));
+        var role = AssessmentUserRole.valueOfById(param.getRoleId());
+        var assigner = loadUserPort.loadById(param.getCurrentUserId())
+            .orElseThrow(() -> new ResourceNotFoundException(ErrorMessageKey.COMMON_USER_NOT_FOUND));
+
+        return new GrantAssessmentUserRolePayLoad(
+            new GrantAssessmentUserRolePayLoad.Assessment(assessment.getTitle()),
+            new GrantAssessmentUserRolePayLoad.User(assigner.getDisplayName()),
+            new GrantAssessmentUserRolePayLoad.Role(role == null ? null : role.getTitle())
+        );
     }
 }
