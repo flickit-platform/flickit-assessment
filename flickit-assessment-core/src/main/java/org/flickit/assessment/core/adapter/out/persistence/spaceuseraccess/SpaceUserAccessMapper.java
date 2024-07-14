3624869c02598dd8c4f952b05e296209f15cc290
mahdirezazadeh <riza.rs2000@gmail.com>
2024-07-14 16:00:07 +0330

    FAC-516:feature:Impl CreateAssessmentSpaceUserAccessPort with SpaceUserAccessPersistenceJpaAdapter

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/spaceuseraccess/SpaceUserAccessMapper.java
@@ -0,0 +1,14 @@
+package org.flickit.assessment.core.adapter.out.persistence.spaceuseraccess;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.flickit.assessment.core.application.port.out.spaceuseraccess.CreateAssessmentSpaceUserAccessPort.Param;
+import org.flickit.assessment.data.jpa.users.spaceuseraccess.SpaceUserAccessJpaEntity;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SpaceUserAccessMapper {
+
+    public static SpaceUserAccessJpaEntity toJpaEntity(Param param, Long spaceId) {
+        return new SpaceUserAccessJpaEntity(spaceId, param.userId(), param.createdBy(), param.creationTime(), param.creationTime());
+    }
+}
--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/spaceuseraccess/SpaceUserAccessPersistenceJpaAdapter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/spaceuseraccess/SpaceUserAccessPersistenceJpaAdapter.java
@@ -1,20 +1,37 @@
 package org.flickit.assessment.core.adapter.out.persistence.spaceuseraccess;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.core.application.port.out.spaceuseraccess.CheckSpaceAccessPort;
+import org.flickit.assessment.core.application.port.out.spaceuseraccess.CreateAssessmentSpaceUserAccessPort;
+import org.flickit.assessment.data.jpa.core.assessment.AssessmentJpaRepository;
 import org.flickit.assessment.data.jpa.users.spaceuseraccess.SpaceUserAccessJpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+import static org.flickit.assessment.core.adapter.out.persistence.spaceuseraccess.SpaceUserAccessMapper.toJpaEntity;
+import static org.flickit.assessment.core.common.ErrorMessageKey.ASSESSMENT_ID_NOT_FOUND;
+
 @Component("coreSpaceUserAccessPersistenceJpaAdapter")
 @RequiredArgsConstructor
-public class SpaceUserAccessPersistenceJpaAdapter implements CheckSpaceAccessPort {
+public class SpaceUserAccessPersistenceJpaAdapter implements
+    CheckSpaceAccessPort,
+    CreateAssessmentSpaceUserAccessPort {
 
     private final SpaceUserAccessJpaRepository repository;
+    private final AssessmentJpaRepository assessmentRepository;
 
     @Override
     public boolean checkIsMember(long spaceId, UUID userId) {
         return repository.existsBySpaceIdAndUserId(spaceId, userId);
+    }
+
+    @Override
+    public void persist(Param param) {
+        Long spaceId = assessmentRepository.findById(param.assessmentId())
+            .orElseThrow(() -> new ResourceNotFoundException(ASSESSMENT_ID_NOT_FOUND))
+            .getSpaceId();
+        repository.save(toJpaEntity(param, spaceId));
     }
 }
