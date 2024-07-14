28ba40d23e33075042c6899fa6af01f32e02b3bb
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-07-14 16:29:33 +0330

    FAC-517: feat: Add Service and ServiceTest [WIP]

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserinvitee/InviteAssessmentUserService.java
@@ -0,0 +1,18 @@
+package org.flickit.assessment.core.application.service.assessmentuserinvitee;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.core.application.port.in.assessmentuserinvitee.InviteAssessmentUserUseCase;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class InviteAssessmentUserService implements InviteAssessmentUserUseCase {
+
+    @Override
+    public void inviteUser(UUID assessmentId, UUID userId, Integer roleId) {
+    }
+}
---
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessmentuserinvitee/InviteAssessmentUserServiceTest.java
@@ -0,0 +1,12 @@
+package org.flickit.assessment.core.application.service.assessmentuserinvitee;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class InviteAssessmentUserServiceTest {
+
+    @InjectMocks
+    private InviteAssessmentUserService service;
+}
