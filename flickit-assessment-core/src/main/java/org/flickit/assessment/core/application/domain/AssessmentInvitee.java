d1d80414bdeafb2ab0376fedc82a8beaec2ad3d7
mahdirezazadeh <riza.rs2000@gmail.com>
2024-07-17 13:31:10 +0330

    FAC-519:feature:Impl GetAssessmentInviteeListService

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AssessmentInvitee.java
@@ -0,0 +1,20 @@
+package org.flickit.assessment.core.application.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Getter
+@RequiredArgsConstructor
+public class AssessmentInvitee {
+
+    private final UUID id;
+    private final String email;
+    private final AssessmentUserRole role;
+    private final LocalDateTime expirationTime;
+    private final LocalDateTime creationTime;
+    private final UUID createdById;
+}
