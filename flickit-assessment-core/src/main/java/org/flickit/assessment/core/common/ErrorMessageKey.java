a39455b0fdac27b9409fedc3eb0c0073e6ae9e54
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-07-18 08:45:02 +0330

    FAC-517: refactor : Rename error message to synchronize with other error message key

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
@@ -168,5 +168,5 @@ public class ErrorMessageKey {
     public static final String INVITE_ASSESSMENT_USER_ASSESSMENT_ID_NOT_NULL = "invite-assessment-user.assessmentId.notNull";
     public static final String INVITE_ASSESSMENT_USER_EMAIL_NOT_NULL = "invite-assessment-user.email.notNull";
     public static final String INVITE_ASSESSMENT_USER_ROLE_ID_NOT_NULL = "invite-assessment-user.roleId.notNull";
-    public static final String INVITE_ASSESSMENT_USER_ROLE_DUPLICATE = "invite-assessment-user.role.duplicate";
+    public static final String INVITE_ASSESSMENT_USER_ROLE_DUPLICATE = "invite-assessment-user.roleId.duplicate";
 }
