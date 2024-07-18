f801f0969fe720dcd5b135c676313fc8d7009068
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-07-18 08:19:08 +0330

    FAC-517: refactor : Remove extra codes

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/spaceuseraccess/SpaceUserAccessPort.java
+++
@@ -1,8 +0,0 @@
-package org.flickit.assessment.core.application.port.out.spaceuseraccess;
-
-import org.flickit.assessment.core.application.domain.SpaceUserAccess;
-
-public interface SpaceUserAccessPort {
-
-    void persist(SpaceUserAccess access);
-}
