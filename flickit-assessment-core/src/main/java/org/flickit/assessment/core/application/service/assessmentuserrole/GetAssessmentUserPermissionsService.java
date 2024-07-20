a278346d99b17c12f53eaa210336117ee15f6a91
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-07-20 17:58:49 +0330

    FAC-524 Add transaction annotation to the service

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/GetAssessmentUserPermissionsService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/GetAssessmentUserPermissionsService.java
@@ -6,12 +6,14 @@ import org.flickit.assessment.core.application.domain.AssessmentUserRole;
 import org.flickit.assessment.core.application.port.in.assessmentuserrole.GetAssessmentUserPermissionsUseCase;
 import org.flickit.assessment.core.application.port.out.assessmentuserrole.LoadUserRoleForAssessmentPort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class GetAssessmentUserPermissionsService implements GetAssessmentUserPermissionsUseCase {
 
