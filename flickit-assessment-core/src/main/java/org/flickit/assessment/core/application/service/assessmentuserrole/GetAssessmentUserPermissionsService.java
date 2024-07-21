6e7307378e572015531f66eb920b8040b57d5427
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-07-21 18:22:15 +0330

    FAC-523: Minor refactor

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/GetAssessmentUserPermissionsService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/GetAssessmentUserPermissionsService.java
@@ -2,15 +2,14 @@ package org.flickit.assessment.core.application.service.assessmentuserrole;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentPermission;
-import org.flickit.assessment.core.application.domain.AssessmentUserRole;
 import org.flickit.assessment.core.application.port.in.assessmentuserrole.GetAssessmentUserPermissionsUseCase;
 import org.flickit.assessment.core.application.port.out.assessmentuserrole.LoadUserRoleForAssessmentPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 @Service
 @Transactional(readOnly = true)
