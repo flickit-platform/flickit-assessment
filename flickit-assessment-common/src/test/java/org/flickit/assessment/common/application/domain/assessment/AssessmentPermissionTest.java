130057c925436c138626d52c533a5767a1438c6e
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-07-21 17:10:29 +0330

    FAC-523: Refactor the test to cover more complete scenarios and conditions

--- flickit-assessment-common/src/test/java/org/flickit/assessment/common/application/domain/assessment/AssessmentPermissionTest.java
+++ flickit-assessment-common/src/test/java/org/flickit/assessment/common/application/domain/assessment/AssessmentPermissionTest.java
@@ -1,33 +1,53 @@
 package org.flickit.assessment.common.application.domain.assessment;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AssessmentPermissionTest {
 
-    private static final int ASSESSMENT_PERMISSION_SIZE = 28;
-    private static final List<String> ASSESSMENT_PERMISSION_CODES = List.of(
-        "createAssessment", "viewAssessment", "updateAssessment", "deleteAssessment",
-        "viewAssessmentList", "viewAssessmentProgress","viewAssessmentReport", "calculateAssessment",
-        "calculateConfidence", "viewAttributeScoreDetail", "viewSubjectProgress", "viewSubjectReport",
-        "createAdvice", "addEvidence", "updateEvidence", "deleteEvidence",
-        "viewEvidenceList", "viewAttributeEvidenceList", "addEvidenceAttachment", "viewEvidenceAttachment",
-        "answerQuestion", "viewQuestionnaireQuestions", "viewAssessmentQuestionnaireList", "grantUserAssessmentRole",
-        "updateUserAssessmentRole", "deleteUserAssessmentRole", "viewAssessmentUserList", "viewAssessmentInviteeList");
-
     @Test
-    void testAssessmentPermissionsLength() {
-        assertEquals(ASSESSMENT_PERMISSION_SIZE, AssessmentPermission.values().length);
-    }
+    void testAssessmentPermission_PermissionCodesShouldNotBeChanged() {
+        Map<AssessmentPermission, String> permissionCodesMap = Map.ofEntries(
+            Map.entry(CREATE_ASSESSMENT, "createAssessment"),
+            Map.entry(VIEW_ASSESSMENT, "viewAssessment"),
+            Map.entry(UPDATE_ASSESSMENT, "updateAssessment"),
+            Map.entry(DELETE_ASSESSMENT, "deleteAssessment"),
+            Map.entry(VIEW_ASSESSMENT_LIST, "viewAssessmentList"),
+            Map.entry(VIEW_ASSESSMENT_PROGRESS, "viewAssessmentProgress"),
+            Map.entry(VIEW_ASSESSMENT_REPORT, "viewAssessmentReport"),
+            Map.entry(CALCULATE_ASSESSMENT, "calculateAssessment"),
+            Map.entry(CALCULATE_CONFIDENCE, "calculateConfidence"),
+            Map.entry(VIEW_ATTRIBUTE_SCORE_DETAIL, "viewAttributeScoreDetail"),
+            Map.entry(VIEW_SUBJECT_PROGRESS, "viewSubjectProgress"),
+            Map.entry(VIEW_SUBJECT_REPORT, "viewSubjectReport"),
+            Map.entry(CREATE_ADVICE, "createAdvice"),
+            Map.entry(ADD_EVIDENCE, "addEvidence"),
+            Map.entry(UPDATE_EVIDENCE, "updateEvidence"),
+            Map.entry(DELETE_EVIDENCE, "deleteEvidence"),
+            Map.entry(VIEW_EVIDENCE_LIST, "viewEvidenceList"),
+            Map.entry(VIEW_ATTRIBUTE_EVIDENCE_LIST, "viewAttributeEvidenceList"),
+            Map.entry(ADD_EVIDENCE_ATTACHMENT, "addEvidenceAttachment"),
+            Map.entry(VIEW_EVIDENCE_ATTACHMENT, "viewEvidenceAttachment"),
+            Map.entry(DELETE_EVIDENCE_ATTACHMENT, "deleteEvidenceAttachment"),
+            Map.entry(ANSWER_QUESTION, "answerQuestion"),
+            Map.entry(VIEW_QUESTIONNAIRE_QUESTIONS, "viewQuestionnaireQuestions"),
+            Map.entry(VIEW_ASSESSMENT_QUESTIONNAIRE_LIST, "viewAssessmentQuestionnaireList"),
+            Map.entry(GRANT_USER_ASSESSMENT_ROLE, "grantUserAssessmentRole"),
+            Map.entry(UPDATE_USER_ASSESSMENT_ROLE, "updateUserAssessmentRole"),
+            Map.entry(DELETE_USER_ASSESSMENT_ROLE, "deleteUserAssessmentRole"),
+            Map.entry(VIEW_ASSESSMENT_USER_LIST, "viewAssessmentUserList"),
+            Map.entry(VIEW_ASSESSMENT_INVITEE_LIST, "viewAssessmentInviteeList"));
 
-    @ParameterizedTest
-    @EnumSource(AssessmentPermission.class)
-    void testAssessmentPermissionCodes(AssessmentPermission assessmentPermission) {
-        assertTrue(ASSESSMENT_PERMISSION_CODES.contains(assessmentPermission.getCode()));
+        permissionCodesMap.forEach((key, value) -> assertEquals(value, key.getCode()));
+        Arrays.stream(AssessmentPermission.values()).forEach(e ->
+            assertTrue(permissionCodesMap.containsValue(e.getCode()))
+        );
+        assertEquals(29, AssessmentPermission.values().length);
     }
 }
