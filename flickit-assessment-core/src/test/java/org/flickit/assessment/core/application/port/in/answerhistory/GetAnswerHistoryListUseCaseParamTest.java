4da5c2a62e40bebe2eab8fb673c27b403b63513b
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-07-24 19:22:01 +0330

    FAC-493: Define test parameters as local variables

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/port/in/answerhistory/GetAnswerHistoryListUseCaseParamTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/port/in/answerhistory/GetAnswerHistoryListUseCaseParamTest.java
@@ -1,7 +1,6 @@
 package org.flickit.assessment.core.application.port.in.answerhistory;
 
 import jakarta.validation.ConstraintViolationException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
@@ -9,70 +8,60 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
 import static org.flickit.assessment.core.common.ErrorMessageKey.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GetAnswerHistoryListUseCaseParamTest {
 
-    Long questionId;
-    UUID assessmentId;
-    UUID currentUserId;
-    int page;
-    int size;
-
-    @BeforeEach
-    void setUp() {
-        questionId = 1L;
-        assessmentId = UUID.randomUUID();
-        currentUserId = UUID.randomUUID();
-        size = 1;
-        page = 5;
-    }
-
     @Test
     void testGetAnswerHistoryList_assessmentIdIsNull_ErrorMessage() {
-        assessmentId = null;
+        UUID assessmentId = null;
+        UUID currentUserId = UUID.randomUUID();
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, questionId, currentUserId, size, page));
+            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, 123L, currentUserId, 5, 1));
         assertThat(throwable).hasMessage("assessmentId: " + GET_ANSWER_HISTORY_LIST_ASSESSMENT_ID_NOT_NULL);
     }
 
     @Test
     void testGetAnswerHistoryList_questionIdIsNull_ErrorMessage() {
-        questionId = null;
+        UUID assessmentId = UUID.randomUUID();
+        UUID currentUserId = UUID.randomUUID();
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, questionId, currentUserId, size, page));
+            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, null, currentUserId, 5, 1));
         assertThat(throwable).hasMessage("questionId: " + GET_ANSWER_HISTORY_LIST_QUESTION_ID_NOT_NULL);
     }
 
     @Test
     void testGetAnswerHistoryList_currentUserIdIsNull_ErrorMessage() {
-        currentUserId = null;
+        UUID assessmentId = UUID.randomUUID();
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, questionId, currentUserId, size, page));
+            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, 123L, null, 5, 1));
         assertThat(throwable).hasMessage("currentUserId: " + COMMON_CURRENT_USER_ID_NOT_NULL);
     }
 
     @Test
     void testGetAnswerHistoryList_PageSizeIsLessThanMin_ErrorMessage() {
-        size = 0;
+        UUID assessmentId = UUID.randomUUID();
+        UUID currentUserId = UUID.randomUUID();
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, questionId, currentUserId, size, page));
+            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, 123L, currentUserId, 0, 1));
         assertThat(throwable).hasMessage("size: " + GET_ANSWER_HISTORY_LIST_SIZE_MIN);
     }
 
     @Test
     void testGetAnswerHistoryList_PageSizeIsGreaterThanMax_ErrorMessage() {
-        size = 101;
+        UUID assessmentId = UUID.randomUUID();
+        UUID currentUserId = UUID.randomUUID();
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, questionId, currentUserId, size, page));
+            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, 123L, currentUserId, 101, 1));
         assertThat(throwable).hasMessage("size: " + GET_ANSWER_HISTORY_LIST_SIZE_MAX);
     }
 
     @Test
     void testGetAnswerHistoryList_PageNumberIsLessThanMin_ErrorMessage() {
-        page = -1;
+        UUID assessmentId = UUID.randomUUID();
+        UUID currentUserId = UUID.randomUUID();
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, questionId, currentUserId, size, page));
+            () -> new GetAnswerHistoryListUseCase.Param(assessmentId, 123L, currentUserId, 5, -1));
         assertThat(throwable).hasMessage("page: " + GET_ANSWER_HISTORY_LIST_PAGE_MIN);
     }
 }
