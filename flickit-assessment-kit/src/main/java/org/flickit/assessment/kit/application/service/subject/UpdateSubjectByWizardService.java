7ecd6a8253210366fbbf413f511c0c9e658f5356
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-07-28 17:34:59 +0330

    FAC-526 Impl update subject by wizard service & its unit tests

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/subject/UpdateSubjectByWizardService.java
@@ -0,0 +1,48 @@
+package org.flickit.assessment.kit.application.service.subject;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.kit.application.domain.AssessmentKit;
+import org.flickit.assessment.kit.application.domain.Subject;
+import org.flickit.assessment.kit.application.port.in.subject.UpdateSubjectByWizardUseCase;
+import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitPort;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.kit.application.port.out.subject.UpdateSubjectByWizardPort;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UpdateSubjectByWizardService implements UpdateSubjectByWizardUseCase {
+
+    private final UpdateSubjectByWizardPort updateSubjectByWizardPort;
+    private final LoadAssessmentKitPort loadAssessmentKitPort;
+    private final LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
+
+    @Override
+    public void updateSubject(Param param) {
+        AssessmentKit kit = loadAssessmentKitPort.load(param.getKitId());
+        Long kitVersionId = kit.getKitVersionId();
+        long expertGroupId = kit.getExpertGroupId();
+        UUID ownerId = loadExpertGroupOwnerPort.loadOwnerId(expertGroupId);
+        if (!ownerId.equals(param.getCurrentUserId()))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+
+        String code = Subject.generateSlugCode(param.getTitle());
+        updateSubjectByWizardPort.updateByWizard(new UpdateSubjectByWizardPort.Param(param.getSubjectId(),
+            kitVersionId,
+            code,
+            param.getTitle(),
+            param.getIndex(),
+            param.getDescription(),
+            param.getWeight(),
+            LocalDateTime.now(),
+            param.getCurrentUserId()));
+    }
+}
