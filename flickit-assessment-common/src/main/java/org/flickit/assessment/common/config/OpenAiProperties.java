5addbccc9e8d3718db46d4394be67edd05c5be7c
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-07-29 09:19:10 +0330

    FAC-534: Use template evaluator to create prompt

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/config/OpenAiProperties.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/config/OpenAiProperties.java
@@ -4,8 +4,11 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.flickit.assessment.common.util.TemplateEvaluator;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.Map;
 
 @Getter
 @Setter
@@ -13,6 +16,8 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties("app.open-ai")
 @RequiredArgsConstructor
 public class OpenAiProperties {
+
+    private final TemplateEvaluator evaluator;
 
     private boolean enabled;
 
@@ -29,13 +34,13 @@ public class OpenAiProperties {
     private String model;
 
     @NotNull
-    private String prompt = "As a software quality assessor, I have evaluated the %s maturity of a system. " +
-        "We define %s as %s The uploaded Excel file contains multiple-choice questions used to assess %s. " +
+    private String prompt = "As a software quality assessor, I have evaluated the {{title}} maturity of a system. " +
+        "We define {{title}} as {{description}} The uploaded Excel file contains multiple-choice questions used to assess {{title}}. " +
         "The Excel columns include the question, a hint, the weight of the question in calculating the overall score," +
         " and the actual score achieved by the software. Please generate an executive summary highlighting the main strengths and weaknesses in less than 100 words. " +
         "Use polite and considerate language, avoiding any derogatory terms, and do not mention the scores of individual questions.";
 
     public String createPrompt(String title, String description) {
-        return String.format(prompt, title, title, description, title);
+        return evaluator.evaluate(prompt, Map.of("title", title, "description", description));
     }
 }
