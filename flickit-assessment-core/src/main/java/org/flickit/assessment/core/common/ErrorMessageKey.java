bcc1fce69af29ea52b9e03cea18e1def54907edc
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-07-30 15:11:33 +0330

    FAC-536: feat : add edit-assessment-invitee-role.inviteId.notFound error message

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
@@ -212,4 +212,5 @@ public class ErrorMessageKey {
 
     public static final String EDIT_ASSESSMENT_INVITEE_ROLE_INVITE_ID_NOT_NULL = "edit-assessment-invitee-role.inviteId.notNull";
     public static final String EDIT_ASSESSMENT_INVITEE_ROLE_ROLE_ID_NOT_NULL = "edit-assessment-invitee-role.roleId.notNull";
+    public static final String EDIT_ASSESSMENT_INVITEE_ROLE_INVITE_ID_NOT_FOUND = "edit-assessment-invitee-role.inviteId.notFound";
 }
