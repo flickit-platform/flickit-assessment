0b7d40f322e7263ab5673671e76d5879f2de1927
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-08-03 14:53:15 +0330

    FAC-552: feat : Add AttributeInsightMother

---
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/test/fixture/application/AttributeInsightMother.java
@@ -0,0 +1,30 @@
+package org.flickit.assessment.core.test.fixture.application;
+
+import org.flickit.assessment.core.application.domain.AttributeInsight;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.UUID;
+
+public class AttributeInsightMother {
+
+    public static AttributeInsight simpleAttributeAiInsight(){
+        return new AttributeInsight(UUID.randomUUID(),
+            new Random().nextLong(),
+            "ai insight ",
+            "assessor insight",
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            "input path");
+    }
+
+    public static AttributeInsight simpleAttributeAiInsightMinInsightTime(){
+        return new AttributeInsight(UUID.randomUUID(),
+            new Random().nextLong(),
+            "ai insight ",
+            "assessor insight",
+            LocalDateTime.MIN,
+            LocalDateTime.now(),
+            "input path");
+    }
+}
