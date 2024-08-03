4b44662089c5f4a47bb2aa7e43750da31b83f544
mahdirezazadeh <riza.rs2000@gmail.com>
2024-08-03 10:51:13 +0330

    FAC-543:feature:Refactor GenerateAttributeValueReportFileAdapter

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/report/GenerateAttributeValueReportFileAdapter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/report/GenerateAttributeValueReportFileAdapter.java
@@ -6,11 +6,11 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.core.adapter.out.persistence.kit.maturitylevel.MaturityLevelMapper;
 import org.flickit.assessment.core.application.domain.*;
-import org.flickit.assessment.core.application.port.out.assessmentresult.LoadCalculateInfoPort;
 import org.flickit.assessment.core.application.port.out.attributevalue.GenerateAttributeValueReportFilePort;
-import org.flickit.assessment.core.application.port.out.attributevalue.LoadAttributeValuePort;
-import org.flickit.assessment.core.application.port.out.maturitylevel.LoadMaturityLevelsPort;
+import org.flickit.assessment.data.jpa.core.assessmentresult.AssessmentResultJpaRepository;
+import org.flickit.assessment.data.jpa.kit.maturitylevel.MaturityLevelJpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
@@ -18,12 +18,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static org.flickit.assessment.core.common.ErrorMessageKey.CREATE_ATTRIBUTE_VALUE_REPORT_FILE_ATTRIBUTE_VALUE_NOT_FOUND;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_ASSESSMENT_RESULT_NOT_FOUND;
 
 @Component
 @AllArgsConstructor
@@ -36,50 +35,39 @@ public class GenerateAttributeValueReportFileAdapter implements GenerateAttribut
     private static final List<String> ATTRIBUTE_SHEET_HEADERS = List.of("Attribute Title", "Attribute Maturity Level");
     private static final List<String> MATURITY_LEVELS_HEADERS = List.of("Title", "Index", "Description");
 
-    private final LoadCalculateInfoPort loadCalculateInfoPort;
-    private final LoadAttributeValuePort loadAttributeValuePort;
-    private final LoadMaturityLevelsPort loadMaturityLevelsPort;
+    private final AssessmentResultJpaRepository assessmentResultRepository;
+    private final MaturityLevelJpaRepository maturityLevelRepository;
 
     @SneakyThrows
     @Override
-    public InputStream generateReport(UUID assessmentId, Long attributeId) {
-        AssessmentResult assessmentResult = loadCalculateInfoPort.load(assessmentId);
-        AttributeValue attributeValue = findAttributeValueByAttributeId(attributeId, assessmentResult);
-        AttributeValue attributeValueDetail = loadAttributeValuePort.load(attributeValue.getId());
+    public InputStream generateReport(UUID assessmentId, AttributeValue attributeValue) {
+        var assessmentResult = assessmentResultRepository.findFirstByAssessment_IdOrderByLastModificationTimeDesc(assessmentId)
+            .orElseThrow(() -> new ResourceNotFoundException(COMMON_ASSESSMENT_RESULT_NOT_FOUND));
+        var maturityLevels = maturityLevelRepository.findAllByKitVersionIdOrderByIndex(assessmentResult.getKitVersionId()).stream()
+            .map(x -> MaturityLevelMapper.mapToDomainModel(x, null))
+            .toList();
 
         Workbook workbook = new XSSFWorkbook();
-        createQuestionsSheet(workbook, attributeId, attributeValue);
-        createAttributeSheet(workbook, attributeValueDetail);
-        createMaturityLevelsSheet(workbook, assessmentResult.getKitVersionId());
+        createQuestionsSheet(attributeValue, workbook);
+        createAttributeSheet(attributeValue, workbook);
+        createMaturityLevelsSheet(maturityLevels, workbook);
         return convertWorkbookToInputStream(workbook);
     }
 
-    private AttributeValue findAttributeValueByAttributeId(Long attributeId, AssessmentResult assessmentResult) {
-        return assessmentResult.getSubjectValues().stream()
-            .flatMap(s -> s.getAttributeValues().stream())
-            .filter(a -> a.getAttribute().getId() == attributeId)
-            .findFirst()
-            .orElseThrow(() -> new ResourceNotFoundException(CREATE_ATTRIBUTE_VALUE_REPORT_FILE_ATTRIBUTE_VALUE_NOT_FOUND));
-    }
-
-    private void createQuestionsSheet(Workbook workbook, Long attributeId, AttributeValue attributeValue) {
+    private void createQuestionsSheet(AttributeValue attributeValue, Workbook workbook) {
         Sheet sheet = initQuestionsSheet(workbook);
-        List<Question> questions = attributeValue.getAttribute().getQuestions();
-        List<Answer> answers = attributeValue.getAnswers();
-        createQuestionRows(attributeId, questions, answers, workbook, sheet);
+        createQuestionRows(attributeValue, workbook, sheet);
     }
 
-    private void createAttributeSheet(Workbook workbook, AttributeValue attributeValue) {
+    private void createAttributeSheet(AttributeValue attributeValue, Workbook workbook) {
         Sheet sheet = initAttributeSheet(workbook);
         CellStyle style = workbook.createCellStyle();
         style.setWrapText(true);
         createAttributeRow(attributeValue, sheet, style);
     }
 
-    private void createMaturityLevelsSheet(Workbook workbook, long kitVersionId) {
+    private void createMaturityLevelsSheet(List<MaturityLevel> maturityLevels, Workbook workbook) {
         Sheet sheet = initMaturityLevelsSheet(workbook);
-
-        List<MaturityLevel> maturityLevels = loadMaturityLevelsPort.loadByKitVersionId(kitVersionId);
         createMaturityLevelRows(maturityLevels, workbook, sheet);
     }
 
@@ -89,7 +77,7 @@ public class GenerateAttributeValueReportFileAdapter implements GenerateAttribut
         sheet.setColumnWidth(1, 10000);
         sheet.setColumnWidth(2, 2000);
         sheet.setColumnWidth(3, 2000);
-        createHeader(workbook, sheet, QUESTIONS_SHEET_HEADERS);
+        createHeader(QUESTIONS_SHEET_HEADERS, workbook, sheet);
         return sheet;
     }
 
@@ -97,7 +85,7 @@ public class GenerateAttributeValueReportFileAdapter implements GenerateAttribut
         Sheet sheet = workbook.createSheet(ATTRIBUTE_SHEET_TITLE);
         sheet.setColumnWidth(0, 4000);
         sheet.setColumnWidth(1, 4000);
-        createHeader(workbook, sheet, ATTRIBUTE_SHEET_HEADERS);
+        createHeader(ATTRIBUTE_SHEET_HEADERS, workbook, sheet);
         return sheet;
     }
 
@@ -106,11 +94,11 @@ public class GenerateAttributeValueReportFileAdapter implements GenerateAttribut
         sheet.setColumnWidth(0, 4000);
         sheet.setColumnWidth(1, 2000);
         sheet.setColumnWidth(2, 10000);
-        createHeader(workbook, sheet, MATURITY_LEVELS_HEADERS);
+        createHeader(MATURITY_LEVELS_HEADERS, workbook, sheet);
         return sheet;
     }
 
-    private void createHeader(Workbook workbook, Sheet sheet, List<String> headers) {
+    private void createHeader(List<String> headers, Workbook workbook, Sheet sheet) {
         CellStyle headerStyle = workbook.createCellStyle();
 
         XSSFFont font = ((XSSFWorkbook) workbook).createFont();
@@ -126,26 +114,29 @@ public class GenerateAttributeValueReportFileAdapter implements GenerateAttribut
         }
     }
 
-    private void createQuestionRows(Long attributeId, List<Question> questions, List<Answer> answers, Workbook workbook, Sheet sheet) {
+    private void createQuestionRows(AttributeValue attributeValue, Workbook workbook, Sheet sheet) {
+        var attribute = attributeValue.getAttribute();
+        var questions = attribute.getQuestions();
+        var answers = attributeValue.getAnswers();
         CellStyle style = workbook.createCellStyle();
         style.setWrapText(true);
 
-        Map<Long, Answer> questionIdToAnswerMap = answers.stream()
+        var questionIdToAnswerMap = answers.stream()
             .filter(a -> Boolean.TRUE.equals(a.getIsNotApplicable()) || a.getSelectedOption() != null)
             .collect(Collectors.toMap(Answer::getQuestionId, Function.identity()));
 
         int rowNumber = 1;
         for (Question question : questions) {
-            Answer answer = questionIdToAnswerMap.get(question.getId());
+            var answer = questionIdToAnswerMap.get(question.getId());
             if (answer != null && answer.getIsNotApplicable())
                 continue;
 
             Row row = sheet.createRow(rowNumber++);
-            createQuestionRow(attributeId, style, row, question, answer);
+            createQuestionRow(attribute.getId(), question, answer, row, style);
         }
     }
 
-    private void createQuestionRow(Long attributeId, CellStyle style, Row row, Question question, Answer answer) {
+    private void createQuestionRow(Long attributeId, Question question, Answer answer, Row row, CellStyle style) {
         Cell cell = row.createCell(0);
         cell.setCellValue(question.getTitle());
         cell.setCellStyle(style);
--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/attributevalue/GenerateAttributeValueReportFilePort.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/attributevalue/GenerateAttributeValueReportFilePort.java
@@ -1,9 +1,11 @@
 package org.flickit.assessment.core.application.port.out.attributevalue;
 
+import org.flickit.assessment.core.application.domain.AttributeValue;
+
 import java.io.InputStream;
 import java.util.UUID;
 
 public interface GenerateAttributeValueReportFilePort {
 
-    InputStream generateReport(UUID assessmentId, Long attributeId);
+    InputStream generateReport(UUID assessmentId, AttributeValue attributeValue);
 }
