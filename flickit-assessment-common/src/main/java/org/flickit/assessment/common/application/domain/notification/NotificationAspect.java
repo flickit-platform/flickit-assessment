3874de230d16c317091a6408d270503fe7bb590d
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-08-07 12:13:23 +0330

    FAC-511: Define NotificationAspect

---
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/application/domain/notification/NotificationAspect.java
@@ -0,0 +1,48 @@
+package org.flickit.assessment.common.application.domain.notification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.toMap;
+import static org.flickit.assessment.common.config.NotificationAspectConfig.NOTIFICATION_SENDER_THREAD_EXECUTOR;
+
+@Slf4j
+@Aspect
+@Component
+public class NotificationAspect {
+
+    private final SendNotificationPort sendNotificationPort;
+    private final Map<Class<?>, NotificationContentProvider> contentProviders;
+
+    public NotificationAspect(SendNotificationPort sendNotificationPort,
+                              List<NotificationContentProvider<?, ?>> contentProviders) {
+        this.sendNotificationPort = sendNotificationPort;
+        this.contentProviders = contentProviders.stream()
+            .collect(toMap(NotificationContentProvider::cmdClass, x -> x));
+    }
+
+    @Pointcut("@annotation(SendNotification)")
+    public void annotatedBySendNotification() {
+    }
+
+    @Async(NOTIFICATION_SENDER_THREAD_EXECUTOR)
+    @AfterReturning(pointcut = "annotatedBySendNotification()", returning = "result")
+    public void sendNotificationAfter(Object result) {
+        if (!(result instanceof HasNotificationCmd hasCmd))
+            return;
+
+        NotificationCmd cmd = hasCmd.notificationCmd();
+        log.debug("{} received.", cmd);
+        var provider = contentProviders.get(cmd.getClass());
+        Optional<NotificationContent> content = provider.create(cmd);
+        content.ifPresent(x -> sendNotificationPort.send(cmd.targetUserId(), x));
+    }
+}
---
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/config/NotificationAspectConfig.java
@@ -0,0 +1,28 @@
+package org.flickit.assessment.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class NotificationAspectConfig {
+
+    public static final String NOTIFICATION_SENDER_THREAD_EXECUTOR = "notificationSenderThreadPoolTaskExecutor";
+
+    @Bean(name = NOTIFICATION_SENDER_THREAD_EXECUTOR)
+    Executor notificationSenderThreadPoolTaskExecutor() {
+        var props = new TaskExecutorProps();
+        var executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(props.getCorePoolSize());
+        executor.setMaxPoolSize(props.getMaxPoolSize());
+        executor.setKeepAliveSeconds(props.getKeepAliveSeconds());
+        executor.setQueueCapacity(props.getQueueCapacity());
+        executor.setAllowCoreThreadTimeOut(props.isAllowCoreThreadTimeOut());
+        executor.setPrestartAllCoreThreads(props.isPrestartAllCoreThreads());
+        return executor;
+    }
+}
