198f5347c51320dfdf76350b704964084d43c10f
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-08-07 18:42:03 +0330

    FAC-562: Refactor : Remove CreateAttributeValueFile

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/minio/MinioAdapter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/minio/MinioAdapter.java
@@ -9,7 +9,6 @@ import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.core.application.port.out.evidenceattachment.UploadEvidenceAttachmentPort;
 import org.flickit.assessment.core.application.port.out.minio.CreateFileDownloadLinkPort;
 import org.flickit.assessment.core.application.port.out.minio.DeleteEvidenceAttachmentFilePort;
-import org.flickit.assessment.core.application.port.out.minio.UploadAttributeScoreExcelPort;
 import org.flickit.assessment.data.config.MinioConfigProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,8 +25,7 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.FILE_STORAGE_F
 public class MinioAdapter implements
     CreateFileDownloadLinkPort,
     UploadEvidenceAttachmentPort,
-    DeleteEvidenceAttachmentFilePort,
-    UploadAttributeScoreExcelPort {
+    DeleteEvidenceAttachmentFilePort {
 
     public static final String SLASH = "/";
     private final MinioClient minioClient;
@@ -112,15 +110,5 @@ public class MinioAdapter implements
             .object(objectName)
             .versionId(latestVersionId)
             .build());
-    }
-
-    @Override
-    public String uploadExcel(InputStream content, String fileName) {
-        String bucketName = properties.getBucketNames().getReport();
-        UUID uniqueDir = UUID.randomUUID();
-
-        String objectName = uniqueDir + SLASH + fileName;
-        writeFile(bucketName, objectName, content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        return bucketName + SLASH + objectName;
     }
 }
