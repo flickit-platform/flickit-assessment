5801b2da9751eaaaeccfeefef8aa4f825cae042e
mahdirezazadeh <riza.rs2000@gmail.com>
2024-08-10 08:41:40 +0330

    FAC-560:feature:Update updateAiInsight method

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/attributeinsight/AttributeInsightJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/attributeinsight/AttributeInsightJpaRepository.java
@@ -17,11 +17,13 @@ public interface AttributeInsightJpaRepository extends JpaRepository<AttributeIn
     @Query("""
             UPDATE AttributeInsightJpaEntity a
             SET a.aiInsight = :aiInsight,
-                a.aiInsightTime = :aiInsightTime
+                a.aiInsightTime = :aiInsightTime,
+                a.aiInputPath = :aiInputPath
             WHERE a.assessmentResultId = :assessmentResultId AND a.attributeId = :attributeId
         """)
     void updateAiInsight(@Param("assessmentResultId") UUID assessmentResultId,
                          @Param("attributeId") Long attributeId,
                          @Param("aiInsight") String aiInsight,
-                         @Param("aiInsightTime") LocalDateTime aiInsightTime);
+                         @Param("aiInsightTime") LocalDateTime aiInsightTime,
+                         @Param("aiInputPath") String aiInputPath);
 }
