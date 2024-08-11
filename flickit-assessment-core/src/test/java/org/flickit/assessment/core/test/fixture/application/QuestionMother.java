a67e2485398ed9e6403ce1e201706651d6269681
mahdirezazadeh <riza.rs2000@gmail.com>
2024-08-11 12:03:08 +0330

    FAC-521:feature:Update AttributeValueTest

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/test/fixture/application/QuestionMother.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/test/fixture/application/QuestionMother.java
@@ -48,4 +48,10 @@ public class QuestionMother {
     public static Question withIdAndImpactsOnLevel23(long id, long attributeId) {
         return new Question(id, "question" + id, null, "question hint", Boolean.FALSE, List.of(QuestionImpactMother.onLevelTwo(1, attributeId), QuestionImpactMother.onLevelThree(1, attributeId)), QuestionnaireMother.createQuestionnaire());
     }
+
+    public static Question withImpactsOnLevel3AndAnotherAttributeLevel4(long attributeId, long anotherAttributeId) {
+        return new Question(id++, "question" + id, index++, null, Boolean.FALSE,
+            List.of(QuestionImpactMother.onLevelThree(1, attributeId), QuestionImpactMother.onLevelFour(1, anotherAttributeId)),
+            QuestionnaireMother.createQuestionnaire());
+    }
 }
