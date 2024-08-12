d0c3f3fd6e509e5f7ab8600406fac11cfa05ab86
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-08-12 16:44:41 +0330

    FAC-541 Impl question create web adapter

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/question/CreateQuestionRequestDto.java
@@ -0,0 +1,10 @@
+package org.flickit.assessment.kit.adapter.in.rest.question;
+
+public record CreateQuestionRequestDto(
+    Integer index,
+    String title,
+    String hint,
+    Boolean mayNotBeApplicable,
+    Boolean advisable,
+    Long questionnaireId
+) {}
---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/question/CreateQuestionRestController.java
@@ -0,0 +1,41 @@
+package org.flickit.assessment.kit.adapter.in.rest.question;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.flickit.assessment.kit.application.port.in.question.CreateQuestionUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class CreateQuestionRestController {
+
+    private final CreateQuestionUseCase useCase;
+    private final UserContext userContext;
+
+    @PostMapping("/assessment-kits/{kitId}/questionnaires/{questionnaireId}/questions")
+    public ResponseEntity<CreateQuestionResponseDto> createQuestion(@PathVariable("kitId") Long kitId,
+                                                                    @PathVariable("questionnaireId") Long questionnaireId,
+                                                                    @RequestBody CreateQuestionRequestDto dto) {
+        UUID currentUserId = userContext.getUser().id();
+        long questionId = useCase.createQuestion(toParam(kitId, dto, currentUserId));
+        return new ResponseEntity<>(new CreateQuestionResponseDto(questionId), HttpStatus.CREATED);
+    }
+
+    private static CreateQuestionUseCase.Param toParam(Long kitId, CreateQuestionRequestDto dto, UUID currentUserId) {
+        return new CreateQuestionUseCase.Param(kitId,
+            dto.index(),
+            dto.title(),
+            dto.hint(),
+            dto.mayNotBeApplicable(),
+            dto.advisable(),
+            dto.questionnaireId(),
+            currentUserId);
+    }
+}
