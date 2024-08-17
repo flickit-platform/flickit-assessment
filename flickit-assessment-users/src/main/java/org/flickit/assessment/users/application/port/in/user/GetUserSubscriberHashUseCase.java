fa5b196486aea81d731674329ffe02a9b38d829c
mahdirezazadeh <riza.rs2000@gmail.com>
2024-08-17 16:47:02 +0330

    FAC-569:feature:Add GetUserSubscriberHashUseCase.java

---
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/port/in/user/GetUserSubscriberHashUseCase.java
@@ -0,0 +1,31 @@
+package org.flickit.assessment.users.application.port.in.user;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.flickit.assessment.common.application.SelfValidating;
+
+import java.util.UUID;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
+
+public interface GetUserSubscriberHashUseCase {
+
+    Result getUserSubscriberHash(Param param);
+
+    @Value
+    @EqualsAndHashCode(callSuper = false)
+    class Param extends SelfValidating<Param> {
+
+        @NotNull(message = COMMON_CURRENT_USER_ID_NOT_NULL)
+        UUID currentUserId;
+
+        public Param(UUID currentUserId) {
+            this.currentUserId = currentUserId;
+            validateSelf();
+        }
+    }
+
+    record Result(String subscriberHash) {
+    }
+}
