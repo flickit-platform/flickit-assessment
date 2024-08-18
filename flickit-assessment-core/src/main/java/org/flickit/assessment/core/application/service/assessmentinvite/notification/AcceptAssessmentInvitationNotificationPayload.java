50e5420033ea645d82c62f9de56d4f61992e30b3
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-08-18 09:38:06 +0330

    FAC-572: feat : add NotificationPayload

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentinvite/notification/AcceptAssessmentInvitationNotificationPayload.java
@@ -0,0 +1,19 @@
+package org.flickit.assessment.core.application.service.assessmentinvite.notification;
+
+import org.flickit.assessment.common.application.domain.notification.NotificationPayload;
+
+import java.util.UUID;
+
+public record AcceptAssessmentInvitationNotificationPayload(AssessmentModel assessmentModel,
+                                                            InviteeModel inviteeModel,
+                                                            RoleModel roleModel) implements NotificationPayload {
+
+    public record AssessmentModel(UUID id, String title) {
+    }
+
+    public record InviteeModel(UUID id, String displayName) {
+    }
+
+    public record RoleModel(String title) {
+    }
+}
