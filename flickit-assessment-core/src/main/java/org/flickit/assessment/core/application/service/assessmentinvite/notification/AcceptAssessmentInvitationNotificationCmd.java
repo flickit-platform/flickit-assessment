c6ec0b6a4eaaff3f91af26096b52645f7b6f0f9f
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-08-18 21:41:52 +0330

    FAC-572: feat : Update cmd to accept list of notification

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentinvite/notification/AcceptAssessmentInvitationNotificationCmd.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentinvite/notification/AcceptAssessmentInvitationNotificationCmd.java
@@ -3,10 +3,14 @@ package org.flickit.assessment.core.application.service.assessmentinvite.notific
 import org.flickit.assessment.common.application.domain.notification.NotificationCmd;
 import org.flickit.assessment.core.application.domain.AssessmentUserRole;
 
+import java.util.List;
 import java.util.UUID;
 
-public record AcceptAssessmentInvitationNotificationCmd(UUID targetUserId,
-                                                        UUID assessmentId,
-                                                        UUID inviteeUserId,
-                                                        AssessmentUserRole assessmentUserRole) implements NotificationCmd {
+public record AcceptAssessmentInvitationNotificationCmd(
+    List<NotificationCmdItem> assessmentTargetUserRoles) implements NotificationCmd {
+    public record NotificationCmdItem(UUID targetUserId,
+                                      UUID assessmentId,
+                                      UUID inviteeId,
+                                      AssessmentUserRole assessmentUserRole) {
+    }
 }
