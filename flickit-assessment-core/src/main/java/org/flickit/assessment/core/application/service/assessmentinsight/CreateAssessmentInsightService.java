a436d6770d6308cb4fec6a1acffbc3aa16c1a397
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-08-21 12:12:27 +0330

    FAC-599: feat - Add loadAssessmentResult check

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentinsight/CreateAssessmentInsightService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentinsight/CreateAssessmentInsightService.java
@@ -3,11 +3,14 @@ package org.flickit.assessment.core.application.service.assessmentinsight;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.core.application.port.in.assessmentinsight.CreateAssessmentInsightUseCase;
+import org.flickit.assessment.core.application.port.out.assessmentresult.LoadAssessmentResultPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.CREATE_ASSESSMENT_INSIGHT;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_ASSESSMENT_RESULT_NOT_FOUND;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
 @Service
@@ -16,11 +19,16 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT
 public class CreateAssessmentInsightService implements CreateAssessmentInsightUseCase {
 
     private final AssessmentAccessChecker assessmentAccessChecker;
+    private final LoadAssessmentResultPort loadAssessmentResultPort;
 
     @Override
     public void createAssessmentInsight(Param param) {
         if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), CREATE_ASSESSMENT_INSIGHT))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+
+        var assessmentResult = loadAssessmentResultPort.loadByAssessmentId(param.getAssessmentId());
+        if (assessmentResult.isEmpty())
+            throw new ResourceNotFoundException(COMMON_ASSESSMENT_RESULT_NOT_FOUND);
 
     }
 }
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessmentinsight/CreateAssessmentInsightServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessmentinsight/CreateAssessmentInsightServiceTest.java
@@ -3,15 +3,19 @@ package org.flickit.assessment.core.application.service.assessmentinsight;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentPermission;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.core.application.port.in.assessmentinsight.CreateAssessmentInsightUseCase;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.core.application.port.in.assessmentinsight.CreateAssessmentInsightUseCase.*;
+import org.flickit.assessment.core.application.port.out.assessmentresult.LoadAssessmentResultPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_ASSESSMENT_RESULT_NOT_FOUND;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
@@ -26,11 +30,14 @@ class CreateAssessmentInsightServiceTest {
     @Mock
     private AssessmentAccessChecker assessmentAccessChecker;
 
+    @Mock
+    private LoadAssessmentResultPort loadAssessmentResultPort;
+
     @Test
-    void testCreateAssessmentInsight_CurrentUserHasNotAccess_ShouldReturnAccessDeniedException() {
+    void testCreateAssessmentInsight_UserWithoutAccess_ShouldThrowAccessDeniedException() {
         var assessmentId = UUID.randomUUID();
         var currentUserId = UUID.randomUUID();
-        var param = new CreateAssessmentInsightUseCase.Param(assessmentId, "assessment insight", currentUserId);
+        var param = new Param(assessmentId, "assessment insight", currentUserId);
 
         when(assessmentAccessChecker.isAuthorized(assessmentId, currentUserId, AssessmentPermission.CREATE_ASSESSMENT_INSIGHT)).thenReturn(false);
 
@@ -38,5 +45,21 @@ class CreateAssessmentInsightServiceTest {
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
 
         verify(assessmentAccessChecker).isAuthorized(assessmentId, currentUserId, AssessmentPermission.CREATE_ASSESSMENT_INSIGHT);
+    }
+
+    @Test
+    void testCreateAssessmentInsight_NoAssessmentResult_ShouldThrowResourceNotFoundException() {
+        var assessmentId = UUID.randomUUID();
+        var currentUserId = UUID.randomUUID();
+        var param = new Param(assessmentId, "assessment insight", currentUserId);
+
+        when(assessmentAccessChecker.isAuthorized(assessmentId, currentUserId, AssessmentPermission.CREATE_ASSESSMENT_INSIGHT)).thenReturn(true);
+        when(loadAssessmentResultPort.loadByAssessmentId(assessmentId)).thenReturn(Optional.empty());
+
+        var throwable = assertThrows(ResourceNotFoundException.class, () -> service.createAssessmentInsight(param));
+        assertEquals(COMMON_ASSESSMENT_RESULT_NOT_FOUND, throwable.getMessage());
+
+        verify(assessmentAccessChecker).isAuthorized(assessmentId, currentUserId, AssessmentPermission.CREATE_ASSESSMENT_INSIGHT);
+        verify(loadAssessmentResultPort).loadByAssessmentId(assessmentId);
     }
 }
