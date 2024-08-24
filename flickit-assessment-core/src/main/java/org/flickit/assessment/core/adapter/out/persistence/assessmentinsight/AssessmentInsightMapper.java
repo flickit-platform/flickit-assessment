e38268d45a884a2aa743a7941fe911fc36494a65
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-08-24 10:50:12 +0330

    FAC-599: Refactor - refactor AssessmentInsightJpaEntity to accept AssessmentInsight instead of CreateAssessmentInsightPort.Param

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/assessmentinsight/AssessmentInsightMapper.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/assessmentinsight/AssessmentInsightMapper.java
@@ -3,19 +3,18 @@ package org.flickit.assessment.core.adapter.out.persistence.assessmentinsight;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.flickit.assessment.core.application.domain.AssessmentInsight;
-import org.flickit.assessment.core.application.port.out.assessmentinsight.CreateAssessmentInsightPort;
 import org.flickit.assessment.data.jpa.core.assessmentinsight.AssessmentInsightJpaEntity;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AssessmentInsightMapper {
 
-    public static AssessmentInsightJpaEntity toJpaEntity(CreateAssessmentInsightPort.Param param) {
+    public static AssessmentInsightJpaEntity toJpaEntity(AssessmentInsight assessmentInsight) {
         return new AssessmentInsightJpaEntity(
             null,
-            param.assessmentResultId(),
-            param.insight(),
-            param.insightTime(),
-            param.insightBy()
+            assessmentInsight.getAssessmentResultId(),
+            assessmentInsight.getInsight(),
+            assessmentInsight.getInsightTime(),
+            assessmentInsight.getInsightBy()
         );
     }
 
