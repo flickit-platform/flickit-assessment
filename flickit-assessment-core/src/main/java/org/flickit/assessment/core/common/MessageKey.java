35d134640a2baff5bb80e17478f53dfe11b60d30
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-08-25 21:18:01 +0330

    FAC-597: feat - Add LOAD_ASSESSMENT_INSIGHT_DEFAULT_INSIGHT_TEXT

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/MessageKey.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/MessageKey.java
@@ -7,4 +7,5 @@ import lombok.NoArgsConstructor;
 public class MessageKey {
 
     public static final String ASSESSMENT_ATTRIBUTE_AI_IS_DISABLED = "create-assessment-attribute-ai-report.ai.disabled";
+    public static final String LOAD_ASSESSMENT_INSIGHT_DEFAULT_INSIGHT_TEXT = "load-assessment-insight.default-insight.text";
 }
