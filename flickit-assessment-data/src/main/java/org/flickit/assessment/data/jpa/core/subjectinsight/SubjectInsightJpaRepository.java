cafa95c7953adee14b8d9931f74e6b16adc52f54
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-08-27 13:58:41 +0330

    FAC-601: Fix query param names

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/subjectinsight/SubjectInsightJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/subjectinsight/SubjectInsightJpaRepository.java
@@ -3,6 +3,7 @@ package org.flickit.assessment.data.jpa.core.subjectinsight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -20,5 +21,9 @@ public interface SubjectInsightJpaRepository extends JpaRepository<SubjectInsigh
                 si.insightBy = :insightBy
             WHERE si.assessmentResultId = :assessmentResultId AND si.subjectId = :subjectId
         """)
-    void updateByAssessmentResultIdAndSubjectId(UUID assessmentResultId, Long subjectId, String insight, LocalDateTime localDateTime, UUID uuid);
+    void updateByAssessmentResultIdAndSubjectId(@Param("assessmentResultId") UUID assessmentResultId,
+                                                @Param("subjectId") Long subjectId,
+                                                @Param("insight") String insight,
+                                                @Param("insightTime") LocalDateTime insightTime,
+                                                @Param("insightBy") UUID insightBy);
 }
