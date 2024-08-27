4c74ec337f4718f06a5c2d5a6d17d2be635a113c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-08-27 21:51:16 +0330

    FAC-605: Feat - Add uniqueness constraint to jpa

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/assessmentinsight/AssessmentInsightJpaEntity.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/assessmentinsight/AssessmentInsightJpaEntity.java
@@ -23,7 +23,7 @@ public class AssessmentInsightJpaEntity {
     @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
 
-    @Column(name = "assessment_result_id", nullable = false)
+    @Column(name = "assessment_result_id", nullable = false, unique = true)
     private UUID assessmentResultId;
 
     @Column(name = "insight", nullable = false)
