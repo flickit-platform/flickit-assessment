493f6ed09d63881bb7c9110dcb27d94cd609345e
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-01 11:38:53 +0330

    FAC-608: Feat - Add domain

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AssessmentAnalysis.java
@@ -0,0 +1,29 @@
+package org.flickit.assessment.core.application.domain;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@RequiredArgsConstructor
+public class AssessmentAnalysis {
+
+    private final UUID id;
+
+    private final UUID assessmentResultId;
+
+    private final int type;
+
+    private final String aiAnalysis;
+
+    private final String assessorAnalysis;
+
+    private final LocalDateTime aiAnalysisTime;
+
+    private final LocalDateTime assessorAnalysisTime;
+
+    private final String inputPath;
+}
