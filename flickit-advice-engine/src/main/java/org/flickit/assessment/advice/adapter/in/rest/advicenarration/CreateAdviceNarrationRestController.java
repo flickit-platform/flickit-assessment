5be6fae4555c30379ea74d35314b547d5fe09666
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-11 17:14:14 +0330

    FAC-627: feat - Add RestController

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/in/rest/advicenarration/CreateAdviceNarrationRestController.java
@@ -0,0 +1,35 @@
+package org.flickit.assessment.advice.adapter.in.rest.advicenarration;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.advice.application.port.in.advicenarration.CreateAdviceNarrationUseCase;
+import org.flickit.assessment.advice.application.port.in.advicenarration.CreateAdviceNarrationUseCase.*;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class CreateAdviceNarrationRestController {
+
+    private final CreateAdviceNarrationUseCase useCase;
+    private final UserContext userContext;
+
+    @PostMapping("assessments/{assessmentId}/advice-narration")
+    ResponseEntity<Void> createAdviceNarration(@PathVariable("assessmentId") UUID assessmentId, @RequestBody CreateAdviceNarrationRequestDto requestDto) {
+        var currentUserId = userContext.getUser().id();
+        var param = toParam(assessmentId, requestDto, currentUserId);
+        useCase.createAdviceNarration(param);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    private Param toParam(UUID assessmentId, CreateAdviceNarrationRequestDto requestDto, UUID currentUserId) {
+        return new Param(assessmentId, requestDto.adviceListItems(), requestDto.attributeLevelTargets(), currentUserId);
+    }
+}
