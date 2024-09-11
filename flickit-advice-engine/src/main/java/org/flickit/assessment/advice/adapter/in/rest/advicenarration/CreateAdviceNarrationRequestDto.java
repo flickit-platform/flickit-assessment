dba6aeae84497fb492cf938834371853232782fb
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-11 17:13:35 +0330

    FAC-627: feat - Add RequestDto

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/in/rest/advicenarration/CreateAdviceNarrationRequestDto.java
@@ -0,0 +1,10 @@
+package org.flickit.assessment.advice.adapter.in.rest.advicenarration;
+
+import org.flickit.assessment.advice.application.domain.AttributeLevelTarget;
+import org.flickit.assessment.advice.application.domain.advice.AdviceListItem;
+
+import java.util.List;
+
+public record CreateAdviceNarrationRequestDto(List<AdviceListItem> adviceListItems,
+                                              List<AttributeLevelTarget> attributeLevelTargets) {
+}
