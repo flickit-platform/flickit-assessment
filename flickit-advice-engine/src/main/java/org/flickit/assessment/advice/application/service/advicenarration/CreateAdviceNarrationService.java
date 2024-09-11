29603d3808b68491dbcd83295e16b812625b6738
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-11 17:15:02 +0330

    FAC-627: feat - Add Service [WIP]

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/service/advicenarration/CreateAdviceNarrationService.java
@@ -0,0 +1,15 @@
+package org.flickit.assessment.advice.application.service.advicenarration;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.advice.application.port.in.advicenarration.CreateAdviceNarrationUseCase;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateAdviceNarrationService implements CreateAdviceNarrationUseCase {
+
+    @Override
+    public void createAdviceNarration(Param param) {
+
+    }
+}
