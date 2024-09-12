8a1f6c575feb1f1cbf7630f2c728a32184329d34
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-12 18:13:55 +0330

    FAC-627: feat - Add `createAdviceAiNarration` for Creating AI Narration and Implement in `OpenAiAdapter`

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/port/out/advicenarration/CreateAdviceAiNarrationPort.java
@@ -0,0 +1,7 @@
+package org.flickit.assessment.advice.application.port.out.advicenarration;
+
+
+public interface CreateAdviceAiNarrationPort {
+
+    String createAdviceAiNarration(String adviceListItems, String attributeLevelTargets);
+}
