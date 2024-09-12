b04e0867b74b290bcfafbdde1e903e83e40c9516
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-09-13 00:24:02 +0330

    FAC-629 Impl get advice narration web adapter

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/in/rest/advice/GetAdviceNarrationRestController.java
@@ -0,0 +1,31 @@
+package org.flickit.assessment.advice.adapter.in.rest.advice;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.advice.application.port.in.GetAdviceNarrationUseCase;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class GetAdviceNarrationRestController {
+
+    private final GetAdviceNarrationUseCase useCase;
+    private final UserContext userContext;
+
+    @GetMapping("/assessments/{assessmentId}/advice-narration")
+    public ResponseEntity<GetAdviceNarrationUseCase.Result> getAdviceNarration(@PathVariable("assessmentId") UUID assessmentId) {
+        UUID currentUserId = userContext.getUser().id();
+        var adviceNarration = useCase.getAdviceNarration(toParam(assessmentId, currentUserId));
+        return new ResponseEntity<>(adviceNarration, HttpStatus.OK);
+    }
+
+    private GetAdviceNarrationUseCase.Param toParam(UUID assessmentId, UUID currentUserId) {
+        return new GetAdviceNarrationUseCase.Param(assessmentId, currentUserId);
+    }
+}
