51db045a5004f9d2269ea2b272e4948719183a6a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-13 11:07:22 +0330

    FAC-627: feat - Add `LoadAssessmentResultPort` and Integrate into Core Module

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/assessmentresult/AssessmentResultJpaAdapter.java
@@ -0,0 +1,23 @@
+package org.flickit.assessment.advice.adapter.out.persistence.assessmentresult;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.advice.application.domain.AssessmentResult;
+import org.flickit.assessment.advice.application.port.out.assessmentresult.LoadAssessmentResultPort;
+import org.flickit.assessment.data.jpa.core.assessmentresult.AssessmentResultJpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class AssessmentResultJpaAdapter implements LoadAssessmentResultPort {
+
+    private final AssessmentResultJpaRepository repository;
+
+    @Override
+    public Optional<AssessmentResult> loadByAssessmentId(UUID assessmentId) {
+        var entity = repository.findFirstByAssessment_IdOrderByLastModificationTimeDesc(assessmentId);
+        return entity.map(AssessmentResultMapper::mapToDomain);
+    }
+}
