d83c7a60a584f4c484822a8a2f073eeec5773d37
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-13 10:51:59 +0330

    FAC-627: feat - Add `findByAssessmentResultId` and `updateAiNarration` Functions to `AdviceNarrationJpaRepository`

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/advice/advicenarration/AdviceNarrationJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/advice/advicenarration/AdviceNarrationJpaRepository.java
@@ -1,10 +1,28 @@
 package org.flickit.assessment.data.jpa.advice.advicenarration;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface AdviceNarrationJpaRepository extends JpaRepository<AdviceNarrationJpaEntity , UUID> {
+public interface AdviceNarrationJpaRepository extends JpaRepository<AdviceNarrationJpaEntity, UUID> {
+
+    Optional<AdviceNarrationJpaEntity> findByAssessmentResultId(UUID assessmentResultId);
+
+    @Modifying
+    @Query("""
+        UPDATE AdviceNarrationJpaEntity a SET
+        a.aiNarration = :aiNarration,
+        a.aiNarrationTime = :aiNarrationTime
+        WHERE a.assessmentResultId = :assessmentResultId
+        """)
+    void updateAiNarration(@Param("assessmentResultId") UUID assessmentResultId,
+                           @Param("aiNarration") String aiNarration,
+                           @Param("aiNarrationTime") LocalDateTime aiNarrationTime);
 }
