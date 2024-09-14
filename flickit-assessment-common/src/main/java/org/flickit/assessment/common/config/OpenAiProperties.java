04ea9805e080aa582365dc08dd397744db512ca7
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-14 10:58:00 +0330

    FAC-627: refactor - Update Prompt

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/config/OpenAiProperties.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/config/OpenAiProperties.java
@@ -31,17 +31,18 @@ public class OpenAiProperties {
         """;
 
     private String adviceAiNarrationPrompt = """
-        As a software quality assessor, I have evaluated the maturity of a system.
-        less than 1000 chars.
-        Here is the uploaded adviceListItems: {adviceListItems}.
-        Here is the uploaded attributeLevelTargets: {attributeLevelTargets}.
+        As a software quality assessor, I have evaluated the maturity level of a system. Below, I have outlined the selected option and provided the recommended option for improvement.
+        Please generate advice in up to 10 concise bullets, with a total character limit of 900 characters, to help achieve the recommended option. You may shorten the bullet list if it improves clarity.
+        The advice should be polite, clear, and considerate, avoiding any mention of individual scores or derogatory terms. Focus on offering actionable suggestions. Do not use bold formatting for emphasis.
+        Provided advice items: {adviceListItems}.
+        Provided attribute level targets: {attributeLevelTargets}.
         """;
 
     public Prompt createAttributeAiInsightPrompt(String title, String description, String excelFile) {
         var promptTemplate = new PromptTemplate(attributeAiInsightPrompt, Map.of("title", title, "description", description, "excelFile", excelFile));
         return new Prompt(promptTemplate.createMessage(), chatOptions);
     }
-    
+
     public Prompt createAdviceAiNarration(String adviceListItems, String attributeLevelTargets) {
         var promptTemplate = new PromptTemplate(adviceAiNarrationPrompt, Map.of("adviceListItems", adviceListItems, "attributeLevelTargets", attributeLevelTargets));
         return new Prompt(promptTemplate.createMessage(), chatOptions);
