eadf840d91f3ae971775d6e10904989fc2a2efc5
mahdirezazadeh <riza.rs2000@gmail.com>
2024-09-14 12:18:24 +0330

    FAC-632:feature:Add tenant to triggerEvent notification request

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/novu/CreateAssessmentNovuRequestConverter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/novu/CreateAssessmentNovuRequestConverter.java
@@ -2,8 +2,11 @@ package org.flickit.assessment.core.adapter.out.novu;
 
 import co.novu.api.common.SubscriberRequest;
 import co.novu.api.events.requests.TriggerEventRequest;
+import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.adapter.out.novu.NovuRequestConverter;
 import org.flickit.assessment.common.application.domain.notification.NotificationEnvelope;
+import org.flickit.assessment.common.application.domain.notification.Tenant;
+import org.flickit.assessment.common.config.NotificationSenderProperties;
 import org.flickit.assessment.core.application.service.assessment.notification.CreateAssessmentNotificationPayload;
 import org.springframework.stereotype.Component;
 
@@ -13,15 +16,23 @@ import java.util.UUID;
 import static org.flickit.assessment.common.adapter.out.novu.NotificationType.CREATE_ASSESSMENT;
 
 @Component
+@RequiredArgsConstructor
 public class CreateAssessmentNovuRequestConverter implements NovuRequestConverter {
+
+    private final NotificationSenderProperties properties;
 
     @Override
     public TriggerEventRequest convert(NotificationEnvelope envelope) {
         var triggerEvent = new TriggerEventRequest();
+        triggerEvent.setTenant(createTenant());
         triggerEvent.setName(CREATE_ASSESSMENT.getCode());
         triggerEvent.setTo(createSubscriberRequest(envelope.targetUserId()));
         triggerEvent.setPayload(Map.of("data", envelope.payload(), "title", envelope.title()));
         return triggerEvent;
+    }
+
+    private Tenant createTenant() {
+        return new Tenant(properties.getNovu().getTenantId());
     }
 
     private SubscriberRequest createSubscriberRequest(UUID targetUserId) {
--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/novu/GrantAssessmentUserRoleNovuRequestConverter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/novu/GrantAssessmentUserRoleNovuRequestConverter.java
@@ -2,8 +2,11 @@ package org.flickit.assessment.core.adapter.out.novu;
 
 import co.novu.api.common.SubscriberRequest;
 import co.novu.api.events.requests.TriggerEventRequest;
+import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.adapter.out.novu.NovuRequestConverter;
 import org.flickit.assessment.common.application.domain.notification.NotificationEnvelope;
+import org.flickit.assessment.common.application.domain.notification.Tenant;
+import org.flickit.assessment.common.config.NotificationSenderProperties;
 import org.flickit.assessment.core.application.service.assessmentuserrole.notification.GrantAssessmentUserRoleNotificationPayload;
 import org.springframework.stereotype.Component;
 
@@ -13,15 +16,23 @@ import java.util.UUID;
 import static org.flickit.assessment.common.adapter.out.novu.NotificationType.GRANT_USER_ASSESSMENT_ROLE;
 
 @Component
+@RequiredArgsConstructor
 public class GrantAssessmentUserRoleNovuRequestConverter implements NovuRequestConverter {
+
+    private final NotificationSenderProperties properties;
 
     @Override
     public TriggerEventRequest convert(NotificationEnvelope envelope) {
         var triggerEvent = new TriggerEventRequest();
+        triggerEvent.setTenant(createTenant());
         triggerEvent.setName(GRANT_USER_ASSESSMENT_ROLE.getCode());
         triggerEvent.setTo(createSubscriberRequest(envelope.targetUserId()));
         triggerEvent.setPayload(Map.of("data", envelope.payload(), "title", envelope.title()));
         return triggerEvent;
+    }
+
+    private Tenant createTenant() {
+        return new Tenant(properties.getNovu().getTenantId());
     }
 
     private SubscriberRequest createSubscriberRequest(UUID targetUserId) {
