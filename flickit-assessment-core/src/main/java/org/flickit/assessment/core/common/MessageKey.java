52fc386c4a0c8d1c211f16a21e820fcbd9644189
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-15 13:55:32 +0330

    FAC-627: feat - Rename ASSESSMENT_AI_IS_DISABLED MessageKey

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/MessageKey.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/MessageKey.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MessageKey {
 
-    public static final String ASSESSMENT_ATTRIBUTE_AI_IS_DISABLED = "create-assessment-attribute-ai-report.ai.disabled";
+    public static final String ASSESSMENT_AI_IS_DISABLED = "assessment.ai.disabled";
 
     public static final String ASSESSMENT_DEFAULT_INSIGHT_DEFAULT_COMPLETED = "assessment-default-insight.completed";
     public static final String ASSESSMENT_DEFAULT_INSIGHT_DEFAULT_INCOMPLETE = "assessment-default-insight.incomplete";
