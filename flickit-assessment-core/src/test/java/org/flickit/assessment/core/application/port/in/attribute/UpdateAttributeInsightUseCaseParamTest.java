c02687761a5583cfc888679b1713590c77640816
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-16 14:30:51 +0330

    FAC-600: feat - Add Limit to `assessorInsight` in `UpdateAttributeInsightUseCase`

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/port/in/attribute/UpdateAttributeInsightUseCaseParamTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/port/in/attribute/UpdateAttributeInsightUseCaseParamTest.java
@@ -1,6 +1,7 @@
 package org.flickit.assessment.core.application.port.in.attribute;
 
 import jakarta.validation.ConstraintViolationException;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
@@ -8,6 +9,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
 import static org.flickit.assessment.core.common.ErrorMessageKey.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UpdateAttributeInsightUseCaseParamTest {
@@ -16,9 +18,9 @@ class UpdateAttributeInsightUseCaseParamTest {
     void testUpdateAttributeInsight_assessmentIdIsNull_ErrorMessage() {
         var currentUserId = UUID.randomUUID();
         var attributeId = 1L;
-        var content = "Some content";
+        var assessorInsight = "Some assessorInsight";
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new UpdateAttributeInsightUseCase.Param(null, attributeId, content, currentUserId));
+            () -> new UpdateAttributeInsightUseCase.Param(null, attributeId, assessorInsight, currentUserId));
         assertThat(throwable).hasMessage("assessmentId: " + UPDATE_ATTRIBUTE_INSIGHT_ASSESSMENT_ID_NOT_NULL);
     }
 
@@ -26,9 +28,9 @@ class UpdateAttributeInsightUseCaseParamTest {
     void testUpdateAttributeInsight_attributeIdIsNull_ErrorMessage() {
         var currentUserId = UUID.randomUUID();
         var assessmentId = UUID.randomUUID();
-        var content = "Some content";
+        var assessorInsight = "Some assessorInsight";
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new UpdateAttributeInsightUseCase.Param(assessmentId, null, content, currentUserId));
+            () -> new UpdateAttributeInsightUseCase.Param(assessmentId, null, assessorInsight, currentUserId));
         assertThat(throwable).hasMessage("attributeId: " + UPDATE_ATTRIBUTE_INSIGHT_ATTRIBUTE_ID_NOT_NULL);
     }
 
@@ -43,12 +45,32 @@ class UpdateAttributeInsightUseCaseParamTest {
     }
 
     @Test
+    void testUpdateAttributeInsight_assessorInsightIsLong_ErrorMessage() {
+        var currentUserId = UUID.randomUUID();
+        var assessmentId = UUID.randomUUID();
+        var assessorInsight = RandomStringUtils.random(1001, true, true);
+        var attributeId = 0L;
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> new UpdateAttributeInsightUseCase.Param(assessmentId, attributeId, assessorInsight, currentUserId));
+        assertThat(throwable).hasMessage("assessorInsight: " + UPDATE_ATTRIBUTE_INSIGHT_ASSESSOR_INSIGHT_SIZE_MAX);
+    }
+
+    @Test
     void testUpdateAttributeInsight_currentUserIsNull_ErrorMessage() {
         var assessmentId = UUID.randomUUID();
         var attributeId = 1L;
-        var content = "Some content";
+        var assessorInsight = "Some assessorInsight";
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new UpdateAttributeInsightUseCase.Param(assessmentId, attributeId, content, null));
+            () -> new UpdateAttributeInsightUseCase.Param(assessmentId, attributeId, assessorInsight, null));
         assertThat(throwable).hasMessage("currentUserId: " + COMMON_CURRENT_USER_ID_NOT_NULL);
+    }
+
+    @Test
+    void testUpdateAttributeInsight_validPrams_Successful() {
+        var currentUserId = UUID.randomUUID();
+        var assessmentId = UUID.randomUUID();
+        var assessorInsight = "assessorInsight";
+        var attributeId = 0L;
+        assertDoesNotThrow(()-> new UpdateAttributeInsightUseCase.Param(assessmentId, attributeId, assessorInsight, currentUserId));
     }
 }
