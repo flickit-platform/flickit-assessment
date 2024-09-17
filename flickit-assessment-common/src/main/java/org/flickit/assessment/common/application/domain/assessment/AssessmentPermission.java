2705c0145a1f549aa8687da153d93f1ca19056f3
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-09-17 19:09:33 +0330

    FAC-609 Add 'CREATE_ASSESSMENT_AI_ANALYSIS' permission

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/application/domain/assessment/AssessmentPermission.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/application/domain/assessment/AssessmentPermission.java
@@ -43,7 +43,8 @@ public enum AssessmentPermission {
     DELETE_ASSESSMENT_INVITE("deleteAssessmentInvite"),
     CREATE_ASSESSMENT_INSIGHT("createAssessmentInsight"),
     CREATE_SUBJECT_INSIGHT("createSubjectInsight"),
-    MANAGE_ADD_ON("manageAddOn");
+    MANAGE_ADD_ON("manageAddOn"),
+    CREATE_ASSESSMENT_AI_ANALYSIS("createAssessmentAiAnalysis");
 
     private final String code;
 }
