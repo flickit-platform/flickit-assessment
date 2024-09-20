f5a25fc8434f57e2186e2f485a5dadfc2c709f78
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-20 08:57:57 +0330

    FAC-634: Refactor - Change in Orders of 'requestDto' parameters

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/maturitylevel/UpdateMaturityLevelRequestDto.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/maturitylevel/UpdateMaturityLevelRequestDto.java
@@ -3,6 +3,6 @@ package org.flickit.assessment.kit.adapter.in.rest.maturitylevel;
 public record UpdateMaturityLevelRequestDto(Long kitId,
                                             String title,
                                             Integer index,
-                                            Integer value,
-                                            String description) {
+                                            String description,
+                                            Integer value) {
 }
