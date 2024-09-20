5cf190319471637b13e8d020d4272bbebc5a1a17
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-20 11:33:43 +0330

    FAC-634: test - Add Happy Path Test to `ServiceTest`

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/maturitylevel/UpdateMaturityLevelServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/maturitylevel/UpdateMaturityLevelServiceTest.java
@@ -3,7 +3,9 @@ package org.flickit.assessment.kit.application.service.maturitylevel;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.kit.application.port.in.maturitylevel.UpdateMaturityLevelUseCase;
+import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadActiveKitVersionIdPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadKitExpertGroupPort;
+import org.flickit.assessment.kit.application.port.out.maturitylevel.UpdateMaturityLevelPort;
 import org.flickit.assessment.kit.test.fixture.application.ExpertGroupMother;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,7 +19,7 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT
 import static org.flickit.assessment.kit.common.ErrorMessageKey.EXPERT_GROUP_ID_NOT_FOUND;
 import static org.flickit.assessment.kit.common.ErrorMessageKey.KIT_ID_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UpdateMaturityLevelServiceTest {
@@ -28,6 +30,12 @@ class UpdateMaturityLevelServiceTest {
     @Mock
     LoadKitExpertGroupPort loadKitExpertGroupPort;
 
+    @Mock
+    LoadActiveKitVersionIdPort loadActiveKitVersionIdPort;
+
+    @Mock
+    UpdateMaturityLevelPort updateMaturityLevelPort;
+
     @Test
     void testUpdateMaturityLevelService_KitNotFound_ResourceNotFoundException() {
         var currentUserId = UUID.randomUUID();
@@ -37,6 +45,7 @@ class UpdateMaturityLevelServiceTest {
         var throwable = assertThrows(ResourceNotFoundException.class, () -> service.updateMaturityLevel(param));
 
         assertEquals(KIT_ID_NOT_FOUND, throwable.getMessage());
+        verifyNoInteractions(loadActiveKitVersionIdPort, updateMaturityLevelPort);
     }
 
     @Test
@@ -48,6 +57,7 @@ class UpdateMaturityLevelServiceTest {
         var throwable = assertThrows(ResourceNotFoundException.class, () -> service.updateMaturityLevel(param));
 
         assertEquals(EXPERT_GROUP_ID_NOT_FOUND, throwable.getMessage());
+        verifyNoInteractions(loadActiveKitVersionIdPort, updateMaturityLevelPort);
     }
 
     @Test
@@ -60,5 +70,34 @@ class UpdateMaturityLevelServiceTest {
         var throwable = assertThrows(AccessDeniedException.class, () -> service.updateMaturityLevel(param));
 
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+        verifyNoInteractions(loadActiveKitVersionIdPort, updateMaturityLevelPort);
+    }
+
+    @Test
+    void testUpdateMaturityLevelService_KitIdNotFound_AccessDeniedException() {
+        var currentUserId = UUID.randomUUID();
+        var param = new UpdateMaturityLevelUseCase.Param(1L, 0L, "title", 1, "description", 2, currentUserId);
+        var expertGroup = ExpertGroupMother.createExpertGroupWithCreatedBy(currentUserId);
+
+        when(loadKitExpertGroupPort.loadKitExpertGroup(param.getKitId())).thenReturn(expertGroup);
+        when(loadActiveKitVersionIdPort.loadKitVersionId(param.getKitId())).thenThrow(new ResourceNotFoundException(KIT_ID_NOT_FOUND));
+
+        var throwable = assertThrows(ResourceNotFoundException.class, () -> service.updateMaturityLevel(param));
+
+        assertEquals(KIT_ID_NOT_FOUND, throwable.getMessage());
+    }
+
+    @Test
+    void testUpdateMaturityLevelService_ValidParameters_SuccessfulUpdate() {
+        var currentUserId = UUID.randomUUID();
+        var param = new UpdateMaturityLevelUseCase.Param(1L, 0L, "title", 1, "description", 2, currentUserId);
+        var expertGroup = ExpertGroupMother.createExpertGroupWithCreatedBy(currentUserId);
+        var kitVersionId = 321L;
+
+        when(loadKitExpertGroupPort.loadKitExpertGroup(param.getKitId())).thenReturn(expertGroup);
+        when(loadActiveKitVersionIdPort.loadKitVersionId(param.getKitId())).thenReturn(kitVersionId);
+        doNothing().when(updateMaturityLevelPort).updateInfo(any(), any(), any(), any());
+
+        assertDoesNotThrow(() -> service.updateMaturityLevel(param));
     }
 }
