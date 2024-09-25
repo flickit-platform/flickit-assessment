8f78128f56aec5f6d698bc00b434a2b1f4872bae
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-09-25 17:16:53 +0330

    FAC-634: Update Service and ServiceTest to Use Better Ports

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/maturitylevel/UpdateMaturityLevelService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/maturitylevel/UpdateMaturityLevelService.java
@@ -2,11 +2,10 @@ package org.flickit.assessment.kit.application.service.maturitylevel;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.kit.application.domain.MaturityLevel;
 import org.flickit.assessment.kit.application.port.in.maturitylevel.UpdateMaturityLevelUseCase;
-import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadActiveKitVersionIdPort;
-import org.flickit.assessment.kit.application.port.out.expertgroup.LoadKitExpertGroupPort;
+import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitPort;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.kit.application.port.out.maturitylevel.UpdateMaturityLevelPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,29 +14,26 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
-import static org.flickit.assessment.kit.common.ErrorMessageKey.EXPERT_GROUP_ID_NOT_FOUND;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UpdateMaturityLevelService implements UpdateMaturityLevelUseCase {
 
-    private final LoadKitExpertGroupPort loadKitExpertGroupPort;
-    private final LoadActiveKitVersionIdPort loadActiveKitVersionIdPort;
+    private final LoadAssessmentKitPort loadAssessmentKitPort;
+    private final LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
     private final UpdateMaturityLevelPort updateMaturityLevelPort;
 
     @Override
     public void updateMaturityLevel(Param param) {
-        var kitVersionId = loadActiveKitVersionIdPort.loadKitVersionId(param.getKitId());
-        var expertGroup = loadKitExpertGroupPort.loadKitExpertGroup(param.getKitId());
-        if (expertGroup == null)
-            throw new ResourceNotFoundException(EXPERT_GROUP_ID_NOT_FOUND);
+        var assessmentKit = loadAssessmentKitPort.load(param.getKitId());
+        var expertGroupOwnerId = loadExpertGroupOwnerPort.loadOwnerId(assessmentKit.getExpertGroupId());
 
-        if (!Objects.equals(expertGroup.getOwnerId(), param.getCurrentUserId()))
+        if (!Objects.equals(expertGroupOwnerId, param.getCurrentUserId()))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
         var maturityLevel = new MaturityLevel(param.getId(), MaturityLevel.generateSlugCode(param.getTitle()),
             param.getTitle(), param.getIndex(), param.getDescription(), param.getValue(), null);
-        updateMaturityLevelPort.updateInfo(maturityLevel, kitVersionId, LocalDateTime.now(), param.getCurrentUserId());
+        updateMaturityLevelPort.updateInfo(maturityLevel, assessmentKit.getKitVersionId(), LocalDateTime.now(), param.getCurrentUserId());
     }
 }
--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/maturitylevel/UpdateMaturityLevelServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/maturitylevel/UpdateMaturityLevelServiceTest.java
@@ -2,11 +2,12 @@ package org.flickit.assessment.kit.application.service.maturitylevel;
 
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.kit.application.domain.AssessmentKit;
 import org.flickit.assessment.kit.application.port.in.maturitylevel.UpdateMaturityLevelUseCase;
-import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadActiveKitVersionIdPort;
-import org.flickit.assessment.kit.application.port.out.expertgroup.LoadKitExpertGroupPort;
+import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitPort;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.kit.application.port.out.maturitylevel.UpdateMaturityLevelPort;
-import org.flickit.assessment.kit.test.fixture.application.ExpertGroupMother;
+import org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,7 +17,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
-import static org.flickit.assessment.kit.common.ErrorMessageKey.EXPERT_GROUP_ID_NOT_FOUND;
 import static org.flickit.assessment.kit.common.ErrorMessageKey.KIT_ID_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -28,10 +28,10 @@ class UpdateMaturityLevelServiceTest {
     UpdateMaturityLevelService service;
 
     @Mock
-    LoadKitExpertGroupPort loadKitExpertGroupPort;
+    LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
 
     @Mock
-    LoadActiveKitVersionIdPort loadActiveKitVersionIdPort;
+    LoadAssessmentKitPort loadAssessmentKitPort;
 
     @Mock
     UpdateMaturityLevelPort updateMaturityLevelPort;
@@ -41,37 +41,23 @@ class UpdateMaturityLevelServiceTest {
         var currentUserId = UUID.randomUUID();
         var param = new UpdateMaturityLevelUseCase.Param(1L, 0L, "title", 1, "description", 2, currentUserId);
 
-        when(loadActiveKitVersionIdPort.loadKitVersionId(param.getKitId())).thenThrow(new ResourceNotFoundException(KIT_ID_NOT_FOUND));
+        when(loadAssessmentKitPort.load(param.getKitId())).thenThrow(new ResourceNotFoundException(KIT_ID_NOT_FOUND));
 
         var throwable = assertThrows(ResourceNotFoundException.class, () -> service.updateMaturityLevel(param));
 
         assertEquals(KIT_ID_NOT_FOUND, throwable.getMessage());
 
-        verifyNoInteractions(loadKitExpertGroupPort, updateMaturityLevelPort);
-    }
-
-    @Test
-    void testUpdateMaturityLevelService_ExpertGroupNotFound_ResourceNotFoundException() {
-        var currentUserId = UUID.randomUUID();
-        var param = new UpdateMaturityLevelUseCase.Param(1L, 0L, "title", 1, "description", 2, currentUserId);
-
-        when(loadActiveKitVersionIdPort.loadKitVersionId(param.getKitId())).thenReturn(1L);
-        when(loadKitExpertGroupPort.loadKitExpertGroup(param.getKitId())).thenReturn(null);
-
-        var throwable = assertThrows(ResourceNotFoundException.class, () -> service.updateMaturityLevel(param));
-
-        assertEquals(EXPERT_GROUP_ID_NOT_FOUND, throwable.getMessage());
-        verifyNoInteractions(updateMaturityLevelPort);
+        verifyNoInteractions(loadExpertGroupOwnerPort, updateMaturityLevelPort);
     }
 
     @Test
     void testUpdateMaturityLevelService_UserIsNotExpertGroupOwner_AccessDeniedException() {
         var currentUserId = UUID.randomUUID();
         var param = new UpdateMaturityLevelUseCase.Param(1L, 0L, "title", 1, "description", 2, currentUserId);
-        var expertGroup = ExpertGroupMother.createExpertGroup();
+        var assessmentKit = mock(AssessmentKit.class);
 
-        when(loadActiveKitVersionIdPort.loadKitVersionId(param.getKitId())).thenReturn(1L);
-        when(loadKitExpertGroupPort.loadKitExpertGroup(param.getKitId())).thenReturn(expertGroup);
+        when(loadAssessmentKitPort.load(param.getKitId())).thenReturn(assessmentKit);
+        when(loadExpertGroupOwnerPort.loadOwnerId(param.getKitId())).thenReturn(UUID.randomUUID());
 
         var throwable = assertThrows(AccessDeniedException.class, () -> service.updateMaturityLevel(param));
 
@@ -83,11 +69,10 @@ class UpdateMaturityLevelServiceTest {
     void testUpdateMaturityLevelService_ValidParameters_SuccessfulUpdate() {
         var currentUserId = UUID.randomUUID();
         var param = new UpdateMaturityLevelUseCase.Param(1L, 0L, "title", 1, "description", 2, currentUserId);
-        var expertGroup = ExpertGroupMother.createExpertGroupWithCreatedBy(currentUserId);
-        var kitVersionId = 321L;
+        var assessmentKit = AssessmentKitMother.simpleKit();
 
-        when(loadActiveKitVersionIdPort.loadKitVersionId(param.getKitId())).thenReturn(kitVersionId);
-        when(loadKitExpertGroupPort.loadKitExpertGroup(param.getKitId())).thenReturn(expertGroup);
+        when(loadAssessmentKitPort.load(param.getKitId())).thenReturn(assessmentKit);
+        when(loadExpertGroupOwnerPort.loadOwnerId(assessmentKit.getExpertGroupId())).thenReturn(currentUserId);
         doNothing().when(updateMaturityLevelPort).updateInfo(any(), any(), any(), any());
 
         assertDoesNotThrow(() -> service.updateMaturityLevel(param));
