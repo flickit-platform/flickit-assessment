477cec003868cf6d7aa92aaf132e092cf3004d19
mahdirezazadeh <riza.rs2000@gmail.com>
2024-09-29 10:44:32 +0330

    FAC-635:feature:Add CreateLevelCompetenceRestController

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/levelcompetence/CreateLevelCompetenceRestController.java
@@ -0,0 +1,39 @@
+package org.flickit.assessment.kit.adapter.in.rest.levelcompetence;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.flickit.assessment.kit.application.port.in.levelcompetence.CreateLevelCompetenceUseCase;
+import org.flickit.assessment.kit.application.port.in.levelcompetence.CreateLevelCompetenceUseCase.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class CreateLevelCompetenceRestController {
+
+    private final CreateLevelCompetenceUseCase useCase;
+    private final UserContext userContext;
+
+    @PostMapping("assessment-kits/{kitVersionId}/level-competences")
+    public ResponseEntity<Void> createLevelCompetence(@PathVariable("kitVersionId") Long kitVersionId,
+                                                      @RequestBody CreateLevelCompetenceRequestDto requestDto) {
+        UUID currentUserId = userContext.getUser().id();
+        useCase.createLevelCompetence(toParam(kitVersionId, requestDto, currentUserId));
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    private Param toParam(Long kitVersionId,
+                          CreateLevelCompetenceRequestDto requestDto, UUID currentUserId) {
+        return new Param(kitVersionId,
+            requestDto.affectedLevelId(),
+            requestDto.effectiveLevelId(),
+            requestDto.value(),
+            currentUserId);
+    }
+}
