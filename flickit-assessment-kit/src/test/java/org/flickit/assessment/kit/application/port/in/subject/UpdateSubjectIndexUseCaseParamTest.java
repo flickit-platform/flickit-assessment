678e9bce6166119501be649c563914e36c2a3035
mahdirezazadeh <riza.rs2000@gmail.com>
2024-09-29 13:57:16 +0330

    FAC-647:feature:Add UpdateSubjectIndexUseCaseParamTest

---
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/port/in/subject/UpdateSubjectIndexUseCaseParamTest.java
@@ -0,0 +1,60 @@
+package org.flickit.assessment.kit.application.port.in.subject;
+
+import jakarta.validation.ConstraintViolationException;
+import org.flickit.assessment.kit.application.port.in.subject.UpdateSubjectIndexUseCase.Param;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
+import static org.flickit.assessment.kit.common.ErrorMessageKey.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class UpdateSubjectIndexUseCaseParamTest {
+
+    @Test
+    void testUpdateSubjectIndexUseCaseParam_kitVersionIdParamViolatesConstraints_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.kitVersionId(null)));
+        assertThat(throwable).hasMessage("kitVersionId: " + UPDATE_SUBJECT_INDEX_KIT_VERSION_ID_NOT_NULL);
+    }
+
+    @Test
+    void testUpdateSubjectIndexUseCaseParam_subjectIdParamViolatesConstraints_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.subjectId(null)));
+        assertThat(throwable).hasMessage("subjectId: " + UPDATE_SUBJECT_INDEX_SUBJECT_ID_NOT_NULL);
+    }
+
+    @Test
+    void testUpdateSubjectIndexUseCaseParam_indexParamViolatesConstraints_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.index(null)));
+        assertThat(throwable).hasMessage("index: " + UPDATE_SUBJECT_INDEX_INDEX_NOT_NULL);
+    }
+
+
+    @Test
+    void testUpdateSubjectIndexUseCaseParam_currentUserParamViolatesConstraints_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.currentUserId(null)));
+        assertThat(throwable).hasMessage("currentUserId: " + COMMON_CURRENT_USER_ID_NOT_NULL);
+    }
+
+    private void createParam(Consumer<Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        paramBuilder.build();
+    }
+
+    private Param.ParamBuilder paramBuilder() {
+        return Param.builder()
+            .kitVersionId(1L)
+            .subjectId(2L)
+            .index(3)
+            .currentUserId(UUID.randomUUID());
+    }
+
+}
