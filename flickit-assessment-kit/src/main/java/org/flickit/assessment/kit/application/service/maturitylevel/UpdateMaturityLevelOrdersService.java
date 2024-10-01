cacf7214f3cd426b60bb3e673a9982d60915b418
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-01 15:33:38 +0330

    FAC-646: Feat - Implement Service

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/maturitylevel/UpdateMaturityLevelOrdersService.java
@@ -0,0 +1,35 @@
+package org.flickit.assessment.kit.application.service.maturitylevel;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.kit.application.port.in.maturitylevel.UpdateMaturityLevelOrdersUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
+import org.flickit.assessment.kit.application.port.out.maturitylevel.UpdateMaturityLevelPort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UpdateMaturityLevelOrdersService implements UpdateMaturityLevelOrdersUseCase {
+
+    private final LoadKitVersionPort loadKitVersionPort;
+    private final LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
+    private final UpdateMaturityLevelPort updateMaturityLevelPort;
+
+    @Override
+    public void changeOrders(Param param) {
+        var kitVersion = loadKitVersionPort.load(param.getKitVersionId());
+        UUID ownerId = loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId());
+
+        if (!ownerId.equals(param.getCurrentUserId()))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+
+        updateMaturityLevelPort.updateOrders(param.getOrders(), param.getKitVersionId(), param.getCurrentUserId());
+    }
+}
