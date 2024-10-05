55f5b05a918f83faf27a7edfb84bcecbd1543540
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-05 11:28:44 +0330

    FAC-646: Refactor - Remove `Value` from RequestDto

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/maturitylevel/UpdateMaturityLevelOrdersRequestDto.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/maturitylevel/UpdateMaturityLevelOrdersRequestDto.java
@@ -3,6 +3,6 @@ package org.flickit.assessment.kit.adapter.in.rest.maturitylevel;
 import java.util.List;
 
 public record UpdateMaturityLevelOrdersRequestDto(List<MaturityLevelOrderDto> orders) {
-    record MaturityLevelOrderDto(Long id, Integer index, Integer value) {
+    record MaturityLevelOrderDto(Long id, Integer index) {
     }
 }
