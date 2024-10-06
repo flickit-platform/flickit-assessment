95246becdf88dbada559a70abb73c8c0d8df0d95
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-06 10:00:29 +0330

    FAC-652: Refactor - Add Service Test [WIP]

---
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/questionnaire/UpdateQuestionnaireOrdersServiceTest.java
@@ -0,0 +1,103 @@
+package org.flickit.assessment.kit.application.service.questionnaire;
+
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.kit.application.domain.KitVersion;
+import org.flickit.assessment.kit.application.domain.QuestionnaireOrder;
+import org.flickit.assessment.kit.application.port.in.questionnaire.UpdateQuestionnaireOrdersUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
+import org.flickit.assessment.kit.application.port.out.questionnaire.UpdateQuestionnairePort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.kit.common.ErrorMessageKey.KIT_VERSION_ID_NOT_FOUND;
+import static org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother.simpleKit;
+import static org.flickit.assessment.kit.test.fixture.application.KitVersionMother.createKitVersion;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class UpdateQuestionnaireOrdersServiceTest {
+
+    @InjectMocks
+    private UpdateQuestionnaireOrdersService service;
+
+    @Mock
+    private LoadKitVersionPort loadKitVersionPort;
+
+    @Mock
+    private LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
+
+    @Mock
+    private UpdateQuestionnairePort updateQuestionnairePort;
+
+    private final KitVersion kitVersion = createKitVersion(simpleKit());
+
+    @Test
+    void testUpdateQuestionnaireOrdersService_kitVersionIdNotFound_NotFoundException() {
+        var param = createParam(UpdateQuestionnaireOrdersUseCase.Param.ParamBuilder::build);
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenThrow(new ResourceNotFoundException(KIT_VERSION_ID_NOT_FOUND));
+
+        var throwable = assertThrows(ResourceNotFoundException.class, () -> service.changeOrders(param));
+        assertEquals(KIT_VERSION_ID_NOT_FOUND, throwable.getMessage());
+
+        verifyNoInteractions(loadExpertGroupOwnerPort, updateQuestionnairePort);
+    }
+
+    @Test
+    void testUpdateQuestionnaireOrdersService_currentUserIsNotOwner_AccessDeniedException() {
+        var param = createParam(UpdateQuestionnaireOrdersUseCase.Param.ParamBuilder::build);
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(UUID.randomUUID());
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.changeOrders(param));
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+
+        verifyNoInteractions(updateQuestionnairePort);
+    }
+
+    @Test
+    void testUpdateQuestionnaireOrdersService_validParameters_SuccessfulUpdate() {
+        var param = createParam(UpdateQuestionnaireOrdersUseCase.Param.ParamBuilder::build);
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(param.getCurrentUserId());
+
+        service.changeOrders(param);
+
+        var questionnaireOrders = List.of(
+            new QuestionnaireOrder(123L, 3),
+            new QuestionnaireOrder(456L, 4)
+        );
+        verify(updateQuestionnairePort, times(1))
+            .updateOrders(questionnaireOrders, param.getKitVersionId(), param.getCurrentUserId());
+    }
+
+    private UpdateQuestionnaireOrdersUseCase.Param createParam(Consumer<UpdateQuestionnaireOrdersUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        paramBuilder.build();
+        return paramBuilder.build();
+    }
+
+    private UpdateQuestionnaireOrdersUseCase.Param.ParamBuilder paramBuilder() {
+        return UpdateQuestionnaireOrdersUseCase.Param.builder()
+            .kitVersionId(1L)
+            .orders(List.of(
+                new UpdateQuestionnaireOrdersUseCase.QuestionnaireParam(123L, 3),
+                new UpdateQuestionnaireOrdersUseCase.QuestionnaireParam(124L, 2)))
+            .currentUserId(UUID.randomUUID());
+    }
+}
