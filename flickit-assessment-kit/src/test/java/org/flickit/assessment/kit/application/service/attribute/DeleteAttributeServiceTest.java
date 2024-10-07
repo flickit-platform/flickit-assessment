a5f1837818716a3dc37f41394a748c512fd69685
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-10-07 13:12:11 +0330

    FAC-658 Impl delete attribute service & its tests

---
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/attribute/DeleteAttributeServiceTest.java
@@ -0,0 +1,96 @@
+package org.flickit.assessment.kit.application.service.attribute;
+
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.kit.application.domain.KitVersion;
+import org.flickit.assessment.kit.application.port.in.attribute.DeleteAttributeUseCase;
+import org.flickit.assessment.kit.application.port.out.attribute.DeleteAttributePort;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother.simpleKit;
+import static org.flickit.assessment.kit.test.fixture.application.KitVersionMother.createActiveKitVersion;
+import static org.flickit.assessment.kit.test.fixture.application.KitVersionMother.createKitVersion;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteAttributeServiceTest {
+
+    @InjectMocks
+    private DeleteAttributeService service;
+
+    @Mock
+    private LoadKitVersionPort loadKitVersionPort;
+
+    @Mock
+    private LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
+
+    @Mock
+    private DeleteAttributePort deleteAttributePort;
+
+    UUID ownerId = UUID.randomUUID();
+
+    private final KitVersion kitVersion = createKitVersion(simpleKit());
+
+    @Test
+    void testDeleteAttribute_WhenCurrentUserIsNotOwnerOfExpertGroup_ThenThrowAccessDeniedException() {
+        var param = createParam(DeleteAttributeUseCase.Param.ParamBuilder::build);
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.deleteAttribute(param));
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+
+        verifyNoInteractions(deleteAttributePort);
+    }
+
+    @Test
+    void testDeleteAttribute_WhenCurrentUserIsOwnerOfExpertGroupAndKitVersionStatusIsNotUpdating_ThenThrowAccessDeniedException() {
+        var param = createParam(b -> b.currentUserId(ownerId));
+        KitVersion kitVersion = createActiveKitVersion(simpleKit());
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.deleteAttribute(param));
+
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+        verifyNoInteractions(deleteAttributePort);
+    }
+
+    @Test
+    void testDeleteAttribute_WhenCurrentUserIsOwnerOfExpertGroupAndKitVersionStatusIsUpdating_ThenDeleteAttribute() {
+        var param = createParam(b -> b.currentUserId(ownerId));
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
+        doNothing().when(deleteAttributePort).delete(param.getKitVersionId(), param.getAttributeId());
+
+        assertDoesNotThrow(() -> service.deleteAttribute(param));
+
+        verify(deleteAttributePort).delete(param.getKitVersionId(), param.getAttributeId());
+    }
+
+    private DeleteAttributeUseCase.Param createParam(Consumer<DeleteAttributeUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        return paramBuilder.build();
+    }
+
+    private DeleteAttributeUseCase.Param.ParamBuilder paramBuilder() {
+        return DeleteAttributeUseCase.Param.builder()
+            .kitVersionId(1L)
+            .attributeId(2L)
+            .currentUserId(UUID.randomUUID());
+    }
+}
