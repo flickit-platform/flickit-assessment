b1834fa132a8f3df722fe750b889cc596dd031a9
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-10-07 15:02:58 +0330

    FAC-670 Impl delete questionnaire service & its tests

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/questionnaire/DeleteQuestionnaireService.java
@@ -0,0 +1,37 @@
+package org.flickit.assessment.kit.application.service.questionnaire;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.kit.application.domain.KitVersion;
+import org.flickit.assessment.kit.application.domain.KitVersionStatus;
+import org.flickit.assessment.kit.application.port.in.questionnaire.DeleteQuestionnaireUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
+import org.flickit.assessment.kit.application.port.out.questionnaire.DeleteQuestionnairePort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class DeleteQuestionnaireService implements DeleteQuestionnaireUseCase {
+
+    private final LoadKitVersionPort loadKitVersionPort;
+    private final LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
+    private final DeleteQuestionnairePort deleteQuestionnairePort;
+
+    @Override
+    public void deleteQuestionnaire(Param param) {
+        KitVersion kitVersion = loadKitVersionPort.load(param.getKitVersionId());
+        UUID ownerId = loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId());
+        if (!ownerId.equals(param.getCurrentUserId()) ||
+            !KitVersionStatus.UPDATING.equals(kitVersion.getStatus()))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+
+        deleteQuestionnairePort.delete(param.getKitVersionId(), param.getQuestionnaireId());
+    }
+}
