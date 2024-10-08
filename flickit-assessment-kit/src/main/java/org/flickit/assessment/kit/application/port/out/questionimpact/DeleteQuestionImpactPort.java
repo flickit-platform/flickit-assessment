1dc61a16c2cc17a4c4f789b703a63a7cd34b97bd
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-08 16:21:00 +0330

    FAC-676: feat - Add `deleteByIdAndKitVersionId` function to `DeleteQuestionImpactPort` and implement it

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/questionimpact/DeleteQuestionImpactPort.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/questionimpact/DeleteQuestionImpactPort.java
@@ -3,4 +3,6 @@ package org.flickit.assessment.kit.application.port.out.questionimpact;
 public interface DeleteQuestionImpactPort {
 
     void delete(Long id);
+
+    void deleteByIdAndKitVersionId(Long id, Long kitVersionId);
 }
