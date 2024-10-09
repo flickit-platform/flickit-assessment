551d2ba5ead2a2457faf6bdcad3a4c1507571d3f
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-09 09:47:28 +0330

    FAC-677: feat - Some Features to `Service` and `ServiceTest`

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/answeroption/DeleteAnswerOptionService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/answeroption/DeleteAnswerOptionService.java
@@ -1,11 +1,14 @@
 package org.flickit.assessment.kit.application.service.answeroption;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.kit.application.port.in.answeroption.DeleteAnswerOptionUseCase;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
 @Service
 @Transactional
@@ -17,6 +20,10 @@ public class DeleteAnswerOptionService implements DeleteAnswerOptionUseCase {
 
     @Override
     public void delete(Param param) {
+        var kitVersion = loadKitVersionPort.load(param.getKitVersionId());
+        var expertGroupOwnerId = loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId());
 
+        if (!expertGroupOwnerId.equals(param.getCurrentUserId()))
+            throw  new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
     }
 }
--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/answeroption/DeleteAnswerOptionServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/answeroption/DeleteAnswerOptionServiceTest.java
@@ -1,13 +1,26 @@
 package org.flickit.assessment.kit.application.service.answeroption;
 
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.kit.application.port.in.answeroption.DeleteAnswerOptionUseCase;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
+import org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother;
+import org.flickit.assessment.kit.test.fixture.application.KitVersionMother;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.kit.common.ErrorMessageKey.KIT_VERSION_ID_NOT_FOUND;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class DeleteAnswerOptionServiceTest {
@@ -21,4 +34,47 @@ class DeleteAnswerOptionServiceTest {
     @Mock
     private LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
 
+    @Test
+    void deleteAnswerOptionServiceTest_kitVersionIdNotExists_shouldThrowResourceNotFoundException() {
+        var param = createParam(DeleteAnswerOptionUseCase.Param.ParamBuilder::build);
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenThrow(new ResourceNotFoundException(KIT_VERSION_ID_NOT_FOUND));
+
+        var throwable = assertThrows(ResourceNotFoundException.class, () -> service.delete(param));
+
+
+        assertEquals(KIT_VERSION_ID_NOT_FOUND, throwable.getMessage());
+
+        verify(loadKitVersionPort, times(1)).load(param.getKitVersionId());
+        verifyNoMoreInteractions(loadExpertGroupOwnerPort);
+    }
+
+    @Test
+    void deleteAnswerOptionServiceTest_currentUserIsNotExpertGroupOwner_shouldThrowAccessDeniedException() {
+        var param = createParam(DeleteAnswerOptionUseCase.Param.ParamBuilder::build);
+        var kitVersion = KitVersionMother.createKitVersion(AssessmentKitMother.simpleKit());
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(UUID.randomUUID());
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.delete(param));
+
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+
+        verify(loadKitVersionPort, times(1)).load(param.getKitVersionId());
+        verify(loadExpertGroupOwnerPort, times(1)).loadOwnerId(kitVersion.getKit().getExpertGroupId());
+    }
+
+    private DeleteAnswerOptionUseCase.Param createParam(Consumer<DeleteAnswerOptionUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        return paramBuilder.build();
+    }
+
+    private DeleteAnswerOptionUseCase.Param.ParamBuilder paramBuilder() {
+        return DeleteAnswerOptionUseCase.Param.builder()
+            .answerOptionId(1L)
+            .kitVersionId(2L)
+            .currentUserId(UUID.randomUUID());
+    }
 }
