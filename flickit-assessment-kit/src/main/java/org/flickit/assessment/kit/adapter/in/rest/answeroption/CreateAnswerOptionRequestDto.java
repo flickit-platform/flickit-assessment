a2a7343e6e94a3e102636cac567be4d8f5c4bbb7
mahdirezazadeh <riza.rs2000@gmail.com>
2024-10-09 13:07:28 +0330

    FAC-672:feature:Impl CreateAnswerOptionRestController

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/answeroption/CreateAnswerOptionRequestDto.java
@@ -0,0 +1,6 @@
+package org.flickit.assessment.kit.adapter.in.rest.answeroption;
+
+public record CreateAnswerOptionRequestDto(Long questionId,
+                                           Integer index,
+                                           String title) {
+}
