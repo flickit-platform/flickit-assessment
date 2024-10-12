dc62f6103f1f544ee8f643223f8c079d119039db
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-12 14:17:46 +0330

    FAC-683: feat - Add ConstrainsChecking to `Service` with their tests

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/question/GetQuestionImpactsService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/question/GetQuestionImpactsService.java
@@ -1,17 +1,29 @@
 package org.flickit.assessment.kit.application.service.question;
 
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.kit.application.port.in.question.GetQuestionImpactsUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroupaccess.CheckExpertGroupAccessPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+
 @Service
 @Transactional(readOnly = true)
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class GetQuestionImpactsService implements GetQuestionImpactsUseCase {
+
+    private final LoadKitVersionPort loadKitVersionPort;
+    private final CheckExpertGroupAccessPort checkExpertGroupAccessPort;
 
     @Override
     public Result getQuestionImpacts(Param param) {
+        var kitVersionId = loadKitVersionPort.load(param.getKitVersionId());
+        if (!checkExpertGroupAccessPort.checkIsMember(kitVersionId.getKit().getExpertGroupId(), param.getCurrentUserId()))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+
         return null;
     }
 }
--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/question/GetQuestionImpactsServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/question/GetQuestionImpactsServiceTest.java
@@ -1,11 +1,79 @@
 package org.flickit.assessment.kit.application.service.question;
 
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.kit.application.port.in.question.GetQuestionImpactsUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroupaccess.CheckExpertGroupAccessPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
+import org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother;
+import org.flickit.assessment.kit.test.fixture.application.KitVersionMother;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
+import java.util.function.Consumer;
 
- @ExtendWith(MockitoExtension.class)
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.kit.common.ErrorMessageKey.KIT_VERSION_ID_NOT_FOUND;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class GetQuestionImpactsServiceTest {
 
+    @InjectMocks
+    GetQuestionImpactsService service;
+
+    @Mock
+    private LoadKitVersionPort loadKitVersionPort;
+
+    @Mock
+    private CheckExpertGroupAccessPort checkExpertGroupAccessPort;
+
+    @Test
+    void testGetQuestionImpactsService_kitVersionIdDoesNotExist_throwsResourceNotFoundException() {
+        var param = createParam(GetQuestionImpactsUseCase.Param.ParamBuilder::build);
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenThrow(new ResourceNotFoundException(KIT_VERSION_ID_NOT_FOUND));
+
+        var throwable = assertThrows(ResourceNotFoundException.class, () -> service.getQuestionImpacts(param));
+
+        assertEquals(KIT_VERSION_ID_NOT_FOUND, throwable.getMessage());
+
+        verify(loadKitVersionPort, times(1)).load(param.getKitVersionId());
+        verifyNoInteractions(checkExpertGroupAccessPort);
+    }
+
+    @Test
+    void testGetQuestionImpactsService_currentUserIsNotExpertGroupMember_throwsAccessDeniedException() {
+        var param = createParam(GetQuestionImpactsUseCase.Param.ParamBuilder::build);
+        var assessmentKit = AssessmentKitMother.simpleKit();
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(KitVersionMother.createKitVersion(assessmentKit));
+        when(checkExpertGroupAccessPort.checkIsMember(assessmentKit.getExpertGroupId(), param.getCurrentUserId())).thenReturn(false);
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.getQuestionImpacts(param));
+
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+
+        verify(loadKitVersionPort, times(1)).load(param.getKitVersionId());
+        verify(checkExpertGroupAccessPort, times(1)).checkIsMember(assessmentKit.getExpertGroupId(), param.getCurrentUserId());
+    }
+
+    private GetQuestionImpactsUseCase.Param createParam(Consumer<GetQuestionImpactsUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        return paramBuilder.build();
+    }
+
+    private GetQuestionImpactsUseCase.Param.ParamBuilder paramBuilder() {
+        return GetQuestionImpactsUseCase.Param.builder()
+            .questionId(1L)
+            .kitVersionId(2L)
+            .currentUserId(UUID.randomUUID());
+    }
 }
