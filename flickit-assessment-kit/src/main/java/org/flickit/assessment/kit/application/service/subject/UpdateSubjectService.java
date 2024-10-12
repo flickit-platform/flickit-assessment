c7d652fc8babab27dafbaa745ed7c3249311b848
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-10-12 11:47:56 +0330

    FAC-526 Use LoadKitVersionPort instead of LoadAssessmentKitPort & fix tests

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/subject/UpdateSubjectService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/subject/UpdateSubjectService.java
@@ -3,11 +3,11 @@ package org.flickit.assessment.kit.application.service.subject;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.kit.application.domain.AssessmentKit;
+import org.flickit.assessment.kit.application.domain.KitVersion;
 import org.flickit.assessment.kit.application.domain.Subject;
 import org.flickit.assessment.kit.application.port.in.subject.UpdateSubjectUseCase;
-import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
 import org.flickit.assessment.kit.application.port.out.subject.UpdateSubjectPort;
 import org.springframework.stereotype.Service;
 
@@ -22,21 +22,19 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT
 public class UpdateSubjectService implements UpdateSubjectUseCase {
 
     private final UpdateSubjectPort updateSubjectPort;
-    private final LoadAssessmentKitPort loadAssessmentKitPort;
+    private final LoadKitVersionPort loadKitVersionPort;
     private final LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
 
     @Override
     public void updateSubject(Param param) {
-        AssessmentKit kit = loadAssessmentKitPort.load(param.getKitId());
-        Long kitVersionId = kit.getActiveVersionId();
-        long expertGroupId = kit.getExpertGroupId();
-        UUID ownerId = loadExpertGroupOwnerPort.loadOwnerId(expertGroupId);
+        KitVersion kitVersion = loadKitVersionPort.load(param.getKitVersionId());
+        UUID ownerId = loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId());
         if (!ownerId.equals(param.getCurrentUserId()))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
         String code = Subject.generateSlugCode(param.getTitle());
         updateSubjectPort.update(new UpdateSubjectPort.Param(param.getSubjectId(),
-            kitVersionId,
+            param.getKitVersionId(),
             code,
             param.getTitle(),
             param.getIndex(),
---
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/subject/UpdateSubjectServiceTest.java
@@ -0,0 +1,87 @@
+package org.flickit.assessment.kit.application.service.subject;
+
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.kit.application.domain.KitVersion;
+import org.flickit.assessment.kit.application.port.in.subject.UpdateSubjectUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
+import org.flickit.assessment.kit.application.port.out.subject.UpdateSubjectPort;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother.simpleKit;
+import static org.flickit.assessment.kit.test.fixture.application.KitVersionMother.createKitVersion;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UpdateSubjectServiceTest {
+
+    @InjectMocks
+    private UpdateSubjectService service;
+
+    @Mock
+    private UpdateSubjectPort updateSubjectPort;
+
+    @Mock
+    private LoadKitVersionPort loadKitVersionPort;
+
+    @Mock
+    private LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
+
+    private final UUID ownerId = UUID.randomUUID();
+
+    private final KitVersion kitVersion = createKitVersion(simpleKit());
+
+    @Test
+    void testUpdateSubject_WhenCurrentUserIsNotExpertGroupOwner_ThenThrowAccessDeniedException() {
+        UpdateSubjectUseCase.Param param = createParam(UpdateSubjectUseCase.Param.ParamBuilder::build);
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.updateSubject(param));
+
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+        verifyNoInteractions(updateSubjectPort);
+    }
+
+    @Test
+    void testUpdateSubject_WhenCurrentUserIsExpertGroupOwnerOwner_ThenUpdateSubject() {
+        UpdateSubjectUseCase.Param param = createParam(b -> b.currentUserId(ownerId));
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
+        doNothing().when(updateSubjectPort).update(any());
+
+        service.updateSubject(param);
+
+        verify(updateSubjectPort).update(any(UpdateSubjectPort.Param.class));
+    }
+
+    private UpdateSubjectUseCase.Param createParam(Consumer<UpdateSubjectUseCase.Param.ParamBuilder> changer) {
+        var param = paramBuilder();
+        changer.accept(param);
+        return param.build();
+    }
+
+    private UpdateSubjectUseCase.Param.ParamBuilder paramBuilder() {
+        return UpdateSubjectUseCase.Param.builder()
+            .kitVersionId(1L)
+            .subjectId(1L)
+            .index(1)
+            .title("subject title")
+            .description("subject description")
+            .weight(1)
+            .currentUserId(UUID.randomUUID());
+    }
+}
