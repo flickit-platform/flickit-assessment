0b5c17bb205eaf6e0a82471493d827d26d518d72
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-10-13 19:27:24 +0330

    FAC-690 Impl delete kit version service & its tests

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/kitversion/DeleteKitVersionService.java
@@ -0,0 +1,34 @@
+package org.flickit.assessment.kit.application.service.kitversion;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.kit.application.domain.KitVersionStatus;
+import org.flickit.assessment.kit.application.port.in.kitversion.DeleteKitVersionUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.DeleteKitVersionPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class DeleteKitVersionService implements DeleteKitVersionUseCase {
+
+    private final LoadKitVersionPort loadKitVersionPort;
+    private final LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
+    private final DeleteKitVersionPort deleteKitVersionPort;
+
+    @Override
+    public void deleteKitVersion(Param param) {
+        var kitVersion = loadKitVersionPort.load(param.getKitVersionId());
+        var expertGroupOwnerId = loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId());
+        if (!expertGroupOwnerId.equals(param.getCurrentUserId()) ||
+            !KitVersionStatus.UPDATING.equals(kitVersion.getStatus()))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+
+        deleteKitVersionPort.deleteKitVersion(param.getKitVersionId());
+    }
+}
