f03d86d325fecb1497daaffeca8adf0324329532
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-13 15:18:58 +0330

    FAC-685: feat - Add `updateOrders` to `UpdateAnswerOptionPort` and Implement It in `AnswerOptionJpaAdapter`

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/answeroption/AnswerOptionPersistenceJpaAdapter.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/answeroption/AnswerOptionPersistenceJpaAdapter.java
@@ -1,14 +1,23 @@
 package org.flickit.assessment.kit.adapter.out.persistence.answeroption;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.data.jpa.kit.answeroption.AnswerOptionJpaEntity;
 import org.flickit.assessment.data.jpa.kit.answeroption.AnswerOptionJpaRepository;
 import org.flickit.assessment.kit.application.domain.AnswerOption;
+import org.flickit.assessment.kit.application.domain.AnswerOptionOrder;
 import org.flickit.assessment.kit.application.port.out.answeroption.CreateAnswerOptionPort;
 import org.flickit.assessment.kit.application.port.out.answeroption.LoadAnswerOptionsByQuestionPort;
 import org.flickit.assessment.kit.application.port.out.answeroption.UpdateAnswerOptionPort;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static org.flickit.assessment.kit.common.ErrorMessageKey.ANSWER_OPTION_ID_NOT_FOUND;
 
 @Component
 @RequiredArgsConstructor
@@ -26,6 +35,26 @@ public class AnswerOptionPersistenceJpaAdapter implements
             param.title(),
             param.lastModificationTime(),
             param.lastModifiedBy());
+    }
+
+    @Override
+    public void updateOrders(List<AnswerOptionOrder> answerOptionOrders, Long kitVersionId, UUID lastModifiedBy) {
+        Map<AnswerOptionJpaEntity.EntityId, AnswerOptionOrder> idToModel = answerOptionOrders.stream()
+            .collect(Collectors.toMap(
+                ao -> new AnswerOptionJpaEntity.EntityId(ao.getId(), kitVersionId),
+                ao -> ao
+            ));
+        List<AnswerOptionJpaEntity> entities = repository.findAllById(idToModel.keySet());
+        if (entities.size() != answerOptionOrders.size())
+            throw new ResourceNotFoundException(ANSWER_OPTION_ID_NOT_FOUND);
+
+        entities.forEach(x -> {
+            AnswerOptionOrder newLevel = idToModel.get(new AnswerOptionJpaEntity.EntityId(x.getId(), kitVersionId));
+            x.setIndex(newLevel.getIndex());
+            x.setLastModificationTime(LocalDateTime.now());
+            x.setLastModifiedBy(lastModifiedBy);
+        });
+        repository.saveAll(entities);
     }
 
     @Override
--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/answeroption/UpdateAnswerOptionPort.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/answeroption/UpdateAnswerOptionPort.java
@@ -1,11 +1,16 @@
 package org.flickit.assessment.kit.application.port.out.answeroption;
 
+import org.flickit.assessment.kit.application.domain.AnswerOptionOrder;
+
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface UpdateAnswerOptionPort {
 
     void update(Param param);
+
+    void updateOrders(List<AnswerOptionOrder> answerOptionOrders, Long kitVersionId, UUID lastModifiedBy);
 
     record Param(
         Long id,
