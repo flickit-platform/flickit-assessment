03b1dbeaa67a1225b24282ec8df64b48419d8bd5
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-10-15 13:42:25 +0330

    FAC-652: Correct API URL

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/questionnaire/UpdateQuestionnaireOrdersRestController.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/questionnaire/UpdateQuestionnaireOrdersRestController.java
@@ -20,7 +20,7 @@ public class UpdateQuestionnaireOrdersRestController {
     private final UpdateQuestionnaireOrdersUseCase useCase;
     private final UserContext userContext;
 
-    @PutMapping("/kit-versions/{kitVersionId}/questionnaires-change-orders")
+    @PutMapping("/kit-versions/{kitVersionId}/questionnaires-change-order")
     ResponseEntity<Void> changeMaturityLevelOrders(@PathVariable("kitVersionId") Long kitVersionId,
                                                    @RequestBody UpdateQuestionnaireOrdersRequestDto requestDto) {
         var currentUserId = userContext.getUser().id();
