4e810598d3c08123119aa35e5778af3a800a3a7b
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-10-16 12:03:22 +0330

    FAC-541 Use 'LoadKitVersionPort' instead of 'LoadAssessmentKitPort'

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/question/CreateQuestionServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/question/CreateQuestionServiceTest.java
@@ -1,24 +1,26 @@
 package org.flickit.assessment.kit.application.service.question;
 
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.kit.application.domain.AssessmentKit;
+import org.flickit.assessment.kit.application.domain.KitVersion;
 import org.flickit.assessment.kit.application.port.in.question.CreateQuestionUseCase;
-import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
 import org.flickit.assessment.kit.application.port.out.question.CreateQuestionPort;
-import org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
+import java.util.function.Consumer;
 
+import static org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother.simpleKit;
+import static org.flickit.assessment.kit.test.fixture.application.KitVersionMother.createKitVersion;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CreateQuestionServiceTest {
@@ -33,67 +35,64 @@ class CreateQuestionServiceTest {
     private LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
 
     @Mock
-    private LoadAssessmentKitPort loadAssessmentKitPort;
+    private LoadKitVersionPort loadKitVersionPort;
 
-    private int index;
-    private String title;
-    private String hint;
-    private Boolean mayNotBeApplicable;
-    private Boolean advisable;
-    private UUID currentUserId;
-    private UUID ownerId;
-    private Long questionnaireId;
-    private AssessmentKit kit;
+    private final UUID ownerId = UUID.randomUUID();
 
-    @BeforeEach
-    void setUp() {
-        index = 1;
-        title = "question title";
-        hint = "question hint";
-        mayNotBeApplicable = true;
-        advisable = true;
-        currentUserId = UUID.randomUUID();
-        ownerId = UUID.randomUUID();
-        questionnaireId = 1L;
-        kit = AssessmentKitMother.simpleKit();
-    }
+    private final KitVersion kitVersion = createKitVersion(simpleKit());
 
     @Test
-    void testCreateQuestion__WhenCurrentUserIsNotOwner_ShouldThrowAccessDeniedException() {
-        CreateQuestionUseCase.Param param = new CreateQuestionUseCase.Param(kit.getId(),
-            index,
-            title,
-            hint,
-            mayNotBeApplicable,
-            advisable,
-            questionnaireId,
-            currentUserId);
+    void testCreateQuestionService_WhenCurrentUserIsNotExpertGroupOwner_ThenThrowAccessDeniedException() {
+        CreateQuestionUseCase.Param param = createParam(CreateQuestionUseCase.Param.ParamBuilder::build);
 
-        when(loadAssessmentKitPort.load(param.getKitId())).thenReturn(kit);
-        when(loadExpertGroupOwnerPort.loadOwnerId(kit.getExpertGroupId())).thenReturn(ownerId);
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
 
         assertThrows(AccessDeniedException.class, () -> createQuestionService.createQuestion(param));
+
+        verifyNoInteractions(createQuestionPort);
     }
 
     @Test
-    void testCreateQuestion_WhenCurrentUserIsOwner_ThenCreateQuestion() {
-        long questionId = 1;
-        ownerId = currentUserId;
-        CreateQuestionUseCase.Param param = new CreateQuestionUseCase.Param(kit.getId(),
-            index,
-            title,
-            hint,
-            mayNotBeApplicable,
-            advisable,
-            questionnaireId,
-            currentUserId);
+    void testCreateQuestionService_WhenCurrentUserIsExpertGroupOwner_ThenCreateQuestion() {
+        long questionId = 1L;
+        CreateQuestionUseCase.Param param = createParam(b -> b.currentUserId(ownerId));
 
-        when(loadAssessmentKitPort.load(param.getKitId())).thenReturn(kit);
-        when(loadExpertGroupOwnerPort.loadOwnerId(kit.getExpertGroupId())).thenReturn(ownerId);
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
         when(createQuestionPort.persist(any(CreateQuestionPort.Param.class))).thenReturn(questionId);
 
         long actualQuestionId = createQuestionService.createQuestion(param);
 
         assertEquals(questionId, actualQuestionId);
+        var outPortParam = ArgumentCaptor.forClass(CreateQuestionPort.Param.class);
+        verify(createQuestionPort).persist(outPortParam.capture());
+        assertNotNull(outPortParam.getValue());
+        assertEquals(param.getKitVersionId(), outPortParam.getValue().kitVersionId());
+        assertEquals(param.getIndex(), outPortParam.getValue().index());
+        assertEquals(param.getTitle(), outPortParam.getValue().title());
+        assertEquals(param.getHint(), outPortParam.getValue().hint());
+        assertEquals(param.getMayNotBeApplicable(), outPortParam.getValue().mayNotBeApplicable());
+        assertEquals(param.getAdvisable(), outPortParam.getValue().advisable());
+        assertEquals(param.getQuestionnaireId(), outPortParam.getValue().questionnaireId());
+        assertEquals(param.getCurrentUserId(), outPortParam.getValue().createdBy());
+    }
+
+    private CreateQuestionUseCase.Param createParam(Consumer<CreateQuestionUseCase.Param.ParamBuilder> changer) {
+        var param = paramBuilder();
+        changer.accept(param);
+        return param.build();
+    }
+
+    private CreateQuestionUseCase.Param.ParamBuilder paramBuilder() {
+        return CreateQuestionUseCase.Param.builder()
+            .kitVersionId(1L)
+            .index(1)
+            .title("abc")
+            .hint("new hint")
+            .mayNotBeApplicable(true)
+            .advisable(false)
+            .questionnaireId(1L)
+            .currentUserId(UUID.randomUUID());
     }
 }
