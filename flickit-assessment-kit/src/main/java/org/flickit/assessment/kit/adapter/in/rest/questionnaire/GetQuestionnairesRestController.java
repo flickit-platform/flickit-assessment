45ac4bfe8d31b588a52813447528bf11339c069f
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-10-19 09:25:27 +0330

    FAC-657 Use GetQuestionnairesResponseDto as output of controller

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/questionnaire/GetQuestionnairesRestController.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/questionnaire/GetQuestionnairesRestController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
 import org.flickit.assessment.common.config.jwt.UserContext;
 import org.flickit.assessment.kit.application.port.in.questionnaire.GetQuestionnairesUseCase;
-import org.flickit.assessment.kit.application.port.in.questionnaire.GetQuestionnairesUseCase.QuestionnaireListItem;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,12 +22,27 @@ public class GetQuestionnairesRestController {
     private final UserContext userContext;
 
     @GetMapping("/kit-versions/{kitVersionId}/questionnaires")
-    public ResponseEntity<PaginatedResponse<QuestionnaireListItem>> getQuestionnaires(@PathVariable("kitVersionId") Long kitVersionId,
+    public ResponseEntity<PaginatedResponse<GetQuestionnairesResponseDto>> getQuestionnaires(@PathVariable("kitVersionId") Long kitVersionId,
                                                                                       @RequestParam(defaultValue = "0") int page,
                                                                                       @RequestParam(defaultValue = "20") int size) {
         UUID currentUserId = userContext.getUser().id();
         var questionnaires = useCase.getQuestionnaires(toParam(kitVersionId, currentUserId, page, size));
-        return new ResponseEntity<>(questionnaires, HttpStatus.OK);
+        List<GetQuestionnairesResponseDto> items = questionnaires.getItems().stream()
+            .map(e -> new GetQuestionnairesResponseDto(e.questionnaire().getId(),
+                e.questionnaire().getTitle(),
+                e.questionnaire().getIndex(),
+                e.questionnaire().getDescription(),
+                e.questionsCount()))
+            .toList();
+
+        var response = new PaginatedResponse<>(items,
+            questionnaires.getPage(),
+            questionnaires.getSize(),
+            questionnaires.getSort(),
+            questionnaires.getOrder(),
+            questionnaires.getTotal());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     private static GetQuestionnairesUseCase.Param toParam(Long kitVersionId, UUID currentUserId, int page, int size) {
