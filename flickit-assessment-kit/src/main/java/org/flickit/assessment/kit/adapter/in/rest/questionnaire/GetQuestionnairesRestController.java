f746d7330e0c3b9d901ee1af2fc601a255d5939f
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-10-19 12:28:12 +0330

    FAC-657: Extract method

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/questionnaire/GetQuestionnairesRestController.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/questionnaire/GetQuestionnairesRestController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
 import org.flickit.assessment.common.config.jwt.UserContext;
 import org.flickit.assessment.kit.application.port.in.questionnaire.GetQuestionnairesUseCase;
+import org.flickit.assessment.kit.application.port.in.questionnaire.GetQuestionnairesUseCase.QuestionnaireListItem;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +29,17 @@ public class GetQuestionnairesRestController {
                                                                                       @RequestParam(defaultValue = "20") int size) {
         UUID currentUserId = userContext.getUser().id();
         var questionnaires = useCase.getQuestionnaires(toParam(kitVersionId, currentUserId, page, size));
+        var response = toResponse(questionnaires);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    private static GetQuestionnairesUseCase.Param toParam(Long kitVersionId, UUID currentUserId, int page, int size) {
+        return new GetQuestionnairesUseCase.Param(kitVersionId, currentUserId, page, size);
+    }
+
+    @NotNull
+    private static PaginatedResponse<GetQuestionnairesResponseDto> toResponse(PaginatedResponse<QuestionnaireListItem> questionnaires) {
         List<GetQuestionnairesResponseDto> items = questionnaires.getItems().stream()
             .map(e -> new GetQuestionnairesResponseDto(e.questionnaire().getId(),
                 e.questionnaire().getTitle(),
@@ -35,17 +48,11 @@ public class GetQuestionnairesRestController {
                 e.questionsCount()))
             .toList();
 
-        var response = new PaginatedResponse<>(items,
+        return new PaginatedResponse<>(items,
             questionnaires.getPage(),
             questionnaires.getSize(),
             questionnaires.getSort(),
             questionnaires.getOrder(),
             questionnaires.getTotal());
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    private static GetQuestionnairesUseCase.Param toParam(Long kitVersionId, UUID currentUserId, int page, int size) {
-        return new GetQuestionnairesUseCase.Param(kitVersionId, currentUserId, page, size);
     }
 }
