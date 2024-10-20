d6ecfb2dfd398c8142e23d373111533ba9fba7ec
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-10-20 13:23:56 +0330

    FAC-673: Minor format

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/questionimpact/UpdateQuestionImpactPort.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/questionimpact/UpdateQuestionImpactPort.java
@@ -7,22 +7,22 @@ public interface UpdateQuestionImpactPort {
 
     void updateWeight(UpdateWeightParam param);
 
-    record UpdateWeightParam(
-        Long id,
-        Long kitVersionId,
-        int weight,
-        Long questionId,
-        LocalDateTime lastModificationTime,
-        UUID lastModifiedBy) {}
+    record UpdateWeightParam(Long id,
+                             Long kitVersionId,
+                             int weight,
+                             Long questionId,
+                             LocalDateTime lastModificationTime,
+                             UUID lastModifiedBy) {
+    }
 
     void update(Param param);
 
-    record Param(
-        Long id,
-        Long kitVersionId,
-        int weight,
-        long attributeId,
-        long maturityLevelId,
-        LocalDateTime lastModificationTime,
-        UUID lastModifiedBy) {}
+    record Param(Long id,
+                 Long kitVersionId,
+                 int weight,
+                 long attributeId,
+                 long maturityLevelId,
+                 LocalDateTime lastModificationTime,
+                 UUID lastModifiedBy) {
+    }
 }
