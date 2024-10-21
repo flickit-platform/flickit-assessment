cea6603dfe69818d1fceede2bb09130f8728dceb
mahdirezazadeh <riza.rs2000@gmail.com>
2024-10-21 16:22:46 +0330

    FAC-48:feature:Rename to DatabaseContainerConnectionTest

--- flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/data/config/DatabaseContainerConnectionTest.java
+++ flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/data/config/DatabaseContainerConnectionTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Testcontainers
-class TestDatabaseContainerConnectionTest {
+class DatabaseContainerConnectionTest {
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
