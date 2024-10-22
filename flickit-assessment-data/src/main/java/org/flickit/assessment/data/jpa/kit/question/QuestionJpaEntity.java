cb88f4617e47549aadf80b08fcde224f39115cb2
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-10-22 19:51:38 +0330

    FAC-691: Remove @GeneratedValue from QuestionJpaEntity

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/question/QuestionJpaEntity.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/question/QuestionJpaEntity.java
@@ -21,8 +21,6 @@ public class QuestionJpaEntity {
 
     @Id
     @EqualsAndHashCode.Include
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fak_question_id_seq")
-    @SequenceGenerator(name = "fak_question_id_seq", sequenceName = "fak_question_id_seq", allocationSize = 1)
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
