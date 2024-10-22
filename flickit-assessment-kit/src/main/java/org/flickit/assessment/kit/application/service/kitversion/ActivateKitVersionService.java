f0e887fe0c6db8a76b942107cafb210b4b1f5170
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-22 11:21:40 +0330

    FAC-702: Feat - Add Feature to Service and ServiceTest Completely

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/kitversion/ActivateKitVersionService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/kitversion/ActivateKitVersionService.java
@@ -3,13 +3,18 @@ package org.flickit.assessment.kit.application.service.kitversion;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.kit.application.domain.KitVersionStatus;
+import org.flickit.assessment.kit.application.domain.SubjectQuestionnaire;
 import org.flickit.assessment.kit.application.port.in.kitversion.ActivateKitVersionUseCase;
 import org.flickit.assessment.kit.application.port.out.assessmentkit.UpdateKitActiveVersionPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
 import org.flickit.assessment.kit.application.port.out.kitversion.UpdateKitVersionStatusPort;
+import org.flickit.assessment.kit.application.port.out.subjectquestionnaire.CreateSubjectQuestionnairePort;
+import org.flickit.assessment.kit.application.port.out.subjectquestionnaire.LoadSubjectQuestionnairePort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
@@ -22,6 +27,8 @@ public class ActivateKitVersionService implements ActivateKitVersionUseCase {
     private final LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
     private final UpdateKitVersionStatusPort updateKitVersionStatusPort;
     private final UpdateKitActiveVersionPort updateKitActiveVersionPort;
+    private final LoadSubjectQuestionnairePort loadSubjectQuestionnairePort;
+    private final CreateSubjectQuestionnairePort createSubjectQuestionnairePort;
 
     @Override
     public void activateKitVersion(Param param) {
@@ -32,7 +39,15 @@ public class ActivateKitVersionService implements ActivateKitVersionUseCase {
 
         if (kit.getActiveVersionId() != null)
             updateKitVersionStatusPort.updateStatus(kit.getActiveVersionId(), KitVersionStatus.ARCHIVE);
+
         updateKitVersionStatusPort.updateStatus(param.getKitVersionId(), KitVersionStatus.ACTIVE);
         updateKitActiveVersionPort.updateActiveVersion(kit.getId(), param.getKitVersionId());
+        var subjectQuestionnaires = loadSubjectQuestionnairePort.extractPairs(param.getKitVersionId())
+            .stream()
+            .collect(Collectors.groupingBy(
+                SubjectQuestionnaire::getQuestionnaireId,
+                Collectors.mapping(SubjectQuestionnaire::getSubjectId, Collectors.toSet())));
+
+        createSubjectQuestionnairePort.persistAll(subjectQuestionnaires, param.getKitVersionId());
     }
 }
--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/kitversion/ActivateKitVersionServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/kitversion/ActivateKitVersionServiceTest.java
@@ -4,19 +4,26 @@ import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.kit.application.domain.AssessmentKit;
 import org.flickit.assessment.kit.application.domain.KitVersion;
 import org.flickit.assessment.kit.application.domain.KitVersionStatus;
+import org.flickit.assessment.kit.application.domain.SubjectQuestionnaire;
 import org.flickit.assessment.kit.application.port.in.kitversion.ActivateKitVersionUseCase.Param;
 import org.flickit.assessment.kit.application.port.out.assessmentkit.UpdateKitActiveVersionPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
 import org.flickit.assessment.kit.application.port.out.kitversion.UpdateKitVersionStatusPort;
+import org.flickit.assessment.kit.application.port.out.subjectquestionnaire.CreateSubjectQuestionnairePort;
+import org.flickit.assessment.kit.application.port.out.subjectquestionnaire.LoadSubjectQuestionnairePort;
 import org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother;
 import org.flickit.assessment.kit.test.fixture.application.KitVersionMother;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
@@ -40,7 +47,19 @@ class ActivateKitVersionServiceTest {
     private UpdateKitVersionStatusPort updateKitVersionStatusPort;
 
     @Mock
+    private LoadSubjectQuestionnairePort loadSubjectQuestionnairePort;
+
+    @Mock
     private UpdateKitActiveVersionPort updateKitActiveVersionPort;
+
+    @Mock
+    private CreateSubjectQuestionnairePort createSubjectQuestionnairePort;
+
+    List<SubjectQuestionnaire> subjectQuestionnaireList = List.of(
+        new SubjectQuestionnaire(null, 11L, 123L),
+        new SubjectQuestionnaire(null, 21L, 123L),
+        new SubjectQuestionnaire(null, 31L, 456L)
+    );
 
     @Test
     void testActivateKitVersion_userHasNotAccess_ThrowsException() {
@@ -55,7 +74,7 @@ class ActivateKitVersionServiceTest {
 
         var exception = assertThrows(AccessDeniedException.class, () -> service.activateKitVersion(param));
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, exception.getMessage());
-        verifyNoInteractions(updateKitVersionStatusPort, updateKitActiveVersionPort);
+        verifyNoInteractions(updateKitVersionStatusPort, updateKitActiveVersionPort, createSubjectQuestionnairePort);
     }
 
     @Test
@@ -74,8 +93,13 @@ class ActivateKitVersionServiceTest {
             .updateStatus(param.getKitVersionId(), KitVersionStatus.ACTIVE);
         doNothing().when(updateKitActiveVersionPort)
             .updateActiveVersion(kit.getId(), param.getKitVersionId());
+        when(loadSubjectQuestionnairePort.extractPairs(param.getKitVersionId()))
+            .thenReturn(subjectQuestionnaireList);
 
         service.activateKitVersion(param);
+
+        @SuppressWarnings("unchecked")
+        ArgumentCaptor<Map<Long, Set<Long>>> captor = ArgumentCaptor.forClass(Map.class);
 
         verify(updateKitVersionStatusPort, times(1))
             .updateStatus(kit.getActiveVersionId(), KitVersionStatus.ARCHIVE);
@@ -83,6 +107,13 @@ class ActivateKitVersionServiceTest {
             .updateStatus(param.getKitVersionId(), KitVersionStatus.ACTIVE);
         verify(updateKitActiveVersionPort, times(1))
             .updateActiveVersion(kit.getId(), param.getKitVersionId());
+        verify(createSubjectQuestionnairePort, times(1))
+            .persistAll(captor.capture(), anyLong());
+
+        assertEquals(2, captor.getValue().size());
+        assertEquals(2, captor.getValue().get(123L).size());
+        assertEquals(1, captor.getValue().get(456L).size());
+
     }
 
     @Test
@@ -99,12 +130,23 @@ class ActivateKitVersionServiceTest {
             .updateStatus(param.getKitVersionId(), KitVersionStatus.ACTIVE);
         doNothing().when(updateKitActiveVersionPort)
             .updateActiveVersion(kit.getId(), param.getKitVersionId());
+        when(loadSubjectQuestionnairePort.extractPairs(param.getKitVersionId()))
+            .thenReturn(subjectQuestionnaireList);
 
         service.activateKitVersion(param);
+
+        @SuppressWarnings("unchecked")
+        ArgumentCaptor<Map<Long, Set<Long>>> captor = ArgumentCaptor.forClass(Map.class);
 
         verify(updateKitVersionStatusPort, times(1))
             .updateStatus(param.getKitVersionId(), KitVersionStatus.ACTIVE);
         verify(updateKitActiveVersionPort, times(1))
             .updateActiveVersion(kit.getId(), param.getKitVersionId());
+        verify(createSubjectQuestionnairePort, times(1))
+            .persistAll(captor.capture(), anyLong());
+
+        assertEquals(2, captor.getValue().size());
+        assertEquals(2, captor.getValue().get(123L).size());
+        assertEquals(1, captor.getValue().get(456L).size());
     }
 }
