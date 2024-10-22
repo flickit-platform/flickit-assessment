0c16b2857c3220343a912ed76269fbd490173bf0
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-10-22 19:22:48 +0330

    FAC-691: Remove @GeneratedValue from AnswerOptionImpactJpaEntity

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/asnweroptionimpact/AnswerOptionImpactJpaEntity.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/asnweroptionimpact/AnswerOptionImpactJpaEntity.java
@@ -19,8 +19,6 @@ public class AnswerOptionImpactJpaEntity {
 
     @Id
     @EqualsAndHashCode.Include
-    @GeneratedValue(generator = "fak_answer_option_impact_id_seq")
-    @SequenceGenerator(name = "fak_answer_option_impact_id_seq", sequenceName = "fak_answer_option_impact_id_seq", allocationSize = 1)
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
