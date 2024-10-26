67f48eb86fa453e1d93ed35bc0ac3b9107025747
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-26 11:03:02 +0330

    FAC-708: Feat - Create `AnswerRange` Table and Entity and Repository

---
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/answerrange/AnswerRangeJpaRepository.java
@@ -0,0 +1,6 @@
+package org.flickit.assessment.data.jpa.kit.answerrange;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRangeJpaRepository extends JpaRepository<AnswerRangeJpaEntity, AnswerRangeJpaEntity.EntityId> {
+}
