b7d5b9db9ca39c2ff971598eac5c8daeb775fb6f
mahdirezazadeh <riza.rs2000@gmail.com>
2024-10-28 10:49:00 +0330

    FAC-707:feature:Remove unused Constants

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/test/fixture/application/Constants.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/test/fixture/application/Constants.java
@@ -30,6 +30,4 @@ public class Constants {
     public static final String OPTION_TITLE = "Title";
     public static final Integer OPTION_INDEX1 = 1;
     public static final Integer OPTION_INDEX2 = 2;
-    public static final Integer OPTION_INDEX3 = 3;
-    public static final Integer OPTION_INDEX4 = 4;
 }
