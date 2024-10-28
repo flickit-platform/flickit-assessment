11cc2c4d2a18e7d047a57153a2978a49e86b031b
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-28 22:27:33 +0330

    FAC-716: feat - Add `ServiceTest`

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/answerrange/GetAnswerRangeListServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/answerrange/GetAnswerRangeListServiceTest.java
@@ -1,11 +1,22 @@
 package org.flickit.assessment.kit.application.service.answerrange;
 
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.kit.application.port.in.answerrange.GetAnswerRangeListUseCase;
+import org.flickit.assessment.kit.application.port.out.expertgroupaccess.CheckExpertGroupAccessPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
+import org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother;
+import org.flickit.assessment.kit.test.fixture.application.KitVersionMother;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.UUID;
+import java.util.function.Consumer;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class GetAnswerRangeListServiceTest {
@@ -13,6 +24,31 @@ class GetAnswerRangeListServiceTest {
     @InjectMocks
     GetAnswerRangeListService service;
 
+    @Mock
+    LoadKitVersionPort loadKitVersionPort;
 
-    void testGetAnswerRangeListService_CurrentUserDoesNotHaveAccess_ThrowsAccessDeniedException() {}
+    @Mock
+    CheckExpertGroupAccessPort checkExpertGroupAccessPort;
+
+    @Test
+    void testGetAnswerRangeListService_CurrentUserDoesNotHaveAccess_ThrowsAccessDeniedException() {
+        var param = createParam(GetAnswerRangeListUseCase.Param.ParamBuilder::build);
+        var kitVersion = KitVersionMother.createKitVersion(AssessmentKitMother.simpleKit());
+
+        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
+        when(checkExpertGroupAccessPort.checkIsMember(kitVersion.getKit().getExpertGroupId(), param.getCurrentUserId())).thenReturn(false);
+        assertThrows(AccessDeniedException.class, () -> service.getAnswerRangeList(param));
+    }
+
+    private GetAnswerRangeListUseCase.Param createParam(Consumer<GetAnswerRangeListUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        return paramBuilder.build();
+    }
+
+    private GetAnswerRangeListUseCase.Param.ParamBuilder paramBuilder() {
+        return GetAnswerRangeListUseCase.Param.builder()
+            .kitVersionId(1L)
+            .currentUserId(UUID.randomUUID());
+    }
 }
