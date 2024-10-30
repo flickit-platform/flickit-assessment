368178195269f2bfd9c6613b5b5a8e297575499a
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-10-30 19:44:12 +0330

    FAC-718 Impl get questionnaire questions out port

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/question/LoadQuestionnaireQuestionsPort.java
@@ -0,0 +1,9 @@
+package org.flickit.assessment.kit.application.port.out.question;
+
+import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
+import org.flickit.assessment.kit.application.domain.Question;
+
+public interface LoadQuestionnaireQuestionsPort {
+
+    PaginatedResponse<Question> loadQuestionnaireQuestions(long questionnaireId, long kitVersionId, int page, int size);
+}
