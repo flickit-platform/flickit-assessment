62c4b78f80776252890aceb3ca91bf5ecc365f1d
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-10-31 19:52:19 +0330

    FAC-716: refactor : Move Merging `AnswerRanges` and `AnswerOptions` from `Service` to `Adapter`

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/answerrange/GetAnswerRangeListService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/answerrange/GetAnswerRangeListService.java
@@ -3,19 +3,14 @@ package org.flickit.assessment.kit.application.service.answerrange;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.kit.application.domain.*;
 import org.flickit.assessment.kit.application.port.in.answerrange.GetAnswerRangeListUseCase;
 import org.flickit.assessment.kit.application.port.out.answerange.LoadAnswerRangePort;
-import org.flickit.assessment.kit.application.port.out.answeroption.LoadAnswerOptionPort;
 import org.flickit.assessment.kit.application.port.out.expertgroupaccess.CheckExpertGroupAccessPort;
 import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import static java.util.stream.Collectors.groupingBy;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
 @Service
@@ -25,7 +20,6 @@ public class GetAnswerRangeListService implements GetAnswerRangeListUseCase {
     private final LoadKitVersionPort loadKitVersionPort;
     private final CheckExpertGroupAccessPort checkExpertGroupAccessPort;
     private final LoadAnswerRangePort loadAnswerRangePort;
-    private final LoadAnswerOptionPort loadAnswerOptionPort;
 
     @Override
     public PaginatedResponse<AnswerRangeListItem> getAnswerRangeList(Param param) {
@@ -33,40 +27,18 @@ public class GetAnswerRangeListService implements GetAnswerRangeListUseCase {
         if (!checkExpertGroupAccessPort.checkIsMember(kitVersion.getKit().getExpertGroupId(), param.getCurrentUserId()))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
-        var paginatedAnswerRanges = loadAnswerRangePort.loadByKitVersionId(param.getKitVersionId(), param.getPage(), param.getSize());
+        var paginatedResponse = loadAnswerRangePort.loadByKitVersionId(param.getKitVersionId(), param.getPage(), param.getSize());
 
-        List<AnswerRange> ranges = new ArrayList<>();
-        if (!paginatedAnswerRanges.getItems().isEmpty()) {
-            var answerRanges = paginatedAnswerRanges.getItems();
-            var answerOptions = loadAnswerOptionPort.loadByKitVersionId(param.getKitVersionId());
-            ranges = getAnswerRangesWithAnswerOptions(answerRanges, answerOptions);
-        }
+        List<AnswerRangeListItem> items = paginatedResponse.getItems().stream()
+            .map(e -> new AnswerRangeListItem(e.getId(), e.getTitle(),
+                e.getAnswerOptions().stream().map(
+                    a -> new AnswerRangeListItem.AnswerOptionListItem(a.getId(), a.getTitle(), a.getIndex())).toList())).toList();
 
-        return new PaginatedResponse<>(ranges.stream().map(e -> new AnswerRangeItemList(e.getId(), e.getTitle(),
-                e.getAnswerOptions().stream().map(a -> new AnswerRangeItemList.AnswerOptionListItem(a.getId(), a.getTitle(), a.getIndex())).toList())).toList(),
-            paginatedAnswerRanges.getPage(),
-            paginatedAnswerRanges.getSize(),
-            paginatedAnswerRanges.getSort(),
-            paginatedAnswerRanges.getOrder(),
-            paginatedAnswerRanges.getTotal());
-    }
-
-    private static List<AnswerRange> getAnswerRangesWithAnswerOptions(List<AnswerRange> answerRanges, List<AnswerOption> answerOptions) {
-        Map<Long, List<AnswerOption>> answerRangeIdToOptions = answerOptions.stream()
-            .collect(groupingBy(AnswerOption::getAnswerRangeId));
-
-        return answerRanges.stream()
-            .map(answerRange -> new AnswerRange(
-                answerRange.getId(),
-                answerRange.getTitle(),
-                answerRangeIdToOptions.getOrDefault(answerRange.getId(), List.of()).stream()
-                    .map(option -> new AnswerOption(
-                        option.getId(),
-                        option.getTitle(),
-                        option.getIndex(),
-                        null,
-                        null
-                    )).toList()
-            )).toList();
+        return new PaginatedResponse<>(items,
+            paginatedResponse.getPage(),
+            paginatedResponse.getSize(),
+            paginatedResponse.getSort(),
+            paginatedResponse.getOrder(),
+            paginatedResponse.getTotal());
     }
 }
