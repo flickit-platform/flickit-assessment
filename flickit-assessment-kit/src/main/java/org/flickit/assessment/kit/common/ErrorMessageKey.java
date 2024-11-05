442144815ac120892f6fa56050fe26de3b6cc10d
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-05 16:37:08 +0330

    FAC-685: refactor : Minor changes

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/common/ErrorMessageKey.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/common/ErrorMessageKey.java
@@ -418,7 +418,7 @@ public class ErrorMessageKey {
 
     public static final String UPDATE_ANSWER_OPTION_ORDERS_KIT_VERSION_ID_NOT_NULL = "update-answer-option-orders.kitVersionId.notNull";
     public static final String UPDATE_ANSWER_OPTION_ORDERS_ORDERS_NOT_NULL = "update-answer-option-orders.orders.notNull";
-    public static final String UPDATE_ANSWER_OPTION_ORDERS_ANSWER_OPTION_ID_NOT_NULL = "update-answer-option-orders.answerOption.notNull";
+    public static final String UPDATE_ANSWER_OPTION_ORDERS_ANSWER_OPTION_ID_NOT_NULL = "update-answer-option-orders.answerOption.id.notNull";
     public static final String UPDATE_ANSWER_OPTION_ORDERS_ANSWER_OPTION_INDEX_NOT_NULL = "update-answer-option-orders.answerOption.index.notNull";
     public static final String UPDATE_ANSWER_OPTION_ORDERS_ANSWER_OPTION_INDEX_MIN = "update-answer-option-orders.answerOption.index.min";
 
