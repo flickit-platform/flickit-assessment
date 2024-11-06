0d0958fd2ec7f3ae0745fa99f4fa1a27f19db8fa
mhzkh <167864555+mhzkh@users.noreply.github.com>
2024-11-06 18:37:37 +0330

    Revert "FAC-733 Add option impacts when creating a question impact"

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/questionimpact/CreateQuestionImpactService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/questionimpact/CreateQuestionImpactService.java
@@ -2,11 +2,8 @@ package org.flickit.assessment.kit.application.service.questionimpact;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.kit.application.domain.AnswerOption;
 import org.flickit.assessment.kit.application.domain.QuestionImpact;
 import org.flickit.assessment.kit.application.port.in.questionimpact.CreateQuestionImpactUseCase;
-import org.flickit.assessment.kit.application.port.out.answeroption.LoadAnswerOptionsByQuestionPort;
-import org.flickit.assessment.kit.application.port.out.answeroptionimpact.CreateAnswerOptionImpactPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
 import org.flickit.assessment.kit.application.port.out.questionimpact.CreateQuestionImpactPort;
@@ -14,8 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
@@ -27,36 +22,16 @@ public class CreateQuestionImpactService implements CreateQuestionImpactUseCase 
     private final LoadKitVersionPort loadKitVersionPort;
     private final LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
     private final CreateQuestionImpactPort createQuestionImpactPort;
-    private final LoadAnswerOptionsByQuestionPort loadAnswerOptionsByQuestionPort;
-    private final CreateAnswerOptionImpactPort createAnswerOptionImpactPort;
 
     @Override
     public long createQuestionImpact(Param param) {
-        Long kitVersionId = param.getKitVersionId();
-        var kitversion = loadKitVersionPort.load(kitVersionId);
+        var kitversion = loadKitVersionPort.load(param.getKitVersionId());
         var expertGroupOwnerId = loadExpertGroupOwnerPort.loadOwnerId(kitversion.getKit().getExpertGroupId());
 
-        UUID currentUserId = param.getCurrentUserId();
-        if (!expertGroupOwnerId.equals(currentUserId))
+        if (!expertGroupOwnerId.equals(param.getCurrentUserId()))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
-        Long questionImpactId = createQuestionImpactPort.persist(toQuestionImpact(param));
-
-        var answerOptions = loadAnswerOptionsByQuestionPort.loadByQuestionId(param.getQuestionId(), kitVersionId);
-        List<Long> optionIds = answerOptions.stream()
-            .map(AnswerOption::getId)
-            .toList();
-
-        List<CreateAnswerOptionImpactPort.Param> optionImpacts = optionIds.stream()
-            .map(optionId -> new CreateAnswerOptionImpactPort.Param(questionImpactId,
-                optionId,
-                null,
-                kitVersionId,
-                currentUserId))
-            .toList();
-        createAnswerOptionImpactPort.persistAll(optionImpacts);
-
-        return questionImpactId;
+        return createQuestionImpactPort.persist(toQuestionImpact(param));
     }
 
     private QuestionImpact toQuestionImpact(Param param) {
--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/questionimpact/CreateQuestionImpactServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/questionimpact/CreateQuestionImpactServiceTest.java
@@ -1,25 +1,19 @@
 package org.flickit.assessment.kit.application.service.questionimpact;
 
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.kit.application.domain.AnswerOption;
 import org.flickit.assessment.kit.application.domain.KitVersion;
 import org.flickit.assessment.kit.application.domain.QuestionImpact;
 import org.flickit.assessment.kit.application.port.in.questionimpact.CreateQuestionImpactUseCase;
-import org.flickit.assessment.kit.application.port.out.answeroption.LoadAnswerOptionsByQuestionPort;
-import org.flickit.assessment.kit.application.port.out.answeroptionimpact.CreateAnswerOptionImpactPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
 import org.flickit.assessment.kit.application.port.out.questionimpact.CreateQuestionImpactPort;
-import org.flickit.assessment.kit.test.fixture.application.AnswerOptionMother;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
 
@@ -44,15 +38,6 @@ class CreateQuestionImpactServiceTest {
     @Mock
     private CreateQuestionImpactPort createQuestionImpactPort;
 
-    @Mock
-    private LoadAnswerOptionsByQuestionPort loadAnswerOptionsByQuestionPort;
-
-    @Mock
-    private CreateAnswerOptionImpactPort createAnswerOptionImpactPort;
-
-    @Captor
-    private ArgumentCaptor<List<CreateAnswerOptionImpactPort.Param>> createAnswerOptionImpactPortCaptor;
-
     private final UUID ownerId = UUID.randomUUID();
     private final KitVersion kitVersion = createKitVersion(simpleKit());
 
@@ -66,47 +51,31 @@ class CreateQuestionImpactServiceTest {
         var throwable = assertThrows(AccessDeniedException.class, () -> service.createQuestionImpact(param));
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
 
-        verifyNoInteractions(loadAnswerOptionsByQuestionPort, createAnswerOptionImpactPort, createQuestionImpactPort);
+        verifyNoInteractions(createQuestionImpactPort);
     }
 
     @Test
     void testCreateQuestionImpact_validParams_successfulCreateQuestionImpact() {
         var param = createParam(b -> b.currentUserId(ownerId));
-        AnswerOption option1 = AnswerOptionMother.createAnswerOption(1, "op1", 1);
-        AnswerOption option2 = AnswerOptionMother.createAnswerOption(1, "op2", 2);
-        var options = List.of(option1, option2);
-        long questionImpactId = 1L;
 
         when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
         when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
-        when(createQuestionImpactPort.persist(any(QuestionImpact.class))).thenReturn(questionImpactId);
-        when(loadAnswerOptionsByQuestionPort.loadByQuestionId(param.getQuestionId(), param.getKitVersionId())).thenReturn(options);
-        doNothing().when(createAnswerOptionImpactPort).persistAll(anyList());
 
-        long result = service.createQuestionImpact(param);
-        assertEquals(questionImpactId, result);
 
-        ArgumentCaptor<QuestionImpact> questionImpactCaptor = ArgumentCaptor.forClass(QuestionImpact.class);
-        verify(createQuestionImpactPort).persist(questionImpactCaptor.capture());
-        assertEquals(param.getKitVersionId(), questionImpactCaptor.getValue().getKitVersionId());
-        assertEquals(param.getAttributeId(), questionImpactCaptor.getValue().getAttributeId());
-        assertEquals(param.getMaturityLevelId(), questionImpactCaptor.getValue().getMaturityLevelId());
-        assertEquals(param.getQuestionId(), questionImpactCaptor.getValue().getQuestionId());
-        assertEquals(param.getWeight(), questionImpactCaptor.getValue().getWeight());
-        assertEquals(param.getCurrentUserId(), questionImpactCaptor.getValue().getCreatedBy());
-        assertEquals(param.getCurrentUserId(), questionImpactCaptor.getValue().getLastModifiedBy());
-        assertNotNull(questionImpactCaptor.getValue().getCreationTime());
-        assertNotNull(questionImpactCaptor.getValue().getLastModificationTime());
+        service.createQuestionImpact(param);
 
-        verify(createAnswerOptionImpactPort).persistAll(createAnswerOptionImpactPortCaptor.capture());
-        List<CreateAnswerOptionImpactPort.Param> portParams = createAnswerOptionImpactPortCaptor.getValue();
-        for (int i = 0; i < portParams.size(); i++) {
-            assertEquals(questionImpactId, portParams.get(i).questionImpactId());
-            assertEquals(options.get(i).getId(), portParams.get(i).optionId());
-            assertNull(portParams.get(i).value());
-            assertEquals(param.getKitVersionId(), portParams.get(i).kitVersionId());
-            assertEquals(param.getCurrentUserId(), portParams.get(i).createdBy());
-        }
+        ArgumentCaptor<QuestionImpact> captor = ArgumentCaptor.forClass(QuestionImpact.class);
+        verify(createQuestionImpactPort).persist(captor.capture());
+
+        assertEquals(param.getKitVersionId(), captor.getValue().getKitVersionId());
+        assertEquals(param.getAttributeId(), captor.getValue().getAttributeId());
+        assertEquals(param.getMaturityLevelId(), captor.getValue().getMaturityLevelId());
+        assertEquals(param.getQuestionId(), captor.getValue().getQuestionId());
+        assertEquals(param.getWeight(), captor.getValue().getWeight());
+        assertEquals(param.getCurrentUserId(), captor.getValue().getCreatedBy());
+        assertEquals(param.getCurrentUserId(), captor.getValue().getLastModifiedBy());
+        assertNotNull(captor.getValue().getCreationTime());
+        assertNotNull(captor.getValue().getLastModificationTime());
     }
 
     private CreateQuestionImpactUseCase.Param createParam(Consumer<CreateQuestionImpactUseCase.Param.ParamBuilder> changer) {
@@ -117,7 +86,7 @@ class CreateQuestionImpactServiceTest {
 
     private CreateQuestionImpactUseCase.Param.ParamBuilder paramBuilder() {
         return CreateQuestionImpactUseCase.Param.builder()
-            .kitVersionId(kitVersion.getId())
+            .kitVersionId(1L)
             .attributeId(2L)
             .maturityLevelId(3L)
             .questionId(4L)
