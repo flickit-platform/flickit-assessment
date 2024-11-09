89db9168f5eb0cfdfcd8e675ce20647554814d13
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-11-09 09:32:49 +0330

    FAC-726: Remove answerRangeId from question dsl model

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/update/impl/QuestionUpdateKitPersister.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/update/impl/QuestionUpdateKitPersister.java
@@ -287,7 +287,7 @@ public class QuestionUpdateKitPersister implements UpdateKitPersister {
                 dslQuestion.getDescription(),
                 dslQuestion.isMayNotBeApplicable(),
                 dslQuestion.isAdvisable(),
-                dslQuestion.getAnswerRangeId(),
+                savedQuestion.getAnswerRangeId(),
                 LocalDateTime.now(),
                 currentUserId
             );
