7bb7437ab1391fe7301cb4c9fd75e37f34ab7048
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-09 16:06:11 +0330

    FAC-738: feat - Add `CreateAnswerRangeRestController` and `CreateAnswerRangeRequestDto`

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/answerrange/CreateAnswerRangeRestController.java
@@ -0,0 +1,31 @@
+package org.flickit.assessment.kit.adapter.in.rest.answerrange;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.flickit.assessment.kit.application.port.in.answerrange.CreateAnswerRangeUseCase;
+import org.flickit.assessment.kit.application.port.in.answerrange.CreateAnswerRangeUseCase.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class CreateAnswerRangeRestController {
+
+    private final UserContext userContext;
+    private final CreateAnswerRangeUseCase useCase;
+
+    @PostMapping("/kit-versions/{kitVersionId}/answer-ranges")
+    public ResponseEntity<Void> crateAnswerRange(@PathVariable("kitVersionId") Long kitVersionId,
+                                                 @RequestBody CreateAnswerRangeRequestDto requestDto) {
+        UUID currentUserId = userContext.getUser().id();
+        useCase.createAnswerRange(toParam(kitVersionId, currentUserId, requestDto));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private static Param toParam(Long kitVersionId, UUID currentUserId, CreateAnswerRangeRequestDto requestDto) {
+        return new Param(kitVersionId, requestDto.title(), currentUserId);
+    }
+}
