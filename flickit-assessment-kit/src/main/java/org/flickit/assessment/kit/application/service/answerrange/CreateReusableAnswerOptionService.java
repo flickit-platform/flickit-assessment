c1cf70d698fc79d24a2741ed45cc56950bbd201d
mahdirezazadeh <riza.rs2000@gmail.com>
2024-11-10 15:30:57 +0330

    FAC-741:feature:Impl CreateReusableAnswerOptionService

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/answerrange/CreateReusableAnswerOptionService.java
@@ -0,0 +1,58 @@
+package org.flickit.assessment.kit.application.service.answerrange;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ValidationException;
+import org.flickit.assessment.kit.application.domain.AnswerRange;
+import org.flickit.assessment.kit.application.domain.KitVersion;
+import org.flickit.assessment.kit.application.port.in.answerrange.CreateReusableAnswerOptionUseCase;
+import org.flickit.assessment.kit.application.port.out.answeroption.CreateAnswerOptionPort;
+import org.flickit.assessment.kit.application.port.out.answerrange.LoadAnswerRangePort;
+import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
+import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.kit.common.ErrorMessageKey.CREATE_ANSWER_OPTION_ANSWER_RANGE_NON_REUSABLE;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CreateReusableAnswerOptionService implements CreateReusableAnswerOptionUseCase {
+
+    private final LoadKitVersionPort loadKitVersionPort;
+    private final LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
+    private final LoadAnswerRangePort loadAnswerRangePort;
+    private final CreateAnswerOptionPort createAnswerOptionPort;
+
+    @Override
+    public Result createReusableAnswerOption(Param param) {
+        checkUserAccess(param);
+        AnswerRange answerRange = loadAnswerRangePort.load(param.getAnswerRangeId(), param.getKitVersionId());
+        if (!answerRange.isReusable())
+            throw new ValidationException(CREATE_ANSWER_OPTION_ANSWER_RANGE_NON_REUSABLE);
+
+        long answerOptionId = createAnswerOptionPort.persist(toCreateParam(param));
+        return new CreateReusableAnswerOptionUseCase.Result(answerOptionId);
+    }
+
+    private void checkUserAccess(CreateReusableAnswerOptionUseCase.Param param) {
+        KitVersion kitVersion = loadKitVersionPort.load(param.getKitVersionId());
+        UUID ownerId = loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId());
+        if (!ownerId.equals(param.getCurrentUserId()))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+    }
+
+    private CreateAnswerOptionPort.Param toCreateParam(Param param) {
+        return new CreateAnswerOptionPort.Param(
+            param.getTitle(),
+            param.getIndex(),
+            param.getAnswerRangeId(),
+            param.getValue(),
+            param.getKitVersionId(),
+            param.getCurrentUserId());
+    }
+}
