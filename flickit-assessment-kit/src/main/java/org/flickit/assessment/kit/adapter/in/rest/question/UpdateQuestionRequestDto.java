3210971aad332bebc4bd278e2ca1547e96f81ad2
mahdirezazadeh <riza.rs2000@gmail.com>
2024-11-11 11:04:51 +0330

    Revert "FAC-726: Minor format"
    This reverts commit 18acda1e3495f4632546a097818fc5526b8ab269.

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/question/UpdateQuestionRequestDto.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/question/UpdateQuestionRequestDto.java
@@ -4,6 +4,6 @@ public record UpdateQuestionRequestDto(Integer index,
                                        String title,
                                        String hint,
                                        Boolean mayNotBeApplicable,
-                                       Boolean advisable,
-                                       Long answerRangeId) {
+                                       Boolean advisable, Long answerRangeId
+) {
 }
