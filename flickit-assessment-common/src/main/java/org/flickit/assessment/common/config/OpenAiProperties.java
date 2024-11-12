b34f9c0f541f17e4799e74fd0ae35ef66203e003
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-12 09:39:19 +0330

    FAC-723: feat - Add a sentence to avoid introductory sentences

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/config/OpenAiProperties.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/config/OpenAiProperties.java
@@ -26,6 +26,7 @@ public class OpenAiProperties {
         We define {attributeTitle} as {attributeDescription}. The uploaded Excel file contains multiple-choice questions used to assess {attributeTitle}.
         The Excel columns include the question, a hint, the weight of the question in calculating the overall score,
          and the actual score achieved by the software. Please generate an executive summary highlighting the main strengths and weaknesses in less than 100 words.
+        Start directly with specific strengths and weaknesses, avoiding introductory sentences.
         Use polite and considerate language, avoiding any derogatory terms, and do not mention the scores of individual questions.
         Here is the uploaded Excel file: {excelFile}.
         """;
