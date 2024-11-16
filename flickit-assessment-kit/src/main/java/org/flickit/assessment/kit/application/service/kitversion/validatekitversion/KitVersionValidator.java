fb719524c1415129a22a21c15881c2a8666bb685
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-16 13:29:16 +0330

    FAC-758: Refactor - Remove useless classes

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/kitversion/validatekitversion/KitVersionValidator.java
+++
@@ -1,9 +0,0 @@
-package org.flickit.assessment.kit.application.service.kitversion.validatekitversion;
-
-import org.flickit.assessment.common.exception.api.Notification;
-import org.flickit.assessment.kit.application.domain.KitVersion;
-
-public interface KitVersionValidator {
-
-    Notification validate(KitVersion kitVersion);
-}
