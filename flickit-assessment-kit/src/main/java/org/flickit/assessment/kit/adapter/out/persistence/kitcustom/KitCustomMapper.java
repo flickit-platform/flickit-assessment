810971e8556350960834d238004e2fc24cbffd22
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-11-17 19:35:47 +0330

    FAC-766 Add private no-arg constructor

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kitcustom/KitCustomMapper.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kitcustom/KitCustomMapper.java
@@ -1,8 +1,11 @@
 package org.flickit.assessment.kit.adapter.out.persistence.kitcustom;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.flickit.assessment.data.jpa.kit.customkit.KitCustomJpaEntity;
 import org.flickit.assessment.kit.application.port.out.kitcustom.CreateKitCustomPort;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class KitCustomMapper {
 
     public static KitCustomJpaEntity mapToJpaEntity(CreateKitCustomPort.Param param) {
