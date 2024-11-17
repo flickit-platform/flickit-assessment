43e7a5e62712b0bacb8d7f92ff9dcb6569409477
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-11-17 19:35:15 +0330

    FAC-766 Remove redundant imports

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/customkit/KitCustomJpaEntity.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/customkit/KitCustomJpaEntity.java
@@ -5,8 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
