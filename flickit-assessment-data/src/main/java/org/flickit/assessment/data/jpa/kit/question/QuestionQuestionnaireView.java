c24a88f5b416cf7b65c07e7d6ee665e5c803248a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-18 08:35:36 +0330

    FAC-758: feat - Add `QuestionQuestionnaireView`

---
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/question/QuestionQuestionnaireView.java
@@ -0,0 +1,10 @@
+package org.flickit.assessment.data.jpa.kit.question;
+
+public interface QuestionQuestionnaireView {
+
+    Long getQuestionId();
+
+    Long getQuestionnaireId();
+
+    String getQuestionnaireTitle();
+}
