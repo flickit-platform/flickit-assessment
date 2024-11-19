60c4c731827ee9603832f9c048ef60a589af9a70
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-19 12:47:51 +0330

    FAC-758: Refactor - Add KitVersion comparison for joining queries in `QuestionJpaRepository`

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/question/QuestionJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/question/QuestionJpaRepository.java
@@ -223,7 +223,7 @@ public interface QuestionJpaRepository extends JpaRepository<QuestionJpaEntity, 
     @Query("""
             SELECT q.index AS questionIndex, qn.id AS questionnaireId, qn.title AS questionnaireTitle
             FROM QuestionJpaEntity q
-            JOIN QuestionnaireJpaEntity qn on qn.id = q.questionnaireId
+            JOIN QuestionnaireJpaEntity qn on qn.id = q.questionnaireId AND qn.kitVersionId = q.kitVersionId
             WHERE q.kitVersionId = :kitVersionId AND q.answerRangeId IS NULL
         """)
     List<QuestionQuestionnaireView> findAllByKitVersionIdAndAnswerRangeIdIsNull(long kitVersionId);
@@ -231,7 +231,7 @@ public interface QuestionJpaRepository extends JpaRepository<QuestionJpaEntity, 
     @Query("""
             SELECT q.index as questionIndex, q.questionnaireId as questionnaireId, qr.title as questionnaireTitle
             FROM QuestionJpaEntity q
-            JOIN QuestionnaireJpaEntity qr on q.questionnaireId = qr.id
+            JOIN QuestionnaireJpaEntity qr on q.questionnaireId = qr.id AND qr.kitVersionId = q.kitVersionId
             Left JOIN QuestionImpactJpaEntity qi on qi.questionId = q.id
             WHERE q.kitVersionId = :kitVersionId and qi.id IS null
         """)
