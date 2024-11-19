4a67fb57c2b279d4d17cffa9bf6abf4a72b1116b
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-11-19 22:36:33 +0330

    FAC-767 Send kitCustomId in request body of the service

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessment/SetKitCustomToAssessmentRestController.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessment/SetKitCustomToAssessmentRestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -18,11 +19,11 @@ public class SetKitCustomToAssessmentRestController {
     private final SetKitCustomToAssessmentUseCase useCase;
     private final UserContext userContext;
 
-    @PutMapping("/assessments/{assessmentId}/kit-customs/{kitCustomId}")
+    @PutMapping("/assessments/{assessmentId}/assign-kit-custom")
     public ResponseEntity<Void> setKitCustomToAssessment(@PathVariable("assessmentId")UUID assessmentId,
-                                                         @PathVariable("kitCustomId") Long kitCustomId) {
+                                                         @RequestBody AssignKitCustomRequestDto requestDto) {
         UUID currentUserId = userContext.getUser().id();
-        useCase.setKitCustomToAssessment(toParam(assessmentId, kitCustomId, currentUserId));
+        useCase.setKitCustomToAssessment(toParam(assessmentId, requestDto.kitCustomId(), currentUserId));
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
