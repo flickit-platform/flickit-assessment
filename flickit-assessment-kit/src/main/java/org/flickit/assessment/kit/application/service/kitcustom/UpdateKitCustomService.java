74fe1d386b02093a3928833fb3b0f1bc5ec66b3a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-20 11:07:49 +0330

    FAC-770: Refactor - Remove Extra codes from `service`

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/kitcustom/UpdateKitCustomService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/kitcustom/UpdateKitCustomService.java
@@ -19,33 +19,4 @@ public class UpdateKitCustomService implements UpdateKitCustomUseCase {
     public void updateKitCustom(Param param) {
 
     }
-
-    private Param createParam(Consumer<Param.ParamBuilder> changer) {
-        var param = paramBuilder();
-        changer.accept(param);
-        return param.build();
-    }
-
-    private UpdateKitCustomUseCase.Param.ParamBuilder paramBuilder() {
-        return UpdateKitCustomUseCase.Param.builder()
-            .id(12L)
-            .kitId(1L)
-            .title("title")
-            .customData(createCustomDataParam(UpdateKitCustomUseCase.Param.KitCustomData.KitCustomDataBuilder::build))
-            .currentUserId(UUID.randomUUID());
-    }
-
-    private UpdateKitCustomUseCase.Param.KitCustomData createCustomDataParam(Consumer<Param.KitCustomData.KitCustomDataBuilder> changer) {
-        var param = KitCustomDataBuilder();
-        changer.accept(param);
-        return param.build();
-    }
-
-    private UpdateKitCustomUseCase.Param.KitCustomData.KitCustomDataBuilder KitCustomDataBuilder() {
-        var customSubject = new UpdateKitCustomUseCase.Param.KitCustomData.CustomSubject(1L, 1);
-        var customAttribute = new UpdateKitCustomUseCase.Param.KitCustomData.CustomAttribute(1L, 1);
-        return UpdateKitCustomUseCase.Param.KitCustomData.builder()
-            .customSubjects(List.of(customSubject))
-            .customAttributes(List.of(customAttribute));
-    }
 }
