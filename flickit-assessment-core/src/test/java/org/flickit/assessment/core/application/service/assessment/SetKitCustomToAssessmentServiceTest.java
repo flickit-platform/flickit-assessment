69992e9b39aa9d8fcc804473f5509b94f6795044
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-11-20 08:14:55 +0330

    FAC-767: Import static AssessmentPermission

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/SetKitCustomToAssessmentServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/SetKitCustomToAssessmentServiceTest.java
@@ -1,7 +1,6 @@
 package org.flickit.assessment.core.application.service.assessment;
 
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
-import org.flickit.assessment.common.application.domain.assessment.AssessmentPermission;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.core.application.port.in.assessment.AssignKitCustomUseCase;
 import org.flickit.assessment.core.application.port.out.assessment.UpdateAssessmentPort;
@@ -14,8 +13,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.MANAGE_KIT_CUSTOM;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,9 +35,7 @@ class AssignKitCustomServiceTest {
     void testAssignKitCustom_WhenCurrentUserDoesNotHaveRequiredPermission_ThenThrowAccessDeniedException() {
         var param = createParam(AssignKitCustomUseCase.Param.ParamBuilder::build);
 
-        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(),
-            param.getCurrentUserId(),
-            AssessmentPermission.MANAGE_KIT_CUSTOM)).thenReturn(false);
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), MANAGE_KIT_CUSTOM)).thenReturn(false);
 
         var throwable = assertThrows(AccessDeniedException.class, () -> service.assignKitCustom(param));
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
@@ -48,9 +47,7 @@ class AssignKitCustomServiceTest {
     void testAssignKitCustom_WhenCurrentUserHasRequiredPermission_ThenUpdateAssessmentKitCustom() {
         var param = createParam(AssignKitCustomUseCase.Param.ParamBuilder::build);
 
-        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(),
-            param.getCurrentUserId(),
-            AssessmentPermission.MANAGE_KIT_CUSTOM)).thenReturn(true);
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), MANAGE_KIT_CUSTOM)).thenReturn(true);
         doNothing().when(updateAssessmentPort).updateKitCustomId(param.getAssessmentId(), param.getKitCustomId());
 
         service.assignKitCustom(param);
