7f1c10653cf24dba42ceb3030c20db89ad391567
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-20 12:57:04 +0330

    FAC-770: feat - Complete `Service` and `ServiceTest`

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/kitcustom/UpdateKitCustomService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/kitcustom/UpdateKitCustomService.java
@@ -1,14 +1,21 @@
 package org.flickit.assessment.kit.application.service.kitcustom;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.util.SlugCodeUtil;
 import org.flickit.assessment.kit.application.domain.AssessmentKit;
+import org.flickit.assessment.kit.application.domain.KitCustomData;
 import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadAssessmentKitPort;
+import org.flickit.assessment.kit.application.port.out.kitcustom.UpdateKitCustomPort;
 import org.flickit.assessment.kit.application.port.out.kituseraccess.CheckKitUserAccessPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import org.flickit.assessment.kit.application.port.in.kitcustom.UpdateKitCustomUseCase;
+
+import java.time.LocalDateTime;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
@@ -19,6 +26,8 @@ public class UpdateKitCustomService implements UpdateKitCustomUseCase {
 
     private final LoadAssessmentKitPort loadAssessmentKitPort;
     private final CheckKitUserAccessPort checkKitUserAccessPort;
+    private final UpdateKitCustomPort updateKitCustomPort;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void updateKitCustom(Param param) {
@@ -26,5 +35,32 @@ public class UpdateKitCustomService implements UpdateKitCustomUseCase {
         if (kit.isPrivate() && !checkKitUserAccessPort.hasAccess(param.getKitId(), param.getCurrentUserId()))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
+        updateKitCustomPort.update(toParam(param));
+    }
+
+    @SneakyThrows
+    UpdateKitCustomPort.Param toParam(Param param) {
+        String code = SlugCodeUtil.generateSlugCode(param.getTitle());
+        KitCustomData kitCustomData = toKitCustomData(param.getCustomData());
+
+        return new UpdateKitCustomPort.Param(param.getId(),
+            param.getKitId(),
+            param.getTitle(),
+            code,
+            objectMapper.writeValueAsString(kitCustomData),
+            LocalDateTime.now(),
+            param.getCurrentUserId());
+    }
+
+    private KitCustomData toKitCustomData(UpdateKitCustomUseCase.Param.KitCustomData customData) {
+        var subjects = customData.customSubjects().stream()
+            .map(e -> new KitCustomData.Subject(e.getId(), e.getWeight()))
+            .toList();
+
+        var attributes = customData.customAttributes().stream()
+            .map(e -> new KitCustomData.Attribute(e.getId(), e.getWeight()))
+            .toList();
+
+        return new KitCustomData(subjects, attributes);
     }
 }
