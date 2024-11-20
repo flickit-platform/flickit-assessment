6baf22fd755d05950ef65cdbd95a9d388bfe6225
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-20 13:03:24 +0330

    FAC-770: feat - Add `KitCustom` Existence check to `KitCustomPersistenceJpaAdapter`

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kitcustom/KitCustomPersistenceJpaAdapter.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kitcustom/KitCustomPersistenceJpaAdapter.java
@@ -1,12 +1,15 @@
 package org.flickit.assessment.kit.adapter.out.persistence.kitcustom;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.data.jpa.kit.customkit.KitCustomJpaEntity;
 import org.flickit.assessment.data.jpa.kit.customkit.KitCustomJpaRepository;
 import org.flickit.assessment.data.jpa.kit.seq.KitDbSequenceGenerators;
 import org.flickit.assessment.kit.application.port.out.kitcustom.CreateKitCustomPort;
 import org.flickit.assessment.kit.application.port.out.kitcustom.UpdateKitCustomPort;
 import org.springframework.stereotype.Component;
+
+import static org.flickit.assessment.kit.common.ErrorMessageKey.KIT_CUSTOM_ID_NOT_FOUND;
 
 @Component
 @RequiredArgsConstructor
@@ -26,6 +29,7 @@ public class KitCustomPersistenceJpaAdapter implements
 
     @Override
     public void update(UpdateKitCustomPort.Param param) {
-
+        if(!repository.existsById(param.id()))
+            throw new ResourceNotFoundException(KIT_CUSTOM_ID_NOT_FOUND);
     }
 }
--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/common/ErrorMessageKey.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/common/ErrorMessageKey.java
@@ -20,6 +20,7 @@ public class ErrorMessageKey {
     public static final String QUESTION_IMPACT_ID_NOT_FOUND = "question-impact.id.notFound";
     public static final String ANSWER_OPTION_ID_NOT_FOUND = "answer-option.id.notFound";
     public static final String ANSWER_RANGE_ID_NOT_FOUND = "answer-range.id.notFound";
+    public static final String KIT_CUSTOM_ID_NOT_FOUND = "kit-custom.id.notFound";
 
     public static final String UPDATE_KIT_BY_DSL_KIT_ID_NOT_NULL = "update-kit-by-dsl.kitId.notNull";
     public static final String UPDATE_KIT_BY_DSL_KIT_DSL_ID_NOT_NULL = "update-kit-by-dsl.kit-dsl-id.notNull";
