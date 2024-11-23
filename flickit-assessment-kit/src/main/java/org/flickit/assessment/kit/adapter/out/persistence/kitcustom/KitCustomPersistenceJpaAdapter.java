aac34404e5511666f921f6518dc0c6432ff041b0
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-11-23 14:11:03 +0330

    FAC-775 Impl load kit custom data port & its jpa adapter

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kitcustom/KitCustomPersistenceJpaAdapter.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kitcustom/KitCustomPersistenceJpaAdapter.java
@@ -7,12 +7,16 @@ import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.data.jpa.kit.customkit.KitCustomJpaEntity;
 import org.flickit.assessment.data.jpa.kit.customkit.KitCustomJpaRepository;
 import org.flickit.assessment.data.jpa.kit.seq.KitDbSequenceGenerators;
+import org.flickit.assessment.kit.application.domain.KitCustomData;
 import org.flickit.assessment.kit.application.port.out.kitcustom.CreateKitCustomPort;
+import org.flickit.assessment.kit.application.port.out.kitcustom.LoadKitCustomPort;
 import org.springframework.stereotype.Component;
+
+import static org.flickit.assessment.kit.common.ErrorMessageKey.KIT_CUSTOM_ID_NOT_FOUND;
 
 @Component
 @RequiredArgsConstructor
-public class KitCustomPersistenceJpaAdapter implements CreateKitCustomPort {
+public class KitCustomPersistenceJpaAdapter implements CreateKitCustomPort, LoadKitCustomPort {
 
     private final KitCustomJpaRepository repository;
     private final KitDbSequenceGenerators sequenceGenerators;
@@ -25,5 +29,15 @@ public class KitCustomPersistenceJpaAdapter implements CreateKitCustomPort {
         KitCustomJpaEntity entity = KitCustomMapper.mapToJpaEntity(param, kitCustomJson);
         entity.setId(sequenceGenerators.generateKitCustomId());
         return repository.save(entity).getId();
+    }
+
+    @Override
+    @SneakyThrows
+    public Result loadById(long kitCustomId) {
+        var kitCustomEntity = repository.findById(kitCustomId)
+            .orElseThrow(() -> new ResourceNotFoundException(KIT_CUSTOM_ID_NOT_FOUND));
+
+        KitCustomData customData = objectMapper.readValue(kitCustomEntity.getCustomData(), KitCustomData.class);
+        return new Result(kitCustomId, kitCustomEntity.getTitle(), kitCustomEntity.getKitId(), customData);
     }
 }
