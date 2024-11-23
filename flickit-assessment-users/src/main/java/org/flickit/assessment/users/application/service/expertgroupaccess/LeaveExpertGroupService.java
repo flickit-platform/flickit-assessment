07f8d877fddaf26174c97a4ccd47d2572642d8e3
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-23 11:06:40 +0330

    FAC-771: Feat - Complete `service` and `serviceTes`

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/expertgroupaccess/LeaveExpertGroupService.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/expertgroupaccess/LeaveExpertGroupService.java
@@ -1,18 +1,30 @@
 package org.flickit.assessment.users.application.service.expertgroupaccess;
 
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.users.application.port.out.expertgroupaccess.DeleteExpertGroupMemberPort;
+import org.flickit.assessment.users.application.port.out.expertgroupaccess.LoadExpertGroupAccessPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import org.flickit.assessment.users.application.port.in.expertgroupaccess.LeaveExpertGroupUseCase;
 
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class LeaveExpertGroupService implements LeaveExpertGroupUseCase {
 
+    private final LoadExpertGroupAccessPort loadExpertGroupAccessPort;
+    private final DeleteExpertGroupMemberPort deleteExpertGroupMemberPort;
+
     @Override
     public void leaveExpertGroup(Param param) {
+        var access = loadExpertGroupAccessPort.loadExpertGroupAccess(param.getExpertGroupId(), param.getCurrentUserId());
+        if (access.isEmpty())
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
+        deleteExpertGroupMemberPort.deleteMember(param.getExpertGroupId(), param.getCurrentUserId());
     }
 }
--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroupaccess/LeaveExpertGroupServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/expertgroupaccess/LeaveExpertGroupServiceTest.java
@@ -1,11 +1,72 @@
 package org.flickit.assessment.users.application.service.expertgroupaccess;
 
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.users.application.domain.ExpertGroupAccess;
+import org.flickit.assessment.users.application.port.in.expertgroupaccess.LeaveExpertGroupUseCase;
+import org.flickit.assessment.users.application.port.out.expertgroupaccess.DeleteExpertGroupMemberPort;
+import org.flickit.assessment.users.application.port.out.expertgroupaccess.LoadExpertGroupAccessPort;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class LeaveExpertGroupServiceTest {
 
+    @InjectMocks
+    private LeaveExpertGroupService service;
+
+    @Mock
+    private LoadExpertGroupAccessPort loadExpertGroupAccessPort;
+
+    @Mock
+    private DeleteExpertGroupMemberPort deleteExpertGroupMemberPort;
+
+
+    @Test
+    void tesLeaveExpertGroup_WhenUserHasNotAccess_ShouldThrowAccessDeniedException() {
+        var param = createParam(LeaveExpertGroupUseCase.Param.ParamBuilder::build);
+
+        when(loadExpertGroupAccessPort.loadExpertGroupAccess(param.getExpertGroupId(), param.getCurrentUserId()))
+            .thenReturn(Optional.empty());
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.leaveExpertGroup(param));
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+    }
+
+    @Test
+    void tesLeaveExpertGroup_ValidParameters_ShouldLeaveExpertGroup() {
+        var param = createParam(LeaveExpertGroupUseCase.Param.ParamBuilder::build);
+
+        when(loadExpertGroupAccessPort.loadExpertGroupAccess(param.getExpertGroupId(), param.getCurrentUserId()))
+            .thenReturn(Optional.ofNullable(mock(ExpertGroupAccess.class)));
+
+        service.leaveExpertGroup(param);
+
+        verify(loadExpertGroupAccessPort, times(1)).loadExpertGroupAccess(param.getExpertGroupId(), param.getCurrentUserId());
+    }
+
+
+
+    private LeaveExpertGroupUseCase.Param createParam(Consumer<LeaveExpertGroupUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        return paramBuilder.build();
+    }
+
+    private LeaveExpertGroupUseCase.Param.ParamBuilder paramBuilder() {
+        return LeaveExpertGroupUseCase.Param.builder()
+            .expertGroupId(123L)
+            .currentUserId(UUID.randomUUID());
+    }
 }
