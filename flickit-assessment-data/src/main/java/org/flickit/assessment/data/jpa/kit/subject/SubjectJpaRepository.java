80efbb45318dafb4d087fd3c654bd965290b3b00
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-11-24 08:41:58 +0330

    FAC-775 Remove redundant import

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/subject/SubjectJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/subject/SubjectJpaRepository.java
@@ -2,7 +2,6 @@ package org.flickit.assessment.data.jpa.kit.subject;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
