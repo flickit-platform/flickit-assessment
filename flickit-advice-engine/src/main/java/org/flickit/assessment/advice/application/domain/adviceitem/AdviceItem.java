16bdd3fec67d5acc894f3cb0797379420f00f2f4
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-27 11:11:23 +0330

    FAC-794: feat - Add Domain and CostType, ImpactType, PriorityType

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/domain/adviceitem/AdviceItem.java
@@ -0,0 +1,19 @@
+package org.flickit.assessment.advice.application.domain.adviceitem;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@RequiredArgsConstructor
+public class AdviceItem {
+
+    private final UUID id;
+    private final UUID assessmentResultId;
+    private final String title;
+    private final String description;
+    private final CostType cost;
+    private final ImpactType impact;
+    private final PriorityType priority;
+}
---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/domain/adviceitem/CostType.java
@@ -0,0 +1,21 @@
+package org.flickit.assessment.advice.application.domain.adviceitem;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum CostType {
+
+    LOW("Low"),
+    MEDIUM("Medium"),
+    HIGH("High");
+
+    private final String title;
+
+    public String getCode() {
+        return name();
+    }
+}
