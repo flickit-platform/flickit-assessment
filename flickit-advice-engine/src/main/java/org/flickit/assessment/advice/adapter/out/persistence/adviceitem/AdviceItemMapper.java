bc2462da674777d5e0fb2b4e6395b68b41a61d3b
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-28 15:57:02 +0330

    FAC-795: feat - Add Mapper to map toJpaEntity

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/adviceitem/AdviceItemMapper.java
@@ -0,0 +1,24 @@
+package org.flickit.assessment.advice.adapter.out.persistence.adviceitem;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.flickit.assessment.advice.application.domain.adviceitem.AdviceItem;
+import org.flickit.assessment.data.jpa.advice.adviceitem.AdviceItemJpaEntity;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AdviceItemMapper {
+
+    public static AdviceItemJpaEntity toJpaEntity(AdviceItem adviceItem) {
+        return new AdviceItemJpaEntity(null,
+            adviceItem.getTitle(),
+            adviceItem.getAssessmentResultId(),
+            adviceItem.getDescription(),
+            adviceItem.getCost().ordinal(),
+            adviceItem.getPriority().ordinal(),
+            adviceItem.getImpact().ordinal(),
+            adviceItem.getCreationTime(),
+            adviceItem.getCreationTime(),
+            adviceItem.getCreatedBy(),
+            adviceItem.getLastModifiedBy());
+    }
+}
