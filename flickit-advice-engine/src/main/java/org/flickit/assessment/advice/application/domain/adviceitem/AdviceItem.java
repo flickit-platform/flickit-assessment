fa73a8c429cae94d41f84e7380599a5204691472
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-28 15:53:09 +0330

    FAC-795: feat - Add port and Adapter

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/domain/adviceitem/AdviceItem.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/domain/adviceitem/AdviceItem.java
@@ -3,6 +3,7 @@ package org.flickit.assessment.advice.application.domain.adviceitem;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -10,10 +11,14 @@ import java.util.UUID;
 public class AdviceItem {
 
     private final UUID id;
-    private final UUID assessmentResultId;
     private final String title;
+    private final UUID assessmentResultId;
     private final String description;
     private final CostType cost;
     private final ImpactType impact;
     private final PriorityType priority;
+    private final LocalDateTime creationTime;
+    private final LocalDateTime lastModificationTime;
+    private final UUID createdBy;
+    private final UUID lastModifiedBy;
 }
