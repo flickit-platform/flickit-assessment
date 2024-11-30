78ae26d61e0a55c50eaf37790fea0e25dc4067a9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-11-30 17:29:40 +0330

    FAC-796 - feat - Add `findByAssessmentResultId` query to `AdviceItemJpaRepository`

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/advice/adviceitem/AdviceItemJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/advice/adviceitem/AdviceItemJpaRepository.java
@@ -1,8 +1,12 @@
 package org.flickit.assessment.data.jpa.advice.adviceitem;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface AdviceItemJpaRepository extends JpaRepository<AdviceItemJpaEntity, UUID> {
+
+    Page<AdviceItemJpaEntity> findByAssessmentResultId(UUID assessmentResultId, PageRequest pageRequest);
 }
