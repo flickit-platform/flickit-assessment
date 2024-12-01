7a65eac9c48d6d2458ee639cedaa419df7c84e83
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-01 11:31:30 +0330

    FAC-796: Refactor - Rename AdviceItemJpaAdapter to AdviceItemPersistenceJpaAdapter

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/adviceitem/AdviceItemPersistenceJpaAdapter.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/adviceitem/AdviceItemPersistenceJpaAdapter.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
-public class AdviceItemJpaAdapter implements
+public class AdviceItemPersistenceJpaAdapter implements
     CreateAdviceItemPort,
     LoadAdviceItemListPort {
 
