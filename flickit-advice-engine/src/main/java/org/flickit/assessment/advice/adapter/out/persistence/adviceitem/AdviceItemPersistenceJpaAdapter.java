e81f47b6c92fa532c559ff4acb27577bf14b1de8
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-01 20:44:51 +0330

    FAC-798: feat - Implement DeleteAdviceItem functionality in persistence adapter

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/adviceitem/AdviceItemPersistenceJpaAdapter.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/adviceitem/AdviceItemPersistenceJpaAdapter.java
@@ -3,14 +3,20 @@ package org.flickit.assessment.advice.adapter.out.persistence.adviceitem;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.advice.application.domain.adviceitem.AdviceItem;
 import org.flickit.assessment.advice.application.port.out.adviceitem.CreateAdviceItemPort;
+import org.flickit.assessment.advice.application.port.out.adviceitem.DeleteAdviceItemPort;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.data.jpa.advice.adviceitem.AdviceItemJpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+import static org.flickit.assessment.advice.common.ErrorMessageKey.ADVICE_ITEM_ID_NOT_FOUND;
+
 @Component
 @RequiredArgsConstructor
-public class AdviceItemPersistenceJpaAdapter implements CreateAdviceItemPort {
+public class AdviceItemPersistenceJpaAdapter implements
+    CreateAdviceItemPort,
+    DeleteAdviceItemPort {
 
     private final AdviceItemJpaRepository repository;
 
@@ -18,5 +24,13 @@ public class AdviceItemPersistenceJpaAdapter implements CreateAdviceItemPort {
     public UUID persist(AdviceItem adviceItem) {
         var entity = AdviceItemMapper.toJpaEntity(adviceItem);
         return repository.save(entity).getId();
+    }
+
+    @Override
+    public void deleteAdviceItem(UUID id) {
+        if (!repository.existsById(id))
+            throw new ResourceNotFoundException(ADVICE_ITEM_ID_NOT_FOUND);
+
+        repository.deleteById(id);
     }
 }
