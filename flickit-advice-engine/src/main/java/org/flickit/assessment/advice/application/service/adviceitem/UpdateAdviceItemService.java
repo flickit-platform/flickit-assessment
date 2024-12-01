967dc96a648468c380824461d8a7c1f1e4aa6acb
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-01 15:55:41 +0330

    FAC-796: Refactor - Complete `Service` and `ServiceTest`

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/service/adviceitem/UpdateAdviceItemService.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/service/adviceitem/UpdateAdviceItemService.java
@@ -1,14 +1,27 @@
 package org.flickit.assessment.advice.application.service.adviceitem;
 
+import org.flickit.assessment.advice.application.domain.adviceitem.AdviceItem;
+import org.flickit.assessment.advice.application.domain.adviceitem.CostLevel;
+import org.flickit.assessment.advice.application.domain.adviceitem.ImpactLevel;
+import org.flickit.assessment.advice.application.domain.adviceitem.PriorityLevel;
+import org.flickit.assessment.advice.application.port.in.adviceitem.UpdateAdviceItemUseCase;
 import org.flickit.assessment.advice.application.port.out.adviceitem.UpdateAdviceItemPort;
 import org.flickit.assessment.advice.application.port.out.assessmentresult.LoadAssessmentResultPort;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.application.port.out.ValidateAssessmentResultPort;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
-import org.flickit.assessment.advice.application.port.in.adviceitem.UpdateAdviceItemUseCase;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.flickit.assessment.advice.common.ErrorMessageKey.UPDATE_ADVICE_ITEM_ASSESSMENT_RESULT_NOT_FOUND;
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.CREATE_ADVICE;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
 @Service
 @Transactional
@@ -22,6 +35,32 @@ public class UpdateAdviceItemService implements UpdateAdviceItemUseCase {
 
     @Override
     public void updateAdviceItem(Param param) {
+        validateUserAccess(param.getAssessmentId(), param.getCurrentUserId());
 
+        var assessmentResult = loadAssessmentResultPort.loadByAssessmentId(param.getAssessmentId())
+            .orElseThrow(() -> new ResourceNotFoundException(UPDATE_ADVICE_ITEM_ASSESSMENT_RESULT_NOT_FOUND));
+
+        validateAssessmentResultPort.validate(param.getAssessmentId());
+
+        updateAdviceItemPort.updateAdviceItem(toAdviceItem(param, assessmentResult.getId()));
+    }
+
+    private void validateUserAccess(UUID assessmentId, UUID currentUserId) {
+        if (!assessmentAccessChecker.isAuthorized(assessmentId, currentUserId, CREATE_ADVICE))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+    }
+
+    private AdviceItem toAdviceItem(UpdateAdviceItemUseCase.Param param, UUID assessmentResultId) {
+        return new AdviceItem(param.getAdviceItemId(),
+            param.getTitle(),
+            assessmentResultId,
+            param.getDescription(),
+            CostLevel.valueOf(param.getCost()),
+            PriorityLevel.valueOf(param.getPriority()),
+            ImpactLevel.valueOf(param.getImpact()),
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            param.getCurrentUserId(),
+            param.getCurrentUserId());
     }
 }
--- flickit-advice-engine/src/test/java/org/flickit/assessment/advice/application/service/adviceitem/UpdateAdviceItemServiceTest.java
+++ flickit-advice-engine/src/test/java/org/flickit/assessment/advice/application/service/adviceitem/UpdateAdviceItemServiceTest.java
@@ -1,14 +1,98 @@
 package org.flickit.assessment.advice.application.service.adviceitem;
 
+import org.flickit.assessment.advice.application.domain.AssessmentResult;
+import org.flickit.assessment.advice.application.domain.adviceitem.AdviceItem;
 import org.flickit.assessment.advice.application.port.in.adviceitem.UpdateAdviceItemUseCase;
+import org.flickit.assessment.advice.application.port.out.adviceitem.UpdateAdviceItemPort;
+import org.flickit.assessment.advice.application.port.out.assessmentresult.LoadAssessmentResultPort;
+import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.application.port.out.ValidateAssessmentResultPort;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import static org.flickit.assessment.advice.common.ErrorMessageKey.UPDATE_ADVICE_ITEM_ASSESSMENT_RESULT_NOT_FOUND;
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.CREATE_ADVICE;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class UpdateAdviceItemServiceTest {
+
+    @InjectMocks
+    UpdateAdviceItemService service;
+
+    @Mock
+    LoadAssessmentResultPort loadAssessmentResultPort;
+
+    @Mock
+    AssessmentAccessChecker assessmentAccessChecker;
+
+    @Mock
+    ValidateAssessmentResultPort validateAssessmentResultPort;
+
+    @Mock
+    UpdateAdviceItemPort updateAdviceItemPort;
+
+    @Test
+    void testUpdateAdviceItem_whenUserIsNotAuthorized_thenThrowAccessDeniedException() {
+        var param = createParam(UpdateAdviceItemUseCase.Param.ParamBuilder::build);
+
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), CREATE_ADVICE)).thenReturn(false);
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.updateAdviceItem(param));
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+
+        verifyNoInteractions(loadAssessmentResultPort, validateAssessmentResultPort, updateAdviceItemPort);
+    }
+
+    @Test
+    void testUpdateAdviceItem_whenAssessmentResultNotExist_thenThrowResourceException() {
+        var param = createParam(UpdateAdviceItemUseCase.Param.ParamBuilder::build);
+
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), CREATE_ADVICE)).thenReturn(true);
+        when(loadAssessmentResultPort.loadByAssessmentId(param.getAssessmentId())).thenReturn(Optional.empty());
+
+        var throwable = assertThrows(ResourceNotFoundException.class, () -> service.updateAdviceItem(param));
+        assertEquals(UPDATE_ADVICE_ITEM_ASSESSMENT_RESULT_NOT_FOUND, throwable.getMessage());
+
+        verifyNoInteractions(validateAssessmentResultPort, updateAdviceItemPort);
+    }
+
+    @Test
+    void testUpdateAdviceItem_whenValidParameter_thenUpdatesAdviceItemSuccessfully() {
+        var param = createParam(UpdateAdviceItemUseCase.Param.ParamBuilder::build);
+        var assessmentResult = new AssessmentResult(UUID.randomUUID(), 1L);
+
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), CREATE_ADVICE)).thenReturn(true);
+        when(loadAssessmentResultPort.loadByAssessmentId(param.getAssessmentId())).thenReturn(Optional.of(assessmentResult));
+
+        service.updateAdviceItem(param);
+
+        ArgumentCaptor<AdviceItem> argumentCaptor = ArgumentCaptor.forClass(AdviceItem.class);
+        verify(updateAdviceItemPort).updateAdviceItem(argumentCaptor.capture());
+        assertEquals(param.getTitle(), argumentCaptor.getValue().getTitle());
+        assertEquals(assessmentResult.getId(), argumentCaptor.getValue().getAssessmentResultId());
+        assertEquals(param.getDescription(), argumentCaptor.getValue().getDescription());
+        assertEquals(param.getCost(), argumentCaptor.getValue().getCost().name());
+        assertEquals(param.getImpact(), argumentCaptor.getValue().getImpact().name());
+        assertEquals(param.getPriority(), argumentCaptor.getValue().getPriority().name());
+        assertEquals(param.getCurrentUserId(), argumentCaptor.getValue().getCreatedBy());
+        assertEquals(param.getCurrentUserId(), argumentCaptor.getValue().getLastModifiedBy());
+
+        verify(validateAssessmentResultPort, times(1)).validate(param.getAssessmentId());
+    }
 
 
     private UpdateAdviceItemUseCase.Param createParam(Consumer<UpdateAdviceItemUseCase.Param.ParamBuilder> changer) {
