0a5418fee2134a92fcbbf91a6750b66fdd292270
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-01 20:46:18 +0330

    FAC-798: feat - Implement DeleteAdviceItem service and tests

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/service/adviceitem/DeleteAdviceItemService.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/service/adviceitem/DeleteAdviceItemService.java
@@ -1,18 +1,36 @@
 package org.flickit.assessment.advice.application.service.adviceitem;
 
+import org.flickit.assessment.advice.application.port.out.adviceitem.DeleteAdviceItemPort;
+import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import org.flickit.assessment.advice.application.port.in.adviceitem.DeleteAdviceItemUseCase;
 
+import java.util.UUID;
+
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.CREATE_ADVICE;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class DeleteAdviceItemService implements DeleteAdviceItemUseCase {
 
+    private final AssessmentAccessChecker assessmentAccessChecker;
+    private final DeleteAdviceItemPort deleteAdviceItemPort;
+
     @Override
     public void deleteAdviceItem(Param param) {
+        validateUserAccess(param.getAssessmentId(), param.getCurrentUserId());
 
+        deleteAdviceItemPort.deleteAdviceItem(param.getAdviceItemId());
+    }
+
+    private void validateUserAccess(UUID assessmentId, UUID currentUserId) {
+        if (!assessmentAccessChecker.isAuthorized(assessmentId, currentUserId, CREATE_ADVICE))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
     }
 }
--- flickit-advice-engine/src/test/java/org/flickit/assessment/advice/application/service/adviceitem/DeleteAdviceItemServiceTest.java
+++ flickit-advice-engine/src/test/java/org/flickit/assessment/advice/application/service/adviceitem/DeleteAdviceItemServiceTest.java
@@ -1,21 +1,62 @@
 package org.flickit.assessment.advice.application.service.adviceitem;
 
 import org.flickit.assessment.advice.application.port.in.adviceitem.DeleteAdviceItemUseCase;
+import org.flickit.assessment.advice.application.port.out.adviceitem.DeleteAdviceItemPort;
+import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.CREATE_ADVICE;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class DeleteAdviceItemServiceTest {
 
-    private void createParam(Consumer<DeleteAdviceItemUseCase.Param.ParamBuilder> changer) {
+    @InjectMocks
+    DeleteAdviceItemService service;
+
+    @Mock
+    AssessmentAccessChecker assessmentAccessChecker;
+
+    @Mock
+    DeleteAdviceItemPort deleteAdviceItemPort;
+
+    @Test
+    void testDeleteAdviceItem_whenUserIsNotAuthorized_thenThrowAccessDeniedException() {
+        var param = createParam(DeleteAdviceItemUseCase.Param.ParamBuilder::build);
+
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), CREATE_ADVICE)).thenReturn(false);
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.deleteAdviceItem(param));
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+
+        verifyNoInteractions(deleteAdviceItemPort);
+    }
+
+    @Test
+    void testDeleteAdviceItem_whenParametersAreValid_thenThrowAccessDeniedException() {
+        var param = createParam(DeleteAdviceItemUseCase.Param.ParamBuilder::build);
+
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), CREATE_ADVICE)).thenReturn(true);
+
+        service.deleteAdviceItem(param);
+
+        verify(deleteAdviceItemPort).deleteAdviceItem(param.getAdviceItemId());
+    }
+
+    private DeleteAdviceItemUseCase.Param createParam(Consumer<DeleteAdviceItemUseCase.Param.ParamBuilder> changer) {
         var paramBuilder = paramBuilder();
         changer.accept(paramBuilder);
-        paramBuilder.build();
+        return paramBuilder.build();
     }
 
     private DeleteAdviceItemUseCase.Param.ParamBuilder paramBuilder() {
