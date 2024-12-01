f8e2140f15bc7548a3b49171de0a5acbefa34dc8
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-01 17:42:02 +0330

    FAC-798: feat - Add `DeleteAdviceItemUseCase`

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/port/in/adviceitem/DeleteAdviceItemUseCase.java
@@ -0,0 +1,44 @@
+package org.flickit.assessment.advice.application.port.in.adviceitem;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.flickit.assessment.common.application.SelfValidating;
+
+import java.util.UUID;
+
+import static org.flickit.assessment.advice.common.ErrorMessageKey.*;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
+
+
+public interface DeleteAdviceItemUseCase {
+
+    Result createAdviceItem(Param param);
+
+    @Value
+    @EqualsAndHashCode(callSuper = false)
+    class Param extends SelfValidating<Param> {
+
+        @NotNull(message = DELETE_ADVICE_ITEM_ID_NOT_NULL)
+        UUID adviceItemId;
+
+        @NotNull(message = DELETE_ADVICE_ITEM_ASSESSMENT_ID_NOT_NULL)
+        UUID assessmentId;
+
+        @NotNull(message = COMMON_CURRENT_USER_ID_NOT_NULL)
+        UUID currentUserId;
+
+        @Builder
+        public Param(UUID adviceItemId, UUID assessmentId, UUID currentUserId) {
+            this.adviceItemId = adviceItemId;
+            this.assessmentId = assessmentId;
+            this.currentUserId = currentUserId;
+            this.validateSelf();
+        }
+
+    }
+
+    record Result(UUID id) {
+    }
+}
