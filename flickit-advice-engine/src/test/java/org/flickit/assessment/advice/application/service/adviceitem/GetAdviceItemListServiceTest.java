3ed13f1a3ba8e2317855a243bed3f7f2d36ba0c8
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-12-02 14:41:19 +0330

    FAC-796: Create AdviceItemMother and use in tests

--- flickit-advice-engine/src/test/java/org/flickit/assessment/advice/application/service/adviceitem/GetAdviceItemListServiceTest.java
+++ flickit-advice-engine/src/test/java/org/flickit/assessment/advice/application/service/adviceitem/GetAdviceItemListServiceTest.java
@@ -1,10 +1,6 @@
 package org.flickit.assessment.advice.application.service.adviceitem;
 
 import org.flickit.assessment.advice.application.domain.AssessmentResult;
-import org.flickit.assessment.advice.application.domain.adviceitem.AdviceItem;
-import org.flickit.assessment.advice.application.domain.adviceitem.CostLevel;
-import org.flickit.assessment.advice.application.domain.adviceitem.ImpactLevel;
-import org.flickit.assessment.advice.application.domain.adviceitem.PriorityLevel;
 import org.flickit.assessment.advice.application.port.in.adviceitem.GetAdviceItemListUseCase;
 import org.flickit.assessment.advice.application.port.out.adviceitem.LoadAdviceItemListPort;
 import org.flickit.assessment.advice.application.port.out.assessmentresult.LoadAssessmentResultPort;
@@ -18,7 +14,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -26,6 +21,7 @@ import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.advice.common.ErrorMessageKey.GET_ADVICE_ITEM_LIST_ASSESSMENT_RESULT_NOT_FOUND;
+import static org.flickit.assessment.advice.test.fixture.application.AdviceItemMother.adviceItem;
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.VIEW_ASSESSMENT_REPORT;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.junit.Assert.assertThrows;
@@ -48,7 +44,7 @@ class GetAdviceItemListServiceTest {
     private LoadAdviceItemListPort loadAdviceItemListPort;
 
     @Test
-    void testGetAdviceItemList_whenUserDoesNotHaveAccess_thenThrowAccessDeniedException() {
+    void testGetAdviceItemList_whenUserIsNotAuthorized_thenThrowAccessDeniedException() {
         var param = createParam(GetAdviceItemListUseCase.Param.ParamBuilder::build);
 
         when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ASSESSMENT_REPORT)).thenReturn(false);
@@ -72,19 +68,24 @@ class GetAdviceItemListServiceTest {
     void testGetAdviceItemList_whenParametersAreValid_thenReturnsPaginatedAdviceItemList() {
         var param = createParam(GetAdviceItemListUseCase.Param.ParamBuilder::build);
         var assessmentResult = new AssessmentResult(UUID.randomUUID(), 123);
-        var items = List.of(
-            new AdviceItem(UUID.randomUUID(), "title1", UUID.randomUUID(), "description1",
-                CostLevel.HIGH, PriorityLevel.LOW, ImpactLevel.MEDIUM, LocalDateTime.now(), LocalDateTime.now(), UUID.randomUUID(), UUID.randomUUID()),
-            new AdviceItem(UUID.randomUUID(), "title2", UUID.randomUUID(), "description2",
-                CostLevel.MEDIUM, PriorityLevel.MEDIUM, ImpactLevel.HIGH, LocalDateTime.now(), LocalDateTime.now(), UUID.randomUUID(), UUID.randomUUID()));
+        var items = List.of(adviceItem(), adviceItem());
 
-        var expectedResult = new PaginatedResponse<>(items, param.getPage(), param.getSize(), "desc", "lastModificationTime", 2);
+        var expectedResult = new PaginatedResponse<>(items,
+            param.getPage(),
+            param.getSize(),
+            "desc,desc,desc",
+            "priority,impact,cost",
+            2);
 
         when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ASSESSMENT_REPORT)).thenReturn(true);
         when(loadAssessmentResultPort.loadByAssessmentId(param.getAssessmentId())).thenReturn(Optional.of(assessmentResult));
         when(loadAdviceItemListPort.loadAdviceItemList(assessmentResult.getId(), param.getPage(), param.getSize())).thenReturn(expectedResult);
 
         var result = service.getAdviceItems(param);
+        assertEquals(param.getPage(), result.getPage());
+        assertEquals(param.getSize(), result.getSize());
+        assertEquals(expectedResult.getItems().size(), result.getTotal());
+
         assertThat(result.getItems())
             .zipSatisfy(expectedResult.getItems(), (actual, expected) -> {
                 assertEquals(expected.getId(), actual.id());
@@ -94,9 +95,6 @@ class GetAdviceItemListServiceTest {
                 assertEquals(expected.getPriority().getTitle(), actual.priority());
                 assertEquals(expected.getImpact().getTitle(), actual.impact());
             });
-        assertEquals(param.getPage(), result.getPage());
-        assertEquals(param.getSize(), result.getSize());
-        assertEquals(expectedResult.getItems().size(), result.getTotal());
     }
 
     private GetAdviceItemListUseCase.Param createParam(Consumer<GetAdviceItemListUseCase.Param.ParamBuilder> changer) {
