bdff5cb621ea581c6bade8d2bf1562d19afbed00
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-02 11:30:44 +0330

    FAC-796: Refactor - Simplify AdviceItemJpaRepository query method name

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/advice/adviceitem/AdviceItemJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/advice/adviceitem/AdviceItemJpaRepository.java
@@ -8,5 +8,5 @@ import java.util.UUID;
 
 public interface AdviceItemJpaRepository extends JpaRepository<AdviceItemJpaEntity, UUID> {
 
-    Page<AdviceItemJpaEntity> findByAssessmentResultIdOrderByPriorityDescImpactDescCost(UUID assessmentResultId, PageRequest pageRequest);
+    Page<AdviceItemJpaEntity> findByAssessmentResultId(UUID assessmentResultId, PageRequest pageRequest);
 }
