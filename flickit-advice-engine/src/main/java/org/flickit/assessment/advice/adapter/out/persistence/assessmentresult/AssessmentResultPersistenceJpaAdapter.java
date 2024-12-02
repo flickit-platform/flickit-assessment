648583f0039d2bbfc61d86d418a79c3213fe4ba6
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-02 19:40:34 +0330

    FAC-808: feat - Add loadById method to LoadAssessmentResultPort and its implementation

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/assessmentresult/AssessmentResultPersistenceJpaAdapter.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/assessmentresult/AssessmentResultPersistenceJpaAdapter.java
@@ -16,6 +16,12 @@ public class AssessmentResultPersistenceJpaAdapter implements LoadAssessmentResu
     private final AssessmentResultJpaRepository repository;
 
     @Override
+    public Optional<AssessmentResult> loadById(UUID assessmentResultId) {
+        return repository.findById(assessmentResultId)
+            .map(AssessmentResultMapper::mapToDomain);
+    }
+
+    @Override
     public Optional<AssessmentResult> loadByAssessmentId(UUID assessmentId) {
         var entity = repository.findFirstByAssessment_IdOrderByLastModificationTimeDesc(assessmentId);
         return entity.map(AssessmentResultMapper::mapToDomain);
