003b9b123048dfc612ab2cdb2b6e0af5f91ace62
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-04 09:34:27 +0330

    FAC-804: Feat - Complete service

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/service/adviceitem/GetAdviceItemPriorityLevelsService.java
@@ -0,0 +1,25 @@
+package org.flickit.assessment.advice.application.service.adviceitem;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.advice.application.domain.adviceitem.CostLevel;
+import org.flickit.assessment.advice.application.domain.adviceitem.PriorityLevel;
+import org.flickit.assessment.advice.application.port.in.adviceitem.GetAdviceItemPriorityLevelsUseCase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class GetAdviceItemPriorityLevelsService implements GetAdviceItemPriorityLevelsUseCase {
+
+    @Override
+    public Result getPriorityLevels() {
+        List<AdviceItemPriorityLevel> items = Arrays.stream(PriorityLevel.values())
+            .map(e -> new AdviceItemPriorityLevel(e.getCode(), e.getTitle()))
+            .toList();
+        return new Result(items);
+    }
+}
