5ff14e73c34c7896b8b557314daacfe717b062a6
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-12-11 12:28:41 +0330

    FAC-778 Impl expert group deletion test scenario

---
+++ flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/test/users/expertgroup/DeleteExpertGroupScenarioTest.java
@@ -0,0 +1,39 @@
+package org.flickit.assessment.scenario.test.users.expertgroup;
+
+import org.flickit.assessment.data.jpa.users.expertgroup.ExpertGroupJpaEntity;
+import org.flickit.assessment.scenario.test.AbstractScenarioTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.flickit.assessment.scenario.fixture.request.CreateExpertGroupRequestDtoMother.createExpertGroupRequestDto;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DeleteExpertGroupScenarioTest extends AbstractScenarioTest {
+
+    @Autowired
+    ExpertGroupTestHelper expertGroupHelper;
+
+    @Test
+    void deleteExpertGroup() {
+        var request = createExpertGroupRequestDto();
+        var response = expertGroupHelper.create(context, request);
+
+        response.then()
+            .statusCode(201)
+            .body("id", notNullValue());
+
+        final Number expertGroupId = response.path("id");
+
+        var deleteResponse = expertGroupHelper.delete(context, expertGroupId.longValue());
+        deleteResponse.then()
+            .statusCode(204);
+
+        ExpertGroupJpaEntity expertGroupEntity = jpaTemplate.load(expertGroupId, ExpertGroupJpaEntity.class);
+        assertTrue(expertGroupEntity.isDeleted());
+
+        expertGroupHelper.create(context, request).then()
+            .statusCode(201)
+            .body("id", notNullValue());
+    }
+}
