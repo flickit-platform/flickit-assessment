223d80f6c49aaca092c2cc9623080993ed67bdbd
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-14 18:47:31 +0330

    FAC-819: Remove unused methods from AnswerOptionImpactJpaRepository

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/asnweroptionimpact/AnswerOptionImpactJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/asnweroptionimpact/AnswerOptionImpactJpaRepository.java
@@ -1,19 +1,12 @@
 package org.flickit.assessment.data.jpa.kit.asnweroptionimpact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 public interface AnswerOptionImpactJpaRepository extends JpaRepository<AnswerOptionImpactJpaEntity, AnswerOptionImpactJpaEntity.EntityId> {
 
-    List<AnswerOptionImpactJpaEntity> findAllByQuestionImpactIdAndKitVersionId(long impactId, long kitVersionId);
-
-    List<AnswerOptionImpactJpaEntity> findAllByKitVersionId(long kitVersonId);
 
     @Query("""
             SELECT oi as optionImpact,
@@ -23,18 +16,4 @@ public interface AnswerOptionImpactJpaRepository extends JpaRepository<AnswerOpt
             WHERE oi.kitVersionId = :kitVersionId AND oi.optionId IN :optionIds
         """)
     List<OptionImpactWithQuestionImpactView> findAllByOptionIdInAndKitVersionId(List<Long> optionIds, long kitVersionId);
-
-    @Modifying
-    @Query("""
-            UPDATE AnswerOptionImpactJpaEntity a
-            SET a.value = :value,
-                a.lastModificationTime = :lastModificationTime,
-                a.lastModifiedBy = :lastModifiedBy
-            WHERE a.id = :id AND a.kitVersionId = :kitVersionId
-        """)
-        void update(@Param("id") Long id,
-                    @Param("kitVersionId") Long kitVersionId,
-                    @Param("value") Double value,
-                    @Param("lastModificationTime") LocalDateTime lastModificationTime,
-                    @Param("lastModifiedBy") UUID lastModifiedBy);
 }
