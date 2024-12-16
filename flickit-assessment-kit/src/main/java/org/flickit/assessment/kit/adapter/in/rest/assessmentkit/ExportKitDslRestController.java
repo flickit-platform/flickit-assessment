85196e486d90fd083e2bee02bf3e3c904c366076
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-16 10:51:31 +0330

    FAC-822: feat - Add RestController

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/assessmentkit/ExportKitDslRestController.java
@@ -0,0 +1,31 @@
+package org.flickit.assessment.kit.adapter.in.rest.assessmentkit;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.flickit.assessment.kit.application.port.in.assessmentkit.ExportKitDslUseCase;
+import org.flickit.assessment.kit.application.port.in.assessmentkit.ExportKitDslUseCase.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class ExportKitDslRestController {
+
+    ExportKitDslUseCase useCase;
+    UserContext userContext;
+
+    @GetMapping("/assessment-kits/{kitId}/export")
+    ResponseEntity<Result> exportKitDsl(@PathVariable Long kitId) {
+        Result result = useCase.export(toParam(kitId, userContext.getUser().id()));
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    private Param toParam(Long kitId, UUID currentUserId) {
+        return new Param(kitId, currentUserId);
+    }
+}
