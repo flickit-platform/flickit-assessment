4eba261e68b664404853730ad9053b41d5a91c5d
m.khorsandi <khorsandi.mahnaz@gmail.com>
2024-12-21 14:44:00 +0330

    FAC-827: Minor format

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/users/expertgroup/ExpertGroupMembersView.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/users/expertgroup/ExpertGroupMembersView.java
@@ -6,12 +6,20 @@ import java.util.UUID;
 public interface ExpertGroupMembersView {
 
     UUID getId();
+
     Long getExpertGroupId();
+
     String getEmail();
+
     String getDisplayName();
+
     String getBio();
+
     String getPicture();
+
     String getLinkedin();
+
     int getStatus();
+
     LocalDateTime getInviteExpirationDate();
 }
