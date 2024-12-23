21d083679ba3e2c48364cd426e3674b88f8846e4
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-23 12:05:27 +0330

    FAC-828: Fix - Minor fix

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/evidence/EvidenceJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/evidence/EvidenceJpaRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
