8632881e1bd3467b9ae5e0cd33e3a00aeae8f607
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-12-29 17:04:29 +0330

    FAC-842 Impl init subject insight web adapter

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/subjectinsight/InitSubjectInsightRestController.java
@@ -0,0 +1,29 @@
+package org.flickit.assessment.core.adapter.in.rest.subjectinsight;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.core.application.port.in.subjectinsight.InitSubjectInsightUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class InitSubjectInsightRestController {
+
+    private final InitSubjectInsightUseCase useCase;
+
+    @PostMapping("/assessments/{assessmentId}/insights/subjects/{subjectId}/init")
+    public ResponseEntity<Void> initSubjectInsight(@PathVariable("assessmentId")UUID assessmentId,
+                                                   @PathVariable("subjectId") Long subjectId) {
+        useCase.initSubjectInsight(toParam(assessmentId, subjectId));
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    private static InitSubjectInsightUseCase.Param toParam(UUID assessmentId, Long subjectId) {
+        return new InitSubjectInsightUseCase.Param(assessmentId, subjectId);
+    }
+}
