b57edd6a9073c9ef67cf0403441fa8391c5656e7
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-12-29 09:31:18 +0330

    FAC-840: feat - Add type to RequestDto

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/in/rest/space/CreateSpaceRequestDto.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/in/rest/space/CreateSpaceRequestDto.java
@@ -3,5 +3,6 @@ package org.flickit.assessment.users.adapter.in.rest.space;
 import lombok.Builder;
 
 @Builder
-public record CreateSpaceRequestDto(String title) {
+public record CreateSpaceRequestDto(String title,
+                                    String type) {
 }
