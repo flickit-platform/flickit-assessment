2b722246ebb1e41b07df3cbc71cb367474620b4e
Ali Sedaghat <a.sedaghat1372@gmail.com>
2024-12-31 18:17:35 +0330

    FAC-838 Correct ApproveSubjectInsightUseCaseParamTest test names

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/subjectinsight/ApproveSubjectInsightService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/subjectinsight/ApproveSubjectInsightService.java
@@ -2,13 +2,13 @@ package org.flickit.assessment.core.application.service.subjectinsight;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
-import org.flickit.assessment.common.application.domain.assessment.AssessmentPermission;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.core.application.port.in.subjectinsight.ApproveSubjectInsightUseCase;
 import org.flickit.assessment.core.application.port.out.subjectinsight.ApproveSubjectInsightPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.APPROVE_SUBJECT_INSIGHT;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
 @Service
@@ -21,9 +21,7 @@ public class ApproveSubjectInsightService implements ApproveSubjectInsightUseCas
 
     @Override
     public void approveSubjectInsight(Param param) {
-        if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(),
-            param.getCurrentUserId(),
-            AssessmentPermission.APPROVE_SUBJECT_INSIGHT))
+        if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), APPROVE_SUBJECT_INSIGHT))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
         approveSubjectInsightPort.approve(param.getAssessmentId(), param.getSubjectId());
