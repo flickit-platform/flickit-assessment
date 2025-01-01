ff94bdfbc573f30c867411273ce7cc8dcabe1353
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-01 09:42:26 +0330

    FAC-836: Refactor - Remove Loading AssessmentResult from `QuestionnairePersistenceJpaAdapter` and add it to service

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/questionnaire/GetAssessmentQuestionnaireListService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/questionnaire/GetAssessmentQuestionnaireListService.java
@@ -4,14 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.core.application.domain.AssessmentResult;
 import org.flickit.assessment.core.application.domain.QuestionnaireListItem;
 import org.flickit.assessment.core.application.port.in.questionnaire.GetAssessmentQuestionnaireListUseCase;
+import org.flickit.assessment.core.application.port.out.assessmentresult.LoadAssessmentResultPort;
 import org.flickit.assessment.core.application.port.out.questionnaire.LoadQuestionnairesByAssessmentIdPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.VIEW_ASSESSMENT_QUESTIONNAIRE_LIST;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.core.common.ErrorMessageKey.GET_ASSESSMENT_QUESTIONNAIRE_LIST_ASSESSMENT_RESULT_ID_NOT_FOUND;
 
 @Service
 @Transactional(readOnly = true)
@@ -20,13 +24,16 @@ public class GetAssessmentQuestionnaireListService implements GetAssessmentQuest
 
     private final LoadQuestionnairesByAssessmentIdPort loadQuestionnairesByAssessmentIdPort;
     private final AssessmentAccessChecker assessmentAccessChecker;
+    private final LoadAssessmentResultPort loadAssessmentResultPort;
 
     @Override
     public PaginatedResponse<QuestionnaireListItem> getAssessmentQuestionnaireList(Param param) {
         if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ASSESSMENT_QUESTIONNAIRE_LIST))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+        var assessmentResult = loadAssessmentResultPort.loadByAssessmentId(param.getAssessmentId())
+            .orElseThrow(() -> new ResourceNotFoundException(GET_ASSESSMENT_QUESTIONNAIRE_LIST_ASSESSMENT_RESULT_ID_NOT_FOUND));
 
-        var questionnaires = loadQuestionnairesByAssessmentIdPort.loadAllByAssessmentId(toPortParam(param));
+        var questionnaires = loadQuestionnairesByAssessmentIdPort.loadAllByAssessmentId(toPortParam(param, assessmentResult));
 
         return buildResultWithIssues(questionnaires);
     }
@@ -61,9 +68,10 @@ public class GetAssessmentQuestionnaireListService implements GetAssessmentQuest
             0);
     }
 
-    private LoadQuestionnairesByAssessmentIdPort.Param toPortParam(Param param) {
+    private LoadQuestionnairesByAssessmentIdPort.Param toPortParam(Param param, AssessmentResult assessmentResult) {
         return new LoadQuestionnairesByAssessmentIdPort.Param(
             param.getAssessmentId(),
+            assessmentResult,
             param.getSize(),
             param.getPage());
     }
