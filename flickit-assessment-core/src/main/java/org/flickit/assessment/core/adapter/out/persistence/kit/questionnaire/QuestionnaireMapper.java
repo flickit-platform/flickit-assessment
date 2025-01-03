12fc9b03186d4b280f996065492a0ff7f4c81a72
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-03 14:09:26 +0330

    FAC-836: Refactor - Remove useless TODO

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/kit/questionnaire/QuestionnaireMapper.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/kit/questionnaire/QuestionnaireMapper.java
@@ -35,7 +35,7 @@ public class QuestionnaireMapper {
             nextQuestion,
             progress,
             subjects,
-            null //TODO: consideration needed
+            null
         );
     }
 
