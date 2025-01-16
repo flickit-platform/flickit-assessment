47e25d880da2728fe5a54bae1b7d009992445f50
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-16 11:41:04 +0330

    FAC-884: Feat - Add New error message

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
@@ -299,5 +299,5 @@ public class ErrorMessageKey {
 
     public static final String GET_QUESTION_ISSUES_QUESTION_ID_NOT_NULL = "get-question-issues.questionId.notNull";
     public static final String GET_QUESTION_ISSUES_ASSESSMENT_ID_NOT_NULL = "get-question-issues.assessmentId.notNull";
-    public static final String GET_QUESTION_ISSUES_CURRENT_USER_ID_NOT_NULL = "get-question-issues.currentUserId.notNull";
+    public static final String GET_QUESTION_ISSUES_ASSESSMENT_RESULT_NOT_FOUND = "get-question-issues.assessmentResult.notFound";
 }
