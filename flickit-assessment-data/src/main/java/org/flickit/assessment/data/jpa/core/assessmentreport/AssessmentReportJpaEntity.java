4b5be6d4e9d61a73ec123bd57f82034efddd5095
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-18 19:41:07 +0330

    FAC-885: Feat: Add `AssessmentReportJpaEntity`

---
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/assessmentreport/AssessmentReportJpaEntity.java
@@ -0,0 +1,33 @@
+package org.flickit.assessment.data.jpa.core.assessmentreport;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldNameConstants;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "fac_assessment_report")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldNameConstants
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class AssessmentReportJpaEntity {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "assessment_result_id" , nullable = false)
+    private UUID assessmentResultId;
+
+    @Column(name = "meta_data", nullable = false)
+    private String metaData;
+}
