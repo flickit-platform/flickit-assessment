1dd4165503d51424aef3bc7976ad8e1a7bfb94ab
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-01-18 17:59:42 +0330

    FAC-879: Add new error code to ErrorCodes constant file

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/exception/handler/UnsupportedOperationExceptionHandler.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/exception/handler/UnsupportedOperationExceptionHandler.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static org.flickit.assessment.common.exception.api.ErrorCodes.UNSUPPORTED_OPERATION;
+
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class UnsupportedOperationExceptionHandler {
@@ -17,7 +19,7 @@ public class UnsupportedOperationExceptionHandler {
     @ResponseBody
     @ExceptionHandler(UnsupportedOperationException.class)
     @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    public ErrorResponseDto handleUnsupportedOperationException(UnsupportedOperationException ex) {
-        return new ErrorResponseDto("UNSUPPORTED_OPERATION", MessageBundle.message(ex.getMessage()));
+    public ErrorResponseDto handle(UnsupportedOperationException ex) {
+        return new ErrorResponseDto(UNSUPPORTED_OPERATION, MessageBundle.message(ex.getMessage()));
     }
 }
