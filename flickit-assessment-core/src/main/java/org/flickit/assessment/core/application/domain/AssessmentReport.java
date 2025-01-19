9ceed33c74e79bcb2877fea413a884b759798195
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-19 13:45:42 +0330

    FAC-888: Feat - add `AssessmentReport` and `AssessmentReportMetadata` to domain

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AssessmentReport.java
@@ -0,0 +1,12 @@
+package org.flickit.assessment.core.application.domain;
+
+import java.util.UUID;
+
+public class AssessmentReport {
+
+    UUID id;
+
+    UUID assessmentResultId;
+
+    AssessmentReportMetadata metadata;
+}
---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AssessmentReportMetadata.java
@@ -0,0 +1,9 @@
+package org.flickit.assessment.core.application.domain;
+
+public class AssessmentReportMetadata {
+
+    private String intro;
+    private String prosAndCons;
+    private String steps;
+    private String participants;
+}
