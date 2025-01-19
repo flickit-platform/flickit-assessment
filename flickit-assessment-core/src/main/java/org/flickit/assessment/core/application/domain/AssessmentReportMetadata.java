25a96ca299120b835669d3745593a137d13dc4de
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-19 15:28:34 +0330

    FAC-888: Refactor - enrich `AssessmentReportMetadata` in domain

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AssessmentReportMetadata.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AssessmentReportMetadata.java
@@ -1,9 +1,14 @@
 package org.flickit.assessment.core.application.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class AssessmentReportMetadata {
 
-    private String intro;
-    private String prosAndCons;
-    private String steps;
-    private String participants;
+    private final String intro;
+    private final String prosAndCons;
+    private final String steps;
+    private final String participants;
 }
