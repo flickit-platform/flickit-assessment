d7c9baf7b9a6f322e2b632b593f59c8ad652a346
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-19 14:01:50 +0330

    FAC-888: Feat - Add `AssessmentReportPersistenceJpaAdapter`

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/assessmentreport/AssessmentReportPersistenceJpaAdapter.java
@@ -0,0 +1,21 @@
+package org.flickit.assessment.core.adapter.out.persistence.assessmentreport;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.core.application.domain.AssessmentReportMetadata;
+import org.flickit.assessment.core.application.port.out.assessmentreport.LoadAssessmentReportMetaDataPort;
+import org.flickit.assessment.data.jpa.core.assessmentreport.AssessmentReportJpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class AssessmentReportPersistenceJpaAdapter implements LoadAssessmentReportMetaDataPort {
+
+    private final AssessmentReportJpaRepository repository;
+
+    @Override
+    public AssessmentReportMetadata loadMetaData(UUID assessmentId) {
+        return null;
+    }
+}
