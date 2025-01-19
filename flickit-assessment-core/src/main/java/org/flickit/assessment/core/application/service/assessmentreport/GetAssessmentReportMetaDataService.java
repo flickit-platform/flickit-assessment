77a86a36300ff2d894728d1d20f55b07ec9de43b
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-19 15:39:49 +0330

    FAC-888: Feet - Complete `Service`

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentreport/GetAssessmentReportMetaDataService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentreport/GetAssessmentReportMetaDataService.java
@@ -3,6 +3,8 @@ package org.flickit.assessment.core.application.service.assessmentreport;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentPermission;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.core.application.domain.AssessmentReportMetadata;
+import org.flickit.assessment.core.application.port.out.assessmentreport.LoadAssessmentReportMetaDataPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -12,17 +14,23 @@ import org.flickit.assessment.core.application.port.in.assessmentreport.GetAsses
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
 @Service
-@Transactional (readOnly = true)
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class GetAssessmentReportMetaDataService implements GetAssessmentReportMetaDataUseCase {
 
     private final AssessmentAccessChecker assessmentAccessChecker;
+    private final LoadAssessmentReportMetaDataPort loadAssessmentReportMetaDataPort;
 
     @Override
     public Result getAssessmentReportMetaData(Param param) {
-        if(!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), AssessmentPermission.VIEW_ASSESSMENT_REPORT))
+        if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), AssessmentPermission.MANAGE_REPORT_METADATA))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
-        return null;
+        var assessmentReportMetaData = loadAssessmentReportMetaDataPort.loadMetadata(param.getAssessmentId());
+        return toResult(assessmentReportMetaData);
+    }
+
+    private Result toResult(AssessmentReportMetadata metadata) {
+        return new Result(metadata.getIntro(), metadata.getProsAndCons(), metadata.getSteps(), metadata.getParticipants());
     }
 }
