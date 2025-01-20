32c596d07c831c7d95c9aa6e244f48aaad3f349f
mahdirezazadeh <riza.rs2000@gmail.com>
2025-01-20 13:37:49 +0330

    FAC-891:impl:Update lastModificationTime in ApproveAttributeInsightService

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/attributeinsight/AttributeInsightPersistenceJpaAdapter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/attributeinsight/AttributeInsightPersistenceJpaAdapter.java
@@ -8,6 +8,7 @@ import org.flickit.assessment.data.jpa.core.assessmentresult.AssessmentResultJpa
 import org.flickit.assessment.data.jpa.core.attributeinsight.AttributeInsightJpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -69,7 +70,7 @@ public class AttributeInsightPersistenceJpaAdapter implements
     }
 
     @Override
-    public void approve(UUID assessmentId, long attributeId) {
+    public void approve(UUID assessmentId, long attributeId, LocalDateTime lastModificationTime) {
         var assessmentResultId = assessmentResultRepository.findFirstByAssessment_IdOrderByLastModificationTimeDesc(assessmentId)
             .orElseThrow(() -> new ResourceNotFoundException(APPROVE_ATTRIBUTE_INSIGHT_ASSESSMENT_RESULT_NOT_FOUND))
             .getId();
@@ -77,6 +78,6 @@ public class AttributeInsightPersistenceJpaAdapter implements
         if (!repository.existsByAssessmentResultIdAndAttributeId(assessmentResultId, attributeId))
             throw new ResourceNotFoundException(ATTRIBUTE_INSIGHT_ID_NOT_FOUND);
 
-        repository.approve(assessmentResultId, attributeId);
+        repository.approve(assessmentResultId, attributeId, lastModificationTime);
     }
 }
