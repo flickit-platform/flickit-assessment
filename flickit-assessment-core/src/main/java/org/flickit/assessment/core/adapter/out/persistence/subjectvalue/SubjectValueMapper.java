4cae63b4b9930667016b7197a0b0beb5e4e32383
mahdirezazadeh <riza.rs2000@gmail.com>
2025-01-25 12:46:18 +0330

    FAC-887:impl:Add maturity level and confidence value to subject

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/subjectvalue/SubjectValueMapper.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/subjectvalue/SubjectValueMapper.java
@@ -3,10 +3,12 @@ package org.flickit.assessment.core.adapter.out.persistence.subjectvalue;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.flickit.assessment.core.adapter.out.persistence.kit.attribute.AttributeMapper;
+import org.flickit.assessment.core.adapter.out.persistence.kit.maturitylevel.MaturityLevelMapper;
 import org.flickit.assessment.core.adapter.out.persistence.kit.subject.SubjectMapper;
 import org.flickit.assessment.core.application.domain.SubjectValue;
 import org.flickit.assessment.data.jpa.core.subjectvalue.SubjectValueJpaEntity;
 import org.flickit.assessment.data.jpa.kit.attribute.AttributeJpaEntity;
+import org.flickit.assessment.data.jpa.kit.maturitylevel.MaturityLevelJpaEntity;
 import org.flickit.assessment.data.jpa.kit.subject.SubjectJpaEntity;
 
 import java.util.ArrayList;
@@ -34,13 +36,19 @@ public class SubjectValueMapper {
         );
     }
 
-    public static SubjectValue mapToDomainModel(SubjectValueJpaEntity entity, SubjectJpaEntity subjectEntity, List<AttributeJpaEntity> attributeEntities) {
+    public static SubjectValue mapToDomainModel(SubjectValueJpaEntity entity,
+                                                SubjectJpaEntity subjectEntity,
+                                                List<AttributeJpaEntity> attributeEntities,
+                                                MaturityLevelJpaEntity maturityLevelEntity) {
         var attributes = attributeEntities.stream().map(AttributeMapper::mapToDomainModel).toList();
         var subject = SubjectMapper.mapToDomainModel(subjectEntity, attributes);
         return new SubjectValue(
             entity.getId(),
             subject,
-            new ArrayList<>()
+            new ArrayList<>(),
+            MaturityLevelMapper.mapToDomainModel(maturityLevelEntity, null),
+            entity.getConfidenceValue(),
+            null
         );
     }
 }
--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/subjectvalue/SubjectValuePersistenceJpaAdapter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/subjectvalue/SubjectValuePersistenceJpaAdapter.java
@@ -11,6 +11,8 @@ import org.flickit.assessment.data.jpa.core.subjectvalue.SubjectValueJpaEntity;
 import org.flickit.assessment.data.jpa.core.subjectvalue.SubjectValueJpaRepository;
 import org.flickit.assessment.data.jpa.kit.attribute.AttributeJpaEntity;
 import org.flickit.assessment.data.jpa.kit.attribute.AttributeJpaRepository;
+import org.flickit.assessment.data.jpa.kit.maturitylevel.MaturityLevelJpaEntity;
+import org.flickit.assessment.data.jpa.kit.maturitylevel.MaturityLevelJpaRepository;
 import org.flickit.assessment.data.jpa.kit.subject.SubjectJpaEntity;
 import org.flickit.assessment.data.jpa.kit.subject.SubjectJpaRepository;
 import org.springframework.stereotype.Component;
@@ -19,8 +21,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.function.Function;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.*;
 import static org.flickit.assessment.core.adapter.out.persistence.subjectvalue.SubjectValueMapper.mapToDomainModel;
 import static org.flickit.assessment.core.common.ErrorMessageKey.ASSESSMENT_ID_NOT_FOUND;
 import static org.flickit.assessment.core.common.ErrorMessageKey.CREATE_SUBJECT_VALUE_ASSESSMENT_RESULT_ID_NOT_FOUND;
@@ -36,6 +37,7 @@ public class SubjectValuePersistenceJpaAdapter implements
     private final AssessmentResultJpaRepository assessmentResultRepository;
     private final SubjectValueJpaRepository subjectValueRepository;
     private final AttributeJpaRepository attributeRepository;
+    private final MaturityLevelJpaRepository maturityLevelRepository;
 
     @Override
     public List<SubjectValue> persistAll(List<Long> subjectIds, UUID assessmentResultId) {
@@ -65,15 +67,22 @@ public class SubjectValuePersistenceJpaAdapter implements
     public List<SubjectValue> loadByAssessmentId(UUID assessmentId) {
         var assessmentResult = assessmentResultRepository.findFirstByAssessment_IdOrderByLastModificationTimeDesc(assessmentId)
             .orElseThrow(() -> new ResourceNotFoundException(ASSESSMENT_ID_NOT_FOUND));
+        var kitVersionId = assessmentResult.getKitVersionId();
         var subjectValuesEntities = subjectValueRepository.findByAssessmentResultId(assessmentResult.getId());
-        var subjectEntities = subjectRepository.findAllByKitVersionIdOrderByIndex(assessmentResult.getKitVersionId());
+        var subjectEntities = subjectRepository.findAllByKitVersionIdOrderByIndex(kitVersionId);
         var subjectIdToValueMap = subjectValuesEntities.stream()
             .collect(toMap(SubjectValueJpaEntity::getSubjectId, Function.identity()));
-        var subjectIdToAttributeMap = attributeRepository.findAllByKitVersionId(assessmentResult.getKitVersionId())
+        var subjectIdToAttributeMap = attributeRepository.findAllByKitVersionId(kitVersionId)
             .stream().collect(groupingBy(AttributeJpaEntity::getSubjectId));
+        var maturityLevelsId = subjectValuesEntities.stream().map(SubjectValueJpaEntity::getMaturityLevelId).collect(toSet());
+        var maturityLevelEntityMap = maturityLevelRepository.findAllByIdInAndKitVersionId(maturityLevelsId, kitVersionId)
+            .stream().collect(toMap(MaturityLevelJpaEntity::getId, Function.identity()));
 
         return subjectEntities.stream()
-            .map(s -> mapToDomainModel(subjectIdToValueMap.get(s.getId()), s, subjectIdToAttributeMap.get(s.getId())))
+            .map(s -> {
+                var sv = subjectIdToValueMap.get(s.getId());
+                return mapToDomainModel(sv, s, subjectIdToAttributeMap.get(s.getId()), maturityLevelEntityMap.get(sv.getMaturityLevelId()));
+            })
             .toList();
     }
 }
