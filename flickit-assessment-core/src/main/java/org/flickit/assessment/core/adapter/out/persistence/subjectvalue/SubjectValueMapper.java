ad153f92e5b7e5bdfdc5527f534daaf492358090
mahdirezazadeh <riza.rs2000@gmail.com>
2025-01-27 11:01:55 +0330

    FAC-887:impl:Remove useless SubjectValueMapper

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/subjectvalue/SubjectValueMapper.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/subjectvalue/SubjectValueMapper.java
@@ -2,17 +2,12 @@ package org.flickit.assessment.core.adapter.out.persistence.subjectvalue;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.flickit.assessment.core.adapter.out.persistence.kit.attribute.AttributeMapper;
-import org.flickit.assessment.core.adapter.out.persistence.kit.maturitylevel.MaturityLevelMapper;
 import org.flickit.assessment.core.adapter.out.persistence.kit.subject.SubjectMapper;
 import org.flickit.assessment.core.application.domain.SubjectValue;
 import org.flickit.assessment.data.jpa.core.subjectvalue.SubjectValueJpaEntity;
-import org.flickit.assessment.data.jpa.kit.attribute.AttributeJpaEntity;
-import org.flickit.assessment.data.jpa.kit.maturitylevel.MaturityLevelJpaEntity;
 import org.flickit.assessment.data.jpa.kit.subject.SubjectJpaEntity;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SubjectValueMapper {
@@ -33,22 +28,6 @@ public class SubjectValueMapper {
             entity.getId(),
             subject,
             new ArrayList<>()
-        );
-    }
-
-    public static SubjectValue mapToDomainModel(SubjectValueJpaEntity entity,
-                                                SubjectJpaEntity subjectEntity,
-                                                List<AttributeJpaEntity> attributeEntities,
-                                                MaturityLevelJpaEntity maturityLevelEntity) {
-        var attributes = attributeEntities.stream().map(AttributeMapper::mapToDomainModel).toList();
-        var subject = SubjectMapper.mapToDomainModel(subjectEntity, attributes);
-        return new SubjectValue(
-            entity.getId(),
-            subject,
-            new ArrayList<>(),
-            MaturityLevelMapper.mapToDomainModel(maturityLevelEntity, null),
-            entity.getConfidenceValue(),
-            null
         );
     }
 }
