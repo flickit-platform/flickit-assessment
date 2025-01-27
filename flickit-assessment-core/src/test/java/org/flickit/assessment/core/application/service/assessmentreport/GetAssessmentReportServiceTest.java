ba77abb6b5a397675a9bab37795e0b20c2c23db6
mahdirezazadeh <riza.rs2000@gmail.com>
2025-01-27 11:04:18 +0330

    FAC-887:impl:Impl GetAssessmentReportServiceTest

---
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessmentreport/GetAssessmentReportServiceTest.java
@@ -0,0 +1,157 @@
+package org.flickit.assessment.core.application.service.assessmentreport;
+
+import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.application.port.out.ValidateAssessmentResultPort;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.CalculateNotValidException;
+import org.flickit.assessment.core.application.domain.AssessmentReport;
+import org.flickit.assessment.core.application.domain.AssessmentReportMetadata;
+import org.flickit.assessment.core.application.domain.MaturityLevel;
+import org.flickit.assessment.core.application.domain.report.AssessmentReportItem;
+import org.flickit.assessment.core.application.domain.report.AssessmentSubjectReportItem;
+import org.flickit.assessment.core.application.domain.report.QuestionnaireReportItem;
+import org.flickit.assessment.core.application.port.in.assessmentreport.GetAssessmentReportUseCase;
+import org.flickit.assessment.core.application.port.out.assessmentreport.LoadAssessmentReportPort;
+import org.flickit.assessment.core.application.port.out.assessmentresult.LoadAssessmentReportInfoPort;
+import org.flickit.assessment.core.test.fixture.application.MaturityLevelMother;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.VIEW_GRAPHICAL_REPORT;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_ASSESSMENT_RESULT_NOT_VALID;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class GetAssessmentReportServiceTest {
+
+    @InjectMocks
+    private GetAssessmentReportService service;
+
+    @Mock
+    private AssessmentAccessChecker assessmentAccessChecker;
+
+    @Mock
+    private LoadAssessmentReportInfoPort loadAssessmentReportInfoPort;
+
+    @Mock
+    private LoadAssessmentReportPort loadAssessmentReportPort;
+
+    @Mock
+    private ValidateAssessmentResultPort validateAssessmentResultPort;
+
+    private static AssessmentReport createReport() {
+        return new AssessmentReport(null, null,
+            new AssessmentReportMetadata("intro", "pros", "steps", "participants"), false);
+    }
+
+    @Test
+    void testGetAssessmentReport_whenCurrentUserDoesNotHaveRequiredPermission_thenThrowAccessDeniedException() {
+        var param = createParam(GetAssessmentReportUseCase.Param.ParamBuilder::build);
+
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_GRAPHICAL_REPORT))
+            .thenReturn(false);
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.getAssessmentReport(param));
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+
+        verifyNoInteractions(loadAssessmentReportInfoPort, loadAssessmentReportPort, validateAssessmentResultPort);
+    }
+
+    @Test
+    void testGetAssessmentReport_AssessmentCalculateIsNotValid_ThrowException() {
+        var param = createParam(GetAssessmentReportUseCase.Param.ParamBuilder::build);
+
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_GRAPHICAL_REPORT))
+            .thenReturn(true);
+        doThrow(new CalculateNotValidException(COMMON_ASSESSMENT_RESULT_NOT_VALID))
+            .when(validateAssessmentResultPort).validate(param.getAssessmentId());
+
+        var throwable = assertThrows(CalculateNotValidException.class, () -> service.getAssessmentReport(param));
+        assertEquals(COMMON_ASSESSMENT_RESULT_NOT_VALID, throwable.getMessage());
+
+        verifyNoInteractions(loadAssessmentReportInfoPort, loadAssessmentReportPort);
+    }
+
+    @Test
+    void testGetAssessmentReport_AssessmentCalculateIsValid_ReturnReport() {
+        var param = createParam(GetAssessmentReportUseCase.Param.ParamBuilder::build);
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_GRAPHICAL_REPORT))
+            .thenReturn(true);
+        doNothing()
+            .when(validateAssessmentResultPort).validate(param.getAssessmentId());
+
+        AssessmentReportItem assessmentReport = createAssessmentReportItem(param);
+
+        MaturityLevel teamLevel = MaturityLevelMother.levelTwo();
+        var subjects = List.of(
+            new AssessmentSubjectReportItem(2L, "team", 2, "subjectDesc2", 58.6, teamLevel, List.of()));
+        var assessmentReportInfo = new LoadAssessmentReportInfoPort.Result(assessmentReport, subjects);
+        AssessmentReport report = createReport();
+
+        when(loadAssessmentReportInfoPort.load(param.getAssessmentId())).thenReturn(assessmentReportInfo);
+        when(loadAssessmentReportPort.load(param.getAssessmentId())).thenReturn(Optional.of(report));
+        var result = service.getAssessmentReport(param);
+
+
+        assertEquals(assessmentReport.title(), result.assessment().title());
+        assertEquals(report.getMetadata().intro(), result.assessment().intro());
+        assertEquals(assessmentReport.insight(), result.assessment().overallInsight());
+        assertEquals(report.getMetadata().prosAndCons(), result.assessment().prosAndCons());
+        assertEquals(assessmentReport.maturityLevel().getId(), result.assessment().maturityLevel().id());
+        assertEquals(assessmentReport.maturityLevel().getTitle(), result.assessment().maturityLevel().title());
+        assertEquals(assessmentReport.assessmentKit().id(), result.assessment().assessmentKit().id());
+        assertEquals(assessmentReport.assessmentKit().title(), result.assessment().assessmentKit().title());
+    }
+
+    private AssessmentReportItem createAssessmentReportItem(GetAssessmentReportUseCase.Param param) {
+        AssessmentReportItem.Space space = new AssessmentReportItem.Space(1563L, "Space");
+        return new AssessmentReportItem(param.getAssessmentId(),
+            "assessmentTitle",
+            "shortAssessmentTitle",
+            "assessment insight",
+            createAssessmentKit(),
+            MaturityLevelMother.levelTwo(),
+            1.5,
+            true,
+            true,
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            space);
+    }
+
+    private AssessmentReportItem.AssessmentKitItem createAssessmentKit() {
+        return new AssessmentReportItem.AssessmentKitItem(15L,
+            "kit title",
+            "kit summary",
+            "about",
+            5,
+            150,
+            MaturityLevelMother.allLevels(),
+            List.of(new QuestionnaireReportItem(14L, "questionnaire title", "questionnaire description", 1, 15)),
+            new AssessmentReportItem.AssessmentKitItem.ExpertGroup(569L, "expert group", null));
+    }
+
+    private GetAssessmentReportUseCase.Param createParam(Consumer<GetAssessmentReportUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        return paramBuilder.build();
+    }
+
+    private GetAssessmentReportUseCase.Param.ParamBuilder paramBuilder() {
+        return GetAssessmentReportUseCase.Param.builder()
+            .assessmentId(UUID.randomUUID())
+            .currentUserId(UUID.randomUUID());
+    }
+}
