dd82147754782ce26b68117eac5a0d388383d011
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-27 09:48:44 +0330

    FAC-906: Feat - Add `persist(List<AdviceItem> adviceItems)` to `CreateAdviceItemPort` and implement it in `AdviceItemPersistenceJpaAdapter`

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/port/out/adviceitem/CreateAdviceItemPort.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/port/out/adviceitem/CreateAdviceItemPort.java
@@ -2,9 +2,12 @@ package org.flickit.assessment.advice.application.port.out.adviceitem;
 
 import org.flickit.assessment.advice.application.domain.adviceitem.AdviceItem;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CreateAdviceItemPort {
 
     UUID persist(AdviceItem adviceItem);
+
+    void persist(List<AdviceItem> adviceItems);
 }
