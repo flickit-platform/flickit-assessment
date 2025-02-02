7a0f2b4206c3ee6a6e4ba84a849cbb09228f87fe
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-02 14:12:54 +0330

    FAC-908: Refactor - Add `ClassUtil`

---
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/util/ClassUtil.java
@@ -0,0 +1,25 @@
+package org.flickit.assessment.common.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.experimental.UtilityClass;
+
+import java.util.Map;
+
+@UtilityClass
+public class ClassUtil {
+
+    public static <T> int countAllFields(Class<T> clazz) {
+        return clazz.getDeclaredFields().length;
+    }
+
+    public static <T> int countUnprovidedFields(T instance) {
+        Map<String, Object> notNullFields = new ObjectMapper().convertValue(instance, new TypeReference<>() {
+        });
+
+        return (int) notNullFields.entrySet()
+            .stream()
+            .filter(entry -> !(entry.getValue() instanceof String str && str.isBlank()))
+            .count();
+    }
+}
