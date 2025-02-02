4ae8a03ff49957803005517216f8da8011cb982d
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-02 16:44:22 +0330

    FAC-906: Feat - add `call` method with Parametrized type input to `CallAiPromptPort` and implement it in `OpenAiAdapter`

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/adapter/openai/OpenAiAdapter.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/adapter/openai/OpenAiAdapter.java
@@ -2,8 +2,10 @@ package org.flickit.assessment.common.adapter.openai;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.port.out.CallAiPromptPort;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,5 +21,14 @@ public class OpenAiAdapter implements CallAiPromptPort {
             .getResult()
             .getOutput()
             .getContent();
+    }
+
+    @Override
+    public <T> T call(String prompt, ParameterizedTypeReference<T> responseType) {
+        return ChatClient.create(chatModel)
+            .prompt()
+            .system(prompt)
+            .call()
+            .entity(responseType);
     }
 }
--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/application/port/out/CallAiPromptPort.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/application/port/out/CallAiPromptPort.java
@@ -1,8 +1,11 @@
 package org.flickit.assessment.common.application.port.out;
 
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.core.ParameterizedTypeReference;
 
 public interface CallAiPromptPort {
 
     String call(Prompt prompt);
+
+    <T> T call(String prompt, ParameterizedTypeReference<T> responseType);
 }
