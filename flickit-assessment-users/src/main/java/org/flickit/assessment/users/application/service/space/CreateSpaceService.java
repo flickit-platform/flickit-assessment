961009ed904eb58dc9ef3eb587fc191ebf538fdc
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-03 15:01:35 +0330

    FAC-911: Feat - Add type to service and serviceTest

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/space/CreateSpaceService.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/space/CreateSpaceService.java
@@ -1,9 +1,13 @@
 package org.flickit.assessment.users.application.service.space;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.application.domain.space.SpaceType;
+import org.flickit.assessment.common.config.AppSpecProperties;
+import org.flickit.assessment.common.exception.UpgradeRequiredException;
 import org.flickit.assessment.users.application.domain.Space;
 import org.flickit.assessment.users.application.domain.SpaceUserAccess;
 import org.flickit.assessment.users.application.port.in.space.CreateSpaceUseCase;
+import org.flickit.assessment.users.application.port.out.space.CountUserSpacesPort;
 import org.flickit.assessment.users.application.port.out.space.CreateSpacePort;
 import org.flickit.assessment.users.application.port.out.spaceuseraccess.CreateSpaceUserAccessPort;
 import org.springframework.stereotype.Service;
@@ -12,6 +16,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.flickit.assessment.common.util.SlugCodeUtil.generateSlugCode;
+import static org.flickit.assessment.users.common.ErrorMessageKey.CREATE_SPACE_PERSONAL_SPACE_MAX;
 
 @Service
 @RequiredArgsConstructor
@@ -19,26 +24,39 @@ public class CreateSpaceService implements CreateSpaceUseCase {
 
     private final CreateSpacePort createSpacePort;
     private final CreateSpaceUserAccessPort createSpaceUserAccessPort;
+    private final CountUserSpacesPort countUserSpacesPort;
+    private final AppSpecProperties appSpecProperties;
 
     @Override
     public Result createSpace(Param param) {
-        long id = createSpacePort.persist(mapToDomain(param.getTitle(), param.getCurrentUserId()));
+        var spaceType = SpaceType.valueOf(param.getType());
+        if (spaceType.equals(SpaceType.PERSONAL))
+            handlePersonalSpaceLimits(param.getCurrentUserId());
+
+        long id = createSpacePort.persist(mapToDomain(param));
 
         createOwnerAccessToSpace(id, param.getCurrentUserId(), param.getCurrentUserId());
         return new Result(id);
     }
 
-    private Space mapToDomain(String title, UUID currentUserId) {
+    private void handlePersonalSpaceLimits(UUID currentUserId) {
+        int userSpaceCount = countUserSpacesPort.countUserSpaces(currentUserId, SpaceType.PERSONAL);
+        if (userSpaceCount >= appSpecProperties.getSpace().getMaxPersonalSpaces())
+            throw new UpgradeRequiredException(CREATE_SPACE_PERSONAL_SPACE_MAX);
+    }
+
+    private Space mapToDomain(Param param) {
         LocalDateTime creationTime = LocalDateTime.now();
         return new Space(null,
-            generateSlugCode(title),
-            title,
-            currentUserId,
+            generateSlugCode(param.getTitle()),
+            param.getTitle(),
+            SpaceType.valueOf(param.getType()),
+            param.getCurrentUserId(),
             null,
             creationTime,
             creationTime,
-            currentUserId,
-            currentUserId
+            param.getCurrentUserId(),
+            param.getCurrentUserId()
         );
     }
 
--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/CreateSpaceServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/CreateSpaceServiceTest.java
@@ -1,7 +1,11 @@
 package org.flickit.assessment.users.application.service.space;
 
+import org.flickit.assessment.common.application.domain.space.SpaceType;
+import org.flickit.assessment.common.config.AppSpecProperties;
+import org.flickit.assessment.common.exception.UpgradeRequiredException;
 import org.flickit.assessment.users.application.domain.SpaceUserAccess;
 import org.flickit.assessment.users.application.port.in.space.CreateSpaceUseCase;
+import org.flickit.assessment.users.application.port.out.space.CountUserSpacesPort;
 import org.flickit.assessment.users.application.port.out.space.CreateSpacePort;
 import org.flickit.assessment.users.application.port.out.spaceuseraccess.CreateSpaceUserAccessPort;
 import org.junit.jupiter.api.Test;
@@ -9,12 +13,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import static org.flickit.assessment.users.common.ErrorMessageKey.CREATE_SPACE_PERSONAL_SPACE_MAX;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -22,17 +29,23 @@ import static org.mockito.Mockito.*;
 class CreateSpaceServiceTest {
 
     @InjectMocks
-    CreateSpaceService service;
+    private CreateSpaceService service;
 
     @Mock
-    CreateSpacePort createSpacePort;
+    private CreateSpacePort createSpacePort;
 
     @Mock
-    CreateSpaceUserAccessPort createSpaceUserAccessPort;
+    private CreateSpaceUserAccessPort createSpaceUserAccessPort;
+
+    @Mock
+    private CountUserSpacesPort countUserSpacesPort;
+
+    @Spy
+    AppSpecProperties appSpecProperties = appSpecProperties();
 
     @Test
-    void testCreateSpace_validParams_successful() {
-        var param = createParam(CreateSpaceUseCase.Param.ParamBuilder::build);
+    void testCreateSpace_whenSpaceTypeIsPremium_thenCreateSpaceWithoutCheckingSpaceLimits() {
+        var param = createParam(b -> b.type("PREMIUM"));
         long createdSpaceId = 0L;
 
         when(createSpacePort.persist(any())).thenReturn(createdSpaceId);
@@ -45,6 +58,43 @@ class CreateSpaceServiceTest {
         assertEquals(createdSpaceId, capturedAccess.getSpaceId());
         assertEquals(param.getCurrentUserId(), capturedAccess.getCreatedBy());
         assertEquals(param.getCurrentUserId(), capturedAccess.getUserId());
+
+        verifyNoInteractions(countUserSpacesPort, appSpecProperties);
+    }
+
+    @Test
+    void testCreateSpace_whenSpaceTypeIsPersonalWithLessThanSpaceLimits_thenCreateSpaceCheckingSpaceLimits() {
+        var param = createParam(CreateSpaceUseCase.Param.ParamBuilder::build);
+        long createdSpaceId = 0L;
+
+        when(createSpacePort.persist(any())).thenReturn(createdSpaceId);
+        when(countUserSpacesPort.countUserSpaces(param.getCurrentUserId(), SpaceType.PERSONAL)).thenReturn(0);
+
+        service.createSpace(param);
+
+        ArgumentCaptor<SpaceUserAccess> captor = ArgumentCaptor.forClass(SpaceUserAccess.class);
+        verify(createSpaceUserAccessPort).persist(captor.capture());
+        var capturedAccess = captor.getValue();
+        assertEquals(createdSpaceId, capturedAccess.getSpaceId());
+        assertEquals(param.getCurrentUserId(), capturedAccess.getCreatedBy());
+        assertEquals(param.getCurrentUserId(), capturedAccess.getUserId());
+    }
+
+    @Test
+    void testCreateSpace_whenSpaceTypeIsPersonalWithMoreThanSpaceLimits_thenCreateSpaceCheckingSpaceLimits() {
+        var param = createParam(CreateSpaceUseCase.Param.ParamBuilder::build);
+
+        when(countUserSpacesPort.countUserSpaces(param.getCurrentUserId(), SpaceType.PERSONAL)).thenReturn(2);
+
+        var throwable = assertThrows(UpgradeRequiredException.class, () -> service.createSpace(param));
+        assertEquals(CREATE_SPACE_PERSONAL_SPACE_MAX, throwable.getMessage());
+    }
+
+    private AppSpecProperties appSpecProperties() {
+        var properties = new AppSpecProperties();
+        properties.setSpace(new AppSpecProperties.Space());
+        properties.getSpace().setMaxPersonalSpaces(1);
+        return properties;
     }
 
     private CreateSpaceUseCase.Param createParam(Consumer<CreateSpaceUseCase.Param.ParamBuilder> changer) {
