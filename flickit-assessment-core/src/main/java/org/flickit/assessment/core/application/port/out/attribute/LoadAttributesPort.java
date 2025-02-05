19c4b0816200df4545a922ccc5e9faef8d20e43b
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-05 13:35:14 +0330

    FAC-924: Feat - Change the return type of the `LoadAttributesPort` and `AttributeMapper.java`

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/attribute/LoadAttributesPort.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/attribute/LoadAttributesPort.java
@@ -1,33 +1,29 @@
 package org.flickit.assessment.core.application.port.out.attribute;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface LoadAttributesPort {
 
-    Result loadAttributes(UUID assessmentId);
+    List<Result> loadAttributes(long kitVersionId);
 
-    record Result(List<Attribute> attributes) {
+    record Result(long id,
+                  String title,
+                  String description,
+                  int index,
+                  int weight,
+                  double confidenceValue,
+                  MaturityLevel maturityLevel,
+                  Subject subject) {
+    }
 
-        public record Attribute(long id,
+    record MaturityLevel(long id,
                          String title,
                          String description,
                          int index,
-                         int weight,
-                         double confidenceValue,
-                         MaturityLevel maturityLevel,
-                         Subject subject) {
-        }
+                         int value) {
+    }
 
-        public record MaturityLevel(long id,
-                                    String title,
-                                    String description,
-                                    int index,
-                                    int value) {
-        }
-
-        public record Subject(long id,
-                              String title) {
-        }
+    record Subject(long id,
+                   String title) {
     }
 }
