943e0eea65a8a47f8bf3d130a89c8de0c8ed21a7
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-05 09:46:16 +0330

    FAC-924: Feat - Add mapper to the result of `UseCase` and write test case for empty case

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/attribute/GetAssessmentAttributesService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/attribute/GetAssessmentAttributesService.java
@@ -2,11 +2,14 @@ package org.flickit.assessment.core.application.service.attribute;
 
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.core.application.port.out.attribute.LoadAttributesPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import org.flickit.assessment.core.application.port.in.attribute.GetAssessmentAttributesUseCase;
+
+import java.util.List;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.VIEW_ASSESSMENT_ATTRIBUTES;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
@@ -17,12 +20,52 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT
 public class GetAssessmentAttributesService implements GetAssessmentAttributesUseCase {
 
     private final AssessmentAccessChecker assessmentAccessChecker;
+    private final LoadAttributesPort loadAttributesPort;
 
     @Override
     public Result getAssessmentAttributes(Param param) {
         if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ASSESSMENT_ATTRIBUTES))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
-        return null;
+        var portResult = loadAttributesPort.loadAttributes(param.getAssessmentId());
+        return toResult(portResult);
+    }
+
+    public Result toResult(LoadAttributesPort.Result portResult) {
+        if (portResult.attributes().isEmpty())
+            return new Result(null);
+
+        List<Result.Attribute> attributes = portResult.attributes().stream()
+            .map(this::toAttribute)
+            .toList();
+
+        return new Result(attributes);
+    }
+
+    private Result.Attribute toAttribute(LoadAttributesPort.Result.Attribute attribute) {
+        return new Result.Attribute(
+            attribute.id(),
+            attribute.title(),
+            attribute.description(),
+            attribute.index(),
+            attribute.weight(),
+            attribute.confidenceValue(),
+            toMaturityLevel(attribute.maturityLevel()),
+            toSubject(attribute.subject())
+        );
+    }
+
+    private Result.MaturityLevel toMaturityLevel(LoadAttributesPort.Result.MaturityLevel maturityLevel) {
+        return new Result.MaturityLevel(
+            maturityLevel.id(),
+            maturityLevel.title(),
+            maturityLevel.description(),
+            maturityLevel.index(),
+            maturityLevel.value()
+        );
+    }
+
+    private Result.Subject toSubject(LoadAttributesPort.Result.Subject subject) {
+        return new Result.Subject(subject.id(), subject.title());
     }
 }
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/attribute/GetAssessmentAttributesServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/attribute/GetAssessmentAttributesServiceTest.java
@@ -3,19 +3,22 @@ package org.flickit.assessment.core.application.service.attribute;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.core.application.port.in.attribute.GetAssessmentAttributesUseCase;
+import org.flickit.assessment.core.application.port.out.attribute.LoadAttributesPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.VIEW_ASSESSMENT_ATTRIBUTES;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,6 +30,12 @@ class GetAssessmentAttributesServiceTest {
     @Mock
     private AssessmentAccessChecker assessmentAccessChecker;
 
+    @Mock
+    private LoadAttributesPort loadAttributesPort;
+
+    @Captor
+    private ArgumentCaptor<LoadAttributesPort.Result> loadAttributesPortResultCaptor;
+
     @Test
     void testGetAssessmentAttributesService_whenCurrentUserDoesNotHaveAccess_thenThrowsAccessDeniedException() {
         var param = createParam(GetAssessmentAttributesUseCase.Param.ParamBuilder::build);
@@ -36,6 +45,20 @@ class GetAssessmentAttributesServiceTest {
 
         var throwable = assertThrows(AccessDeniedException.class, () -> service.getAssessmentAttributes(param));
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+    }
+
+    @Test
+    void testGetAssessmentAttributesService_whenNoAttributesExist_thenReturnEmptyResult() {
+        var param = createParam(GetAssessmentAttributesUseCase.Param.ParamBuilder::build);
+
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ASSESSMENT_ATTRIBUTES))
+            .thenReturn(true);
+        when(loadAttributesPort.loadAttributes(param.getAssessmentId()))
+            .thenReturn(new LoadAttributesPort.Result(List.of()));
+
+        var result = service.getAssessmentAttributes(param);
+        assertNotNull(result);
+        assertNull(result.attributes());
     }
 
     private GetAssessmentAttributesUseCase.Param createParam(Consumer<GetAssessmentAttributesUseCase.Param.ParamBuilder> changer) {
