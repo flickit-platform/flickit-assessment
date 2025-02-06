ee6cf66675e278988c3b1650a86ff2c198c0a8da
mahdirezazadeh <riza.rs2000@gmail.com>
2025-02-06 17:59:11 +0330

    FAC-915:impl:Add FieldNameConstants to SubjectQuestionnaireJpaEntity

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/subjectquestionnaire/SubjectQuestionnaireJpaEntity.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/subjectquestionnaire/SubjectQuestionnaireJpaEntity.java
@@ -1,7 +1,11 @@
 package org.flickit.assessment.data.jpa.kit.subjectquestionnaire;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
+import lombok.experimental.FieldNameConstants;
 
 @Entity
 @Table(name = "fak_subject_questionnaire")
@@ -9,6 +13,7 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldNameConstants
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class SubjectQuestionnaireJpaEntity {
 
