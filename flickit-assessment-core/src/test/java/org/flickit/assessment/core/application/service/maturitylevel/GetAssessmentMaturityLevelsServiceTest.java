16232408b0ba4c48d6b4bc1d6b788f90e5142d8a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-09 09:51:02 +0330

    FAC-931: Feat - Complete `ServiceTest`

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/maturitylevel/GetAssessmentMaturityLevelsServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/maturitylevel/GetAssessmentMaturityLevelsServiceTest.java
@@ -2,21 +2,30 @@ package org.flickit.assessment.core.application.service.maturitylevel;
 
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.core.application.domain.Assessment;
+import org.flickit.assessment.core.application.internal.ValidateAssessmentResult;
 import org.flickit.assessment.core.application.port.in.maturitylevel.GetAssessmentMaturityLevelsUseCase;
+import org.flickit.assessment.core.application.port.out.assessment.LoadAssessmentPort;
+import org.flickit.assessment.core.application.port.out.maturitylevel.LoadMaturityLevelsPort;
+import org.flickit.assessment.core.test.fixture.application.AssessmentMother;
+import org.flickit.assessment.core.test.fixture.application.MaturityLevelMother;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.VIEW_ASSESSMENT_MATURITY_LEVELS;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class GetAssessmentMaturityLevelsServiceTest {
@@ -27,6 +36,15 @@ class GetAssessmentMaturityLevelsServiceTest {
     @Mock
     private AssessmentAccessChecker assessmentAccessChecker;
 
+    @Mock
+    private LoadAssessmentPort loadAssessmentPort;
+
+    @Mock
+    private ValidateAssessmentResult validateAssessmentResult;
+
+    @Mock
+    private LoadMaturityLevelsPort loadMaturityLevelsPort;
+
     @Test
     void testGetAssessmentMaturityLevels_whenUserHasNotAccess_thenThrowsAccessDeniedException() {
         var param = createParam(GetAssessmentMaturityLevelsUseCase.Param.ParamBuilder::build);
@@ -36,6 +54,35 @@ class GetAssessmentMaturityLevelsServiceTest {
 
         var throwable = assertThrows(AccessDeniedException.class, () -> service.getAssessmentMaturityLevels(param));
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+
+        verifyNoInteractions(validateAssessmentResult,
+            validateAssessmentResult,
+            loadMaturityLevelsPort,
+            loadAssessmentPort);
+    }
+
+    @Test
+    void testGetAssessmentMaturityLevels_whenParamsAreValid_thenReturnMaturityLevels() {
+        var param = createParam(GetAssessmentMaturityLevelsUseCase.Param.ParamBuilder::build);
+        var maturityLevels = List.of(MaturityLevelMother.levelOne(), MaturityLevelMother.levelTwo());
+        Assessment assessment = AssessmentMother.assessment();
+
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ASSESSMENT_MATURITY_LEVELS))
+            .thenReturn(true);
+        when(loadAssessmentPort.getAssessmentById(param.getAssessmentId()))
+            .thenReturn(Optional.of(assessment));
+        when(loadMaturityLevelsPort.loadByKitVersionId(assessment.getAssessmentKit().getKitVersion())).thenReturn(maturityLevels);
+
+        var result = service.getAssessmentMaturityLevels(param);
+        assertThat(result.maturityLevels())
+            .zipSatisfy(maturityLevels, (actual, expected) -> {
+                assertEquals(expected.getId(), actual.id());
+                assertEquals(expected.getTitle(), actual.title());
+                assertEquals(expected.getDescription(), actual.description());
+                assertEquals(expected.getValue(), actual.value());
+                assertEquals(expected.getIndex(), actual.index());
+            });
+        verify(validateAssessmentResult).validate(param.getAssessmentId());
     }
 
     private GetAssessmentMaturityLevelsUseCase.Param createParam(Consumer<GetAssessmentMaturityLevelsUseCase.Param.ParamBuilder> changer) {
