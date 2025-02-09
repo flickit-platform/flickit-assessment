1cfffb24b7f0b4bc61b7a9ae14d539dc559e45d4
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-09 08:59:14 +0330

    FAC-931: Feat - Add description to the Result of useCase

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/maturitylevel/GetAssessmentMaturityLevelsService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/maturitylevel/GetAssessmentMaturityLevelsService.java
@@ -2,7 +2,12 @@ package org.flickit.assessment.core.application.service.maturitylevel;
 
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.core.application.domain.MaturityLevel;
+import org.flickit.assessment.core.application.internal.ValidateAssessmentResult;
 import org.flickit.assessment.core.application.port.in.maturitylevel.GetAssessmentMaturityLevelsUseCase;
+import org.flickit.assessment.core.application.port.out.assessment.LoadAssessmentPort;
+import org.flickit.assessment.core.application.port.out.maturitylevel.LoadMaturityLevelsPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +21,33 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT
 public class GetAssessmentMaturityLevelsService implements GetAssessmentMaturityLevelsUseCase {
 
     private final AssessmentAccessChecker assessmentAccessChecker;
+    private final ValidateAssessmentResult validateAssessmentResult;
+    private final LoadAssessmentPort loadAssessmentPort;
+    private final LoadMaturityLevelsPort loadMaturityLevelsPort;
 
     @Override
     public Result getAssessmentMaturityLevels(Param param) {
         if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ASSESSMENT_MATURITY_LEVELS))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
-        return null;
+        validateAssessmentResult.validate(param.getAssessmentId());
+
+        var assessment = loadAssessmentPort.getAssessmentById(param.getAssessmentId())
+            .orElseThrow(() -> new ResourceNotFoundException("Assessment not found"));
+
+        var maturityLevels = loadMaturityLevelsPort.loadByKitVersionId(assessment.getAssessmentKit().getKitVersion())
+            .stream()
+            .map(this::toResult)
+            .toList();
+
+        return new Result(maturityLevels);
+    }
+
+    private Result.MaturityLevel toResult(MaturityLevel maturityLevel) {
+        return new Result.MaturityLevel(maturityLevel.getId(),
+            maturityLevel.getTitle(),
+            maturityLevel.getDescription(),
+            maturityLevel.getIndex(),
+            maturityLevel.getValue());
     }
 }
