9c0d541b65d76c520beabaeaf82cc297607503eb
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-09 11:36:17 +0330

    FAC-924: Refactor - Add `validateAssessmentResultPort` to service and minor improvements

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/attribute/GetAssessmentAttributesService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/attribute/GetAssessmentAttributesService.java
@@ -1,16 +1,14 @@
 package org.flickit.assessment.core.application.service.attribute;
 
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.application.port.out.ValidateAssessmentResultPort;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.core.application.port.out.assessmentresult.LoadAssessmentResultPort;
 import org.flickit.assessment.core.application.port.out.attribute.LoadAttributesPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import org.flickit.assessment.core.application.port.in.attribute.GetAssessmentAttributesUseCase;
-
-import java.util.List;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.VIEW_ASSESSMENT_ATTRIBUTES;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
@@ -21,26 +19,19 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT
 public class GetAssessmentAttributesService implements GetAssessmentAttributesUseCase {
 
     private final AssessmentAccessChecker assessmentAccessChecker;
+    private final ValidateAssessmentResultPort validateAssessmentResultPort;
     private final LoadAttributesPort loadAttributesPort;
-    private final LoadAssessmentResultPort loadAssessmentResultPort;
 
     @Override
     public Result getAssessmentAttributes(Param param) {
-/*        if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ASSESSMENT_ATTRIBUTES))
-            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);*/
+        if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ASSESSMENT_ATTRIBUTES))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+        validateAssessmentResultPort.validate(param.getAssessmentId());
 
-        var portResult = loadAttributesPort.loadAttributes(param.getAssessmentId());
-        return toResult(portResult);
-    }
-
-    public Result toResult(List<LoadAttributesPort.Result> portResult) {
-        if (portResult.isEmpty())
-            return new Result(null);
-
-        List<Result.Attribute> attributes = portResult.stream()
+        var attributes = loadAttributesPort.loadAttributes(param.getAssessmentId())
+            .stream()
             .map(this::toAttribute)
             .toList();
-
         return new Result(attributes);
     }
 
