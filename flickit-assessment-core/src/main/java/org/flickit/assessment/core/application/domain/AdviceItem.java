cb923d16ba6640e878c2e40c0ea36fd95058c400
mahdirezazadeh <riza.rs2000@gmail.com>
2025-02-11 11:33:56 +0330

    FAC-938:impl:Enrich AdviceItem fields

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AdviceItem.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AdviceItem.java
@@ -2,6 +2,9 @@ package org.flickit.assessment.core.application.domain;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.application.domain.adviceitem.CostLevel;
+import org.flickit.assessment.common.application.domain.adviceitem.ImpactLevel;
+import org.flickit.assessment.common.application.domain.adviceitem.PriorityLevel;
 
 import java.util.UUID;
 
@@ -12,7 +15,7 @@ public class AdviceItem {
     private final UUID id;
     private final String title;
     private final String description;
-    private final String cost;
-    private final String priority;
-    private final String impact;
+    private final CostLevel cost;
+    private final PriorityLevel priority;
+    private final ImpactLevel impact;
 }
