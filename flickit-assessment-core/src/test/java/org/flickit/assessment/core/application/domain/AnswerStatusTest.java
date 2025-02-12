b2b87c78f07e0947b76718b48fe9c2e0a7da790e
mahdirezazadeh <riza.rs2000@gmail.com>
2025-02-12 11:41:50 +0330

    FAC-930:Impl:Add AnswerStatusTest

---
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/domain/AnswerStatusTest.java
@@ -0,0 +1,31 @@
+package org.flickit.assessment.core.application.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AnswerStatusTest {
+
+    @Test
+    void testAnswerStatus_OrderOfItemsShouldNotBeChanged() {
+        assertEquals(0, AnswerStatus.APPROVED.ordinal());
+        assertEquals(1, AnswerStatus.UNAPPROVED.ordinal());
+    }
+
+    @Test
+    void testAnswerStatus_NameOfItemsShouldNotBeChanged() {
+        assertEquals("APPROVED", AnswerStatus.APPROVED.name());
+        assertEquals("UNAPPROVED", AnswerStatus.UNAPPROVED.name());
+    }
+
+    @Test
+    void testAnswerStatus_TitleOfItemsShouldNotBeChanged() {
+        assertEquals("Approved", AnswerStatus.APPROVED.getTitle());
+        assertEquals("Unapproved", AnswerStatus.UNAPPROVED.getTitle());
+    }
+
+    @Test
+    void testAnswerStatus_SizeOfItemsShouldNotBeChanged() {
+        assertEquals(2, AnswerStatus.values().length);
+    }
+}
