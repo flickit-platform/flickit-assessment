60ebca58ebffbff614d515b3697a4a132efb387b
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-02-16 13:57:02 +0330

    FAC-944: Add javadoc to approveAll methods

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/subjectinsight/ApproveSubjectInsightPort.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/subjectinsight/ApproveSubjectInsightPort.java
@@ -1,5 +1,7 @@
 package org.flickit.assessment.core.application.port.out.subjectinsight;
 
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -7,5 +9,13 @@ public interface ApproveSubjectInsightPort {
 
     void approve(UUID assessmentId, long subjectId, LocalDateTime lastModificationTime);
 
+    /**
+     * Approves all the unapproved insights associated with the subjects of assessment specified with assessment ID,
+     * and updates the last modification time of those insights.
+     *
+     * @param assessmentId         The unique identifier (UUID) of the assessment.
+     * @param lastModificationTime The {@link LocalDateTime} to set as the last modification time for the approved insights.
+     * @throws ResourceNotFoundException If no assessment result is found for the given assessment ID.
+     */
     void approveAll(UUID assessmentId, LocalDateTime lastModificationTime);
 }
