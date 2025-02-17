93d8b3678c3abb29dfaa4d275551267c313378a4
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-17 08:45:55 +0330

    FAC-951: Feat : Add `RestController`

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/assessmentkit/GetKitLanguagesRestController.java
@@ -0,0 +1,22 @@
+package org.flickit.assessment.kit.adapter.in.rest.assessmentkit;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.kit.application.port.in.assessmentkit.GetKitLanguagesUseCase;
+import org.flickit.assessment.kit.application.port.in.assessmentkit.GetKitLanguagesUseCase.Result;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GetKitLanguagesRestController {
+
+    private final GetKitLanguagesUseCase getKitLanguagesUseCase;
+
+    @GetMapping("/kit-languages")
+    public ResponseEntity<Result> getKitLanguages() {
+        var response = getKitLanguagesUseCase.getKitLanguages();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
