13350668f24a71500531988786c0007eaaecd88c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-17 16:20:22 +0330

    FAC-920: Feat : Minor renaming

--- flickit-assessment-common/src/test/java/org/flickit/assessment/common/util/ClassUtilsTest.java
+++ flickit-assessment-common/src/test/java/org/flickit/assessment/common/util/ClassUtilsTest.java
@@ -12,51 +12,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ClassUtilsTest {
 
     @Test
-    public void testMetadataIsEmpty_withNull_shouldReturnTrue() {
+    public void testIsMetadataEmpty_withNull_shouldReturnTrue() {
         assertThat(ClassUtils.isMetadataEmpty(null)).isTrue();
     }
 
     @Test
-    public void testMetadataIsEmpty_withBlankString_shouldReturnTrue() {
+    public void testIsMetadataEmpty_withBlankString_shouldReturnTrue() {
         assertThat(ClassUtils.isMetadataEmpty("")).isTrue();
         assertThat(ClassUtils.isMetadataEmpty("   ")).isTrue();
     }
 
     @Test
-    public void testMetadataIsEmpty_withEmptyCollection_shouldReturnTrue() {
+    public void testIsMetadataEmpty_withEmptyCollection_shouldReturnTrue() {
         assertThat(ClassUtils.isMetadataEmpty(Collections.emptyList())).isTrue();
         assertThat(ClassUtils.isMetadataEmpty(Collections.emptySet())).isTrue();
     }
 
     @Test
-    public void testMetadataIsEmpty_withEmptyArray_shouldReturnTrue() {
+    public void testIsMetadataEmpty_withEmptyArray_shouldReturnTrue() {
         assertThat(ClassUtils.isMetadataEmpty(new int[]{})).isTrue();
         assertThat(ClassUtils.isMetadataEmpty(new String[]{})).isTrue();
     }
 
     @Test
-    public void testMetadataIsEmpty_withEmptyMap_shouldReturnTrue() {
+    public void testIsMetadataEmpty_withEmptyMap_shouldReturnTrue() {
         assertThat(ClassUtils.isMetadataEmpty(Collections.emptyMap())).isTrue();
     }
 
     @Test
-    public void testMetadataIsEmpty_withNonBlankString_shouldReturnFalse() {
+    public void testIsMetadataEmpty_withNonBlankString_shouldReturnFalse() {
         assertThat(ClassUtils.isMetadataEmpty("  a  ")).isFalse();
     }
 
     @Test
-    public void testMetadataIsEmpty_withNonEmptyCollection_shouldReturnFalse() {
+    public void testIsMetadataEmpty_withNonEmptyCollection_shouldReturnFalse() {
         assertThat(ClassUtils.isMetadataEmpty(List.of(1, 2, 3))).isFalse();
         assertThat(ClassUtils.isMetadataEmpty(Set.of("a", "b"))).isFalse();
     }
 
     @Test
-    public void testMetadataIsEmpty_withNonEmptyMap_shouldReturnFalse() {
+    public void testIsMetadataEmpty_withNonEmptyMap_shouldReturnFalse() {
         assertThat(ClassUtils.isMetadataEmpty(Map.of(1, "one", 2, "two"))).isFalse();
     }
 
     @Test
-    public void testMetadataIsEmpty_withNonEmptyArray_shouldReturnFalse() {
+    public void testIsMetadataEmpty_withNonEmptyArray_shouldReturnFalse() {
         assertThat(ClassUtils.isMetadataEmpty(new int[]{1, 2, 3})).isFalse();
         assertThat(ClassUtils.isMetadataEmpty(new String[]{"A"})).isFalse();
     }
