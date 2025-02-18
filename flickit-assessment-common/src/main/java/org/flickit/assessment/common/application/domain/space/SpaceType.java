e3440e6f76fac0e9404a3690c0adc4e2773f79ef
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-18 09:06:05 +0330

    FAC-952: Feat - Add `Title` to `SpaceType` enum and Translate its title

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/application/domain/space/SpaceType.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/application/domain/space/SpaceType.java
@@ -1,8 +1,24 @@
 package org.flickit.assessment.common.application.domain.space;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.application.MessageBundle;
+
+import static org.flickit.assessment.common.error.MessageKey.SPACE_TYPE_BASIC;
+import static org.flickit.assessment.common.error.MessageKey.SPACE_TYPE_PREMIUM;
+
+@Getter
+@RequiredArgsConstructor
 public enum SpaceType {
-    BASIC,
-    PREMIUM;
+
+    BASIC(SPACE_TYPE_BASIC),
+    PREMIUM(SPACE_TYPE_PREMIUM);
+
+    private final String title;
+
+    public String getTitle() {
+        return MessageBundle.message(title);
+    }
 
     public int getId() {
         return this.ordinal();
