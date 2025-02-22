a88c729ec46ee417043494d1576d855c35cb9990
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-22 10:43:45 +0330

    FAC-956: Test - Refactor and enrich the `ServiceTest`

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/GetSpaceServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/GetSpaceServiceTest.java
@@ -2,7 +2,6 @@ package org.flickit.assessment.users.application.service.space;
 
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
-import org.flickit.assessment.users.application.domain.Space;
 import org.flickit.assessment.users.application.port.in.space.GetSpaceUseCase;
 import org.flickit.assessment.users.application.port.out.space.LoadSpaceDetailsPort;
 import org.flickit.assessment.users.application.port.out.spaceuseraccess.CheckSpaceAccessPort;
@@ -14,13 +13,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
+import java.util.function.Consumer;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.flickit.assessment.users.common.ErrorMessageKey.SPACE_ID_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class GetSpaceServiceTest {
@@ -34,63 +32,77 @@ class GetSpaceServiceTest {
     @Mock
     LoadSpaceDetailsPort loadSpaceDetailsPort;
 
+    private final GetSpaceUseCase.Param param = createParam(GetSpaceUseCase.Param.ParamBuilder::build);
+
     @Test
     void testGetSpace_WhenUserIsOwner_ThenReturnSpaceWithEditableTrue() {
-        UUID currentUserId = UUID.randomUUID();
-        Space space = SpaceMother.basicSpace(currentUserId);
-        GetSpaceUseCase.Param param = new GetSpaceUseCase.Param(space.getId(), currentUserId);
-        LoadSpaceDetailsPort.Result portResult = new LoadSpaceDetailsPort.Result(space, 1, 1);
+        var space = SpaceMother.basicSpace(param.getCurrentUserId());
+        var portResult = new LoadSpaceDetailsPort.Result(space, 1, 2);
 
-        when(checkSpaceAccessPort.checkIsMember(space.getId(), currentUserId)).thenReturn(true);
-        when(loadSpaceDetailsPort.loadSpace(space.getId())).thenReturn(portResult);
+        when(checkSpaceAccessPort.checkIsMember(param.getId(), param.getCurrentUserId())).thenReturn(true);
+        when(loadSpaceDetailsPort.loadSpace(param.getId())).thenReturn(portResult);
 
         var result = service.getSpace(param);
-
-        assertTrue(result.editable(), "'editable' should be true");
-        verify(loadSpaceDetailsPort).loadSpace(space.getId());
+        assertEquals(portResult.space().getId(), result.space().getId());
+        assertEquals(portResult.space().getCode(), result.space().getCode());
+        assertEquals(portResult.space().getTitle(), result.space().getTitle());
+        assertEquals(space.getType().getCode(), result.space().getType().getCode());
+        assertEquals(space.getType().getTitle(), result.space().getType().getTitle());
+        assertTrue(result.editable());
+        assertEquals(portResult.space().getLastModificationTime(), result.space().getLastModificationTime());
+        assertEquals(portResult.membersCount(), result.membersCount());
+        assertEquals(portResult.assessmentsCount(), result.assessmentsCount());
     }
 
     @Test
     void testGetSpace_WhenUserIsNotOwner_ThenReturnSpaceWithEditableFalse() {
-        UUID ownerId = UUID.randomUUID();
-        Space space = SpaceMother.basicSpace(ownerId);
-        UUID currentUserId = UUID.randomUUID();
-        GetSpaceUseCase.Param param = new GetSpaceUseCase.Param(space.getId(), currentUserId);
-        LoadSpaceDetailsPort.Result portResult = new LoadSpaceDetailsPort.Result(space, 1, 1);
+        var space = SpaceMother.basicSpace(UUID.randomUUID());
+        var portResult = new LoadSpaceDetailsPort.Result(space, 1, 2);
 
-        when(checkSpaceAccessPort.checkIsMember(space.getId(), currentUserId)).thenReturn(true);
-        when(loadSpaceDetailsPort.loadSpace(space.getId())).thenReturn(portResult);
+        when(checkSpaceAccessPort.checkIsMember(param.getId(), param.getCurrentUserId())).thenReturn(true);
+        when(loadSpaceDetailsPort.loadSpace(param.getId())).thenReturn(portResult);
 
         var result = service.getSpace(param);
-        assertFalse(result.editable(), "'editable' should be false");
-        verify(loadSpaceDetailsPort).loadSpace(anyLong());
+        assertEquals(portResult.space().getId(), result.space().getId());
+        assertEquals(portResult.space().getCode(), result.space().getCode());
+        assertEquals(portResult.space().getTitle(), result.space().getTitle());
+        assertEquals(space.getType().getCode(), result.space().getType().getCode());
+        assertEquals(space.getType().getTitle(), result.space().getType().getTitle());
+        assertFalse(result.editable());
+        assertEquals(portResult.space().getLastModificationTime(), result.space().getLastModificationTime());
+        assertEquals(portResult.membersCount(), result.membersCount());
+        assertEquals(portResult.assessmentsCount(), result.assessmentsCount());
     }
 
     @Test
     void testGetSpace_WhenSpaceDoesNotExist_ThenThrowResourceNotFoundException() {
-        long spaceId = 0L;
-        UUID currentUserId = UUID.randomUUID();
-        GetSpaceUseCase.Param param = new GetSpaceUseCase.Param(spaceId, currentUserId);
-
-        when(checkSpaceAccessPort.checkIsMember(spaceId, currentUserId)).thenReturn(true);
-        when(loadSpaceDetailsPort.loadSpace(spaceId))
+        when(checkSpaceAccessPort.checkIsMember(param.getId(), param.getCurrentUserId())).thenReturn(true);
+        when(loadSpaceDetailsPort.loadSpace(param.getId()))
             .thenThrow(new ResourceNotFoundException(SPACE_ID_NOT_FOUND));
 
-        assertThrows(ResourceNotFoundException.class, () -> service.getSpace(param));
-        verify(loadSpaceDetailsPort).loadSpace(anyLong());
+        var throwable = assertThrows(ResourceNotFoundException.class, () -> service.getSpace(param));
+        assertEquals(SPACE_ID_NOT_FOUND, throwable.getMessage());
     }
 
     @Test
     void testGetSpace_WhenSpaceAccessNotFound_ThenThrowAccessDeniedException() {
-        long spaceId = 0L;
-        UUID currentUserId = UUID.randomUUID();
-        GetSpaceUseCase.Param param = new GetSpaceUseCase.Param(spaceId, currentUserId);
-
-        when(checkSpaceAccessPort.checkIsMember(spaceId, currentUserId)).thenReturn(false);
+        when(checkSpaceAccessPort.checkIsMember(param.getId(), param.getCurrentUserId())).thenReturn(false);
 
         var throwable = assertThrows(AccessDeniedException.class, () -> service.getSpace(param));
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
 
-        verify(checkSpaceAccessPort).checkIsMember(spaceId, currentUserId);
+        verifyNoInteractions(loadSpaceDetailsPort);
+    }
+
+    private GetSpaceUseCase.Param createParam(Consumer<GetSpaceUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        return paramBuilder.build();
+    }
+
+    private GetSpaceUseCase.Param.ParamBuilder paramBuilder() {
+        return GetSpaceUseCase.Param.builder()
+            .id(123L)
+            .currentUserId(UUID.randomUUID());
     }
 }
