10dcfe25542d06a9014ae12e5707a1b02848bc0c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-24 09:44:35 +0330

    FAC-960: Feat - Add JavaDoc to  `countQuestionnaireUnapprovedAnswers` method of `CountAnswersPort`

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/answer/CountAnswersPort.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/answer/CountAnswersPort.java
@@ -25,5 +25,13 @@ public interface CountAnswersPort {
      */
     int countUnapprovedAnswers(UUID assessmentResultId);
 
+    /**
+     * Counts the number of unapproved answers for each questionnaire
+     * in a given assessment result.
+     *
+     * @param assessmentResultId the unique identifier of the assessment result
+     * @param questionnaireIds   a set of questionnaire IDs for which the unapproved answers are counted
+     * @return a map where the key is the questionnaire ID and the value is the count of unapproved answers
+     */
     Map<Long, Integer> countQuestionnaireUnapprovedAnswers(UUID assessmentResultId, Set<Long> questionnaireIds);
 }
