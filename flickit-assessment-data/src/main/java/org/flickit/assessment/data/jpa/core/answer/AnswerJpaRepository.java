65173863e16de73de0ebce532e64288e8ad9d41d
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-24 18:52:22 +0330

    FAC-961: Refactor - Minor revert

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/answer/AnswerJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/answer/AnswerJpaRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface AnswerJpaRepository extends JpaRepository<AnswerJpaEntity, UUID> {
 
