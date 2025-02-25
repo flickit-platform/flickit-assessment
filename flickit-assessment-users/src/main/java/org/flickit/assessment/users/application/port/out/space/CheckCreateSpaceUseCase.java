82595d3fc8cae3895e4bb0aba9416ed1d755628b
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-25 11:21:37 +0330

    FAC-966: Feat - Add `CheckCreateSpaceUseCase`

---
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/port/out/space/CheckCreateSpaceUseCase.java
@@ -0,0 +1,34 @@
+package org.flickit.assessment.users.application.port.out.space;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.flickit.assessment.common.application.SelfValidating;
+import org.flickit.assessment.users.application.port.in.space.CreateSpaceUseCase;
+
+import java.util.UUID;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
+
+public interface CheckCreateSpaceUseCase {
+
+    Result checkCreateSpace(Param param);
+
+    @Value
+    @EqualsAndHashCode(callSuper = false)
+    class Param extends SelfValidating<CreateSpaceUseCase.Param> {
+
+        @NotNull(message = COMMON_CURRENT_USER_ID_NOT_NULL)
+        UUID currentUserId;
+
+        @Builder
+        public Param(UUID currentUserId) {
+            this.currentUserId = currentUserId;
+            this.validateSelf();
+        }
+    }
+
+    record Result(boolean allowCreateBasic) {
+    }
+}
