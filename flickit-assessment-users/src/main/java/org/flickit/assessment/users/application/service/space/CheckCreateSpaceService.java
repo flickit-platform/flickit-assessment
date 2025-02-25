b5e3829f5aa9f85395d99c20e90f6ed893ff7f16
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-25 11:36:26 +0330

    FAC-966: Feat - Add `Service` and `ServiceTest` [WIP]

---
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/space/CheckCreateSpaceService.java
@@ -0,0 +1,20 @@
+package org.flickit.assessment.users.application.service.space;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.config.AppSpecProperties;
+import org.flickit.assessment.users.application.port.out.space.CheckCreateSpaceUseCase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CheckCreateSpaceService implements CheckCreateSpaceUseCase {
+
+    private final AppSpecProperties appSpecProperties;
+
+    @Override
+    public Result checkCreateSpace(Param param) {
+        return null;
+    }
+}
---
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/CheckCreateSpaceServiceTest.java
@@ -0,0 +1,15 @@
+package org.flickit.assessment.users.application.service.space;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CheckCreateSpaceServiceTest {
+
+    @Test
+    public void testCheckCreateSpace_WhenValidParams_ThenSuccess() {}
+
+}
