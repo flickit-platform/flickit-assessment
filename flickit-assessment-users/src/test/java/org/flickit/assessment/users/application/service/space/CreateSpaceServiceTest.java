1459870dc62539d07a3de572ab318ecaa1f91b7a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-25 08:56:41 +0330

    FAC-911: Feat - Add `Service` and `ServiceTest` with error for situation when private spaces exceeds max in basic plan

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/CreateSpaceServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/CreateSpaceServiceTest.java
@@ -1,9 +1,12 @@
 package org.flickit.assessment.users.application.service.space;
 
 import org.flickit.assessment.common.application.domain.space.SpaceType;
+import org.flickit.assessment.common.config.AppSpecProperties;
+import org.flickit.assessment.common.exception.UpgradeRequiredException;
 import org.flickit.assessment.users.application.domain.Space;
 import org.flickit.assessment.users.application.domain.SpaceUserAccess;
 import org.flickit.assessment.users.application.port.in.space.CreateSpaceUseCase;
+import org.flickit.assessment.users.application.port.out.space.CountSpacePort;
 import org.flickit.assessment.users.application.port.out.space.CreateSpacePort;
 import org.flickit.assessment.users.application.port.out.spaceuseraccess.CreateSpaceUserAccessPort;
 import org.junit.jupiter.api.Test;
@@ -11,17 +14,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 import java.util.function.Consumer;
 
 import static org.flickit.assessment.common.util.SlugCodeUtil.generateSlugCode;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.flickit.assessment.users.common.ErrorMessageKey.CREATE_SPACE_BASIC_SPACE_MAX;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CreateSpaceServiceTest {
@@ -35,11 +38,30 @@ class CreateSpaceServiceTest {
     @Mock
     CreateSpaceUserAccessPort createSpaceUserAccessPort;
 
+    @Mock
+    CountSpacePort countSpacePort;
+
+    @Spy
+    AppSpecProperties appSpecProperties = appSpecProperties();
+
+    @Test
+    void testCreateSpace_basicSpacesIn_successful() { //TODO: Consider
+        var param = createParam(CreateSpaceUseCase.Param.ParamBuilder::build);
+
+        when(countSpacePort.countBasicSpaces(param.getCurrentUserId())).thenReturn(1);
+
+        var throwable = assertThrows(UpgradeRequiredException.class, () -> service.createSpace(param));
+        assertEquals(CREATE_SPACE_BASIC_SPACE_MAX, throwable.getMessage());
+
+        verifyNoInteractions(createSpacePort);
+    }
+
     @Test
     void testCreateSpace_validParams_successful() {
         var param = createParam(CreateSpaceUseCase.Param.ParamBuilder::build);
         long createdSpaceId = 0L;
 
+        when(countSpacePort.countBasicSpaces(param.getCurrentUserId())).thenReturn(0);
         when(createSpacePort.persist(any())).thenReturn(createdSpaceId);
 
         service.createSpace(param);
@@ -64,6 +86,14 @@ class CreateSpaceServiceTest {
         assertEquals(createdSpaceId, capturedAccess.getSpaceId());
         assertEquals(param.getCurrentUserId(), capturedAccess.getCreatedBy());
         assertEquals(param.getCurrentUserId(), capturedAccess.getUserId());
+    }
+
+    AppSpecProperties appSpecProperties() {
+        AppSpecProperties properties = new AppSpecProperties();
+        AppSpecProperties.Space space = new AppSpecProperties.Space();
+        space.setMaxBasicSpaces(1);
+        properties.setSpace(space);
+        return properties;
     }
 
     private CreateSpaceUseCase.Param createParam(Consumer<CreateSpaceUseCase.Param.ParamBuilder> changer) {
