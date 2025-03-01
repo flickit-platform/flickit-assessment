d08382ef5e344d9d3ce807c73fc566a1a03f6306
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-01 16:48:52 +0330

    FAC-970- Refactor - Add `SpaceModel` to `CreateSpaceNotificationPayload`

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/space/notification/CreateSpaceNotificationPayload.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/service/space/notification/CreateSpaceNotificationPayload.java
@@ -1,7 +1,15 @@
 package org.flickit.assessment.users.application.service.space.notification;
 
-public record CreateSpaceNotificationPayload(UserModel userModel) {
+import org.flickit.assessment.common.application.domain.notification.NotificationPayload;
+
+import java.time.LocalDateTime;
+
+public record CreateSpaceNotificationPayload(UserModel userModel,
+                                             SpaceModel spaceModel) implements NotificationPayload {
 
     public record UserModel(String displayName, String email) {
+    }
+
+    public record SpaceModel(String title, LocalDateTime createdAt) {
     }
 }
