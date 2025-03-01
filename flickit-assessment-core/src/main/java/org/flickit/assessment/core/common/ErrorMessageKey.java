6a28903e57103f311842e8c5cf6c1872de886b70
mahdirezazadeh <riza.rs2000@gmail.com>
2025-03-01 14:08:38 +0330

    FAC-969:impl:Add ApproveExpiredInsightsUseCase

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
@@ -326,4 +326,6 @@ public class ErrorMessageKey {
     public static final String APPROVE_ALL_ASSESSMENT_INSIGHTS_ASSESSMENT_ID_NOT_NULL = "approve-all-assessment-insights.assessmentId.notNull";
 
     public static final String GENERATE_ALL_ASSESSMENT_INSIGHTS_ASSESSMENT_ID_NOT_NULL = "generate-all-assessment-insights.assessmentId.notNull";
+
+    public static final String APPROVE_EXPIRED_INSIGHTS_ASSESSMENT_ID_NOT_NULL = "approve-expired-insights.assessmentId.notNull";
 }
