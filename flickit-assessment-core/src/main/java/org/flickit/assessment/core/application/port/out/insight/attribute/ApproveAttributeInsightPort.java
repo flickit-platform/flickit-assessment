1e83606ff357083b70890bd5e9ec592b941a5b9d
mahdirezazadeh <riza.rs2000@gmail.com>
2025-03-01 14:10:01 +0330

    FAC-969:impl:Impl ApproveExpiredInsightsService

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/insight/attribute/ApproveAttributeInsightPort.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/insight/attribute/ApproveAttributeInsightPort.java
@@ -3,6 +3,7 @@ package org.flickit.assessment.core.application.port.out.insight.attribute;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.UUID;
 
 public interface ApproveAttributeInsightPort {
@@ -18,4 +19,15 @@ public interface ApproveAttributeInsightPort {
      * @throws ResourceNotFoundException If no assessment result is found for the given assessment ID.
      */
     void approveAll(UUID assessmentId, LocalDateTime lastModificationTime);
+
+    /**
+     * Approves all the expired insights associated with the attributes of assessment specified with assessment ID
+     * and attribute IDs, and updates the last modification time of those insights.
+     *
+     * @param assessmentId         The unique identifier (UUID) of the assessment.
+     * @param attributeIds         A collection of unique identifiers (Long) of the attributes.
+     * @param lastModificationTime The {@link LocalDateTime} to set as the last modification time for the approved insights.
+     * @throws ResourceNotFoundException If no assessment result is found for the given assessment ID.
+     */
+    void approveAll(UUID assessmentId, Collection<Long> attributeIds, LocalDateTime lastModificationTime);
 }
