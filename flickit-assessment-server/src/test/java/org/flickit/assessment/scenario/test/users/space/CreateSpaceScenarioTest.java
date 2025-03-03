80a6c33acecbbefacb69d0b816dce27a92cae1f7
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-03-03 14:17:10 +0330

    FAC-970: Remove EntityManager from adapter

--- flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/test/users/space/CreateSpaceScenarioTest.java
+++ flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/test/users/space/CreateSpaceScenarioTest.java
@@ -7,7 +7,7 @@ import org.flickit.assessment.scenario.test.AbstractScenarioTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.flickit.assessment.common.exception.api.ErrorCodes.ALREADY_EXISTS;
+import static org.flickit.assessment.common.exception.api.ErrorCodes.INVALID_INPUT;
 import static org.flickit.assessment.common.util.SlugCodeUtil.generateSlugCode;
 import static org.flickit.assessment.scenario.fixture.request.CreateSpaceRequestDtoMother.createSpaceRequestDto;
 import static org.hamcrest.Matchers.notNullValue;
@@ -59,10 +59,10 @@ class CreateSpaceScenarioTest extends AbstractScenarioTest {
         // Second invoke with the same request
         var response2 = spaceHelper.create(context, request);
         var error = response2.then()
-            .statusCode(409)
+            .statusCode(400)
             .extract().as(ErrorResponseDto.class);
 
-        assertEquals(ALREADY_EXISTS, error.code());
+        assertEquals(INVALID_INPUT, error.code());
         assertNotNull(error.message());
 
         int countAfter = jpaTemplate.count(SpaceJpaEntity.class);
