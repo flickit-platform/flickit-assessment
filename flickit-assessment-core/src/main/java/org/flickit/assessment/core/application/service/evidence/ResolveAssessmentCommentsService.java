e7ffa1c453b5180612634ccb77a2098c89518fb3
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-04 15:22:48 +0330

    FAC-981: Feat - Complete `Service` and `ServiceTest`

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/evidence/ResolveAssessmentCommentsService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/evidence/ResolveAssessmentCommentsService.java
@@ -5,7 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.core.application.port.in.evidence.ResolveAssessmentCommentsUseCase;
+import org.flickit.assessment.core.application.port.out.evidence.ResolveCommentPort;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.RESOLVE_ALL_COMMENTS;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
@@ -16,11 +19,15 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT
 public class ResolveAssessmentCommentsService implements ResolveAssessmentCommentsUseCase {
 
     private final AssessmentAccessChecker assessmentAccessChecker;
+    private final ResolveCommentPort resolveCommentPort;
 
     @Override
     public void resolveAllComments(Param param) {
         if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), RESOLVE_ALL_COMMENTS))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
+        resolveCommentPort.resolveAllComments(param.getAssessmentId(),
+            param.getCurrentUserId(),
+            LocalDateTime.now());
     }
 }
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/evidence/ResolveAssessmentCommentsServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/evidence/ResolveAssessmentCommentsServiceTest.java
@@ -3,12 +3,15 @@ package org.flickit.assessment.core.application.service.evidence;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.core.application.port.in.evidence.ResolveAssessmentCommentsUseCase;
+import org.flickit.assessment.core.application.port.out.evidence.ResolveCommentPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 import java.util.function.Consumer;
 
@@ -16,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.RESOLVE_ALL_COMMENTS;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.junit.Assert.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ResolveAssessmentCommentsServiceTest {
@@ -27,6 +30,9 @@ class ResolveAssessmentCommentsServiceTest {
     @Mock
     private AssessmentAccessChecker assessmentAccessChecker;
 
+    @Mock
+    private ResolveCommentPort resolveCommentPort;
+
     private final ResolveAssessmentCommentsUseCase.Param param = createParam(ResolveAssessmentCommentsUseCase.Param.ParamBuilder::build);
 
     @Test
@@ -36,6 +42,25 @@ class ResolveAssessmentCommentsServiceTest {
 
         var throwable = assertThrows(AccessDeniedException.class, () -> service.resolveAllComments(param));
         assertThat(throwable).hasMessage(COMMON_CURRENT_USER_NOT_ALLOWED);
+
+        verifyNoInteractions(resolveCommentPort);
+    }
+
+    @Test
+    void testResolveAssessmentComments_whenParametersAreValid_thenResolveAllComments() {
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), RESOLVE_ALL_COMMENTS))
+            .thenReturn(true);
+
+        ArgumentCaptor<UUID> assessmentIdCaptor = ArgumentCaptor.forClass(UUID.class);
+        ArgumentCaptor<UUID> lastModifiedByCaptor = ArgumentCaptor.forClass(UUID.class);
+        ArgumentCaptor<LocalDateTime> lastModificationTimeCaptor = ArgumentCaptor.forClass(LocalDateTime.class);
+
+        service.resolveAllComments(param);
+
+        verify(resolveCommentPort).resolveAllComments(assessmentIdCaptor.capture(), lastModifiedByCaptor.capture(), lastModificationTimeCaptor.capture());
+        assertThat(assessmentIdCaptor.getValue()).isEqualTo(param.getAssessmentId());
+        assertThat(lastModifiedByCaptor.getValue()).isEqualTo(param.getCurrentUserId());
+        assertThat(lastModificationTimeCaptor.getValue()).isNotNull();
     }
 
     private ResolveAssessmentCommentsUseCase.Param createParam(Consumer<ResolveAssessmentCommentsUseCase.Param.ParamBuilder> changer) {
@@ -49,5 +74,4 @@ class ResolveAssessmentCommentsServiceTest {
             .assessmentId(UUID.randomUUID())
             .currentUserId(UUID.randomUUID());
     }
-
 }
