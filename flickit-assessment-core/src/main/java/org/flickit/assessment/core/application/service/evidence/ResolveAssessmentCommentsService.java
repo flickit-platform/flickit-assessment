c807cfca4851266b78441e583bb013f6fc85b041
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-04 14:59:02 +0330

    FAC-981: Feat - Service and ServiceTest [WIP]

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/evidence/ResolveAssessmentCommentsService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/evidence/ResolveAssessmentCommentsService.java
@@ -2,16 +2,25 @@ package org.flickit.assessment.core.application.service.evidence;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.core.application.port.in.evidence.ResolveAssessmentCommentsUseCase;
 import org.springframework.stereotype.Service;
+
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.RESOLVE_ALL_COMMENTS;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ResolveAssessmentCommentsService implements ResolveAssessmentCommentsUseCase {
 
+    private final AssessmentAccessChecker assessmentAccessChecker;
+
     @Override
     public void resolveAllComments(Param param) {
+        if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), RESOLVE_ALL_COMMENTS))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
     }
 }
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/evidence/ResolveAssessmentCommentsServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/evidence/ResolveAssessmentCommentsServiceTest.java
@@ -1,17 +1,53 @@
 package org.flickit.assessment.core.application.service.evidence;
 
+import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.core.application.port.in.evidence.ResolveAssessmentCommentsUseCase;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.RESOLVE_ALL_COMMENTS;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ResolveAssessmentCommentsServiceTest {
 
+    @InjectMocks
+    private ResolveAssessmentCommentsService service;
+
+    @Mock
+    private AssessmentAccessChecker assessmentAccessChecker;
+
+    private final ResolveAssessmentCommentsUseCase.Param param = createParam(ResolveAssessmentCommentsUseCase.Param.ParamBuilder::build);
+
     @Test
     void testResolveAssessmentComments_WhenCurrentUserDoesNotHaveRequiredPermission_ThenThrowAccessDeniedException() {
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), RESOLVE_ALL_COMMENTS))
+            .thenReturn(false);
 
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.resolveAllComments(param));
+        assertThat(throwable).hasMessage(COMMON_CURRENT_USER_NOT_ALLOWED);
+    }
+
+    private ResolveAssessmentCommentsUseCase.Param createParam(Consumer<ResolveAssessmentCommentsUseCase.Param.ParamBuilder> changer) {
+        var param = paramBuilder();
+        changer.accept(param);
+        return param.build();
+    }
+
+    private ResolveAssessmentCommentsUseCase.Param.ParamBuilder paramBuilder() {
+        return ResolveAssessmentCommentsUseCase.Param.builder()
+            .assessmentId(UUID.randomUUID())
+            .currentUserId(UUID.randomUUID());
     }
 
 }
