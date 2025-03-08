c4780e4d7619961e3996a0245eacac78fad6b708
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-08 12:51:36 +0330

    FAC-983: Feat - Complete `Service` and `ServiceTest`

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/evidence/ApproveAssessmentAnswersService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/evidence/ApproveAssessmentAnswersService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.core.application.port.in.evidence.ApproveAssessmentAnswersUseCase;
+import org.flickit.assessment.core.application.port.out.answer.ApproveAnswerPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,10 +17,13 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT
 public class ApproveAssessmentAnswersService implements ApproveAssessmentAnswersUseCase {
 
     private final AssessmentAccessChecker assessmentAccessChecker;
+    private final ApproveAnswerPort approveAnswerPort;
 
     @Override
     public void approveAllAnswers(Param param) {
         if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), APPROVE_ALL_ANSWERS))
             throw new AccessDeniedException(COMMON_CURRENT_USER_ID_NOT_NULL);
+
+        approveAnswerPort.approveAll(param.getAssessmentId(), param.getCurrentUserId());
     }
 }
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/evidence/ApproveAssessmentAnswersServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/evidence/ApproveAssessmentAnswersServiceTest.java
@@ -3,6 +3,7 @@ package org.flickit.assessment.core.application.service.evidence;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.core.application.port.in.evidence.ApproveAssessmentAnswersUseCase;
+import org.flickit.assessment.core.application.port.out.answer.ApproveAnswerPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,7 +17,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.APPROVE_ALL_ANSWERS;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ApproveAssessmentAnswersServiceTest {
@@ -26,6 +27,10 @@ class ApproveAssessmentAnswersServiceTest {
 
     @Mock
     private AssessmentAccessChecker assessmentAccessChecker;
+
+    @Mock
+    private ApproveAnswerPort approveAnswerPort;
+
 
     private ApproveAssessmentAnswersUseCase.Param param = createParam(ApproveAssessmentAnswersUseCase.Param.ParamBuilder::build);
 
@@ -37,6 +42,18 @@ class ApproveAssessmentAnswersServiceTest {
         var throwable = assertThrows(AccessDeniedException.class, () -> service.approveAllAnswers(param));
 
         assertThat(throwable.getMessage()).isEqualTo(COMMON_CURRENT_USER_ID_NOT_NULL);
+
+        verifyNoInteractions(approveAnswerPort);
+    }
+
+    @Test
+    void testApproveAllAnswers_whenParametersAreValid_thenSuccessfullyApproved() {
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), APPROVE_ALL_ANSWERS))
+            .thenReturn(true);
+
+        service.approveAllAnswers(param);
+
+        verify(approveAnswerPort).approveAll(param.getAssessmentId(), param.getCurrentUserId());
     }
 
     private ApproveAssessmentAnswersUseCase.Param createParam(Consumer<ApproveAssessmentAnswersUseCase.Param.ParamBuilder> changer) {
