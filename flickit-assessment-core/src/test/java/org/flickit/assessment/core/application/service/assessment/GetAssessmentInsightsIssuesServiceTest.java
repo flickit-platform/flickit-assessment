38d5726e0bc661a8a912c0c0e6fb1b69d6b5d6a1
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-11 12:11:16 +0330

    FAC-986: Refactor - Add `GetAssessmentInsightsIssuesServiceTest` [WIP]

---
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/GetAssessmentInsightsIssuesServiceTest.java
@@ -0,0 +1,15 @@
+package org.flickit.assessment.core.application.service.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class GetAssessmentInsightsIssuesServiceTest {
+
+    @Test
+    void testGetAssessmentInsightsIssues() {
+    }
+}
