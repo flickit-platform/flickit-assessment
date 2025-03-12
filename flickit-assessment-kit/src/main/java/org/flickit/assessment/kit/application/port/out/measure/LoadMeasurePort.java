ef73564ca8e886e9418ffd2195d256d65070b098
mahdirezazadeh <riza.rs2000@gmail.com>
2025-03-12 10:08:00 +0330

    FAC-987:impl:Update measures order in UpdateQuestionnaireOrdersService

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/measure/LoadMeasurePort.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/measure/LoadMeasurePort.java
@@ -2,7 +2,11 @@ package org.flickit.assessment.kit.application.port.out.measure;
 
 import org.flickit.assessment.kit.application.domain.Measure;
 
+import java.util.List;
+
 public interface LoadMeasurePort {
 
     Measure loadByCode(Long kitVersionId, String code);
+
+    List<Measure> loadAllByKitVersionId(Long kitVersionId);
 }
--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/measure/UpdateMeasurePort.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/measure/UpdateMeasurePort.java
@@ -1,6 +1,7 @@
 package org.flickit.assessment.kit.application.port.out.measure;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface UpdateMeasurePort {
@@ -16,5 +17,15 @@ public interface UpdateMeasurePort {
         String description,
         LocalDateTime lastModificationTime,
         UUID lastModifiedBy) {
+    }
+
+    void updateOrders(UpdateOrderParam param);
+
+    record UpdateOrderParam(List<MeasureOrder> orders,
+                            long kitVersionId,
+                            LocalDateTime lastModificationTime,
+                            UUID lastModifiedBy) {
+        public record MeasureOrder(long measureId, int index) {
+        }
     }
 }
