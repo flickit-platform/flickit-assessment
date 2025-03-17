14165f4bd2759f7bb18e17800d4170197e490db9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-17 17:49:17 +0330

    FAC-994: Feat - Add `currentUserIsNotExpertGroupOwner` scenarioTest

--- flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/test/kit/assessmentkit/GrantUserAccessToKitScenarioTest.java
+++ flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/test/kit/assessmentkit/GrantUserAccessToKitScenarioTest.java
@@ -1,5 +1,6 @@
 package org.flickit.assessment.scenario.test.kit.assessmentkit;
 
+import org.flickit.assessment.common.exception.api.ErrorResponseDto;
 import org.flickit.assessment.scenario.fixture.request.CreateUserRequestDtoMother;
 import org.flickit.assessment.scenario.test.AbstractScenarioTest;
 import org.flickit.assessment.scenario.test.kit.kitdsl.KitDslTestHelper;
@@ -12,9 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import java.util.UUID;
 
+import static org.flickit.assessment.common.exception.api.ErrorCodes.ACCESS_DENIED;
 import static org.flickit.assessment.scenario.fixture.request.CreateExpertGroupRequestDtoMother.createExpertGroupRequestDto;
 import static org.flickit.assessment.scenario.fixture.request.CreateKitByDslRequestDtoMother.createKitByDslRequestDto;
 import static org.flickit.assessment.scenario.fixture.request.GrantUserAccessToKitRequestDtoMother.grantUserAccessToKitRequestDto;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class GrantUserAccessToKitScenarioTest extends AbstractScenarioTest {
 
@@ -37,6 +41,7 @@ public class GrantUserAccessToKitScenarioTest extends AbstractScenarioTest {
     void grantUserAccessToKit() {
         var expertGroupId = createExpertGroup();
         int kitId = createKit(expertGroupId);
+        // Generate a new user ID and create the user
         UUID userId = UUID.randomUUID();
         userHelper.create(CreateUserRequestDtoMother.createUserRequestDto(b -> b.id(userId)));
 
@@ -44,6 +49,24 @@ public class GrantUserAccessToKitScenarioTest extends AbstractScenarioTest {
 
         kitHelper.grantUserAccessToKit(context, request, kitId)
             .then().statusCode(200);
+    }
+
+    @Test
+    void grantUserAccessToKit_currentUserIsNotExpertGroupOwner() {
+        var expertGroupId = createExpertGroup();
+        int kitId = createKit(expertGroupId);
+        // Generate a new user ID and create the user
+        UUID userId = UUID.randomUUID();
+        userHelper.create(CreateUserRequestDtoMother.createUserRequestDto(b -> b.id(userId)));
+        //Change the current user
+        context.getNextCurrentUser();
+        var request = grantUserAccessToKitRequestDto(b -> b.userId(userId));
+        var error = kitHelper.grantUserAccessToKit(context, request, kitId)
+            .then().statusCode(403)
+            .extract().as(ErrorResponseDto.class);
+
+        assertEquals(ACCESS_DENIED, error.code());
+        assertNotNull(error.message());
     }
 
     Integer createKit(long expertGroupId) {
