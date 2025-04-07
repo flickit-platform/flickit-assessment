9974b8f73c309367d2170b38c8906f22c4634fa9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-04-07 08:25:52 +0330

    FAC-1004: Fix - Change JpaRepository id type from long to KitLanguageJpaEntity.EntityId

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/kitlanguage/KitLanguageJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/kitlanguage/KitLanguageJpaRepository.java
@@ -2,5 +2,5 @@ package org.flickit.assessment.data.jpa.kit.kitlanguage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface KitLanguageJpaRepository extends JpaRepository<KitLanguageJpaEntity, Long> {
+public interface KitLanguageJpaRepository extends JpaRepository<KitLanguageJpaEntity, KitLanguageJpaEntity.EntityId> {
 }
