6be838e73ac9908d1fcd0d0e273e8312777c0115
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-04-07 16:23:20 +0330

    FAC-1007: Test - Add GetMeasuresUseCaseParamTest.java

---
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/port/in/measure/GetMeasuresUseCaseParamTest.java
@@ -0,0 +1,63 @@
+package org.flickit.assessment.kit.application.port.in.measure;
+
+
+import jakarta.validation.ConstraintViolationException;
+import org.flickit.assessment.kit.application.port.in.questionnaire.GetQuestionnairesUseCase;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
+import static org.flickit.assessment.kit.common.ErrorMessageKey.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetQMeasuresUseCaseParamTest {
+
+    @Test
+    void testGetMeasuresUseCaseParam_kitVersionIdParamViolatesConstraints_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.kitVersionId(null)));
+        assertThat(throwable).hasMessage("kitVersionId: " + GET_MEASURES_KIT_VERSION_ID_NOT_NULL);
+    }
+
+    @Test
+    void testGetMeasuresUseCaseParam_currentUserIdParamViolatesConstraints_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.currentUserId(null)));
+        assertThat(throwable).hasMessage("currentUserId: " + COMMON_CURRENT_USER_ID_NOT_NULL);
+    }
+
+    @Test
+    void testGetMeasuresUseCaseParam_pageParamViolatesConstraints_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.page(-1)));
+        assertThat(throwable).hasMessage("page: " + GET_MEASURES_PAGE_MIN);
+    }
+
+    @Test
+    void testGetMeasuresUseCaseParam_sizeParamViolatesConstraints_ConstraintViolationException() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.size(0)));
+        assertThat(throwable).hasMessage("size: " + GET_MEASURES_SIZE_MIN);
+
+        throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.size(101)));
+        assertThat(throwable).hasMessage("size: " + GET_MEASURES_SIZE_MAX);
+    }
+
+    private void createParam(Consumer<GetMeasuresUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        paramBuilder.build();
+    }
+
+    private GetMeasuresUseCase.Param.ParamBuilder paramBuilder() {
+        return GetMeasuresUseCase.Param.builder()
+            .kitVersionId(1L)
+            .currentUserId(UUID.randomUUID())
+            .page(1)
+            .size(10);
+    }
+}
