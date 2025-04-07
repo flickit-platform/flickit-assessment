523abc0b80acc7c28efccd914b70967e58ebf79b
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-04-07 08:21:08 +0330

    FAC-1004: Feat - Add KitLanguageJpaRepository

---
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/kitlanguage/KitLanguageJpaRepository.java
@@ -0,0 +1,6 @@
+package org.flickit.assessment.data.jpa.kit.kitlanguage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KitLanguageJpaRepository extends JpaRepository<KitLanguageJpaEntity, Long> {
+}
