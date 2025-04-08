aea5eec2171aa6c6b1ee347bb24aa361376eeefb
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-04-08 07:56:22 +0330

    FAC-1007: Test - Minor code improvement

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/measure/GetMeasuresResponseDto.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/measure/GetMeasuresResponseDto.java
@@ -1,4 +1,8 @@
 package org.flickit.assessment.kit.adapter.in.rest.measure;
 
-public record GetMeasuresResponseDto(long id, String title, int index, String description, int questionsCount) {
+public record GetMeasuresResponseDto(long id,
+                                     String title,
+                                     int index,
+                                     String description,
+                                     int questionsCount) {
 }
