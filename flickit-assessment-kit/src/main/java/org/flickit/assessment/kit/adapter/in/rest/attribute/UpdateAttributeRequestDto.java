27fbb9d8122be009192763d38b5fee6f9d7360c7
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-04-08 14:43:47 +0330

    FAC-1009: Add translations field to UpdateAttributeUseCase param

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/attribute/UpdateAttributeRequestDto.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/attribute/UpdateAttributeRequestDto.java
@@ -1,8 +1,13 @@
 package org.flickit.assessment.kit.adapter.in.rest.attribute;
 
+import org.flickit.assessment.common.application.domain.kit.translate.AttributeTranslation;
+
+import java.util.Map;
+
 public record UpdateAttributeRequestDto(Integer index,
                                         String title,
                                         String description,
                                         Integer weight,
-                                        Long subjectId) {
+                                        Long subjectId,
+                                        Map<String, AttributeTranslation> translations) {
 }
