e827bf356fb3f2c3a30179590a60e3534ee59920
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-04-08 14:47:23 +0330

    FAC-1009: Add translations field to Attribute domain class

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/domain/Attribute.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/domain/Attribute.java
@@ -3,8 +3,12 @@ package org.flickit.assessment.kit.application.domain;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.flickit.assessment.common.application.domain.kit.KitLanguage;
+import org.flickit.assessment.common.application.domain.kit.translate.AttributeTranslation;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.UUID;
 
 @Getter
@@ -22,4 +26,6 @@ public class Attribute {
     @EqualsAndHashCode.Exclude private final LocalDateTime lastModificationTime;
     private final UUID createdBy;
     private final UUID lastModifiedBy;
+    @Setter
+    private Map<KitLanguage, AttributeTranslation> translations;
 }
