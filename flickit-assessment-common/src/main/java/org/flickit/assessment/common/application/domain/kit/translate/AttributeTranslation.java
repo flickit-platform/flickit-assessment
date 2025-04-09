82fb26041163a09bd6c409b2076d82ac5825ede6
mahdirezazadeh <riza.rs2000@gmail.com>
2025-04-09 12:10:08 +0330

    FAC-1009:impl:Update AttributeTranslation max char validation to 500

--- flickit-assessment-common/src/main/java/org/flickit/assessment/common/application/domain/kit/translate/AttributeTranslation.java
+++ flickit-assessment-common/src/main/java/org/flickit/assessment/common/application/domain/kit/translate/AttributeTranslation.java
@@ -7,12 +7,13 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.*;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record AttributeTranslation(
+
     @Size(min = 3, message = TRANSLATE_ATTRIBUTE_TITLE_SIZE_MIN)
     @Size(max = 100, message = TRANSLATE_ATTRIBUTE_TITLE_SIZE_MAX)
     String title,
 
     @Size(min = 3, message = TRANSLATE_ATTRIBUTE_DESCRIPTION_SIZE_MIN)
-    @Size(max = 100, message = TRANSLATE_ATTRIBUTE_DESCRIPTION_SIZE_MAX)
+    @Size(max = 500, message = TRANSLATE_ATTRIBUTE_DESCRIPTION_SIZE_MAX)
     String description
 ) {
 }
