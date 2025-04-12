b634143da123734b1d5de328e65989b1352d481f
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-04-12 08:39:46 +0330

    FAC-1021: Feat : Add translations to QuestionUpdateKitPersister

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/updatebydsl/impl/QuestionUpdateKitPersister.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/assessmentkit/updatebydsl/impl/QuestionUpdateKitPersister.java
@@ -273,6 +273,7 @@ public class QuestionUpdateKitPersister implements UpdateKitPersister {
                 dslQuestion.isMayNotBeApplicable(),
                 dslQuestion.isAdvisable(),
                 savedQuestion.getAnswerRangeId(),
+                savedQuestion.getTranslations(),
                 LocalDateTime.now(),
                 currentUserId
             );
