0ecf1b764a47a8a8cb2e7717c56f75ec210169c6
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-04-12 14:05:02 +0330

    FAC-1026: Feat : Add CreateMeasureRequestDto and CreateMeasureRestController

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/measure/CreateMeasureRestController.java
@@ -0,0 +1,40 @@
+package org.flickit.assessment.kit.adapter.in.rest.measure;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.flickit.assessment.kit.application.port.in.measure.CreateMeasureUseCase;
+import org.flickit.assessment.kit.application.port.in.measure.CreateMeasureUseCase.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class CreateMeasureRestController {
+
+    private final CreateMeasureUseCase useCase;
+    private final UserContext userContext;
+
+    @PostMapping("/kit-versions/{kitVersionId}/measures")
+    public ResponseEntity<Result> createMeasure(@PathVariable("kitVersionId") Long kitVersionId,
+                                                @RequestBody CreateMeasureRequestDto requestDto) {
+        UUID currentUserId = userContext.getUser().id();
+        long measureId = useCase.createMeasure(toParam(kitVersionId, requestDto, currentUserId));
+        return new ResponseEntity<>(new Result(measureId), HttpStatus.CREATED);
+    }
+
+    private Param toParam(Long kitVersionId,
+                          CreateMeasureRequestDto requestDto,
+                          UUID currentUserId) {
+        return new Param(kitVersionId,
+            requestDto.index(),
+            requestDto.title(),
+            requestDto.description(),
+            currentUserId);
+    }
+}
