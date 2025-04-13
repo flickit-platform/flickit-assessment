dccaded044074a0d5e00bba403fe11224a7724fa
mahdirezazadeh <riza.rs2000@gmail.com>
2025-04-13 15:39:13 +0330

    FAC-1018:impl:Add translations to CreateSubjectUseCase

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/subject/CreateSubjectRequestDto.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/subject/CreateSubjectRequestDto.java
@@ -1,7 +1,12 @@
 package org.flickit.assessment.kit.adapter.in.rest.subject;
 
+import org.flickit.assessment.common.application.domain.kit.translation.SubjectTranslation;
+
+import java.util.Map;
+
 public record CreateSubjectRequestDto(Integer index,
                                       String title,
                                       String description,
-                                      Integer weight) {
+                                      Integer weight,
+                                      Map<String, SubjectTranslation> translations) {
 }
