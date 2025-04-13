1e2ac2695901f85083844b6908167beca72beba4
mahdirezazadeh <riza.rs2000@gmail.com>
2025-04-13 16:33:01 +0330

    FAC-1018:impl:Update CreateSubjectUseCaseParamTest

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/port/in/subject/CreateSubjectUseCaseParamTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/port/in/subject/CreateSubjectUseCaseParamTest.java
@@ -2,13 +2,16 @@ package org.flickit.assessment.kit.application.port.in.subject;
 
 import jakarta.validation.ConstraintViolationException;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.flickit.assessment.common.application.domain.kit.translation.SubjectTranslation;
+import org.flickit.assessment.common.exception.ValidationException;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
+import static org.flickit.assessment.common.error.ErrorMessageKey.*;
 import static org.flickit.assessment.kit.common.ErrorMessageKey.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -66,6 +69,32 @@ class CreateSubjectUseCaseParamTest {
     }
 
     @Test
+    void testCreateSubjectUseCaseParam_translationsLanguageViolations_ErrorMessage() {
+        var throwable = assertThrows(ValidationException.class,
+            () -> createParam(a -> a.translations(Map.of("FR", new SubjectTranslation("title", "desc")))));
+        assertEquals(COMMON_KIT_LANGUAGE_NOT_VALID, throwable.getMessageKey());
+    }
+
+    @Test
+    void testCreateSubjectUseCaseParam_translationsFieldsViolations_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(a -> a.translations(Map.of("EN", new SubjectTranslation("t", "desc")))));
+        assertThat(throwable).hasMessage("translations[EN].title: " + TRANSLATION_SUBJECT_TITLE_SIZE_MIN);
+
+        throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(a -> a.translations(Map.of("EN", new SubjectTranslation(RandomStringUtils.randomAlphabetic(101), "desc")))));
+        assertThat(throwable).hasMessage("translations[EN].title: " + TRANSLATION_SUBJECT_TITLE_SIZE_MAX);
+
+        throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(a -> a.translations(Map.of("EN", new SubjectTranslation("title", "de")))));
+        assertThat(throwable).hasMessage("translations[EN].description: " + TRANSLATION_SUBJECT_DESCRIPTION_SIZE_MIN);
+
+        throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(a -> a.translations(Map.of("EN", new SubjectTranslation("title", RandomStringUtils.randomAlphabetic(501))))));
+        assertThat(throwable).hasMessage("translations[EN].description: " + TRANSLATION_SUBJECT_DESCRIPTION_SIZE_MAX);
+    }
+
+    @Test
     void testCreateSubjectUseCaseParam_currentUserParamViolatesConstraints_ErrorMessage() {
         var throwable = assertThrows(ConstraintViolationException.class,
             () -> createParam(b -> b.currentUserId(null)));
@@ -85,6 +114,7 @@ class CreateSubjectUseCaseParamTest {
             .title("Team")
             .description("team description")
             .weight(2)
+            .translations(Map.of("EN", new SubjectTranslation("titl", "desc")))
             .currentUserId(UUID.randomUUID());
     }
 }
