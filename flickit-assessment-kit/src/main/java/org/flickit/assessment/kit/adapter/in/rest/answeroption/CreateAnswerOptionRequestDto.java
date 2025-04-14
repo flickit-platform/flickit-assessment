17a56a8a4a2a158784e8116fdc643ffe6932c416
mahdirezazadeh <riza.rs2000@gmail.com>
2025-04-14 15:26:53 +0330

    FAC-1037:impl:Add translations to CreateAnswerOptionRequestDto

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/answeroption/CreateAnswerOptionRequestDto.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/answeroption/CreateAnswerOptionRequestDto.java
@@ -1,7 +1,12 @@
 package org.flickit.assessment.kit.adapter.in.rest.answeroption;
 
+import org.flickit.assessment.common.application.domain.kit.translation.AnswerOptionTranslation;
+
+import java.util.Map;
+
 public record CreateAnswerOptionRequestDto(Long questionId,
                                            Integer index,
                                            String title,
-                                           Double value) {
+                                           Double value,
+                                           Map<String, AnswerOptionTranslation> translations) {
 }
