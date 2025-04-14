73f3a8a15103303eae08aaf4b69e5b51ae670d06
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-04-14 17:04:01 +0330

    FAC-1032: Set Attribute translations in mapper

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/attribute/AttributeMapper.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/attribute/AttributeMapper.java
@@ -2,6 +2,8 @@ package org.flickit.assessment.kit.adapter.out.persistence.attribute;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.flickit.assessment.common.application.domain.kit.KitLanguage;
+import org.flickit.assessment.common.application.domain.kit.translation.AttributeTranslation;
 import org.flickit.assessment.common.util.JsonUtils;
 import org.flickit.assessment.data.jpa.kit.attribute.AttributeJpaEntity;
 import org.flickit.assessment.data.jpa.kit.subject.SubjectJoinAttributeView;
@@ -20,6 +22,7 @@ public class AttributeMapper {
             entity.getIndex(),
             entity.getDescription(),
             entity.getWeight(),
+            JsonUtils.fromJsonToMap(entity.getTranslations(), KitLanguage.class, AttributeTranslation.class),
             entity.getCreationTime(),
             entity.getLastModificationTime(),
             entity.getCreatedBy(),
