462ad18a84d58ec358558fa299ff97f794152647
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-04-14 17:17:46 +0330

    FAC-1036: Feat : Add GetKitSliderBannersService and GetKitSliderBannersServiceTest [WIP]

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/kitbanner/GetKitSliderBannersService.java
@@ -0,0 +1,19 @@
+package org.flickit.assessment.kit.application.service.kitbanner;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.kit.application.port.in.kitbanner.GetKitSliderBannersUseCase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class GetKitSliderBannersService implements GetKitSliderBannersUseCase {
+
+    @Override
+    public List<Result> getSliderBanners() {
+        return List.of();
+    }
+}
