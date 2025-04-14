a4b59d5029f64daf0de2a97c9ea59cae46a03a55
mahdirezazadeh <riza.rs2000@gmail.com>
2025-04-14 10:03:25 +0330

    FAC-1022:impl:Add translations to CreateMaturityLevelUseCase.Param

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/in/maturitylevel/CreateMaturityLevelUseCase.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/in/maturitylevel/CreateMaturityLevelUseCase.java
@@ -1,15 +1,21 @@
 package org.flickit.assessment.kit.application.port.in.maturitylevel;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.flickit.assessment.common.application.SelfValidating;
+import org.flickit.assessment.common.application.domain.kit.KitLanguage;
+import org.flickit.assessment.common.application.domain.kit.translation.MaturityLevelTranslation;
 
+import java.util.Map;
 import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_KIT_LANGUAGE_NOT_VALID;
+import static org.flickit.assessment.common.validation.EnumValidateUtils.validateAndConvert;
 import static org.flickit.assessment.kit.common.ErrorMessageKey.*;
 
 public interface CreateMaturityLevelUseCase {
@@ -39,6 +45,9 @@ public interface CreateMaturityLevelUseCase {
         @NotNull(message = CREATE_MATURITY_LEVEL_VALUE_NOT_NULL)
         Integer value;
 
+        @Valid
+        Map<KitLanguage, MaturityLevelTranslation> translations;
+
         @NotNull(message = COMMON_CURRENT_USER_ID_NOT_NULL)
         UUID currentUserId;
 
@@ -48,12 +57,14 @@ public interface CreateMaturityLevelUseCase {
                      String title,
                      String description,
                      Integer value,
+                     Map<String, MaturityLevelTranslation> translations,
                      UUID currentUserId) {
             this.kitVersionId = kitVersionId;
             this.index = index;
             this.title = title != null && !title.isBlank() ? title.trim() : null;
             this.description = description != null && !description.isBlank() ? description.trim() : null;
             this.value = value;
+            this.translations = validateAndConvert(translations, KitLanguage.class, COMMON_KIT_LANGUAGE_NOT_VALID);
             this.currentUserId = currentUserId;
             this.validateSelf();
         }
