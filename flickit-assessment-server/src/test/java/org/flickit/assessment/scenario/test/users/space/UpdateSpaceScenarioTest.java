f648e6bbe13b756aa87563684655836d37d6dbcb
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-04-20 09:37:00 +0330

    FAC-999: Minor refactor-extract method

--- flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/test/users/space/UpdateSpaceScenarioTest.java
+++ flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/test/users/space/UpdateSpaceScenarioTest.java
@@ -6,13 +6,14 @@ import org.flickit.assessment.scenario.test.AbstractScenarioTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.common.exception.api.ErrorCodes.ACCESS_DENIED;
 import static org.flickit.assessment.common.exception.api.ErrorCodes.INVALID_INPUT;
 import static org.flickit.assessment.common.util.SlugCodeUtil.generateSlugCode;
 import static org.flickit.assessment.scenario.fixture.request.CreateSpaceRequestDtoMother.createSpaceRequestDto;
 import static org.flickit.assessment.scenario.fixture.request.UpdateSpaceRequestDtoMother.updateSpaceRequestDto;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class UpdateSpaceScenarioTest extends AbstractScenarioTest {
 
@@ -21,14 +22,7 @@ class UpdateSpaceScenarioTest extends AbstractScenarioTest {
 
     @Test
     void updateSpace() {
-        var createRequest = createSpaceRequestDto();
-        var createResponse = spaceHelper.create(context, createRequest);
-
-        createResponse.then()
-                .statusCode(201)
-                .body("id", notNullValue());
-
-        Number spaceId = createResponse.body().path("id");
+        Number spaceId = createSpace();
         SpaceJpaEntity createdSpace = jpaTemplate.load(spaceId, SpaceJpaEntity.class);
 
         var updateRequest = updateSpaceRequestDto();
@@ -40,128 +34,109 @@ class UpdateSpaceScenarioTest extends AbstractScenarioTest {
         assertEquals(updateRequest.title(), updatedSpace.getTitle());
         assertEquals(generateSlugCode(updateRequest.title()), updatedSpace.getCode());
         assertEquals(context.getCurrentUser().getUserId(), updatedSpace.getLastModifiedBy());
-        assertTrue(updatedSpace.getLastModificationTime().isAfter(createdSpace.getLastModificationTime()));
+        assertThat(updatedSpace.getLastModificationTime()).isAfter(createdSpace.getLastModificationTime());
     }
 
     @Test
     void updateSpace_withSameTitleForDifferentUsers() {
-        var firstCreateRequest = createSpaceRequestDto();
-        var firstCreateResponse = spaceHelper.create(context, firstCreateRequest);
         // Create first space
-        firstCreateResponse.then()
-                .statusCode(201);
+        Number firstSpaceId = createSpace();
+        SpaceJpaEntity firstSpace = jpaTemplate.load(firstSpaceId, SpaceJpaEntity.class);
+
         // Change currentUser
         context.getNextCurrentUser();
-        var secondCreateRequest = createSpaceRequestDto();
-        // Create second space for different user
-        var secondCreateResponse = spaceHelper.create(context, secondCreateRequest);
-        secondCreateResponse.then()
-                .statusCode(201)
-                .body("id", notNullValue());
 
-        Number secondSpaceId = secondCreateResponse.body().path("id");
+        // Create second space for different user
+        Number secondSpaceId = createSpace();
         SpaceJpaEntity secondSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
+
         // Update the second space with first space's title
-        var updateRequest = updateSpaceRequestDto(b -> b.title(firstCreateRequest.title()));
+        var updateRequest = updateSpaceRequestDto(b -> b.title(firstSpace.getTitle()));
         spaceHelper.update(context, updateRequest, secondSpaceId).then()
                 .statusCode(200);
 
         SpaceJpaEntity updatedSecondSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
 
-        assertEquals(firstCreateRequest.title(), updatedSecondSpace.getTitle());
-        assertEquals(generateSlugCode(updatedSecondSpace.getTitle()), updatedSecondSpace.getCode());
+        assertEquals(firstSpace.getTitle(), updatedSecondSpace.getTitle());
+        assertEquals(firstSpace.getCode(), updatedSecondSpace.getCode());
         assertEquals(context.getCurrentUser().getUserId(), secondSpace.getLastModifiedBy());
-        assertTrue(updatedSecondSpace.getLastModificationTime().isAfter(secondSpace.getLastModificationTime()));
+        assertThat(updatedSecondSpace.getLastModificationTime()).isAfter(secondSpace.getLastModificationTime());
     }
 
     @Test
     void updateSpace_withSameTitleAsDeleted() {
-        var firstCreateRequest = createSpaceRequestDto();
         // Create first space
-        var firstCreateResponse = spaceHelper.create(context, firstCreateRequest);
-        firstCreateResponse.then()
-                .statusCode(201)
-                .body("id", notNullValue());
+        Number firstSpaceId = createSpace();
+        SpaceJpaEntity firstSpace = jpaTemplate.load(firstSpaceId, SpaceJpaEntity.class);
 
-        Number firstSpaceId = firstCreateResponse.body().path("id");
+        // Create second space with different title
+        Number secondSpaceId = createSpace();
+        SpaceJpaEntity secondSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
+
         // Delete the first space
         spaceHelper.delete(context, firstSpaceId);
 
-        var secondCreateRequest = createSpaceRequestDto();
-        // Create second space with different request
-        var secondCreateResponse = spaceHelper.create(context, secondCreateRequest);
-        secondCreateResponse.then()
-                .statusCode(201)
-                .body("id", notNullValue());
-
-        Number secondSpaceId = secondCreateResponse.body().path("id");
         // Update the second space's title to match the deleted space's title
-        var updateRequest = updateSpaceRequestDto(b -> b.title(firstCreateRequest.title()));
+        var updateRequest = updateSpaceRequestDto(b -> b.title(firstSpace.getTitle()));
         spaceHelper.update(context, updateRequest, secondSpaceId).then()
                 .statusCode(200);
 
-        SpaceJpaEntity space = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
+        SpaceJpaEntity updatedSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
 
-        assertEquals(updateRequest.title(), space.getTitle());
-        assertEquals(generateSlugCode(updateRequest.title()), space.getCode());
-        assertEquals(context.getCurrentUser().getUserId(), space.getLastModifiedBy());
-        assertTrue(space.getLastModificationTime().isAfter(space.getCreationTime()));
+        assertEquals(firstSpace.getTitle(), updatedSpace.getTitle());
+        assertEquals(firstSpace.getCode(), updatedSpace.getCode());
+        assertEquals(context.getCurrentUser().getUserId(), updatedSpace.getLastModifiedBy());
+        assertThat(updatedSpace.getLastModificationTime()).isAfter(secondSpace.getLastModificationTime());
     }
 
     @Test
     void updateSpace_userIsNotOwner() {
-        var createRequest = createSpaceRequestDto();
-        var createResponse = spaceHelper.create(context, createRequest);
         // Create space
-        createResponse.then()
-                .statusCode(201)
-                .body("id", notNullValue());
+        Number spaceId = createSpace();
 
-        Number spaceId = createResponse.body().path("id");
-
-        var updateRequest = updateSpaceRequestDto();
         // Change currentUser which is not owner (creator) of the space
         context.getNextCurrentUser();
+
+        var updateRequest = updateSpaceRequestDto();
         var error = spaceHelper.update(context, updateRequest, spaceId).then()
                 .statusCode(403)
                 .extract().as(ErrorResponseDto.class);
 
         SpaceJpaEntity space = jpaTemplate.load(spaceId, SpaceJpaEntity.class);
 
-        assertEquals(ACCESS_DENIED, error.code());
-        assertEquals(createRequest.title(), space.getTitle());
         assertEquals(space.getCreationTime(), space.getLastModificationTime());
+        assertEquals(ACCESS_DENIED, error.code());
         assertNotNull(error.message());
     }
 
     @Test
     void updateSpace_withSameTitle() {
-        var firstCreateRequest = createSpaceRequestDto();
         // Create first space
-        var firstCreateResponse = spaceHelper.create(context, firstCreateRequest);
-        firstCreateResponse.then()
-                .statusCode(201)
-                .body("id", notNullValue());
+        Number firstSpaceId = createSpace();
+        SpaceJpaEntity firstSpace = jpaTemplate.load(firstSpaceId, SpaceJpaEntity.class);
 
-        var secondCreateRequest = createSpaceRequestDto();
-        // Create second space with different request
-        var secondCreateResponse = spaceHelper.create(context, secondCreateRequest);
-        secondCreateResponse.then()
-                .statusCode(201)
-                .body("id", notNullValue());
+        // Create second space with different title
+        Number secondSpaceId = createSpace();
+        SpaceJpaEntity secondSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
 
-        Number secondSpaceId = secondCreateResponse.body().path("id");
         // Update the second space's title to match the first space's title
-        var updateRequest = updateSpaceRequestDto(b -> b.title(firstCreateRequest.title()));
+        var updateRequest = updateSpaceRequestDto(b -> b.title(firstSpace.getTitle()));
         var error = spaceHelper.update(context, updateRequest, secondSpaceId).then()
                 .statusCode(400)
                 .extract().as(ErrorResponseDto.class);
 
-        SpaceJpaEntity space = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
+        SpaceJpaEntity updatedSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
 
+        assertEquals(secondSpace.getTitle(), updatedSpace.getTitle());
+        assertEquals(updatedSpace.getCreationTime(), updatedSpace.getLastModificationTime());
         assertEquals(INVALID_INPUT, error.code());
-        assertEquals(secondCreateRequest.title(), space.getTitle());
-        assertEquals(space.getCreationTime(), space.getLastModificationTime());
         assertNotNull(error.message());
+    }
+
+    private long createSpace() {
+        var request = createSpaceRequestDto();
+        var response = spaceHelper.create(context, request);
+        Number id = response.path("id");
+        return id.longValue();
     }
 }
