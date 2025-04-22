6490d8fc20a907466dae3b1ca3d0c889373115e0
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-04-22 10:37:48 +0330

    FAC-1057: Add `titleOrDefault` to KitTranslation

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/assessment/AssessmentPersistenceJpaAdapter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/assessment/AssessmentPersistenceJpaAdapter.java
@@ -2,9 +2,11 @@ package org.flickit.assessment.core.adapter.out.persistence.assessment;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
+import org.flickit.assessment.common.application.domain.kit.KitLanguage;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.core.adapter.out.persistence.answer.AnswerMapper;
 import org.flickit.assessment.core.adapter.out.persistence.kit.answeroption.AnswerOptionMapper;
+import org.flickit.assessment.core.adapter.out.persistence.kit.assessmentkit.AssessmentKitMapper;
 import org.flickit.assessment.core.adapter.out.persistence.kit.question.QuestionMapper;
 import org.flickit.assessment.core.adapter.out.persistence.kit.questionimpact.QuestionImpactMapper;
 import org.flickit.assessment.core.application.domain.Assessment;
@@ -151,7 +153,12 @@ public class AssessmentPersistenceJpaAdapter implements
             .map(e -> {
                 AssessmentKitJpaEntity kitEntity = kitIdToKitEntity.get(e.getAssessment().getAssessmentKitId());
                 List<MaturityLevelJpaEntity> kitLevelEntities = kitVersionIdToMaturityLevelEntities.get(e.getAssessmentResult().getKitVersionId());
-                AssessmentListItem.Kit kit = new AssessmentListItem.Kit(kitEntity.getId(), kitEntity.getTitle(), kitLevelEntities.size());
+
+                var language = Objects.equals(e.getAssessmentResult().getLangId(), kitEntity.getLanguageId())
+                    ? null
+                    : KitLanguage.valueOfById(e.getAssessmentResult().getLangId());
+
+                AssessmentListItem.Kit kit = AssessmentKitMapper.mapToAssessmentListItemKit(kitEntity, kitLevelEntities.size(), language);
                 AssessmentListItem.Space space = null;
                 AssessmentListItem.MaturityLevel maturityLevel = null;
                 if (Boolean.TRUE.equals(e.getAssessmentResult().getIsCalculateValid())) {
