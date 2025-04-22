bdd9cbdb040c045bd1925b4416753ada14209192
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-04-22 13:00:15 +0330

    FAC-1053: Refactor new use case param test

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/port/in/assessment/CreateAssessmentUseCaseParamTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/port/in/assessment/CreateAssessmentUseCaseParamTest.java
@@ -2,14 +2,9 @@ package org.flickit.assessment.core.application.port.in.assessment;
 
 import jakarta.validation.ConstraintViolationException;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.flickit.assessment.common.config.AppSpecProperties;
-import org.flickit.assessment.common.util.SpringUtil;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.context.ApplicationContext;
 
 import java.util.UUID;
 import java.util.function.Consumer;
@@ -17,21 +12,11 @@ import java.util.function.Consumer;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
 import static org.flickit.assessment.core.common.ErrorMessageKey.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.lenient;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 class CreateAssessmentUseCaseParamTest {
-
-    @Mock
-    ApplicationContext applicationContext;
-
-    @BeforeEach
-    void prepare() {
-        var props = new AppSpecProperties();
-        lenient().doReturn(props).when(applicationContext).getBean(AppSpecProperties.class);
-        new SpringUtil(applicationContext);
-    }
 
     @Test
     void testCreateAssessmentUseCaseParam_titleParamViolatesConstraints_ErrorMessage() {
@@ -77,16 +62,15 @@ class CreateAssessmentUseCaseParamTest {
     }
 
     @Test
-    void testCreateAssessmentUseCaseParam_whenLangParamIsInvalid_thenSetLangToDefault() {
+    void testCreateAssessmentUseCaseParam_langParamIsNotValid_ErrorMessage() {
         var throwable = assertThrows(ConstraintViolationException.class,
             () -> createParam(b -> b.lang("FR")));
         assertThat(throwable).hasMessage("lang: " + CREATE_ASSESSMENT_LANGUAGE_INVALID);
     }
 
     @Test
-    void testCreateAssessmentUseCaseParam_whenLangParamIsNull_thenSetLangToNull() {
-        var param = createParam(b -> b.lang(null));
-        assertNull(param.getLang());
+    void testCreateAssessmentUseCaseParam_langParamIsNull_Success() {
+        assertDoesNotThrow(() -> createParam(b -> b.lang(null)));
     }
 
     @Test
