14511ce53b3082880da9563d5b3ac31e213f0d80
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-04-25 22:23:39 +0330

    FAC-1070: Use the `getTranslations` method of MaturityLevelMapper and SubjectMapper

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/kit/attribute/AttributeMapper.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/kit/attribute/AttributeMapper.java
@@ -5,16 +5,14 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.flickit.assessment.common.application.domain.kit.KitLanguage;
 import org.flickit.assessment.common.application.domain.kit.translation.AttributeTranslation;
-import org.flickit.assessment.common.application.domain.kit.translation.MaturityLevelTranslation;
-import org.flickit.assessment.common.application.domain.kit.translation.SubjectTranslation;
 import org.flickit.assessment.common.util.JsonUtils;
+import org.flickit.assessment.core.adapter.out.persistence.kit.maturitylevel.MaturityLevelMapper;
+import org.flickit.assessment.core.adapter.out.persistence.kit.subject.SubjectMapper;
 import org.flickit.assessment.core.application.domain.Attribute;
 import org.flickit.assessment.core.application.domain.Question;
 import org.flickit.assessment.core.application.port.out.attribute.LoadAttributesPort;
 import org.flickit.assessment.data.jpa.core.attribute.AttributeMaturityLevelSubjectView;
 import org.flickit.assessment.data.jpa.kit.attribute.AttributeJpaEntity;
-import org.flickit.assessment.data.jpa.kit.maturitylevel.MaturityLevelJpaEntity;
-import org.flickit.assessment.data.jpa.kit.subject.SubjectJpaEntity;
 
 import java.util.List;
 
@@ -69,8 +67,8 @@ public class AttributeMapper {
 
     public static LoadAttributesPort.Result mapToResult(AttributeMaturityLevelSubjectView view, Integer weight, @Nullable KitLanguage language) {
         var attributeTranslation = getTranslation(view.getAttribute(), language);
-        var maturityLevelTranslation = getTranslation(view.getMaturityLevel(), language);
-        var subjectTranslation = getTranslation(view.getSubject(), language);
+        var maturityLevelTranslation = MaturityLevelMapper.getTranslation(view.getMaturityLevel(), language);
+        var subjectTranslation = SubjectMapper.getTranslation(view.getSubject(), language);
 
         return new LoadAttributesPort.Result(
             view.getAttribute().getId(),
@@ -97,24 +95,6 @@ public class AttributeMapper {
         var translation = new AttributeTranslation(null, null);
         if (language != null) {
             var translations = JsonUtils.fromJsonToMap(entity.getTranslations(), KitLanguage.class, AttributeTranslation.class);
-            translation = translations.getOrDefault(language, translation);
-        }
-        return translation;
-    }
-
-    private static MaturityLevelTranslation getTranslation(MaturityLevelJpaEntity entity, @Nullable KitLanguage language) {
-        var translation = new MaturityLevelTranslation(null, null);
-        if (language != null) {
-            var translations = JsonUtils.fromJsonToMap(entity.getTranslations(), KitLanguage.class, MaturityLevelTranslation.class);
-            translation = translations.getOrDefault(language, translation);
-        }
-        return translation;
-    }
-
-    private static SubjectTranslation getTranslation(SubjectJpaEntity entity, @Nullable KitLanguage language) {
-        var translation = new SubjectTranslation(null, null);
-        if (language != null) {
-            var translations = JsonUtils.fromJsonToMap(entity.getTranslations(), KitLanguage.class, SubjectTranslation.class);
             translation = translations.getOrDefault(language, translation);
         }
         return translation;
