dcbd43be022917d5f0da3ddae361cc473a9739f3
mahdirezazadeh <riza.rs2000@gmail.com>
2025-04-28 11:49:29 +0330

    FAC-1087:impl:Return all fields translated in LoadCreatedAdviceDetailsPort

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/attribute/AttributeMapper.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/attribute/AttributeMapper.java
@@ -3,7 +3,12 @@ package org.flickit.assessment.advice.adapter.out.persistence.attribute;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.flickit.assessment.advice.application.domain.Attribute;
+import org.flickit.assessment.advice.application.domain.advice.AdviceAttribute;
+import org.flickit.assessment.common.application.domain.kit.KitLanguage;
+import org.flickit.assessment.common.application.domain.kit.translation.AttributeTranslation;
+import org.flickit.assessment.common.util.JsonUtils;
 import org.flickit.assessment.data.jpa.kit.attribute.AttributeJpaEntity;
+import org.jetbrains.annotations.Nullable;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AttributeMapper {
@@ -12,5 +17,19 @@ public class AttributeMapper {
         return new Attribute(
             entity.getId(),
             entity.getTitle());
+    }
+
+    public static AdviceAttribute toAdviceItem(AttributeJpaEntity entity, @Nullable KitLanguage language) {
+        var translation = getTranslation(entity, language);
+        return new AdviceAttribute(entity.getId(), translation.titleOrDefault(entity.getTitle()));
+    }
+
+    public static AttributeTranslation getTranslation(AttributeJpaEntity entity, @Nullable KitLanguage language) {
+        var translation = new AttributeTranslation(null, null);
+        if (language != null) {
+            var translations = JsonUtils.fromJsonToMap(entity.getTranslations(), KitLanguage.class, AttributeTranslation.class);
+            translation = translations.getOrDefault(language, translation);
+        }
+        return translation;
     }
 }
