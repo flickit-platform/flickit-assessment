3678fb749a316118d996f664d76fcd7417ff4d5a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-04-28 20:53:24 +0330

    FAC-1091: Add ImageSize enum test

---
+++ flickit-assessment-common/src/test/java/org/flickit/assessment/common/application/domain/kit/ImageSizeTest.java
@@ -0,0 +1,20 @@
+package org.flickit.assessment.common.application.domain.kit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ImageSizeTest {
+
+    @Test
+    void testImageSize_OrderOfItemsShouldNotBeChanged() {
+        assertEquals(0, ImageSize.SMALL.ordinal());
+        assertEquals(1, ImageSize.LARGE.ordinal());
+    }
+
+    @Test
+    void testImageSize_NameOfItemsShouldBeChanged() {
+        assertEquals("SMALL", ImageSize.SMALL.name());
+        assertEquals("LARGE", ImageSize.LARGE.name());
+    }
+}
