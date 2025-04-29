8711b3e066768995d63416adca85f958f10608ae
mahdirezazadeh <riza.rs2000@gmail.com>
2025-04-29 18:16:23 +0330

    FAC-1092:impl:Impl JpaSpecificationExecutor with AssessmentKitJpaRepository

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/assessmentkit/AssessmentKitJpaRepository.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/assessmentkit/AssessmentKitJpaRepository.java
@@ -5,6 +5,7 @@ import org.flickit.assessment.data.jpa.users.user.UserJpaEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +13,9 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.*;
 
-public interface AssessmentKitJpaRepository extends JpaRepository<AssessmentKitJpaEntity, Long> {
+public interface AssessmentKitJpaRepository extends
+    JpaRepository<AssessmentKitJpaEntity, Long>,
+    JpaSpecificationExecutor<AssessmentKitJpaEntity> {
 
     Optional<AssessmentKitJpaEntity> findByKitVersionId(long kitVersionId);
 
