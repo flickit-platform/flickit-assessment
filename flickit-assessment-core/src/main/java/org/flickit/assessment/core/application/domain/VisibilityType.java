be37847f6e6d06f6a8e80e1ddf654f218030cbeb
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-04 16:15:01 +0330

    FAC-1099: Remove extra method from VisibilityType enum

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/VisibilityType.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/VisibilityType.java
@@ -1,17 +1,7 @@
 package org.flickit.assessment.core.application.domain;
 
-import java.util.Objects;
-import java.util.stream.Stream;
-
 public enum VisibilityType {
     RESTRICTED, PUBLIC;
-
-    public static VisibilityType valueOfById(Integer id) {
-        return Stream.of(VisibilityType.values())
-            .filter(x -> Objects.equals(x.getId(), id))
-            .findAny()
-            .orElse(getDefault());
-    }
 
     public static VisibilityType getDefault() {
         return RESTRICTED;
