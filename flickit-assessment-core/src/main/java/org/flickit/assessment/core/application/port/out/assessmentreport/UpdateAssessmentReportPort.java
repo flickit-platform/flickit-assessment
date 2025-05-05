bef3a3d67ce2948362e282193046c2e37a8880e0
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-05 07:27:11 +0330

    FAC-1100: Add `updateVisibility` to UpdateAssessmentReportPort

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/assessmentreport/UpdateAssessmentReportPort.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/assessmentreport/UpdateAssessmentReportPort.java
@@ -1,6 +1,7 @@
 package org.flickit.assessment.core.application.port.out.assessmentreport;
 
 import org.flickit.assessment.core.application.domain.AssessmentReportMetadata;
+import org.flickit.assessment.core.application.domain.VisibilityType;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -21,5 +22,13 @@ public interface UpdateAssessmentReportPort {
                               boolean published,
                               LocalDateTime lastModificationTime,
                               UUID lastModifiedBy) {
+    }
+
+    void updateVisibility(UpdateVisibilityParam param);
+
+    record UpdateVisibilityParam(UUID assessmentResultId,
+             VisibilityType visibility,
+             LocalDateTime lastModificationTime,
+             UUID lastModifiedBy) {
     }
 }
