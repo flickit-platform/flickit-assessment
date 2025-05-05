aa7b4048bce82a03b2f50eff51efa61d66e2a1df
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-05 07:25:49 +0330

    FAC-1100: Add RestController and RequestDto

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessmentreport/UpdateAssessmentReportRestController.java
@@ -0,0 +1,35 @@
+package org.flickit.assessment.core.adapter.in.rest.assessmentreport;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.flickit.assessment.core.application.port.in.assessmentreport.UpdateAssessmentReportVisibilityUseCase;
+import org.flickit.assessment.core.application.port.in.assessmentreport.UpdateAssessmentReportVisibilityUseCase.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class UpdateAssessmentReportRestController {
+
+    private final UpdateAssessmentReportVisibilityUseCase useCase;
+    private final UserContext userContext;
+
+    @PutMapping("/assessments/{assessmentId}/report-visibility-status")
+    public ResponseEntity<Void> updateAssessmentReportVisibility(@PathVariable UUID assessmentId,
+                                                                 @RequestBody UpdateAssessmentReportVisibilityRequestDto request){
+        UUID currentUserId = userContext.getUser().id();
+        useCase.updateReportVisibility(toParam(assessmentId, request.visibility(), currentUserId));
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Param toParam(UUID assessmentId, Integer visibility, UUID currentUserId) {
+        return new Param(assessmentId, visibility, currentUserId);
+    }
+}
