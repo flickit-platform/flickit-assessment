0d0c63fd63bf891419ce478eeacfd401b3ac3a0d
mahdirezazadeh <riza.rs2000@gmail.com>
2025-05-06 12:30:39 +0330

    FAC-1093:impl:Import ErrorMessageKeys static

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/attribute/AttributePersistenceJpaAdapter.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/attribute/AttributePersistenceJpaAdapter.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.advice.application.domain.Attribute;
 import org.flickit.assessment.advice.application.port.out.atribute.LoadAttributesPort;
 import org.flickit.assessment.common.application.domain.kit.KitLanguage;
-import org.flickit.assessment.common.error.ErrorMessageKey;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.data.jpa.core.assessmentresult.AssessmentResultJpaEntity;
 import org.flickit.assessment.data.jpa.core.assessmentresult.AssessmentResultJpaRepository;
@@ -15,6 +14,9 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_ASSESSMENT_KIT_NOT_FOUND;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_ASSESSMENT_RESULT_NOT_FOUND;
 
 @Component("adviceAttributePersistenceJpaAdapter")
 @RequiredArgsConstructor
@@ -27,7 +29,7 @@ public class AttributePersistenceJpaAdapter implements LoadAttributesPort {
     @Override
     public List<Attribute> loadByIdsAndAssessmentId(List<Long> attributeIds, UUID assessmentId) {
         var assessmentResult = assessmentResultRepository.findFirstByAssessment_IdOrderByLastModificationTimeDesc(assessmentId)
-            .orElseThrow(() -> new ResourceNotFoundException(ErrorMessageKey.COMMON_ASSESSMENT_RESULT_NOT_FOUND));
+            .orElseThrow(() -> new ResourceNotFoundException(COMMON_ASSESSMENT_RESULT_NOT_FOUND));
 
         var translationLanguage = resolveLanguage(assessmentResult);
         return repository.findAllByIdInAndKitVersionId(attributeIds, assessmentResult.getKitVersionId()).stream()
@@ -37,7 +39,7 @@ public class AttributePersistenceJpaAdapter implements LoadAttributesPort {
 
     private KitLanguage resolveLanguage(AssessmentResultJpaEntity assessmentResult) {
         var assessmentKit = assessmentKitRepository.findByKitVersionId(assessmentResult.getKitVersionId())
-            .orElseThrow(() -> new ResourceNotFoundException(ErrorMessageKey.COMMON_ASSESSMENT_KIT_NOT_FOUND));
+            .orElseThrow(() -> new ResourceNotFoundException(COMMON_ASSESSMENT_KIT_NOT_FOUND));
         return Objects.equals(assessmentResult.getLangId(), assessmentKit.getLanguageId())
             ? null
             : KitLanguage.valueOfById(assessmentResult.getLangId());
