4f4f2e3d65d8f7160fa27bdb14b8928154f9846b
mahdirezazadeh <riza.rs2000@gmail.com>
2025-05-06 12:23:59 +0330

    FAC-1093:impl:Merge main into FAC-1093

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/assessment/AssessmentPersistenceJpaAdapter.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/out/persistence/assessment/AssessmentPersistenceJpaAdapter.java
@@ -2,12 +2,9 @@ package org.flickit.assessment.advice.adapter.out.persistence.assessment;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.advice.application.domain.Assessment;
-import org.flickit.assessment.advice.application.port.out.assessment.LoadAssessmentKitLanguagePort;
-import org.flickit.assessment.advice.application.port.out.assessment.LoadAssessmentKitVersionIdPort;
 import org.flickit.assessment.advice.application.port.out.assessment.LoadAssessmentPort;
 import org.flickit.assessment.advice.application.port.out.assessment.LoadSelectedAttributeIdsRelatedToAssessmentPort;
 import org.flickit.assessment.advice.application.port.out.assessment.LoadSelectedLevelIdsRelatedToAssessmentPort;
-import org.flickit.assessment.common.application.domain.kit.KitLanguage;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.data.jpa.core.assessment.AssessmentJpaRepository;
 import org.springframework.stereotype.Component;
@@ -16,16 +13,13 @@ import java.util.Set;
 import java.util.UUID;
 
 import static org.flickit.assessment.advice.common.ErrorMessageKey.ASSESSMENT_ID_NOT_FOUND;
-import static org.flickit.assessment.advice.common.ErrorMessageKey.CREATE_ADVICE_ASSESSMENT_RESULT_NOT_FOUND;
 
 @Component("adviceAssessmentPersistenceJpaAdapter")
 @RequiredArgsConstructor
 public class AssessmentPersistenceJpaAdapter implements
     LoadSelectedAttributeIdsRelatedToAssessmentPort,
     LoadSelectedLevelIdsRelatedToAssessmentPort,
-    LoadAssessmentPort,
-    LoadAssessmentKitLanguagePort,
-    LoadAssessmentKitVersionIdPort {
+    LoadAssessmentPort {
 
     private final AssessmentJpaRepository repository;
 
@@ -43,12 +37,5 @@ public class AssessmentPersistenceJpaAdapter implements
     public Assessment loadById(UUID assessmentId) {
         return repository.findById(assessmentId).map(AssessmentMapper::mapToDomain)
             .orElseThrow(() -> new ResourceNotFoundException(ASSESSMENT_ID_NOT_FOUND));
-    }
-
-    @Override
-    public KitLanguage loadKitLanguage(UUID assessmentId) {
-        int languageId = repository.loadKitLanguageByAssessmentId(assessmentId)
-            .orElseThrow(() -> new ResourceNotFoundException(ASSESSMENT_ID_NOT_FOUND));
-        return KitLanguage.valueOfById(languageId);
     }
 }
