9913f234fa2fba1991d5b38d78d8d483c5df1135
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-05-06 13:59:08 +0330

    FAC-1100: Rename rest controller class and method names

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessmentreport/UpdateAssessmentReportVisibilityRestController.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessmentreport/UpdateAssessmentReportVisibilityRestController.java
@@ -3,7 +3,8 @@ package org.flickit.assessment.core.adapter.in.rest.assessmentreport;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.config.jwt.UserContext;
 import org.flickit.assessment.core.application.port.in.assessmentreport.UpdateAssessmentReportVisibilityUseCase;
-import org.flickit.assessment.core.application.port.in.assessmentreport.UpdateAssessmentReportVisibilityUseCase.*;
+import org.flickit.assessment.core.application.port.in.assessmentreport.UpdateAssessmentReportVisibilityUseCase.Param;
+import org.flickit.assessment.core.application.port.in.assessmentreport.UpdateAssessmentReportVisibilityUseCase.Result;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,14 +16,14 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-public class UpdateAssessmentReportRestController {
+public class UpdateAssessmentReportVisibilityRestController {
 
     private final UpdateAssessmentReportVisibilityUseCase useCase;
     private final UserContext userContext;
 
     @PutMapping("/assessments/{assessmentId}/report-visibility-status")
-    public ResponseEntity<Result> updateAssessmentReportVisibility(@PathVariable UUID assessmentId,
-                                                                   @RequestBody UpdateAssessmentReportVisibilityRequestDto request) {
+    public ResponseEntity<Result> updateReportVisibility(@PathVariable UUID assessmentId,
+                                                         @RequestBody UpdateAssessmentReportVisibilityRequestDto request) {
         UUID currentUserId = userContext.getUser().id();
         var result = useCase.updateReportVisibility(toParam(assessmentId, request.visibility(), currentUserId));
 
