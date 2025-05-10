270052bd740f2de9dace714db9b5406412899372
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-10 14:34:02 +0330

    FAC-1111: Add Builder annotation to GetPublishedKitUseCase.java

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/port/in/assessmentkit/GetPublishedKitUseCaseParamTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/port/in/assessmentkit/GetPublishedKitUseCaseParamTest.java
@@ -4,26 +4,36 @@ import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
+import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
 import static org.flickit.assessment.kit.common.ErrorMessageKey.GET_PUBLISHED_KIT_KIT_ID_NOT_NULL;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GetPublishedKitUseCaseParamTest {
 
     @Test
-    void testGetPublishedKit_kitIdIsNull_ErrorMessage() {
-        var currentUserId = UUID.randomUUID();
+    void testGetPublishedKitUseCaseParam_kitIdIsNull_ErrorMessage() {
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new GetPublishedKitUseCase.Param(null, currentUserId));
+            () -> createParam(b -> b.kitId(null)));
         assertThat(throwable).hasMessage("kitId: " + GET_PUBLISHED_KIT_KIT_ID_NOT_NULL);
     }
 
     @Test
-    void testGetPublishedKit_currentUserIdIsNull_ErrorMessage() {
-        var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new GetPublishedKitUseCase.Param(25L, null));
-        assertThat(throwable).hasMessage("currentUserId: " + COMMON_CURRENT_USER_ID_NOT_NULL);
+    void testGetPublishedKitUseCaseParam_currentUserIdParamIsNull_ErrorMessage() {
+        assertDoesNotThrow(() -> createParam(b -> b.currentUserId(null)));
+    }
+
+    private void createParam(Consumer<GetPublishedKitUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        paramBuilder.build();
+    }
+
+    private GetPublishedKitUseCase.Param.ParamBuilder paramBuilder() {
+        return GetPublishedKitUseCase.Param.builder()
+            .kitId(0L)
+            .currentUserId(UUID.randomUUID());
     }
 }
