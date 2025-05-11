a5d6883bf9ad16c7f686194057f6ff1d10c5f151
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-11 18:31:41 +0330

    FAC-1113: Add CalculateMaturityLevelHelper

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentreport/CalculateMaturityLevelHelper.java
@@ -0,0 +1,38 @@
+package org.flickit.assessment.core.application.service.assessmentreport;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.core.application.domain.*;
+import org.flickit.assessment.core.application.port.out.assessment.UpdateAssessmentPort;
+import org.flickit.assessment.core.application.port.out.assessmentresult.LoadCalculateInfoPort;
+import org.flickit.assessment.core.application.port.out.assessmentresult.UpdateCalculatedResultPort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CalculateMaturityLevelHelper {
+
+    private final LoadCalculateInfoPort loadCalculateInfoPort;
+    private final UpdateCalculatedResultPort updateCalculatedResultPort;
+    private final UpdateAssessmentPort updateAssessmentPort;
+    private final InitializeAssessmentResultHelper initializeAssessmentResultHelper;
+
+    public void calculateMaturityLevel(UUID assessmentId) {
+        initializeAssessmentResultHelper.reinitializeAssessmentResultIfRequired(assessmentId);
+
+        AssessmentResult assessmentResult = loadCalculateInfoPort.load(assessmentId);
+
+        MaturityLevel calcResult = assessmentResult.calculate();
+        assessmentResult.setMaturityLevel(calcResult);
+        assessmentResult.setIsCalculateValid(Boolean.TRUE);
+        assessmentResult.setLastModificationTime(LocalDateTime.now());
+        assessmentResult.setLastCalculationTime(LocalDateTime.now());
+
+        updateCalculatedResultPort.updateCalculatedResult(assessmentResult);
+        updateAssessmentPort.updateLastModificationTime(assessmentId, assessmentResult.getLastModificationTime());
+    }
+}
