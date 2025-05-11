d546ad85297515660135593395124748e8f6bede
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-11 18:32:02 +0330

    FAC-1113: Add CalculateConfidenceHelper

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentreport/CalculateConfidenceHelper.java
@@ -0,0 +1,40 @@
+package org.flickit.assessment.core.application.service.assessmentreport;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.core.application.domain.*;
+import org.flickit.assessment.core.application.port.out.assessment.UpdateAssessmentPort;
+import org.flickit.assessment.core.application.port.out.assessmentresult.LoadConfidenceLevelCalculateInfoPort;
+import org.flickit.assessment.core.application.port.out.assessmentresult.UpdateCalculatedConfidencePort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CalculateConfidenceHelper {
+
+    private final InitializeAssessmentResultHelper initializeAssessmentResultHelper;
+    private final LoadConfidenceLevelCalculateInfoPort loadConfidenceLevelCalculateInfoPort;
+    private final UpdateCalculatedConfidencePort updateCalculatedConfidenceLevelResultPort;
+    private final UpdateAssessmentPort updateAssessmentPort;
+
+    public void calculate(UUID assessmentId) {
+        initializeAssessmentResultHelper.reinitializeAssessmentResultIfRequired(assessmentId);
+
+        AssessmentResult assessmentResult = loadConfidenceLevelCalculateInfoPort.load(assessmentId);
+
+        Double confidenceValue = assessmentResult.calculateConfidenceValue();
+
+        assessmentResult.setConfidenceValue(confidenceValue);
+        assessmentResult.setIsConfidenceValid(Boolean.TRUE);
+        assessmentResult.setLastModificationTime(LocalDateTime.now());
+        assessmentResult.setLastConfidenceCalculationTime(LocalDateTime.now());
+
+        updateCalculatedConfidenceLevelResultPort.updateCalculatedConfidence(assessmentResult);
+
+        updateAssessmentPort.updateLastModificationTime(assessmentId, assessmentResult.getLastModificationTime());
+    }
+}
