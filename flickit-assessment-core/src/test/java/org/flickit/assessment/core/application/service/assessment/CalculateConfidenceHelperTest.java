46942aac0ae0cd6a414209524264b67f2c412cd3
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-14 18:30:37 +0330

    FAC-1113: Refactor CalculateConfidenceHelperTest

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/CalculateConfidenceHelperTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/CalculateConfidenceHelperTest.java
@@ -1,21 +1,29 @@
 package org.flickit.assessment.core.application.service.assessment;
 
-import org.flickit.assessment.core.application.domain.AssessmentResult;
+import org.flickit.assessment.core.application.domain.*;
 import org.flickit.assessment.core.application.port.out.assessment.UpdateAssessmentPort;
+import org.flickit.assessment.core.application.port.out.assessmentkit.LoadKitLastMajorModificationTimePort;
+import org.flickit.assessment.core.application.port.out.assessmentresult.LoadAssessmentResultPort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.LoadConfidenceLevelCalculateInfoPort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.UpdateCalculatedConfidencePort;
-import org.flickit.assessment.core.test.fixture.application.AssessmentResultMother;
+import org.flickit.assessment.core.application.port.out.attributevalue.CreateAttributeValuePort;
+import org.flickit.assessment.core.application.port.out.subject.LoadSubjectsPort;
+import org.flickit.assessment.core.application.port.out.subjectvalue.CreateSubjectValuePort;
+import org.flickit.assessment.core.test.fixture.application.SubjectValueMother;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import static org.flickit.assessment.core.test.fixture.application.AssessmentResultMother.invalidResultWithSubjectValues;
+import static org.flickit.assessment.core.test.fixture.application.AttributeValueMother.toBeCalcAsConfidenceLevelWithWeight;
+import static org.flickit.assessment.core.test.fixture.application.SubjectValueMother.withAttributeValues;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -26,6 +34,9 @@ class CalculateConfidenceHelperTest {
     private CalculateConfidenceHelper helper;
 
     @Mock
+    private LoadAssessmentResultPort loadAssessmentResultPort;
+
+    @Mock
     private LoadConfidenceLevelCalculateInfoPort loadConfidenceLevelCalculateInfoPort;
 
     @Mock
@@ -34,30 +45,103 @@ class CalculateConfidenceHelperTest {
     @Mock
     private UpdateAssessmentPort updateAssessmentPort;
 
-    @Captor
-    private ArgumentCaptor<AssessmentResult> assessmentResultCaptor;
+    @Mock
+    private LoadKitLastMajorModificationTimePort loadKitLastMajorModificationTimePort;
+
+    @Mock
+    private LoadSubjectsPort loadSubjectsPort;
+
+    @Mock
+    private CreateSubjectValuePort createSubjectValuePort;
+
+    @Mock
+    private CreateAttributeValuePort createAttributeValuePort;
 
     @Test
-    void calculateConfidence_whenValidInput_thenUpdatesStateAndReturnsConfidenceValue() {
-        UUID assessmentId = UUID.randomUUID();
-        double expectedConfidence = 0.85;
-        var assessmentResult = spy(AssessmentResultMother.resultWithValidations(true, false, LocalDateTime.now(), LocalDateTime.MIN));
+    void testCalculateConfidenceLevel_ValidInput_ValidResults() {
+        List<AttributeValue> s1AttributeValues = List.of(
+            toBeCalcAsConfidenceLevelWithWeight(2, ConfidenceLevel.COMPLETELY_UNSURE.getId()), //6 questions with 5 answers with cl=1, attrCl=5/30
+            toBeCalcAsConfidenceLevelWithWeight(2, ConfidenceLevel.COMPLETELY_SURE.getId()), //6 questions with 5 answers with cl=5, attrCl = 25/30
+            toBeCalcAsConfidenceLevelWithWeight(3, ConfidenceLevel.SOMEWHAT_UNSURE.getId()), //6 questions with 5 answers with cl=3, attrCl = 15/30
+            toBeCalcAsConfidenceLevelWithWeight(3, ConfidenceLevel.COMPLETELY_SURE.getId()) //6 questions with 5 answers with cl=5, attrCl = 25/30
+        );
 
-        when(loadConfidenceLevelCalculateInfoPort.load(assessmentId)).thenReturn(assessmentResult);
-        doReturn(expectedConfidence).when(assessmentResult).calculateConfidenceValue();
+        List<AttributeValue> s2AttributeValues = List.of(
+            toBeCalcAsConfidenceLevelWithWeight(4, ConfidenceLevel.SOMEWHAT_UNSURE.getId()), //6 questions with 5 answers with cl=3, attrCl = 15/30
+            toBeCalcAsConfidenceLevelWithWeight(1, ConfidenceLevel.COMPLETELY_SURE.getId()) //6 questions with 5 answers with cl=5, attrCl = 25/30
+        );
 
-        double result = helper.calculate(assessmentId);
+        List<SubjectValue> subjectValues = List.of(
+            SubjectValueMother.withAttributeValues(s1AttributeValues, 1),
+            SubjectValueMother.withAttributeValues(s2AttributeValues, 1)
+        );
 
-        verify(updateCalculatedConfidenceLevelResultPort).updateCalculatedConfidence(assessmentResultCaptor.capture());
-        verify(updateAssessmentPort).updateLastModificationTime(eq(assessmentId), any(LocalDateTime.class));
+        List<Subject> subjects = new ArrayList<>(subjectValues.stream().map(SubjectValue::getSubject).toList());
 
-        AssessmentResult capturedResult = assessmentResultCaptor.getValue();
+        AssessmentResult assessmentResult = invalidResultWithSubjectValues(subjectValues);
 
-        assertEquals(expectedConfidence, result);
-        assertEquals(expectedConfidence, capturedResult.getConfidenceValue());
-        assertTrue(capturedResult.getIsConfidenceValid());
-        assertNotNull(capturedResult.getLastModificationTime());
-        assertNotNull(capturedResult.getLastConfidenceCalculationTime());
-        assertTrue(capturedResult.getLastConfidenceCalculationTime().isAfter(LocalDateTime.MIN));
+        when(loadAssessmentResultPort.loadByAssessmentId(assessmentResult.getAssessment().getId())).thenReturn(Optional.of(assessmentResult));
+        when(loadConfidenceLevelCalculateInfoPort.load(assessmentResult.getAssessment().getId())).thenReturn(assessmentResult);
+        when(loadSubjectsPort.loadByKitVersionIdWithAttributes(any())).thenReturn(subjects);
+
+        LocalDateTime kitLastMajorModificationTime = LocalDateTime.now();
+        when(loadKitLastMajorModificationTimePort.loadLastMajorModificationTime(any())).thenReturn(kitLastMajorModificationTime);
+
+        var confidenceValue = helper.calculate(assessmentResult.getAssessment().getId());
+        verify(updateCalculatedConfidenceLevelResultPort, times(1)).updateCalculatedConfidence(any(AssessmentResult.class));
+        verify(updateAssessmentPort, times(1)).updateLastModificationTime(any(), any());
+
+        assertNotNull(confidenceValue);
+        var sValue1ConfidenceLevel = (((5.0 / 30.0) * 2) + ((25.0 / 30.0) * 2) + ((15.0 / 30.0) * 3) + ((25.0 / 30.0) * 3)) / 10;
+        var sValue2ConfidenceLevel = (((15.0 / 30.0) * 4) + ((25.0 / 30.0) * 1)) / 5;
+        var resultCl = ((sValue1ConfidenceLevel * 1 + sValue2ConfidenceLevel * 1) / 2) * 100;
+        assertEquals(resultCl, confidenceValue, 0.01);
+    }
+
+    @Test
+    void testCalculateMaturityLevel_KitChanged_CreatesNewAttributeAnSubjectValuesAndCalculates() {
+        List<AttributeValue> s1AttributeValues = List.of(
+            toBeCalcAsConfidenceLevelWithWeight(2, ConfidenceLevel.COMPLETELY_UNSURE.getId()), //6 questions with 5 answers with cl=1, attrCl=5/30,
+            toBeCalcAsConfidenceLevelWithWeight(2, ConfidenceLevel.COMPLETELY_SURE.getId()), //6 questions with 5 answers with cl=5, attrCl = 25/30
+            toBeCalcAsConfidenceLevelWithWeight(3, ConfidenceLevel.SOMEWHAT_UNSURE.getId()), //6 questions with 5 answers with cl=3, attrCl = 15/30
+            toBeCalcAsConfidenceLevelWithWeight(3, ConfidenceLevel.COMPLETELY_SURE.getId()) //6 questions with 5 answers with cl=5, attrCl = 25/30
+        );
+
+        List<AttributeValue> s2AttributeValues = List.of(
+            toBeCalcAsConfidenceLevelWithWeight(4, ConfidenceLevel.SOMEWHAT_UNSURE.getId()),
+            toBeCalcAsConfidenceLevelWithWeight(1, ConfidenceLevel.COMPLETELY_SURE.getId())
+        );
+
+        List<SubjectValue> subjectValues = List.of(
+            withAttributeValues(s1AttributeValues, 1),
+            withAttributeValues(s2AttributeValues, 1)
+        );
+
+        var newAttributeValue = toBeCalcAsConfidenceLevelWithWeight(4, ConfidenceLevel.SOMEWHAT_UNSURE.getId()); //6 questions with 5 answers with cl=3, attrCl = 15/30
+        var newSubjectValue = withAttributeValues(List.of(), 1);
+
+        List<Subject> subjects = new ArrayList<>(subjectValues.stream().map(SubjectValue::getSubject).toList());
+        subjects.add(newSubjectValue.getSubject());
+
+        AssessmentResult assessmentResult = invalidResultWithSubjectValues(subjectValues);
+        assessmentResult.setLastCalculationTime(LocalDateTime.now());
+
+        when(loadAssessmentResultPort.loadByAssessmentId(assessmentResult.getAssessment().getId())).thenReturn(Optional.of(assessmentResult));
+        when(loadConfidenceLevelCalculateInfoPort.load(assessmentResult.getAssessment().getId())).thenReturn(assessmentResult);
+        when(loadKitLastMajorModificationTimePort.loadLastMajorModificationTime(any())).thenReturn(LocalDateTime.now());
+        when(loadSubjectsPort.loadByKitVersionIdWithAttributes(any())).thenReturn(subjects);
+        when(createSubjectValuePort.persistAll(anyList(), any())).thenReturn(List.of(newSubjectValue));
+        when(createAttributeValuePort.persistAll(anySet(), any())).thenReturn(List.of(newAttributeValue));
+
+        var confidenceValue = helper.calculate(assessmentResult.getAssessment().getId());
+        verify(updateCalculatedConfidenceLevelResultPort, times(1)).updateCalculatedConfidence(any(AssessmentResult.class));
+        verify(updateAssessmentPort, times(1)).updateLastModificationTime(any(), any());
+
+        assertNotNull(confidenceValue);
+        var sValue1ConfidenceLevel = (((5.0 / 30.0) * 2) + ((25.0 / 30.0) * 2) + ((15.0 / 30.0) * 3) + ((25.0 / 30.0) * 3)) / 10;
+        var sValue2ConfidenceLevel = (((15.0 / 30.0) * 4) + ((25.0 / 30.0) * 1) + ((15.0 / 30.0) * 4) )/ 9;
+        var sValue3ConfidenceLevel = 0; // Because of sum.getValue() == 0 ? 0 : weightedSum.getValue() / sum.getValue() in attrVal
+        var resultCl = ((sValue1ConfidenceLevel * 1 + sValue2ConfidenceLevel * 1 + sValue3ConfidenceLevel) / 3) * 100;
+        assertEquals(resultCl, confidenceValue, 0.01);
     }
 }
