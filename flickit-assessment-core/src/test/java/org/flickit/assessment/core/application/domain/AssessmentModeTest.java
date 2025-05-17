d2bf83e582a0c9a1e55ae44baca5e00364d2058d
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-17 15:48:57 +0330

    FAC-1121: Add AssessmentMode and AssessmentModeTest

---
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/domain/AssessmentModeTest.java
@@ -0,0 +1,31 @@
+package org.flickit.assessment.core.application.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssessmentModeTest {
+
+    @Test
+    void testAssessmentMode_OrderOfItemsShouldNotBeChanged() {
+        assertEquals(0, AssessmentMode.QUICK.ordinal());
+        assertEquals(1, AssessmentMode.ADVANCED.ordinal());
+    }
+
+    @Test
+    void testAssessmentMode_IdOfItemsShouldNotBeChanged() {
+        assertEquals(0, AssessmentMode.QUICK.getId());
+        assertEquals(1, AssessmentMode.ADVANCED.getId());
+    }
+
+    @Test
+    void testAssessmentMode_NameOfItemsShouldNotBeChanged() {
+        assertEquals("QUICK", AssessmentMode.QUICK.name());
+        assertEquals("ADVANCED", AssessmentMode.ADVANCED.name());
+    }
+
+    @Test
+    void testAssessmentMode_SizeOfItemsShouldNotBeChanged() {
+        assertEquals(2, AssessmentMode.values().length);
+    }
+}
