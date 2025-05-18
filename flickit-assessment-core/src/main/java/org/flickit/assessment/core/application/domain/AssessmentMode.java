220903ba056c5b7bf09c5f3aa0e1208e51a6d724
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-18 07:33:11 +0330

    FAC-1121: Remove extra code from AssessmentMode

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AssessmentMode.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AssessmentMode.java
@@ -2,31 +2,15 @@ package org.flickit.assessment.core.application.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum AssessmentMode {
 
     QUICK,
     ADVANCED;
 
-    public String getCode() {
-        return name();
-    }
-
     public int getId() {
         return this.ordinal();
-    }
-
-    public static AssessmentMode valueOfById(int id) {
-        if (!isValidId(id))
-            return null;
-        return values()[id];
-    }
-
-    public static boolean isValidId(int id) {
-        return id >= 0 && id < AssessmentMode.values().length;
     }
 }
