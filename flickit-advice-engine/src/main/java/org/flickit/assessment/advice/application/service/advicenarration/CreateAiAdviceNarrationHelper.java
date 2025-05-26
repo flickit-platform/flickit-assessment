e6d9cc95e7adfdd720009bf55f064cbc78b8a85a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-26 15:00:05 +0330

    FAC-1132: Add CreateAiAdviceNarrationHelper and its test

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/service/advicenarration/CreateAiAdviceNarrationHelper.java
@@ -0,0 +1,164 @@
+package org.flickit.assessment.advice.application.service.advicenarration;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.advice.application.domain.*;
+import org.flickit.assessment.advice.application.domain.advice.AdviceListItem;
+import org.flickit.assessment.advice.application.port.out.adviceitem.CreateAdviceItemPort;
+import org.flickit.assessment.advice.application.port.out.advicenarration.CreateAdviceNarrationPort;
+import org.flickit.assessment.advice.application.port.out.advicenarration.LoadAdviceNarrationPort;
+import org.flickit.assessment.advice.application.port.out.advicenarration.UpdateAdviceNarrationPort;
+import org.flickit.assessment.advice.application.port.out.assessment.LoadAssessmentPort;
+import org.flickit.assessment.advice.application.port.out.atribute.LoadAttributesPort;
+import org.flickit.assessment.advice.application.port.out.attributevalue.LoadAttributeCurrentAndTargetLevelIndexPort;
+import org.flickit.assessment.advice.application.port.out.maturitylevel.LoadMaturityLevelsPort;
+import org.flickit.assessment.common.application.MessageBundle;
+import org.flickit.assessment.common.application.domain.adviceitem.CostLevel;
+import org.flickit.assessment.common.application.domain.adviceitem.ImpactLevel;
+import org.flickit.assessment.common.application.domain.adviceitem.PriorityLevel;
+import org.flickit.assessment.common.application.port.out.CallAiPromptPort;
+import org.flickit.assessment.common.config.AppAiProperties;
+import org.flickit.assessment.common.exception.ValidationException;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static org.flickit.assessment.advice.common.ErrorMessageKey.CREATE_AI_ADVICE_NARRATION_ATTRIBUTE_LEVEL_TARGETS_SIZE_MIN;
+import static org.flickit.assessment.advice.common.MessageKey.ADVICE_NARRATION_AI_IS_DISABLED;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CreateAiAdviceNarrationHelper {
+
+    private final LoadAttributeCurrentAndTargetLevelIndexPort loadAttributeCurrentAndTargetLevelIndexPort;
+    private final LoadMaturityLevelsPort loadMaturityLevelsPort;
+    private final LoadAttributesPort loadAttributesPort;
+    private final LoadAdviceNarrationPort loadAdviceNarrationPort;
+    private final LoadAssessmentPort loadAssessmentPort;
+    private final CreateAdviceNarrationPort createAdviceNarrationPort;
+    private final CallAiPromptPort callAiPromptPort;
+    private final CreateAdviceItemPort createAdviceItemPort;
+    private final AppAiProperties appAiProperties;
+    private final UpdateAdviceNarrationPort updateAdviceNarrationPort;
+
+    public String createAiAdviceNarration(AssessmentResult assessmentResult,
+                                          List<AdviceListItem> adviceListItems,
+                                          List<AttributeLevelTarget> attributeLevelTargets) {
+        if (!appAiProperties.isEnabled())
+            return MessageBundle.message(ADVICE_NARRATION_AI_IS_DISABLED);
+
+
+        var adviceNarration = loadAdviceNarrationPort.loadByAssessmentResultId(assessmentResult.getId());
+
+        attributeLevelTargets = filterValidAttributeLevelTargets(assessmentResult.getAssessmentId(), attributeLevelTargets);
+
+
+        var prompt = createPrompt(adviceListItems, attributeLevelTargets, assessmentResult);
+        AdviceDto aiAdvice = callAiPromptPort.call(prompt, AdviceDto.class);
+
+        var adviceItems = aiAdvice.adviceItems().stream()
+            .map(i -> i.toDomainModel(assessmentResult.getId()))
+            .toList();
+        createAdviceItemPort.persistAll(adviceItems);
+
+        if (adviceNarration.isPresent()) {
+            UUID narrationId = adviceNarration.get().getId();
+            var updateParam = new UpdateAdviceNarrationPort.AiNarrationParam(narrationId, aiAdvice.narration(), LocalDateTime.now());
+            updateAdviceNarrationPort.updateAiNarration(updateParam);
+        } else {
+            UUID assessmentResultId = assessmentResult.getId();
+            createAdviceNarrationPort.persist(toAdviceNarration(assessmentResultId, aiAdvice.narration()));
+        }
+        return aiAdvice.narration();
+    }
+
+    private List<AttributeLevelTarget> filterValidAttributeLevelTargets(UUID assessmentId, List<AttributeLevelTarget> attributeLevelTargets) {
+        var attributeCurrentAndTargetLevelIndexes = loadAttributeCurrentAndTargetLevelIndexPort.load(assessmentId, attributeLevelTargets);
+        var validAttributeIds = attributeCurrentAndTargetLevelIndexes.stream()
+            .filter(a -> a.targetMaturityLevelIndex() > a.currentMaturityLevelIndex())
+            .map(LoadAttributeCurrentAndTargetLevelIndexPort.Result::attributeId)
+            .collect(Collectors.toSet());
+        if (validAttributeIds.isEmpty())
+            throw new ValidationException(CREATE_AI_ADVICE_NARRATION_ATTRIBUTE_LEVEL_TARGETS_SIZE_MIN);
+
+        return attributeLevelTargets.stream()
+            .filter(a -> validAttributeIds.contains(a.getAttributeId()))
+            .toList();
+    }
+
+    private Prompt createPrompt(List<AdviceListItem> adviceItems, List<AttributeLevelTarget> targets, AssessmentResult assessmentResult) {
+        var assessment = loadAssessmentPort.loadById(assessmentResult.getAssessmentId());
+        var assessmentTitle = assessment.getShortTitle() != null ? assessment.getShortTitle() : assessment.getTitle();
+
+        var maturityLevelsMap = loadMaturityLevelsPort.loadAll(assessmentResult.getAssessmentId()).stream()
+            .collect(Collectors.toMap(MaturityLevel::getId, MaturityLevel::getTitle));
+
+        List<Long> targetAttributeIds = targets.stream()
+            .map(AttributeLevelTarget::getAttributeId)
+            .toList();
+        var attributesMap = loadAttributesPort.loadByIdsAndAssessmentId(targetAttributeIds, assessmentResult.getAssessmentId()).stream()
+            .collect(Collectors.toMap(Attribute::getId, Attribute::getTitle));
+
+        var adviceRecommendations = adviceItems.stream()
+            .map(a -> new AdviceRecommendation(a.question().title(),
+                a.answeredOption() != null ? a.answeredOption().title() : null,
+                a.recommendedOption().title()))
+            .toList();
+
+        List<TargetAttribute> targetAttributes = targets.stream()
+            .map(target -> new TargetAttribute(
+                attributesMap.get(target.getAttributeId()),
+                maturityLevelsMap.getOrDefault(target.getMaturityLevelId(), "Unknown")))
+            .toList();
+
+        return new PromptTemplate(appAiProperties.getPrompt().getAdviceNarrationAndAdviceItems(),
+            Map.of("assessmentTitle", assessmentTitle,
+                "attributeTargets", targetAttributes,
+                "adviceRecommendations", adviceRecommendations,
+                "language", assessmentResult.getLanguage().getTitle()))
+            .create();
+    }
+
+    record AdviceDto(String narration, List<AdviceItemDto> adviceItems) {
+
+        record AdviceItemDto(String title, String description, int cost, int priority, int impact) {
+
+            AdviceItem toDomainModel(UUID assessmentResultId) {
+                return new AdviceItem(null,
+                    title,
+                    assessmentResultId,
+                    description,
+                    CostLevel.valueOfById(cost),
+                    PriorityLevel.valueOfById(priority),
+                    ImpactLevel.valueOfById(impact),
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    null,
+                    null);
+            }
+        }
+    }
+
+    record AdviceRecommendation(String question, String currentOption, String recommendedOption) {
+    }
+
+    record TargetAttribute(String attribute, String targetMaturityLevel) {
+    }
+
+    AdviceNarration toAdviceNarration(UUID assessmentResultId, String aiNarration) {
+        return new AdviceNarration(null,
+            assessmentResultId,
+            aiNarration,
+            null,
+            LocalDateTime.now(),
+            null,
+            null);
+    }
+}
---
+++ flickit-advice-engine/src/test/java/org/flickit/assessment/advice/application/service/advicenarration/CreateAiAdviceNarrationHelperTest.java
@@ -0,0 +1,286 @@
+package org.flickit.assessment.advice.application.service.advicenarration;
+
+import org.flickit.assessment.advice.application.domain.*;
+import org.flickit.assessment.advice.application.domain.advice.AdviceListItem;
+import org.flickit.assessment.advice.application.port.out.adviceitem.CreateAdviceItemPort;
+import org.flickit.assessment.advice.application.port.out.advicenarration.CreateAdviceNarrationPort;
+import org.flickit.assessment.advice.application.port.out.advicenarration.LoadAdviceNarrationPort;
+import org.flickit.assessment.advice.application.port.out.advicenarration.UpdateAdviceNarrationPort;
+import org.flickit.assessment.advice.application.port.out.assessment.LoadAssessmentPort;
+import org.flickit.assessment.advice.application.port.out.atribute.LoadAttributesPort;
+import org.flickit.assessment.advice.application.port.out.attributevalue.LoadAttributeCurrentAndTargetLevelIndexPort;
+import org.flickit.assessment.advice.application.port.out.maturitylevel.LoadMaturityLevelsPort;
+import org.flickit.assessment.advice.application.service.advicenarration.CreateAiAdviceNarrationHelper.AdviceDto;
+import org.flickit.assessment.advice.test.fixture.application.AdviceListItemMother;
+import org.flickit.assessment.advice.test.fixture.application.AttributeLevelTargetMother;
+import org.flickit.assessment.common.application.MessageBundle;
+import org.flickit.assessment.common.application.port.out.CallAiPromptPort;
+import org.flickit.assessment.common.config.AppAiProperties;
+import org.flickit.assessment.common.exception.ValidationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.flickit.assessment.advice.common.ErrorMessageKey.CREATE_AI_ADVICE_NARRATION_ATTRIBUTE_LEVEL_TARGETS_SIZE_MIN;
+import static org.flickit.assessment.advice.common.MessageKey.ADVICE_NARRATION_AI_IS_DISABLED;
+import static org.flickit.assessment.advice.test.fixture.application.AssessmentMother.assessmentWithShortTitle;
+import static org.flickit.assessment.advice.test.fixture.application.AssessmentResultMother.createAssessmentResult;
+import static org.flickit.assessment.advice.test.fixture.application.AttributeLevelTargetMother.createAttributeLevelTarget;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CreateAiAdviceNarrationHelperTest {
+
+    @InjectMocks
+    private CreateAiAdviceNarrationHelper helper;
+
+    @Mock
+    private LoadAssessmentPort loadAssessmentPort;
+
+    @Mock
+    private LoadAttributeCurrentAndTargetLevelIndexPort loadAttributeCurrentAndTargetLevelIndexPort;
+
+    @Mock
+    private LoadMaturityLevelsPort loadMaturityLevelsPort;
+
+    @Mock
+    private LoadAttributesPort loadAttributesPort;
+
+    @Mock
+    private CallAiPromptPort callAiPromptPort;
+
+    @Mock
+    private LoadAdviceNarrationPort loadAdviceNarrationPort;
+
+    @Mock
+    private CreateAdviceNarrationPort createAdviceNarrationPort;
+
+    @Mock
+    private UpdateAdviceNarrationPort updateAdviceNarrationPort;
+
+    @Mock
+    private CreateAdviceItemPort createAdviceItemPort;
+
+    @Captor
+    private ArgumentCaptor<AdviceNarration> adviceNarrationCaptor;
+
+    @Captor
+    private ArgumentCaptor<UpdateAdviceNarrationPort.AiNarrationParam> updateNarrationCaptor;
+
+    @Captor
+    private ArgumentCaptor<List<AdviceItem>> adviceItemsCaptor;
+
+    @Captor
+    private ArgumentCaptor<Prompt> promptArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<Class<AdviceDto>> classCaptor;
+
+    @Spy
+    private AppAiProperties appAiProperties = appAiProperties();
+
+    private final List<AdviceListItem> adviceListItems = List.of(AdviceListItemMother.createSimpleAdviceListItem());
+    private final List<AttributeLevelTarget> attributeLevelTargets = List.of(AttributeLevelTargetMother.createAttributeLevelTarget());
+
+    private final String aiNarration = "aiNarration";
+    private final AssessmentResult assessmentResult = createAssessmentResult();
+    private final List<AdviceDto.AdviceItemDto> adviceItems = List.of(
+        new AdviceDto.AdviceItemDto("title1", "description1", 0, 1, 2),
+        new AdviceDto.AdviceItemDto("title1", "description1", 2, 0, 1)
+    );
+    private final AdviceDto aiAdvice = new AdviceDto(aiNarration, adviceItems);
+    private final List<Attribute> attributes = List.of(new Attribute(attributeLevelTargets.getFirst().getAttributeId(), "Reliability", 1));
+    private final List<MaturityLevel> maturityLevels = List.of(new MaturityLevel(attributeLevelTargets.getFirst().getMaturityLevelId(), "Great", 0));
+
+    @Test
+    void testCreateAiAdviceNarration_whenAiIsDisabled_thenReturnAiIsDisabledMessage() {
+        when(appAiProperties.isEnabled()).thenReturn(false);
+
+        var result = helper.createAiAdviceNarration(assessmentResult, adviceListItems, attributeLevelTargets);
+        assertEquals(MessageBundle.message(ADVICE_NARRATION_AI_IS_DISABLED), result);
+
+        verifyNoInteractions(
+            callAiPromptPort,
+            loadAdviceNarrationPort,
+            createAdviceNarrationPort,
+            loadAttributeCurrentAndTargetLevelIndexPort,
+            loadAttributesPort,
+            loadAssessmentPort,
+            loadMaturityLevelsPort,
+            createAdviceItemPort);
+    }
+
+    @Test
+    void testCreateAiAdviceNarration_whenNoValidTargetExists_thenThrowValidationException() {
+        var attributeLevelTargets = List.of(createAttributeLevelTarget());
+        var adviceNarration = new AdviceNarration(UUID.randomUUID(), assessmentResult.getId(), aiNarration, null, LocalDateTime.now(), null, UUID.randomUUID());
+
+        when(loadAdviceNarrationPort.loadByAssessmentResultId(assessmentResult.getId())).thenReturn(Optional.of(adviceNarration));
+        when(loadAttributeCurrentAndTargetLevelIndexPort.load(assessmentResult.getAssessmentId(), attributeLevelTargets))
+            .thenReturn(List.of(new LoadAttributeCurrentAndTargetLevelIndexPort.Result(attributeLevelTargets.getFirst().getAttributeId(), 1, 1)));
+
+        var throwable = assertThrows(ValidationException.class,
+            () -> helper.createAiAdviceNarration(assessmentResult, adviceListItems, attributeLevelTargets));
+        assertEquals(CREATE_AI_ADVICE_NARRATION_ATTRIBUTE_LEVEL_TARGETS_SIZE_MIN, throwable.getMessageKey());
+
+        verifyNoInteractions(loadAttributesPort,
+            loadMaturityLevelsPort,
+            callAiPromptPort,
+            createAdviceNarrationPort);
+    }
+
+    @Test
+    void testCreateAiAdviceNarration_whenAdviceNarrationDoesNotExist_thenCreateAdviceNarration() {
+        var assessment = assessmentWithShortTitle("ShortTitle");
+
+        var expectedPrompt = new PromptTemplate(appAiProperties.getPrompt().getAdviceNarrationAndAdviceItems(),
+            Map.of("assessmentTitle", assessment.getShortTitle(),
+                "attributeTargets", "TargetAttribute[attribute=Reliability, targetMaturityLevel=Great]",
+                "adviceRecommendations", "AdviceRecommendation[question=title, currentOption=answeredOption, recommendedOption=recommendedOption]",
+                "language", assessmentResult.getLanguage().getTitle())).create();
+
+        when(loadAdviceNarrationPort.loadByAssessmentResultId(assessmentResult.getId())).thenReturn(Optional.empty());
+        when(loadAttributeCurrentAndTargetLevelIndexPort.load(assessmentResult.getAssessmentId(), attributeLevelTargets))
+            .thenReturn(List.of(new LoadAttributeCurrentAndTargetLevelIndexPort.Result(attributeLevelTargets.getFirst().getAttributeId(), 1, 2)));
+        when(loadMaturityLevelsPort.loadAll(assessmentResult.getAssessmentId())).thenReturn(maturityLevels);
+        when(loadAttributesPort.loadByIdsAndAssessmentId(List.of(attributeLevelTargets.getFirst().getAttributeId()), assessmentResult.getAssessmentId())).thenReturn(attributes);
+        when(loadAssessmentPort.loadById(assessmentResult.getAssessmentId())).thenReturn(assessment);
+        when(callAiPromptPort.call(promptArgumentCaptor.capture(), classCaptor.capture())).thenReturn(aiAdvice);
+
+        helper.createAiAdviceNarration(assessmentResult, adviceListItems, attributeLevelTargets);
+
+        verify(createAdviceNarrationPort).persist(adviceNarrationCaptor.capture());
+        verify(createAdviceItemPort).persistAll(adviceItemsCaptor.capture());
+
+        var capturedAdviceNarration = adviceNarrationCaptor.getValue();
+        assertEquals(aiNarration, adviceNarrationCaptor.getValue().getAiNarration());
+        assertEquals(expectedPrompt.getContents(), promptArgumentCaptor.getValue().getContents());
+        assertEquals(AdviceDto.class, classCaptor.getValue());
+        assertNull(capturedAdviceNarration.getCreatedBy());
+        assertNull(capturedAdviceNarration.getAssessorNarration());
+        assertNotNull(capturedAdviceNarration.getAiNarrationTime());
+        assertNull(capturedAdviceNarration.getAssessorNarrationTime());
+        assertEquals(assessmentResult.getId(), capturedAdviceNarration.getAssessmentResultId());
+
+        var capturedAdviceItems = adviceItemsCaptor.getValue();
+        var expectedAdviceItems = aiAdvice.adviceItems();
+        assertAdviceItems(expectedAdviceItems, capturedAdviceItems);
+
+        verifyNoInteractions(updateAdviceNarrationPort);
+    }
+
+    @Test
+    void testCreateAiAdviceNarration_whenAdviceNarrationExistsAndShortTitleNotExists_thenUpdateAdviceNarration() {
+        var assessment = assessmentWithShortTitle(null);
+
+        var adviceNarration = new AdviceNarration(UUID.randomUUID(), assessmentResult.getId(), aiNarration, null, LocalDateTime.now(), null, UUID.randomUUID());
+        var expectedPrompt = new PromptTemplate(appAiProperties.getPrompt().getAdviceNarrationAndAdviceItems(),
+            Map.of("assessmentTitle", assessment.getTitle(),
+                "attributeTargets", "TargetAttribute[attribute=Reliability, targetMaturityLevel=Great]",
+                "adviceRecommendations", "AdviceRecommendation[question=title, currentOption=answeredOption, recommendedOption=recommendedOption]",
+                "language", assessmentResult.getLanguage().getTitle())).create();
+
+        when(loadAdviceNarrationPort.loadByAssessmentResultId(assessmentResult.getId())).thenReturn(Optional.of(adviceNarration));
+        when(loadAttributeCurrentAndTargetLevelIndexPort.load(assessmentResult.getAssessmentId(), attributeLevelTargets))
+            .thenReturn(List.of(new LoadAttributeCurrentAndTargetLevelIndexPort.Result(attributeLevelTargets.getFirst().getAttributeId(), 1, 2)));
+        when(loadMaturityLevelsPort.loadAll(assessmentResult.getAssessmentId())).thenReturn(maturityLevels);
+        when(loadAttributesPort.loadByIdsAndAssessmentId(List.of(attributeLevelTargets.getFirst().getAttributeId()), assessmentResult.getAssessmentId())).thenReturn(attributes);
+        when(loadAssessmentPort.loadById(assessmentResult.getAssessmentId())).thenReturn(assessment);
+        when(callAiPromptPort.call(promptArgumentCaptor.capture(), classCaptor.capture())).thenReturn(aiAdvice);
+
+        helper.createAiAdviceNarration(assessmentResult, adviceListItems, attributeLevelTargets);
+
+        verify(updateAdviceNarrationPort).updateAiNarration(updateNarrationCaptor.capture());
+        verify(createAdviceItemPort).persistAll(adviceItemsCaptor.capture());
+
+        var capturedAdviceNarration = updateNarrationCaptor.getValue();
+        assertEquals(adviceNarration.getId(), capturedAdviceNarration.id());
+        assertEquals(aiNarration, capturedAdviceNarration.narration());
+        assertEquals(expectedPrompt.getContents(), promptArgumentCaptor.getValue().getContents());
+        assertNotNull(capturedAdviceNarration.narrationTime());
+
+        var capturedAdviceItems = adviceItemsCaptor.getValue();
+        var expectedAdviceItems = aiAdvice.adviceItems();
+        assertAdviceItems(expectedAdviceItems, capturedAdviceItems);
+
+        verifyNoInteractions(createAdviceNarrationPort);
+    }
+
+    @Test
+    void testCreateAiAdviceNarration_whenAdviceNarrationExistsAndShortTitleExists_thenUpdateAdviceNarration() {
+        var assessment = assessmentWithShortTitle("ShortTitle");
+
+        var adviceNarration = new AdviceNarration(UUID.randomUUID(), assessmentResult.getId(), aiNarration, null, LocalDateTime.now(), null, UUID.randomUUID());
+        var expectedPrompt = new PromptTemplate(appAiProperties.getPrompt().getAdviceNarrationAndAdviceItems(),
+            Map.of("assessmentTitle", assessment.getShortTitle(),
+                "attributeTargets", "TargetAttribute[attribute=Reliability, targetMaturityLevel=Great]",
+                "adviceRecommendations", "AdviceRecommendation[question=title, currentOption=answeredOption, recommendedOption=recommendedOption]",
+                "language", assessmentResult.getLanguage().getTitle())).create();
+
+        when(loadAdviceNarrationPort.loadByAssessmentResultId(assessmentResult.getId())).thenReturn(Optional.of(adviceNarration));
+        when(loadAttributeCurrentAndTargetLevelIndexPort.load(assessmentResult.getAssessmentId(), attributeLevelTargets))
+            .thenReturn(List.of(new LoadAttributeCurrentAndTargetLevelIndexPort.Result(attributeLevelTargets.getFirst().getAttributeId(), 1, 2)));
+        when(loadMaturityLevelsPort.loadAll(assessmentResult.getAssessmentId())).thenReturn(maturityLevels);
+        when(loadAttributesPort.loadByIdsAndAssessmentId(List.of(attributeLevelTargets.getFirst().getAttributeId()), assessmentResult.getAssessmentId())).thenReturn(attributes);
+        when(loadAssessmentPort.loadById(assessmentResult.getAssessmentId())).thenReturn(assessment);
+        when(callAiPromptPort.call(promptArgumentCaptor.capture(), classCaptor.capture())).thenReturn(aiAdvice);
+
+        helper.createAiAdviceNarration(assessmentResult, adviceListItems, attributeLevelTargets);
+
+        verify(createAdviceItemPort).persistAll(adviceItemsCaptor.capture());
+        verify(updateAdviceNarrationPort).updateAiNarration(updateNarrationCaptor.capture());
+
+        var capturedAdviceNarration = updateNarrationCaptor.getValue();
+        assertEquals(adviceNarration.getId(), capturedAdviceNarration.id());
+        assertEquals(aiNarration, capturedAdviceNarration.narration());
+        assertEquals(expectedPrompt.getContents(), promptArgumentCaptor.getValue().getContents());
+        assertNotNull(capturedAdviceNarration.narrationTime());
+
+        var capturedAdviceItems = adviceItemsCaptor.getValue();
+        var expectedAdviceItems = aiAdvice.adviceItems();
+        assertAdviceItems(expectedAdviceItems, capturedAdviceItems);
+
+        verifyNoInteractions(createAdviceNarrationPort);
+    }
+
+
+    private void assertAdviceItems(List<AdviceDto.AdviceItemDto> expectedAdviceItems, List<AdviceItem> capturedAdviceItems) {
+        assertEquals(expectedAdviceItems.size(), capturedAdviceItems.size());
+        assertEquals(AdviceDto.class, classCaptor.getValue());
+        assertThat(capturedAdviceItems)
+            .zipSatisfy(expectedAdviceItems, (actual, expected) -> {
+                assertEquals(expected.title(), actual.getTitle());
+                assertEquals(expected.description(), actual.getDescription());
+                assertEquals(assessmentResult.getId(), actual.getAssessmentResultId());
+                assertEquals(expected.cost(), actual.getCost().getId());
+                assertEquals(expected.impact(), actual.getImpact().getId());
+                assertEquals(expected.priority(), actual.getPriority().getId());
+                assertNotNull(actual.getCreationTime());
+                assertNotNull(actual.getLastModificationTime());
+                assertNull(actual.getCreatedBy());
+                assertNull(actual.getLastModifiedBy());
+            });
+    }
+
+    private AppAiProperties appAiProperties() {
+        var properties = new AppAiProperties();
+        properties.setEnabled(true);
+        properties.setPrompt(new AppAiProperties.Prompt());
+        properties.setSaveAiInputFileEnabled(true);
+        properties.getPrompt().setAdviceNarrationAndAdviceItems("The assessment \"{assessmentTitle}\" " +
+            "with attribute targets {attributeTargets} and recommendations {adviceRecommendations} has been evaluated. " +
+            "Provide the result in the {language} language");
+        return properties;
+    }
+}
