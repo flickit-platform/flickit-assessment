c7d1a58080dcc8e9a223ca824558ddb26f1f95f0
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-26 11:10:09 +0330

    FAC-1132: Add RefreshAssessmentAdviceRestController and Add the Request to Postman collection

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/adapter/in/rest/advicenarration/RefreshAssessmentAdviceRestController.java
@@ -0,0 +1,28 @@
+package org.flickit.assessment.advice.adapter.in.rest.advicenarration;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.advice.application.port.in.advicenarration.RefreshAssessmentAdviceUseCase;
+import org.flickit.assessment.advice.application.port.in.advicenarration.RefreshAssessmentAdviceUseCase.Param;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class RefreshAssessmentAdviceRestController {
+
+    private final RefreshAssessmentAdviceUseCase useCase;
+    private final UserContext userContext;
+
+    @PostMapping("/assessments/{assessmentId}/refresh-advice")
+    ResponseEntity<Void> refreshAssessmentAdvice(@PathVariable("assessmentId") UUID assessmentId) {
+        var currentUserId = userContext.getUser().id();
+        useCase.refreshAssessmentAdvice(new Param(assessmentId, currentUserId));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
