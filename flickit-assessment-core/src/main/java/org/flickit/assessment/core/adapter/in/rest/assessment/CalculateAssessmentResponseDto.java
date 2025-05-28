a2b82f2dac51ebf31395882ca1d0debe5a8b3408
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-28 17:56:36 +0330

    FAC-1144: Add ResultAffected to ResponseDto

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessment/CalculateAssessmentResponseDto.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/assessment/CalculateAssessmentResponseDto.java
@@ -1,3 +1,7 @@
 package org.flickit.assessment.core.adapter.in.rest.assessment;
 
-public record CalculateAssessmentResponseDto(long id, String title, int value, int index) {}
+public record CalculateAssessmentResponseDto(MaturityLevelDto maturityLevel, boolean resultAffected) {
+
+    public record MaturityLevelDto(long id, String title, int value, int index) {
+    }
+}
