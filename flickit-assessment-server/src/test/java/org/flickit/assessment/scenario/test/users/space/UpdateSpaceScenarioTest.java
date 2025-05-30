a57682e6691fbd2a0d6ffb68fdfc3a16b1f47446
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-30 19:18:57 +0330

    FAC-1141: Refactor UpdateSpaceScenarioTest to accept space with duplicate title

--- flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/test/users/space/UpdateSpaceScenarioTest.java
+++ flickit-assessment-server/src/test/java/org/flickit/assessment/scenario/test/users/space/UpdateSpaceScenarioTest.java
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.common.exception.api.ErrorCodes.ACCESS_DENIED;
-import static org.flickit.assessment.common.exception.api.ErrorCodes.INVALID_INPUT;
 import static org.flickit.assessment.common.util.SlugCodeUtil.generateSlugCode;
 import static org.flickit.assessment.scenario.fixture.request.CreateSpaceRequestDtoMother.createSpaceRequestDto;
 import static org.flickit.assessment.scenario.fixture.request.UpdateSpaceRequestDtoMother.updateSpaceRequestDto;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UpdateSpaceScenarioTest extends AbstractScenarioTest {
 
@@ -37,57 +35,6 @@ class UpdateSpaceScenarioTest extends AbstractScenarioTest {
         assertThat(updatedSpace.getLastModificationTime()).isAfter(createdSpace.getLastModificationTime());
     }
 
-    @Test
-    void updateSpace_withSameTitleForDifferentUsers() {
-        // Create first space
-        var firstSpaceId = createSpace();
-        SpaceJpaEntity firstSpace = jpaTemplate.load(firstSpaceId, SpaceJpaEntity.class);
-
-        // Change currentUser
-        context.getNextCurrentUser();
-
-        // Create second space for different user
-        var secondSpaceId = createSpace();
-        SpaceJpaEntity secondSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
-
-        // Update the second space with first space's title
-        var updateRequest = updateSpaceRequestDto(b -> b.title(firstSpace.getTitle()));
-        spaceHelper.update(context, updateRequest, secondSpaceId).then()
-                .statusCode(200);
-
-        SpaceJpaEntity updatedSecondSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
-
-        assertEquals(firstSpace.getTitle(), updatedSecondSpace.getTitle());
-        assertEquals(firstSpace.getCode(), updatedSecondSpace.getCode());
-        assertEquals(getCurrentUserId(), secondSpace.getLastModifiedBy());
-        assertThat(updatedSecondSpace.getLastModificationTime()).isAfter(secondSpace.getLastModificationTime());
-    }
-
-    @Test
-    void updateSpace_withSameTitleAsDeleted() {
-        // Create first space
-        var firstSpaceId = createSpace();
-        SpaceJpaEntity firstSpace = jpaTemplate.load(firstSpaceId, SpaceJpaEntity.class);
-
-        // Create second space with different title
-        var secondSpaceId = createSpace();
-        SpaceJpaEntity secondSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
-
-        // Delete the first space
-        spaceHelper.delete(context, firstSpaceId);
-
-        // Update the second space's title to match the deleted space's title
-        var updateRequest = updateSpaceRequestDto(b -> b.title(firstSpace.getTitle()));
-        spaceHelper.update(context, updateRequest, secondSpaceId).then()
-                .statusCode(200);
-
-        SpaceJpaEntity updatedSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
-
-        assertEquals(firstSpace.getTitle(), updatedSpace.getTitle());
-        assertEquals(firstSpace.getCode(), updatedSpace.getCode());
-        assertEquals(getCurrentUserId(), updatedSpace.getLastModifiedBy());
-        assertThat(updatedSpace.getLastModificationTime()).isAfter(secondSpace.getLastModificationTime());
-    }
 
     @Test
     void updateSpace_userIsNotOwner() {
@@ -117,20 +64,16 @@ class UpdateSpaceScenarioTest extends AbstractScenarioTest {
 
         // Create second space with different title
         Number secondSpaceId = createSpace();
-        SpaceJpaEntity secondSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
 
         // Update the second space's title to match the first space's title
         var updateRequest = updateSpaceRequestDto(b -> b.title(firstSpace.getTitle()));
-        var error = spaceHelper.update(context, updateRequest, secondSpaceId).then()
-                .statusCode(400)
-                .extract().as(ErrorResponseDto.class);
+        spaceHelper.update(context, updateRequest, secondSpaceId).then()
+                .statusCode(200);
 
         SpaceJpaEntity updatedSpace = jpaTemplate.load(secondSpaceId, SpaceJpaEntity.class);
 
-        assertEquals(secondSpace.getTitle(), updatedSpace.getTitle());
-        assertEquals(updatedSpace.getCreationTime(), updatedSpace.getLastModificationTime());
-        assertEquals(INVALID_INPUT, error.code());
-        assertNotNull(error.message());
+        assertEquals(firstSpace.getTitle(), updatedSpace.getTitle());
+        assertTrue(updatedSpace.getLastModificationTime().isAfter(updatedSpace.getCreationTime()));
     }
 
     private long createSpace() {
