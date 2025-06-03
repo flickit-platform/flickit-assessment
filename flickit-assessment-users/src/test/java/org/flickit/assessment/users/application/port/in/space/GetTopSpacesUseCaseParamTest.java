2c2313cd8db219f64d70dd172b3eea09ea1b0f4c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-06-03 19:42:37 +0330

    FAC-1145: Refactor test GetTopSpacesUseCaseParamTest to use builder pattern

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/port/in/space/GetTopSpacesUseCaseParamTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/port/in/space/GetTopSpacesUseCaseParamTest.java
@@ -3,6 +3,9 @@ package org.flickit.assessment.users.application.port.in.space;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+import java.util.function.Consumer;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,7 +15,18 @@ class GetTopSpacesUseCaseParamTest {
     @Test
     void testGetTopSpacesUseCaseParam_currentUserIdIsNull_ErrorMessage() {
         var throwable = assertThrows(ConstraintViolationException.class,
-            () -> new GetTopSpacesUseCase.Param(null));
+            () -> createParam(b -> b.currentUserId(null)));
         assertThat(throwable).hasMessage("currentUserId: " + COMMON_CURRENT_USER_ID_NOT_NULL);
+    }
+
+    private void createParam(Consumer<GetTopSpacesUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        paramBuilder.build();
+    }
+
+    private GetTopSpacesUseCase.Param.ParamBuilder paramBuilder() {
+        return GetTopSpacesUseCase.Param.builder()
+                .currentUserId(UUID.randomUUID());
     }
 }
