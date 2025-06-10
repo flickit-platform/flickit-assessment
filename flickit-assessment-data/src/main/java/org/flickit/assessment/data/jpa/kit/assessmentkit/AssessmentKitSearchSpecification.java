0a842a4d6be838f0b568c838d0b99916b88cc8df
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-06-10 20:03:18 +0330

    FAC-1160: Add paid constraint to `toAccessCheckPredicate` of AssessmentKitSearchSpecification

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/assessmentkit/AssessmentKitSearchSpecification.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/kit/assessmentkit/AssessmentKitSearchSpecification.java
@@ -49,17 +49,22 @@ public class AssessmentKitSearchSpecification implements Specification<Assessmen
     private Predicate toAccessCheckPredicate(Root<AssessmentKitJpaEntity> root,
                                              CriteriaQuery<?> query,
                                              CriteriaBuilder cb) {
-        Predicate isPublic = cb.isFalse(root.get(AssessmentKitJpaEntity.Fields.isPrivate));
-
         Subquery<Long> subQuery = query.subquery(Long.class);
         Root<KitUserAccessJpaEntity> subRoot = subQuery.from(KitUserAccessJpaEntity.class);
         subQuery.select(subRoot.get(KitUserAccessJpaEntity.Fields.kitId))
             .where(cb.equal(subRoot.get(KitUserAccessJpaEntity.Fields.userId), userId));
 
+        Predicate isFree = cb.equal(root.get(AssessmentKitJpaEntity.Fields.price), 0);
+        Predicate isPublic = cb.isFalse(root.get(AssessmentKitJpaEntity.Fields.isPrivate));
+        Predicate freeAndPublic = cb.and(isFree, isPublic);
+
         Predicate isPrivate = cb.isTrue(root.get(AssessmentKitJpaEntity.Fields.isPrivate));
         Predicate userHasAccess = cb.in(root.get(AssessmentKitJpaEntity.Fields.id)).value(subQuery);
         Predicate privateAndAccessible = cb.and(isPrivate, userHasAccess);
 
-        return cb.or(isPublic, privateAndAccessible);
+        Predicate isPaid = cb.gt(root.get(AssessmentKitJpaEntity.Fields.price), 0);
+        Predicate paidAndAccessible = cb.and(isPaid, userHasAccess);
+
+        return cb.or(freeAndPublic, privateAndAccessible, paidAndAccessible);
     }
 }
