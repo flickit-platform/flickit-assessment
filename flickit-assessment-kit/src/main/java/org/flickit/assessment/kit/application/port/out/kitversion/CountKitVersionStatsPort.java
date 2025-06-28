2dd9b889589a72bf60e050151a2ff92ba92b1ca3
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-06-28 16:54:38 +0330

    FAC-1117: Remove attributeIndexToMeasuresCountMap from CountKitVersionStatsPort and its implementation

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/kitversion/CountKitVersionStatsPort.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/kitversion/CountKitVersionStatsPort.java
@@ -1,11 +1,9 @@
 package org.flickit.assessment.kit.application.port.out.kitversion;
 
-import java.util.Map;
-
 public interface CountKitVersionStatsPort {
 
     Result countKitVersionStats(long kitVersionId);
 
-    record Result(int subjectCount, int questionnaireCount, int questionCount, int maturityLevelCount, Map<String, Long> attributeIndexToMeasuresCountMap) {
+    record Result(int subjectCount, int questionnaireCount, int questionCount, int maturityLevelCount) {
     }
 }
