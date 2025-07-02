30d0f57dab914f20250fe041a2b1dad7c4721536
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-07-02 21:06:20 +0330

    FAC-1181: Some minor serviceTest cleanUp

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/DeleteSpaceServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/service/space/DeleteSpaceServiceTest.java
@@ -2,13 +2,11 @@ package org.flickit.assessment.users.application.service.space;
 
 import org.assertj.core.api.Assertions;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.common.exception.ValidationException;
 import org.flickit.assessment.users.application.port.in.space.DeleteSpaceUseCase;
 import org.flickit.assessment.users.application.port.out.space.CountSpaceAssessmentPort;
 import org.flickit.assessment.users.application.port.out.space.DeleteSpacePort;
 import org.flickit.assessment.users.application.port.out.space.LoadSpaceOwnerPort;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,7 +17,6 @@ import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.flickit.assessment.users.common.ErrorMessageKey.DELETE_SPACE_ASSESSMENT_EXIST;
-import static org.flickit.assessment.users.common.ErrorMessageKey.SPACE_ID_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
@@ -41,21 +38,7 @@ class DeleteSpaceServiceTest {
     DeleteSpacePort deleteSpacePort;
 
     @Test
-    @DisplayName("Deleting a space that the owner is null causes ResourceNotFoundException")
-    void testDeleteSpase_ownerIsNull_resourceNotFound() {
-        long spaceId = 0L;
-        UUID currentUserId = UUID.randomUUID();
-        DeleteSpaceUseCase.Param param = new DeleteSpaceUseCase.Param(spaceId, currentUserId);
-
-        when(loadSpaceOwnerPort.loadOwnerId(spaceId)).thenThrow(new ResourceNotFoundException(SPACE_ID_NOT_FOUND));
-
-        var throwable = assertThrows(ResourceNotFoundException.class, () -> service.deleteSpace(param));
-        Assertions.assertThat(throwable).hasMessage(SPACE_ID_NOT_FOUND);
-    }
-
-    @Test
-    @DisplayName("Deleting a space should be accomplished by owner")
-    void testDeleteSpase_currentUserIsNotOwner_accessDeniedException() {
+    void testDeleteSpase_whenCurrentUserIsNotOwner_thenThrowAccessDeniedException() {
         long spaceId = 0L;
         UUID currentUserId = UUID.randomUUID();
         DeleteSpaceUseCase.Param param = new DeleteSpaceUseCase.Param(spaceId, currentUserId);
@@ -67,8 +50,7 @@ class DeleteSpaceServiceTest {
     }
 
     @Test
-    @DisplayName("Deleting a space should be accomplished on a space with no assessments.")
-    void testDeleteSpase_assessmentsCountIsNotZero_resourceNotFound() {
+    void testDeleteSpase_whenAssessmentsCountIsNotZero_thenThrowResourceNotFound() {
         long spaceId = 0L;
         UUID currentUserId = UUID.randomUUID();
         DeleteSpaceUseCase.Param param = new DeleteSpaceUseCase.Param(spaceId, currentUserId);
@@ -81,8 +63,7 @@ class DeleteSpaceServiceTest {
     }
 
     @Test
-    @DisplayName("Deleting a space with valid parameters should cause a successful delete.")
-    void testDeleteSpase_validParameters_successful() {
+    void testDeleteSpase_whenParametersAreValid_thenSuccessfulDelete() {
         long spaceId = 0L;
         UUID currentUserId = UUID.randomUUID();
         DeleteSpaceUseCase.Param param = new DeleteSpaceUseCase.Param(spaceId, currentUserId);
