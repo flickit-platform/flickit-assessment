a3d47a0de08d12613e712790ed9c9a4ff7abdb06
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-07-10 18:36:44 +0330

    FAC-1187: remove required args constructor

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/domain/Attribute.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/domain/Attribute.java
@@ -1,18 +1,15 @@
 package org.flickit.assessment.advice.application.domain;
 
-import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class Attribute {
 
     private final long id;
 
     private final String title;
 
-    @Nullable private Integer weight;
+    private final Integer weight;
 }
