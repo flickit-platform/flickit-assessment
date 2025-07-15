a34a8b7f591bdee220ad1c92bbed766f42a387c1
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-07-15 07:24:38 +0330

    FAC-1170: Remove extra AttributeMiniTranslation.java and use AttributeTranslation instead

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/attribute/AttributeMapper.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/attribute/AttributeMapper.java
@@ -4,7 +4,6 @@ import jakarta.annotation.Nullable;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.flickit.assessment.common.application.domain.kit.KitLanguage;
-import org.flickit.assessment.common.application.domain.kit.translation.AttributeMiniTranslation;
 import org.flickit.assessment.common.application.domain.kit.translation.AttributeTranslation;
 import org.flickit.assessment.common.util.JsonUtils;
 import org.flickit.assessment.data.jpa.kit.attribute.AttributeJpaEntity;
@@ -68,7 +67,7 @@ public class AttributeMapper {
     }
 
     public static AttributeMini mapToAttributeMiniDomainModel(AttributeJpaEntity entity, KitLanguage language) {
-        var translation = getAttributeMiniTranslation(entity, language);
+        var translation = getTranslation(entity, language);
         return new AttributeMini(
             entity.getId(),
             translation.titleOrDefault(entity.getTitle())
@@ -90,15 +89,6 @@ public class AttributeMapper {
         var translation = new AttributeTranslation(null, null);
         if (language != null) {
             var translations = JsonUtils.fromJsonToMap(entity.getTranslations(), KitLanguage.class, AttributeTranslation.class);
-            translation = translations.getOrDefault(language, translation);
-        }
-        return translation;
-    }
-
-    private static AttributeMiniTranslation getAttributeMiniTranslation(AttributeJpaEntity entity, @Nullable KitLanguage language) {
-        var translation = new AttributeMiniTranslation(null);
-        if (language != null) {
-            var translations = JsonUtils.fromJsonToMap(entity.getTranslations(), KitLanguage.class, AttributeMiniTranslation.class);
             translation = translations.getOrDefault(language, translation);
         }
         return translation;
