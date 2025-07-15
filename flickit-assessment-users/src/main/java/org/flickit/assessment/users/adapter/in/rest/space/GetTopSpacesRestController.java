fb9730cd8cf3f93b5fb57612b443fcb2f67755b9
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-07-15 18:52:20 +0330

    FAC-1152: Refactor api response type

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/in/rest/space/GetTopSpacesRestController.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/in/rest/space/GetTopSpacesRestController.java
@@ -2,13 +2,14 @@ package org.flickit.assessment.users.adapter.in.rest.space;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.config.jwt.UserContext;
+import org.flickit.assessment.users.adapter.in.rest.space.GetTopSpacesResponseDto.SpaceListItemDto;
 import org.flickit.assessment.users.application.port.in.space.GetTopSpacesUseCase;
-import org.flickit.assessment.users.application.port.in.space.GetTopSpacesUseCase.*;
+import org.flickit.assessment.users.application.port.in.space.GetTopSpacesUseCase.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.flickit.assessment.users.adapter.in.rest.space.GetTopSpacesResponseDto.SpaceListItemDto;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,20 +20,20 @@ public class GetTopSpacesRestController {
     private final UserContext userContext;
 
     @GetMapping("/top-spaces")
-    public GetTopSpacesResponseDto getTopSpaces() {
+    public ResponseEntity<GetTopSpacesResponseDto> getTopSpaces() {
         UUID currentUserId = userContext.getUser().id();
         var result = getTopSpacesUseCase.getSpaceList(new Param(currentUserId));
-        return toResponseDto(result.items());
+        return new ResponseEntity<>(toResponseDto(result), HttpStatus.OK);
     }
 
-    private GetTopSpacesResponseDto toResponseDto(List<Result.SpaceListItem> items) {
-        var result = items.stream()
+    private GetTopSpacesResponseDto toResponseDto(GetTopSpacesUseCase.Result result) {
+        var itemDtos = result.items().stream()
             .map(r -> new SpaceListItemDto(r.id(),
                 r.title(),
                 SpaceListItemDto.TypeDto.of(r.type()),
                 r.isDefault()))
             .toList();
 
-        return new GetTopSpacesResponseDto(result);
+        return new GetTopSpacesResponseDto(itemDtos);
     }
 }
