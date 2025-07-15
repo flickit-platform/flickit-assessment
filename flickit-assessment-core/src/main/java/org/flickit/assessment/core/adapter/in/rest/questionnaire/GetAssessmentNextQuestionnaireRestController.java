5d5cb3daba04811bb104603121588408f25104de
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-07-15 15:20:03 +0330

    FAC-1134: Add GetAssessmentNextQuestionnaireResponseDto

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/questionnaire/GetAssessmentNextQuestionnaireRestController.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/in/rest/questionnaire/GetAssessmentNextQuestionnaireRestController.java
@@ -3,14 +3,15 @@ package org.flickit.assessment.core.adapter.in.rest.questionnaire;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.config.jwt.UserContext;
 import org.flickit.assessment.core.application.port.in.questionnaire.GetAssessmentNextQuestionnaireUseCase;
-import org.flickit.assessment.core.application.port.in.questionnaire.GetAssessmentNextQuestionnaireUseCase.Result;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
-import static org.flickit.assessment.core.application.port.in.questionnaire.GetAssessmentNextQuestionnaireUseCase.*;
+import static org.flickit.assessment.core.application.port.in.questionnaire.GetAssessmentNextQuestionnaireUseCase.Param;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,14 +21,19 @@ public class GetAssessmentNextQuestionnaireRestController {
     private final UserContext userContext;
 
     @GetMapping("/assessments/{assessmentId}/questionnaires/{questionnaireId}/next")
-    public ResponseEntity<Result> getAssessmentNextQuestionnaire(@PathVariable("assessmentId") UUID assessmentId,
+    public ResponseEntity<GetAssessmentNextQuestionnaireResponseDto> getAssessmentNextQuestionnaire(@PathVariable("assessmentId") UUID assessmentId,
                                                                  @PathVariable("questionnaireId") Long questionnaireId) {
         var currentUserId = userContext.getUser().id();
         var result = useCase.getNextQuestionnaire(toParam(assessmentId, questionnaireId, currentUserId));
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        var response = toResponse(result);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     private Param toParam(UUID assessmentId, Long questionnaireId, UUID currentUserId) {
         return new Param(assessmentId, questionnaireId, currentUserId);
+    }
+
+    private GetAssessmentNextQuestionnaireResponseDto toResponse(GetAssessmentNextQuestionnaireUseCase.Result result) {
+        return GetAssessmentNextQuestionnaireResponseDto.of(result);
     }
 }
