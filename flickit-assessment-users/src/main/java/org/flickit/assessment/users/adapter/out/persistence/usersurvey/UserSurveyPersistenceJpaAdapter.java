dfe26424637f8c8f3c06a093428b55f40bdf0490
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-07-26 18:51:44 +0330

    FAC-1204: Add Mapper and adapter

---
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/usersurvey/UserSurveyPersistenceJpaAdapter.java
@@ -0,0 +1,23 @@
+package org.flickit.assessment.users.adapter.out.persistence.usersurvey;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.data.jpa.users.usersurvey.UserSurveyJpaRepository;
+import org.flickit.assessment.users.application.domain.UserSurvey;
+import org.flickit.assessment.users.application.port.out.usersurvey.LoadUserSurveyPort;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class UserSurveyPersistenceJpaAdapter implements LoadUserSurveyPort {
+
+    private final UserSurveyJpaRepository userSurveyRepository;
+
+    @Override
+    public Optional<UserSurvey> loadByUserId(UUID userId) {
+        return userSurveyRepository.findByUserId(userId)
+            .map(UserSurveyMapper::mapToDomain);
+    }
+}
