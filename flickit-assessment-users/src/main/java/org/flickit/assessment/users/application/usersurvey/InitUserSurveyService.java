dd5037afe57e8311a9d8f8c24a78ea3c674e4fb3
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-07-27 18:13:53 +0330

    FAC-1205: Add Service and ServiceTest [WIP]

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/usersurvey/InitUserSurveyService.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/application/usersurvey/InitUserSurveyService.java
@@ -3,15 +3,37 @@ package org.flickit.assessment.users.application.usersurvey;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.users.application.port.in.usersurvey.InitUserSurveyUseCase;
+import org.flickit.assessment.users.application.port.out.usersurvey.CreateUserSurveyPort;
+import org.flickit.assessment.users.application.port.out.usersurvey.LoadUserSurveyPort;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class InitUserSurveyService implements InitUserSurveyUseCase {
 
+    private final LoadUserSurveyPort loadUserSurveyPort;
+    private final CreateUserSurveyPort createUserSurveyPort;
+
     @Override
     public Result initUserSurvey(Param param) {
-        return null;
+        return loadUserSurveyPort.loadByUserId(param.getCurrentUserId())
+            .map(userSurvey -> new Result(userSurvey.getId(), ""))
+            .orElseGet(() -> {
+                var surveyId = createUserSurveyPort.persist(
+                    toParam(param.getCurrentUserId(), param.getAssessmentId())
+                );
+                return new Result(surveyId, "");
+            });
+    }
+
+    private CreateUserSurveyPort.Param toParam(UUID userId, UUID assessmentId) {
+        return new CreateUserSurveyPort.Param(
+            userId,
+            assessmentId,
+            LocalDateTime.now());
     }
 }
--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/usersurvey/InitUserSurveyServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/usersurvey/InitUserSurveyServiceTest.java
@@ -1,10 +1,25 @@
 package org.flickit.assessment.users.application.usersurvey;
 
+import org.flickit.assessment.users.application.port.in.usersurvey.InitUserSurveyUseCase;
+import org.flickit.assessment.users.application.port.out.usersurvey.CreateUserSurveyPort;
+import org.flickit.assessment.users.application.port.out.usersurvey.LoadUserSurveyPort;
+import org.flickit.assessment.users.test.fixture.application.UserSurveyMother;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class InitUserSurveyServiceTest {
@@ -12,4 +27,58 @@ class InitUserSurveyServiceTest {
     @InjectMocks
     private InitUserSurveyService service;
 
+    @Mock
+    private LoadUserSurveyPort loadUserSurveyPort;
+
+    @Mock
+    private CreateUserSurveyPort createUserSurveyPort;
+
+    @Captor
+    private ArgumentCaptor<CreateUserSurveyPort.Param> paramCaptor;
+
+    private final InitUserSurveyUseCase.Param param = createParam(InitUserSurveyUseCase.Param.ParamBuilder::build);
+
+    @Test
+    void testInitUserSurvey_whenUserSurveyExists_thenReturnUserSurvey() {
+        var survey = UserSurveyMother.simpleUserSurvey();
+
+        when(loadUserSurveyPort.loadByUserId(param.getCurrentUserId())).thenReturn(Optional.of(survey));
+
+        var result = service.initUserSurvey(param);
+
+        assertEquals(survey.getId(), result.userSurveyId());
+        assertEquals("", result.redirectUrl());
+
+        verifyNoInteractions(createUserSurveyPort);
+    }
+
+    @Test
+    void testInitUserSurvey_whenUserSurveyDoesNotExist_thenCreateUserSurvey() {
+        long surveyId = 123L;
+
+        when(loadUserSurveyPort.loadByUserId(param.getCurrentUserId())).thenReturn(Optional.empty());
+        when(createUserSurveyPort.persist(paramCaptor.capture())).thenReturn(surveyId);
+
+        var result = service.initUserSurvey(param);
+
+        var createParam = paramCaptor.getValue();
+        assertEquals(param.getAssessmentId(), createParam.assessmentId());
+        assertEquals(param.getCurrentUserId(), createParam.userId());
+        assertNotNull(createParam.currentDateTime());
+
+        assertEquals(surveyId, result.userSurveyId());
+        assertEquals("", result.redirectUrl());
+    }
+
+    private InitUserSurveyUseCase.Param createParam(Consumer<InitUserSurveyUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        return paramBuilder.build();
+    }
+
+    private InitUserSurveyUseCase.Param.ParamBuilder paramBuilder() {
+        return InitUserSurveyUseCase.Param.builder()
+                .assessmentId(UUID.randomUUID())
+                .currentUserId(UUID.randomUUID());
+    }
 }
