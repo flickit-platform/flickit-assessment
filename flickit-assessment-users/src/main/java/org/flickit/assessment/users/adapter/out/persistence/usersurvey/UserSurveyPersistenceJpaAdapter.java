f89dc23da56432aec5526cfde39e32354eb0512c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-07-27 18:04:21 +0330

    FAC-1205: Add and Implement CreateUserSurveyPort

--- flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/usersurvey/UserSurveyPersistenceJpaAdapter.java
+++ flickit-assessment-users/src/main/java/org/flickit/assessment/users/adapter/out/persistence/usersurvey/UserSurveyPersistenceJpaAdapter.java
@@ -3,6 +3,7 @@ package org.flickit.assessment.users.adapter.out.persistence.usersurvey;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.data.jpa.users.usersurvey.UserSurveyJpaRepository;
 import org.flickit.assessment.users.application.domain.UserSurvey;
+import org.flickit.assessment.users.application.port.out.usersurvey.CreateUserSurveyPort;
 import org.flickit.assessment.users.application.port.out.usersurvey.LoadUserSurveyPort;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,9 @@ import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
-public class UserSurveyPersistenceJpaAdapter implements LoadUserSurveyPort {
+public class UserSurveyPersistenceJpaAdapter implements
+    LoadUserSurveyPort,
+    CreateUserSurveyPort {
 
     private final UserSurveyJpaRepository repository;
 
@@ -19,5 +22,11 @@ public class UserSurveyPersistenceJpaAdapter implements LoadUserSurveyPort {
     public Optional<UserSurvey> loadByUserId(UUID userId) {
         return repository.findByUserId(userId)
             .map(UserSurveyMapper::mapToDomain);
+    }
+
+    @Override
+    public long persist(CreateUserSurveyPort.Param param) {
+        var savedEntity = repository.save(UserSurveyMapper.mapCreateParamToJpaEntity(param));
+        return savedEntity.getId();
     }
 }
