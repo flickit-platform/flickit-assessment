d41df69dd44ee25ada169e181e47cc371e983e8b
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-07-28 18:48:35 +0330

    FAC-1206: Add service and ServiceTest

--- flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/usersurvey/SetUserSurveyDoNotShowAgainServiceTest.java
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/usersurvey/SetUserSurveyDoNotShowAgainServiceTest.java
@@ -3,13 +3,19 @@ package org.flickit.assessment.users.application.usersurvey;
 import org.flickit.assessment.users.application.port.in.usersurvey.SetUserSurveyDoNotShowAgainUseCase;
 import org.flickit.assessment.users.application.port.out.usersurvey.CreateUserSurveyPort;
 import org.flickit.assessment.users.application.port.out.usersurvey.LoadUserSurveyPort;
+import org.flickit.assessment.users.application.port.out.usersurvey.UpdateUserSurveyPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.flickit.assessment.users.test.fixture.application.UserSurveyMother.createSimpleUserSurvey;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SetUserSurveyDoNotShowAgainServiceTest {
@@ -23,15 +29,41 @@ class SetUserSurveyDoNotShowAgainServiceTest {
     @Mock
     private CreateUserSurveyPort createUserSurveyPort;
 
+    @Mock
+    private UpdateUserSurveyPort updateUserSurveyPort;
+
+    private final SetUserSurveyDoNotShowAgainUseCase.Param param = createParam(SetUserSurveyDoNotShowAgainUseCase.Param.ParamBuilder::build);
+
     @Test
     void testSetUserSurveyDoNotShowAgain_whenUserSurveyDoesNotExist_thenCreateUserSurveyAndSetAsDontShowAgain() {
+        when(loadUserSurveyPort.loadByUserId(param.getCurrentUserId())).thenReturn(Optional.of(createSimpleUserSurvey()));
 
+        service.setDontShowAgain(param);
 
+        verify(updateUserSurveyPort).updateDontShowAgain(param.getCurrentUserId(), true);
+        verifyNoInteractions(createUserSurveyPort);
     }
 
     @Test
     void testSetUserSurveyDoNotShowAgain_whenUserSurveyExists_thenSetAsDontShowAgain() {
+        var userSurvey = createSimpleUserSurvey();
+        when(loadUserSurveyPort.loadByUserId(param.getCurrentUserId())).thenReturn(Optional.of(userSurvey));
 
+        service.setDontShowAgain(param);
 
+        verify(updateUserSurveyPort).updateDontShowAgain(param.getCurrentUserId(), true);
+        verifyNoInteractions(createUserSurveyPort);
+    }
+
+    private SetUserSurveyDoNotShowAgainUseCase.Param createParam(Consumer<SetUserSurveyDoNotShowAgainUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        return paramBuilder.build();
+    }
+
+    private SetUserSurveyDoNotShowAgainUseCase.Param.ParamBuilder paramBuilder() {
+        return SetUserSurveyDoNotShowAgainUseCase.Param.builder()
+            .assessmentId(UUID.randomUUID())
+            .currentUserId(UUID.randomUUID());
     }
 }
