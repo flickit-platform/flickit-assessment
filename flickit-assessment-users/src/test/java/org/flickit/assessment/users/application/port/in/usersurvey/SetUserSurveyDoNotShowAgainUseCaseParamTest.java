f3bffa838b9554cec09ce257036fa60bd12f60c4
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-07-28 18:33:52 +0330

    FAC-1206: Add `assessmentId` to SetUserSurveyDoNotShowAgainUseCase

---
+++ flickit-assessment-users/src/test/java/org/flickit/assessment/users/application/port/in/usersurvey/SetUserSurveyDoNotShowAgainUseCaseParamTest.java
@@ -0,0 +1,44 @@
+package org.flickit.assessment.users.application.port.in.usersurvey;
+
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
+import static org.flickit.assessment.users.common.ErrorMessageKey.SET_USER_SURVEY_DONT_SHOW_AGAIN_ASSESSMENT_ID_NOT_NULL;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class SetUserSurveyDoNotShowAgainUseCaseParamTest {
+
+    @Test
+    void testSetUserSurveyDoNotShowAgainUseCaseParam_assessmentIdParamViolatesConstraint_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.assessmentId(null)));
+        assertThat(throwable).hasMessage("assessmentId: " + SET_USER_SURVEY_DONT_SHOW_AGAIN_ASSESSMENT_ID_NOT_NULL);
+    }
+
+    @Test
+    void testSetUserSurveyDoNotShowAgainUseCaseParam_currentUserIdParamViolatesConstraint_ErrorMessage() {
+        var throwable = assertThrows(ConstraintViolationException.class,
+            () -> createParam(b -> b.currentUserId(null)));
+        assertThat(throwable).hasMessage("currentUserId: " + COMMON_CURRENT_USER_ID_NOT_NULL);
+    }
+
+    private void createParam(Consumer<SetUserSurveyDoNotShowAgainUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        paramBuilder.build();
+    }
+
+    private SetUserSurveyDoNotShowAgainUseCase.Param.ParamBuilder paramBuilder() {
+        return SetUserSurveyDoNotShowAgainUseCase.Param.builder()
+            .assessmentId(UUID.randomUUID())
+            .currentUserId(UUID.randomUUID());
+    }
+}
