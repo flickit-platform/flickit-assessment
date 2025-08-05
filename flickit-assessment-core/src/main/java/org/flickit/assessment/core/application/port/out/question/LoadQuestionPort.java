f44a34f9ba1c388a9ebaaaeb52ef77ed67d782ef
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-08-05 10:25:27 +0330

    FAC-1208: Add `loadNextUnansweredQuestionIndex` to LoadQuestionPort and Implement it

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/question/LoadQuestionPort.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/question/LoadQuestionPort.java
@@ -2,7 +2,11 @@ package org.flickit.assessment.core.application.port.out.question;
 
 import org.flickit.assessment.core.application.domain.Question;
 
+import java.util.UUID;
+
 public interface LoadQuestionPort {
 
     Question loadByIdAndKitVersionId(Long id, Long kitVersionId);
+
+    int loadNextUnansweredQuestionIndex(long questionnaireId, UUID assessmentResultId);
 }
