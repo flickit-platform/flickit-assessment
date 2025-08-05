a11e17f6fe98de1fde9126feb748d9e2d6236dff
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-08-05 09:31:28 +0330

    FAC-1208: Remove extra QuestionnaireIdQuestionIndexView.java

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/core/answer/QuestionnaireIdQuestionIndexView.java
+++
@@ -1,8 +0,0 @@
-package org.flickit.assessment.data.jpa.core.answer;
-
-public interface QuestionnaireIdQuestionIndexView {
-
-    Long getQuestionnaireId();
-
-    Integer getFirstUnansweredQuestionIndex();
-}
