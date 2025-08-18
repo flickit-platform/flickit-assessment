0f96a3be60435c93020a47690ba907ec629954c6
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-08-18 14:10:03 +0330

    FAC-1180: Minor refactor

--- flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/users/space/SpaceJpaEntity.java
+++ flickit-assessment-data/src/main/java/org/flickit/assessment/data/jpa/users/space/SpaceJpaEntity.java
@@ -2,6 +2,7 @@ package org.flickit.assessment.data.jpa.users.space;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.FieldNameConstants;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldNameConstants
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class SpaceJpaEntity {
 
