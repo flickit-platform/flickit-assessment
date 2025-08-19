c384fd2591bdebfcd3b660681a47c3c52a505fb2
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-08-19 19:42:33 +0330

    FAC-1231: Add error key and i18n messages for `get-assessment-move-targets.assessmentId.notNull`

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/common/ErrorMessageKey.java
@@ -370,4 +370,6 @@ public class ErrorMessageKey {
     public static final String GET_ASSESSMENT_NEXT_QUESTIONNAIRE_ASSESSMENT_ID_NOT_NULL = "get-assessment-next-questionnaire.assessmentId.notNull";
     public static final String GET_ASSESSMENT_NEXT_QUESTIONNAIRE_QUESTIONNAIRE_ID_NOT_NULL = "get-assessment-next-questionnaire.questionnaireId.notNull";
     public static final String GET_ASSESSMENT_NEXT_QUESTIONNAIRE_ASSESSMENT_RESULT_NOT_FOUND = "get-assessment-next-questionnaire.assessmentResult.notFound";
+
+    public static final String GET_ASSESSMENT_MOVE_TARGETS_ASSESSMENT_ID_NOT_NULL = "get-assessment-move-targets.assessmentId.notNull";
 }
