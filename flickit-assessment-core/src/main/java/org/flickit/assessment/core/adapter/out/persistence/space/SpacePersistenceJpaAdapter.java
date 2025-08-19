27cdf8ee475a65f3bef2402d9bad7d2365618e60
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-08-19 22:13:02 +0330

    FAC-1231: Implement LoadSpaceListPort and its adapter method in SpacePersistenceJpaAdapter

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/space/SpacePersistenceJpaAdapter.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/space/SpacePersistenceJpaAdapter.java
@@ -1,17 +1,21 @@
 package org.flickit.assessment.core.adapter.out.persistence.space;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.application.domain.space.SpaceStatus;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.core.application.domain.Space;
 import org.flickit.assessment.core.application.port.out.space.LoadSpaceOwnerPort;
 import org.flickit.assessment.core.application.port.out.space.LoadSpacePort;
+import org.flickit.assessment.core.application.port.out.space.LoadSpaceListPort;
 import org.flickit.assessment.data.jpa.users.space.SpaceJpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_SPACE_ID_NOT_FOUND;
+import static org.flickit.assessment.core.adapter.out.persistence.space.SpaceMapper.mapToDomain;
 import static org.flickit.assessment.core.common.ErrorMessageKey.ASSESSMENT_ID_NOT_FOUND;
 
 
@@ -19,7 +23,8 @@ import static org.flickit.assessment.core.common.ErrorMessageKey.ASSESSMENT_ID_N
 @RequiredArgsConstructor
 public class SpacePersistenceJpaAdapter implements
     LoadSpaceOwnerPort,
-    LoadSpacePort {
+    LoadSpacePort,
+    LoadSpaceListPort {
 
     private final SpaceJpaRepository repository;
 
@@ -45,5 +50,17 @@ public class SpacePersistenceJpaAdapter implements
     public Optional<Space> loadByAssessmentId(UUID assessmentId) {
         return repository.findByAssessmentIdAndDeletedFalse(assessmentId)
             .map(SpaceMapper::mapToDomain);
+    }
+
+    @Override
+    public List<SpaceWithAssessmentCount> loadSpaceList(UUID currentUserId) {
+        var result = Optional.ofNullable(repository.findByUserIdOrderByLastSeenDesc(currentUserId, SpaceStatus.ACTIVE.getId()))
+            .orElseGet(List::of);
+
+        return result.stream()
+            .map(entity -> new SpaceWithAssessmentCount(
+                mapToDomain(entity.getSpace()),
+                entity.getAssessmentsCount()))
+            .toList();
     }
 }
