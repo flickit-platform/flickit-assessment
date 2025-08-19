421479c3a88c6912baf8ff84040e4f8ee7ddc358
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-08-19 22:18:12 +0330

    FAC-1231: Complete GetAssessmentMoveTargetsService implementation with capacity validation logic and additional exceptions

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/space/GetAssessmentMoveTargetsService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/space/GetAssessmentMoveTargetsService.java
@@ -1,25 +1,106 @@
 package org.flickit.assessment.core.application.service.space;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.application.domain.space.SpaceType;
+import org.flickit.assessment.common.config.AppSpecProperties;
+import org.flickit.assessment.common.exception.InvalidStateException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.common.exception.UpgradeRequiredException;
+import org.flickit.assessment.core.application.domain.Space;
 import org.flickit.assessment.core.application.port.in.space.GetAssessmentMoveTargetsUseCase;
+import org.flickit.assessment.core.application.port.in.space.GetAssessmentMoveTargetsUseCase.Result.SpaceListItem;
+import org.flickit.assessment.core.application.port.out.space.LoadSpaceListPort;
 import org.flickit.assessment.core.application.port.out.space.LoadSpacePort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.flickit.assessment.core.common.ErrorMessageKey.GET_ASSESSMENT_MOVE_TARGETS_SPACE_NOT_FOUND;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.flickit.assessment.core.common.ErrorMessageKey.*;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class GetAssessmentMoveTargetsService implements GetAssessmentMoveTargetsUseCase {
 
+    private static final int TOP_SPACES_LIMIT = 10;
+
     private final LoadSpacePort loadSpacePort;
+    private final LoadSpaceListPort loadSpaceListPort;
+    private final AppSpecProperties appSpecProperties;
 
     @Override
     public Result getSpaceList(Param param) {
         var space = loadSpacePort.loadByAssessmentId(param.getAssessmentId())
             .orElseThrow(() -> new ResourceNotFoundException(GET_ASSESSMENT_MOVE_TARGETS_SPACE_NOT_FOUND));
-        return null;
+        var loadedSpaces = loadSpaceListPort.loadSpaceList(param.getCurrentUserId());
+
+        if (loadedSpaces.isEmpty())
+            throw new InvalidStateException(GET_ASSESSMENT_MOVE_TARGETS_NO_SPACE_FOUND); // Can't happen
+
+        final int maxBasicAssessments = appSpecProperties.getSpace().getMaxBasicSpaceAssessments();
+        var spaces = Optional.of(extractSpacesWithCapacity(loadedSpaces, space.getId(), maxBasicAssessments))
+            .filter(list -> !list.isEmpty())
+            .map(list -> list.size() == 1
+                ? List.of(toSpaceListItem(list.getFirst().space()))
+                : getMultipleBasicsAndPremium(list))
+            .orElseThrow(() -> new UpgradeRequiredException(GET_ASSESSMENT_MOVE_TARGETS_NO_SPACE_AVAILABLE));
+
+        return new Result(spaces);
+    }
+
+    private List<LoadSpaceListPort.SpaceWithAssessmentCount> extractSpacesWithCapacity(List<LoadSpaceListPort.SpaceWithAssessmentCount> items, long currentSpaceId, int maxBasicAssessments) {
+        return items.stream()
+            .filter(item -> item.space().getId() != currentSpaceId
+                && (item.space().getType() == SpaceType.PREMIUM
+                || (item.space().getType() == SpaceType.BASIC && item.assessmentCount() < maxBasicAssessments)))
+            .limit(TOP_SPACES_LIMIT)
+            .toList();
+    }
+
+    private SpaceListItem toSpaceListItem(Space space) {
+        return new SpaceListItem(
+            space.getId(),
+            space.getTitle(),
+            SpaceListItem.Type.of(space.getType()),
+            true,
+            space.isDefault());
+    }
+
+    private static List<SpaceListItem> getMultipleBasicsAndPremium(List<LoadSpaceListPort.SpaceWithAssessmentCount> availableSpaces) {
+        var selectedSpaceId = selectTargetSpace(availableSpaces);
+
+        return availableSpaces.stream()
+            .map(item -> {
+                var space = item.space();
+                boolean selected = space.getId() == selectedSpaceId;
+                return new SpaceListItem(
+                    space.getId(),
+                    space.getTitle(),
+                    new SpaceListItem.Type(
+                        space.getType().getCode(),
+                        space.getType().getTitle()
+                    ),
+                    selected,
+                    space.isDefault()
+                );
+            })
+            .toList();
+    }
+
+    private static Long selectTargetSpace(List<LoadSpaceListPort.SpaceWithAssessmentCount> availableSpaces) {
+        return availableSpaces.stream()
+            .map(LoadSpaceListPort.SpaceWithAssessmentCount::space)
+            .collect(Collectors.collectingAndThen(
+                Collectors.toList(),
+                spaces -> spaces.stream()
+                    .filter(space -> space.getType().equals(SpaceType.PREMIUM))
+                    .findFirst()
+                    .or(() -> spaces.stream().findFirst())
+                    .map(Space::getId)
+                    .orElseThrow(() -> new UpgradeRequiredException(GET_ASSESSMENT_MOVE_TARGETS_NO_SPACE_AVAILABLE)) // can't happen
+            ));
     }
 }
