751201e3afe7ee2b5bab46f7ef93ddafc567f4fa
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-08-20 07:32:34 +0330

    FAC-1231: Add methods to create spaces with specified owner in SpaceMother for enhanced test flexibility

--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/test/fixture/application/SpaceMother.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/test/fixture/application/SpaceMother.java
@@ -11,27 +11,39 @@ public class SpaceMother {
     static long id = 0;
 
     public static Space createBasicSpace() {
+        return createBasicSpaceWithOwner(UUID.randomUUID());
+    }
+
+    public static Space createBasicSpaceWithOwner(UUID ownerId) {
         return new Space(++id,
             "title",
-            UUID.randomUUID(),
+            ownerId,
             SpaceType.BASIC,
             false,
             LocalDateTime.now());
     }
 
     public static Space createPremiumSpace() {
+        return createPremiumSpaceWithOwner(UUID.randomUUID());
+    }
+
+    public static Space createPremiumSpaceWithOwner(UUID ownerId) {
         return new Space(++id,
             "title",
-            UUID.randomUUID(),
+            ownerId,
             SpaceType.PREMIUM,
             false,
             LocalDateTime.now());
     }
 
     public static Space createDefaultSpace() {
+        return createDefaultSpaceWithOwner(UUID.randomUUID());
+    }
+
+    public static Space createDefaultSpaceWithOwner(UUID ownerId) {
         return new Space(++id,
             "title",
-            UUID.randomUUID(),
+            ownerId,
             SpaceType.BASIC,
             true,
             LocalDateTime.now());
