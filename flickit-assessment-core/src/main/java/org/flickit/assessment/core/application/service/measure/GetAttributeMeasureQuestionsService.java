0867e87eff3b68812bf903e2c6bf686520d26770
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-08-23 21:38:39 +0330

    FAC-1234: Add an authorization check to GetAttributeMeasureQuestionsService and corresponding unit test

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/measure/GetAttributeMeasureQuestionsService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/measure/GetAttributeMeasureQuestionsService.java
@@ -1,17 +1,26 @@
 package org.flickit.assessment.core.application.service.measure;
 
 import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.core.application.port.in.measure.GetAttributeMeasureQuestionsUseCase;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.VIEW_ATTRIBUTE_MEASURE_QUESTIONS;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class GetAttributeMeasureQuestionsService implements GetAttributeMeasureQuestionsUseCase {
 
+    private final AssessmentAccessChecker assessmentAccessChecker;
+
     @Override
     public Result getAttributeMeasureQuestions(Param param) {
+        if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ATTRIBUTE_MEASURE_QUESTIONS))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
         return null;
     }
 }
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/measure/GetAttributeMeasureQuestionsServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/measure/GetAttributeMeasureQuestionsServiceTest.java
@@ -1,16 +1,42 @@
 package org.flickit.assessment.core.application.service.measure;
 
+import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.core.application.port.in.measure.GetAttributeMeasureQuestionsUseCase;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.VIEW_ATTRIBUTE_MEASURE_QUESTIONS;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class GetAttributeMeasureQuestionsServiceTest {
+
+    @InjectMocks
+    private GetAttributeMeasureQuestionsService service;
+
+    @Mock
+    private AssessmentAccessChecker assessmentAccessChecker;
+
+    private final GetAttributeMeasureQuestionsUseCase.Param param =
+        createParam(GetAttributeMeasureQuestionsUseCase.Param.ParamBuilder::build);
+
+    @Test
+    void testGetAttributeMeasureQuestions_whenCurrentUserDoesNotHaveRequiredPermission_thenThrowAccessDeniedException() {
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), VIEW_ATTRIBUTE_MEASURE_QUESTIONS))
+            .thenReturn(false);
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.getAttributeMeasureQuestions(param));
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+    }
 
     private GetAttributeMeasureQuestionsUseCase.Param createParam(Consumer<GetAttributeMeasureQuestionsUseCase.Param.ParamBuilder> changer) {
         var paramBuilder = paramBuilder();
