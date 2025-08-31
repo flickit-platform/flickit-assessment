bfa53de98c19f38e61feff7e73473a5823d5f68e
m.khorsandi <khorsandi.mahnaz@gmail.com>
2025-08-31 16:51:45 +0330

    FAC-1227: Add condition to prevent null value

--- flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/service/advicenarration/RefreshAssessmentAdviceService.java
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/service/advicenarration/RefreshAssessmentAdviceService.java
@@ -183,7 +183,7 @@ public class RefreshAssessmentAdviceService implements RefreshAssessmentAdviceUs
         }
 
         List<AdviceListItem> improvableQuestions = createAdviceHelper.createAdvice(result.getAssessmentId(), List.copyOf(attributeTargets));
-        while (improvableQuestions.size() < MIN_REQUIRED_IMPROVABLE_QUESTIONS_SIZE) {
+        while (improvableQuestions.size() < MIN_REQUIRED_IMPROVABLE_QUESTIONS_SIZE && !nonWeakAttributeTargets.isEmpty()) {
             AttributeLevelTarget next = nonWeakAttributeTargets.pollFirst();
             attributeTargets.add(next);
             improvableQuestions = createAdviceHelper.createAdvice(result.getAssessmentId(), List.copyOf(attributeTargets));
