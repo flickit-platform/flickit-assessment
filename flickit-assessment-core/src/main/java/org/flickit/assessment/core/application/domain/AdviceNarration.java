a4daec620def3fd71f9369586e9c3176af8f945a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-08-31 18:13:11 +0330

    FAC-1238: Add `AdviceNarration` domain model with relevant fields

---
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/domain/AdviceNarration.java
@@ -0,0 +1,18 @@
+package org.flickit.assessment.core.application.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class AdviceNarration {
+
+    private final UUID id;
+    private final boolean approved;
+    private final LocalDateTime aiNarrationTime;
+    private final LocalDateTime assessorNarrationTime;
+    private final UUID createdBy;
+}
