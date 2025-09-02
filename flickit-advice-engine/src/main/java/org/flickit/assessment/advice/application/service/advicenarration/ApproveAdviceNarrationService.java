037a725692b7c5d5bd4c7b1151c68a64f76098a9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-09-02 11:10:17 +0330

    FAC-1239: Add ApproveAdviceNarrationService and ApproveAdviceNarrationServiceTest

---
+++ flickit-advice-engine/src/main/java/org/flickit/assessment/advice/application/service/advicenarration/ApproveAdviceNarrationService.java
@@ -0,0 +1,29 @@
+package org.flickit.assessment.advice.application.service.advicenarration;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.advice.application.port.in.advicenarration.ApproveAdviceNarrationUseCase;
+import org.flickit.assessment.advice.application.port.out.advicenarration.UpdateAdviceNarrationPort;
+import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.springframework.stereotype.Service;
+
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.APPROVE_ADVICE_NARRATION;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ApproveAdviceNarrationService implements ApproveAdviceNarrationUseCase {
+
+    private final AssessmentAccessChecker assessmentAccessChecker;
+    private final UpdateAdviceNarrationPort updateAdviceNarrationPort;
+
+    @Override
+    public void approveAdviceNarration(Param param) {
+        if (!assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), APPROVE_ADVICE_NARRATION))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
+
+        updateAdviceNarrationPort.approveAdviceNarration(param.getAssessmentId());
+    }
+}
---
+++ flickit-advice-engine/src/test/java/org/flickit/assessment/advice/application/service/advicenarration/ApproveAdviceNarrationServiceTest.java
@@ -0,0 +1,67 @@
+package org.flickit.assessment.advice.application.service.advicenarration;
+
+import org.flickit.assessment.advice.application.port.in.advicenarration.ApproveAdviceNarrationUseCase;
+import org.flickit.assessment.advice.application.port.out.advicenarration.UpdateAdviceNarrationPort;
+import org.flickit.assessment.common.application.domain.assessment.AssessmentAccessChecker;
+import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+import java.util.function.Consumer;
+
+import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.APPROVE_ADVICE_NARRATION;
+import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ApproveAdviceNarrationServiceTest {
+
+    @InjectMocks
+    private ApproveAdviceNarrationService service;
+
+    @Mock
+    private AssessmentAccessChecker assessmentAccessChecker;
+
+    @Mock
+    private UpdateAdviceNarrationPort updateAdviceNarrationPort;
+
+    private final ApproveAdviceNarrationUseCase.Param param = createParam(ApproveAdviceNarrationUseCase.Param.ParamBuilder::build);
+
+    @Test
+    void testApproveAdviceNarration_whenCurrentUserDoesNotHaveRequiredPermission_thenThrowAccessDeniedException() {
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), APPROVE_ADVICE_NARRATION))
+            .thenReturn(false);
+
+        var throwable = assertThrows(AccessDeniedException.class, () -> service.approveAdviceNarration(param));
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
+
+        verifyNoInteractions(updateAdviceNarrationPort);
+    }
+
+    @Test
+    void testApproveAdviceNarration_whenParamsAreValid_thenSuccessfulApprove() {
+        when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), APPROVE_ADVICE_NARRATION))
+            .thenReturn(true);
+
+        service.approveAdviceNarration(param);
+
+        verify(updateAdviceNarrationPort).approveAdviceNarration(param.getAssessmentId());
+    }
+
+    private ApproveAdviceNarrationUseCase.Param createParam(Consumer<ApproveAdviceNarrationUseCase.Param.ParamBuilder> changer) {
+        var paramBuilder = paramBuilder();
+        changer.accept(paramBuilder);
+        return paramBuilder.build();
+    }
+
+    private ApproveAdviceNarrationUseCase.Param.ParamBuilder paramBuilder() {
+        return ApproveAdviceNarrationUseCase.Param.builder()
+            .assessmentId(UUID.randomUUID())
+            .currentUserId(UUID.randomUUID());
+    }
+}
