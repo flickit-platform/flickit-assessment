b428e04717f86b618c7d4b75ea792b8eea1faf2a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-09-09 09:02:03 +0330

    FAC-1250: Add `loadRoleItem` to LoadUserRoleForAssessmentPort for loading RoleItem and implement it

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/assessmentuserrole/AssessmentUserRoleMapper.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/adapter/out/persistence/assessmentuserrole/AssessmentUserRoleMapper.java
@@ -2,6 +2,7 @@ package org.flickit.assessment.core.adapter.out.persistence.assessmentuserrole;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.flickit.assessment.core.application.domain.AssessmentUserRole;
 import org.flickit.assessment.core.application.domain.AssessmentUserRoleItem;
 import org.flickit.assessment.data.jpa.core.assessmentuserrole.AssessmentUserRoleJpaEntity;
 
@@ -14,5 +15,13 @@ public class AssessmentUserRoleMapper {
             item.getRole().getId(),
             item.getCreatedBy(),
             item.getCreationTime());
+    }
+
+    public static AssessmentUserRoleItem mapToRoleItem(AssessmentUserRoleJpaEntity jpaEntity) {
+        return new AssessmentUserRoleItem(jpaEntity.getAssessmentId(),
+            jpaEntity.getUserId(),
+            AssessmentUserRole.valueOfById(jpaEntity.getRoleId()),
+            jpaEntity.getCreatedBy(),
+            jpaEntity.getCreationTime());
     }
 }
