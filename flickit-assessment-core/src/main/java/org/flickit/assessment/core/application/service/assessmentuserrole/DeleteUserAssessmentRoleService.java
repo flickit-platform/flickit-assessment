82e870ec4ce66f8b5fe27469698065dfdbe382e9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-09-14 13:23:50 +0330

    FAC-1250: Refactor Service and ServiceTest with correct logic

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/DeleteUserAssessmentRoleService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessmentuserrole/DeleteUserAssessmentRoleService.java
@@ -3,6 +3,7 @@ package org.flickit.assessment.core.application.service.assessmentuserrole;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.application.domain.assessment.AssessmentPermissionChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.common.exception.ValidationException;
 import org.flickit.assessment.core.application.domain.AssessmentUserRole;
 import org.flickit.assessment.core.application.port.in.assessmentuserrole.DeleteUserAssessmentRoleUseCase;
@@ -17,6 +18,8 @@ import java.util.Objects;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.DELETE_USER_ASSESSMENT_ROLE;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.core.common.ErrorMessageKey.DELETE_ASSESSMENT_USER_ROLE_ASSESSMENT_ROLE_NOT_FOUND;
+import static org.flickit.assessment.core.common.ErrorMessageKey.DELETE_ASSESSMENT_USER_ROLE_USER_ID_IS_SPACE_OWNER;
 
 @Service
 @Transactional
@@ -31,25 +34,25 @@ public class DeleteUserAssessmentRoleService implements DeleteUserAssessmentRole
 
     @Override
     public void deleteAssessmentUserRole(Param param) {
-        if (!permissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE))
+        validatePermission(param);
+
+        if (!loadAssessmentPort.isAssessmentSpaceMember(param.getAssessmentId(), param.getCurrentUserId()))
             throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
         var spaceOwnerId = loadSpaceOwnerPort.loadOwnerId(param.getAssessmentId());
         if (Objects.equals(param.getUserId(), spaceOwnerId))
-            throw new ValidationException(org.flickit.assessment.core.common.ErrorMessageKey.DELETE_ASSESSMENT_USER_ROLE_USER_ID_IS_SPACE_OWNER);
-
-        validateUserRoleDeletionRights(param);
+            throw new ValidationException(DELETE_ASSESSMENT_USER_ROLE_USER_ID_IS_SPACE_OWNER);
 
         deleteUserAssessmentRolePort.delete(param.getAssessmentId(), param.getUserId());
     }
 
-    private void validateUserRoleDeletionRights(Param param) {
-        if (!loadAssessmentPort.isAssessmentSpaceMember(param.getAssessmentId(), param.getCurrentUserId())) {
-            loadUserRoleForAssessmentPort.loadRoleItem(param.getAssessmentId(), param.getUserId())
-                .filter(userRoleItem ->
-                    userRoleItem.getRole().equals(AssessmentUserRole.REPORT_VIEWER)
-                        && param.getCurrentUserId().equals(userRoleItem.getCreatedBy()))
-                .orElseThrow(() -> new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED));
+    void validatePermission(Param param) {
+        var hasAccess = permissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE);
+        if (!hasAccess) {
+            var userRoleItem = loadUserRoleForAssessmentPort.loadRoleItem(param.getAssessmentId(), param.getUserId())
+                .orElseThrow(() -> new ResourceNotFoundException(DELETE_ASSESSMENT_USER_ROLE_ASSESSMENT_ROLE_NOT_FOUND));
+            if (!userRoleItem.getCreatedBy().equals(param.getCurrentUserId()) || !userRoleItem.getRole().equals(AssessmentUserRole.REPORT_VIEWER))
+                throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
         }
     }
 }
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessmentuserrole/DeleteUserAssessmentRoleServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessmentuserrole/DeleteUserAssessmentRoleServiceTest.java
@@ -2,6 +2,7 @@ package org.flickit.assessment.core.application.service.assessmentuserrole;
 
 import org.flickit.assessment.common.application.domain.assessment.AssessmentPermissionChecker;
 import org.flickit.assessment.common.exception.AccessDeniedException;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.common.exception.ValidationException;
 import org.flickit.assessment.core.application.domain.AssessmentUserRole;
 import org.flickit.assessment.core.application.domain.AssessmentUserRoleItem;
@@ -23,6 +24,7 @@ import java.util.function.Consumer;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.DELETE_USER_ASSESSMENT_ROLE;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.core.common.ErrorMessageKey.DELETE_ASSESSMENT_USER_ROLE_ASSESSMENT_ROLE_NOT_FOUND;
 import static org.flickit.assessment.core.common.ErrorMessageKey.DELETE_ASSESSMENT_USER_ROLE_USER_ID_IS_SPACE_OWNER;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -53,83 +55,90 @@ class DeleteUserAssessmentRoleServiceTest {
         new AssessmentUserRoleItem(param.getAssessmentId(), param.getUserId(), AssessmentUserRole.REPORT_VIEWER, param.getCurrentUserId(), LocalDateTime.now());
 
     @Test
-    void testDeleteAssessmentUserRole_whenCurrentUserDoesNotHaveRequiredPermission_thenThrowAccessDenied() {
+    void testDeleteAssessmentUserRole_whenCurrentUserDoesNotHaveRequiredPermissionAndUserRoleIsNotReportViewer_thenThrowAccessDenied() {
+        assessmentUserRoleItem =
+            new AssessmentUserRoleItem(param.getAssessmentId(), param.getUserId(), AssessmentUserRole.MANAGER, param.getCurrentUserId(), LocalDateTime.now());
+
         when(assessmentPermissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE))
             .thenReturn(false);
+        when(loadUserRoleForAssessmentPort.loadRoleItem(param.getAssessmentId(), param.getUserId())).thenReturn(Optional.ofNullable(assessmentUserRoleItem));
+
+        var exception = assertThrows(AccessDeniedException.class, () -> service.deleteAssessmentUserRole(param));
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, exception.getMessage());
+
+        verifyNoInteractions(loadAssessmentPort,
+            loadSpaceOwnerPort,
+            deleteUserAssessmentRolePort);
+    }
+
+    @Test
+    void testDeleteAssessmentUserRole_whenUserAssessmentRoleNotFound_thenThrowResourceNotFound() {
+        when(assessmentPermissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE))
+            .thenReturn(false);
+        when(loadUserRoleForAssessmentPort.loadRoleItem(param.getAssessmentId(), param.getUserId())).thenReturn(Optional.empty());
+
+        var exception = assertThrows(ResourceNotFoundException.class, () -> service.deleteAssessmentUserRole(param));
+        assertEquals(DELETE_ASSESSMENT_USER_ROLE_ASSESSMENT_ROLE_NOT_FOUND, exception.getMessage());
+
+        verifyNoInteractions(loadAssessmentPort,
+            loadSpaceOwnerPort,
+            deleteUserAssessmentRolePort);
+    }
+
+    @Test
+    void testDeleteAssessmentUserRole_whenCurrentUserDoesNotHaveRequiredPermissionAndIsNotUserInviter_thenThrowAccessDenied() {
+        assessmentUserRoleItem =
+            new AssessmentUserRoleItem(param.getAssessmentId(), param.getUserId(), AssessmentUserRole.REPORT_VIEWER, UUID.randomUUID(), LocalDateTime.now());
+
+        when(assessmentPermissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE))
+            .thenReturn(false);
+        when(loadUserRoleForAssessmentPort.loadRoleItem(param.getAssessmentId(), param.getUserId())).thenReturn(Optional.ofNullable(assessmentUserRoleItem));
+
+        var exception = assertThrows(AccessDeniedException.class, () -> service.deleteAssessmentUserRole(param));
+        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, exception.getMessage());
+
+        verifyNoInteractions(loadAssessmentPort,
+            loadSpaceOwnerPort,
+            deleteUserAssessmentRolePort);
+    }
+
+    @Test
+    void testDeleteAssessmentUserRole_whenCurrentUserIsNotSpaceMember_thenThrowAccessDenied() {
+        when(assessmentPermissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE))
+            .thenReturn(true);
+        when(loadAssessmentPort.isAssessmentSpaceMember(param.getAssessmentId(), param.getCurrentUserId())).thenReturn(false);
 
         var exception = assertThrows(AccessDeniedException.class, () -> service.deleteAssessmentUserRole(param));
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, exception.getMessage());
 
         verifyNoInteractions(loadUserRoleForAssessmentPort,
-            loadAssessmentPort,
+            loadSpaceOwnerPort,
             deleteUserAssessmentRolePort);
     }
 
     @Test
-    void testDeleteAssessmentUserRole_whenCurrentUserIsNotSpaceMemberAndIsNotInviter_thenThrowAccessDenied() {
-        assessmentUserRoleItem = new AssessmentUserRoleItem(param.getAssessmentId(), param.getUserId(), AssessmentUserRole.REPORT_VIEWER, UUID.randomUUID(), LocalDateTime.now());
-
+    void testDeleteAssessmentUserRole_whenUserIsSpaceOwner_thenThrowValidationException() {
         when(assessmentPermissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE))
             .thenReturn(true);
-        when(loadUserRoleForAssessmentPort.loadRoleItem(param.getAssessmentId(), param.getUserId())).thenReturn(Optional.of(assessmentUserRoleItem));
-        when(loadAssessmentPort.isAssessmentSpaceMember(param.getAssessmentId(), param.getCurrentUserId())).thenReturn(false);
-
-        var exception = assertThrows(AccessDeniedException.class, () -> service.deleteAssessmentUserRole(param));
-        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, exception.getMessage());
-
-        verifyNoInteractions(deleteUserAssessmentRolePort);
-    }
-
-    @Test
-    void testDeleteAssessmentUserRole_whenRoleIsNotReportViewer_thenThrowAccessDenied() {
-        assessmentUserRoleItem = new AssessmentUserRoleItem(param.getAssessmentId(), param.getUserId(), AssessmentUserRole.ASSESSOR, param.getCurrentUserId(), LocalDateTime.now());
-
-        when(assessmentPermissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE))
-            .thenReturn(true);
-        when(loadUserRoleForAssessmentPort.loadRoleItem(param.getAssessmentId(), param.getUserId())).thenReturn(Optional.of(assessmentUserRoleItem));
-        when(loadAssessmentPort.isAssessmentSpaceMember(param.getAssessmentId(), param.getCurrentUserId())).thenReturn(false);
-
-        var exception = assertThrows(AccessDeniedException.class, () -> service.deleteAssessmentUserRole(param));
-        assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, exception.getMessage());
-
-        verify(assessmentPermissionChecker).isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE);
-        verifyNoInteractions(deleteUserAssessmentRolePort);
-    }
-
-    @Test
-    void testUpdateAssessmentUserRole_whenUserIsSpaceOwner_thenThrowValidationException() {
-        when(assessmentPermissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE))
-            .thenReturn(true);
+        when(loadAssessmentPort.isAssessmentSpaceMember(param.getAssessmentId(), param.getCurrentUserId())).thenReturn(true);
         when(loadSpaceOwnerPort.loadOwnerId(param.getAssessmentId())).thenReturn(param.getUserId());
 
         var exception = assertThrows(ValidationException.class, () -> service.deleteAssessmentUserRole(param));
         assertEquals(DELETE_ASSESSMENT_USER_ROLE_USER_ID_IS_SPACE_OWNER, exception.getMessageKey());
 
-        verifyNoInteractions(deleteUserAssessmentRolePort);
+        verifyNoInteractions(loadUserRoleForAssessmentPort, deleteUserAssessmentRolePort);
     }
 
     @Test
     void testDeleteAssessmentUserRole_whenParametersAreValidAndCurrentUserIsSpaceMember_thenSuccessfulDelete() {
         when(assessmentPermissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE))
-            .thenReturn(true);
-        when(loadAssessmentPort.isAssessmentSpaceMember(param.getAssessmentId(), param.getCurrentUserId())).thenReturn(true);
-
-        assertDoesNotThrow(() -> service.deleteAssessmentUserRole(param));
-
-        verify(assessmentPermissionChecker).isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE);
-        verify(deleteUserAssessmentRolePort).delete(param.getAssessmentId(), param.getUserId());
-    }
-
-    @Test
-    void testDeleteAssessmentUserRole_whenParametersAreValidAndCurrentUserIsNotSpaceMemberButInviter_thenSuccessfulDelete() {
-        when(assessmentPermissionChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE))
-            .thenReturn(true);
-        when(loadAssessmentPort.isAssessmentSpaceMember(param.getAssessmentId(), param.getCurrentUserId())).thenReturn(false);
+            .thenReturn(false);
         when(loadUserRoleForAssessmentPort.loadRoleItem(param.getAssessmentId(), param.getUserId())).thenReturn(Optional.of(assessmentUserRoleItem));
+        when(loadAssessmentPort.isAssessmentSpaceMember(param.getAssessmentId(), param.getCurrentUserId())).thenReturn(true);
+        when(loadSpaceOwnerPort.loadOwnerId(param.getAssessmentId())).thenReturn(UUID.randomUUID());
 
-        assertDoesNotThrow(() -> service.deleteAssessmentUserRole(param));
+        service.deleteAssessmentUserRole(param);
 
-        verify(assessmentPermissionChecker).isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), DELETE_USER_ASSESSMENT_ROLE);
         verify(deleteUserAssessmentRolePort).delete(param.getAssessmentId(), param.getUserId());
     }
 
