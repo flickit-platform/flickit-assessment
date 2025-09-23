c25dc52ef94752f35b1ff9511c095223f4a4c8d6
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-09-23 20:44:23 +0330

    FAC-1248: Add  `convert` method to KitDslPersistenceJpaAdapter [WIP]

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kitdsl/KitDslPersistenceJpaAdapter.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kitdsl/KitDslPersistenceJpaAdapter.java
@@ -1,19 +1,23 @@
 package org.flickit.assessment.kit.adapter.out.persistence.kitdsl;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.*;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.data.jpa.kit.assessmentkitdsl.KitDslJpaEntity;
 import org.flickit.assessment.data.jpa.kit.assessmentkitdsl.KitDslJpaRepository;
 import org.flickit.assessment.data.jpa.kit.seq.KitDbSequenceGenerators;
+import org.flickit.assessment.kit.application.domain.dsl.AssessmentKitDslModel;
 import org.flickit.assessment.kit.application.port.out.kitdsl.CreateKitDslPort;
 import org.flickit.assessment.kit.application.port.out.kitdsl.LoadDslFilePathPort;
 import org.flickit.assessment.kit.application.port.out.kitdsl.LoadDslJsonPathPort;
 import org.flickit.assessment.kit.application.port.out.kitdsl.UpdateKitDslPort;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static org.flickit.assessment.kit.adapter.out.persistence.kitdsl.KitDslMapper.toJpaEntity;
 import static org.flickit.assessment.kit.common.ErrorMessageKey.CREATE_KIT_BY_DSL_KIT_DSL_NOT_FOUND;
@@ -34,6 +38,34 @@ public class KitDslPersistenceJpaAdapter implements
         var entity = toJpaEntity(dslFilePath, jsonFilePath, createdBy);
         entity.setId(sequenceGenerators.generateKitDslId());
         return repository.save(entity).getId();
+    }
+
+    @Override
+    public AssessmentKitDslModel convert(MultipartFile excelFile) {
+        ExcelToDslModelConverter converter = new ExcelToDslModelConverter();
+        try (InputStream is = excelFile.getInputStream();
+             Workbook workbook = WorkbookFactory.create(is)) {
+
+            var qualityAttributes = workbook.getSheet("QualityAttributes");
+
+            var subjects = converter.convertSubjects(qualityAttributes);
+            System.out.println(subjects);
+            var attributes = converter.convertAttributes(qualityAttributes);
+            System.out.println(attributes);
+            var Questionnaires = converter.convertQuestionnaires(workbook.getSheet("Questionnaires"));
+            System.out.println(Questionnaires);
+            var questions = converter.convertQuestions(workbook.getSheet("Questions"));
+            System.out.println(questions);
+            var sheet2 = workbook.getSheet("sheet2");
+            var sheet3 = workbook.getSheet("sheet3");
+            var sheet4 = workbook.getSheet("sheet4");
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
     }
 
     @Override
--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/kitdsl/CreateKitDslPort.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/out/kitdsl/CreateKitDslPort.java
@@ -1,9 +1,13 @@
 package org.flickit.assessment.kit.application.port.out.kitdsl;
 
+import org.flickit.assessment.kit.application.domain.dsl.AssessmentKitDslModel;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.UUID;
 
 public interface CreateKitDslPort {
 
     Long create(String dslFilePath, String jsonFilePath, UUID createdBy);
 
+    AssessmentKitDslModel convert(MultipartFile excelFile);
 }
