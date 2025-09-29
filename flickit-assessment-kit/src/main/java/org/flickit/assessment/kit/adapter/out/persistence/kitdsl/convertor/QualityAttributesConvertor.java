c4f768d40ae50a390211bc84915bf61c94acca14
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-09-29 11:14:39 +0330

    FAC-1248: AddQualityAttributesConvertor

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kitdsl/convertor/QualityAttributesConvertor.java
@@ -0,0 +1,88 @@
+package org.flickit.assessment.kit.adapter.out.persistence.kitdsl.convertor;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.flickit.assessment.kit.application.domain.dsl.AttributeDslModel;
+import org.flickit.assessment.kit.application.domain.dsl.QuestionnaireDslModel;
+import org.flickit.assessment.kit.application.domain.dsl.SubjectDslModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.flickit.assessment.kit.adapter.out.persistence.kitdsl.convertor.ExcelToDslModelConverter.*;
+
+public class QualityAttributesConvertor {
+
+    private static final String SUBJECT_NAME = "Subject Name";
+    private static final String SUBJECT_TITLE = "Subject Title";
+    private static final String SUBJECT_WEIGHT = "Subject Weight";
+    private static final String SUBJECT_DESCRIPTION = "Subject Description";
+
+    static final int SUBJECT_HEADER_ROW_NUM = 0;
+    static final int SUBJECT_HEADER_START_COL = 0;
+    static final int SUBJECT_HEADER_END_COL = 3;
+    static final int SUBJECT_DATA_START_ROW = 1;
+
+    private static final String ATTRIBUTE_NAME = "Attribute Name";
+    private static final String ATTRIBUTE_TITLE = "Attribute Title";
+    private static final String ATTRIBUTE_WEIGHT = "Attribute Weight";
+    private static final String ATTRIBUTE_DESCRIPTION = "Attribute Description";
+
+    static final int ATTRIBUTE_HEADER_ROW_NUM = 0;
+    static final int ATTRIBUTE_HEADER_START_COL = 0;
+    static final int ATTRIBUTE_HEADER_END_COL = 7;
+    static final int ATTRIBUTE_DATA_START_ROW = 1;
+
+    static List<SubjectDslModel> convertSubjects(Sheet sheet) {
+        var columnMap = getSheetHeader(sheet, SUBJECT_HEADER_ROW_NUM, SUBJECT_HEADER_START_COL, SUBJECT_HEADER_END_COL);
+
+        List<Row> validRows = IntStream.range(SUBJECT_DATA_START_ROW, sheet.getLastRowNum() + SUBJECT_DATA_START_ROW)
+            .mapToObj(sheet::getRow)
+            .filter(row -> {
+                String code = getCellString(row, columnMap.get(SUBJECT_NAME));
+                return !ExcelToDslModelConverter.isBlankRow(row) && code != null && !code.isBlank();
+            })
+            .toList();
+
+        return IntStream.range(0, validRows.size())
+            .mapToObj(idx -> {
+                Row row = validRows.get(idx);
+                return SubjectDslModel.builder()
+                    .code(getCellString(row, columnMap.get(SUBJECT_NAME)))
+                    .title(getCellString(row, columnMap.get(SUBJECT_TITLE)))
+                    .weight(getCellInteger(row, columnMap.get(SUBJECT_WEIGHT)))
+                    .description(getCellString(row, columnMap.get(SUBJECT_DESCRIPTION)))
+                    .index(idx + 1)
+                    .build();
+            })
+            .collect(Collectors.toList());
+
+    }
+
+    static List<AttributeDslModel> convertAttributes(Sheet sheet) {
+        var columnMap = getSheetHeader(sheet, ATTRIBUTE_HEADER_ROW_NUM, ATTRIBUTE_HEADER_START_COL, ATTRIBUTE_HEADER_END_COL);
+
+        return IntStream.rangeClosed(ATTRIBUTE_DATA_START_ROW, sheet.getLastRowNum())
+            .mapToObj(sheet::getRow)
+            .filter(row -> !ExcelToDslModelConverter.isBlankRow(row))
+            .collect(ArrayList::new, (list, row) -> {
+                String subjectCode = getCellString(row, columnMap.get(SUBJECT_NAME));
+                String effectiveSubjectCode = (subjectCode != null && !subjectCode.isBlank())
+                    ? subjectCode
+                    : list.isEmpty() ? null : list.getLast().getSubjectCode();
+
+                AttributeDslModel attribute = AttributeDslModel.builder()
+                    .subjectCode(effectiveSubjectCode)
+                    .code(getCellString(row, columnMap.get(ATTRIBUTE_NAME)))
+                    .weight(getCellInteger(row, columnMap.get(ATTRIBUTE_WEIGHT)))
+                    .title(getCellString(row, columnMap.get(ATTRIBUTE_TITLE)))
+                    .description(getCellString(row, columnMap.get(ATTRIBUTE_DESCRIPTION)))
+                    .index(list.size() + 1)
+                    .build();
+
+                list.add(attribute);
+            }, List::addAll);
+    }
+}
