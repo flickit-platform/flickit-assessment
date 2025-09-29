eb03d8b4ce2b63637be1257a4e36eaa0978fff17
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-09-29 11:38:48 +0330

    FAC-1248: add FAC-1248: Add QuestionnairesConvertor [WIP]

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/out/persistence/kitdsl/convertor/QuestionsConvertor.java
@@ -0,0 +1,75 @@
+package org.flickit.assessment.kit.adapter.out.persistence.kitdsl.convertor;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.flickit.assessment.kit.application.domain.dsl.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.flickit.assessment.kit.adapter.out.persistence.kitdsl.convertor.ExcelToDslModelConverter.*;
+
+public class QuestionsConvertor {
+
+    static final int HEADER_ROW_NUM = 1;
+    static final int HEADER_START_COL = 0;
+    static final int HEADER_END_COL = 8;
+    static final int DATA_START_ROW = 2;
+
+    private static final String QUESTION_QUESTION = "Question";
+    private static final String QUESTION_QUESTIONNAIRES = "Questionnaires";
+    private static final String QUESTION_CODE = "Code";
+    private static final String QUESTION_OPTIONS = "Options";
+    private static final String QUESTION_DESCRIPTION = "Description";
+    private static final String QUESTION_NOT_APPLICABLE = "Not Applicable";
+    private static final String QUESTION_ADVISABLE = "Advisable";
+    private static final String QUESTION_MATURITY = "Maturity";
+
+    static List<QuestionDslModel> convert(Sheet sheet, Map<String, List<AnswerOptionDslModel>> answerRangeCodeToAnswerOptionsMap,
+                                          Map<String, MaturityLevelDslModel> maturityLevelCodeToMaturityLevelDslMap, List<AttributeDslModel> attributeDslModels) {
+
+
+        var columnMap = getSheetHeader(sheet, HEADER_ROW_NUM, HEADER_START_COL, HEADER_END_COL);
+
+        return IntStream.rangeClosed(DATA_START_ROW, sheet.getLastRowNum())
+            .filter(i -> !isBlankRow(sheet.getRow(i)))
+            .mapToObj(i -> {
+                Row row = sheet.getRow(i);
+                String options = getCellString(row, columnMap.get(QUESTION_OPTIONS));
+                String attributeCode;
+                QuestionImpactDslModel questionImpact = null;
+                List<QuestionImpactDslModel> questionImpacts = new ArrayList<>();
+
+                for (int j = 0; j < attributeDslModels.size(); j++) {
+                    Integer weight = getCellInteger(row, HEADER_END_COL + j);
+                    if (weight != null) {
+                        attributeCode = attributeDslModels.get(j).getCode();
+                        questionImpact = QuestionImpactDslModel.builder()
+                            .attributeCode(attributeCode)
+                            .maturityLevel(maturityLevelCodeToMaturityLevelDslMap.get(getCellString(row, columnMap.get(QUESTION_MATURITY))))
+                            .weight(weight)
+                            .build();
+                    }
+                    questionImpacts.add(questionImpact);
+
+                }
+
+                return QuestionDslModel.builder()
+                    .title(getCellString(row, columnMap.get(QUESTION_QUESTION)))
+                    .questionnaireCode(getCellString(row, columnMap.get(QUESTION_QUESTIONNAIRES)))
+                    .code(getCellString(row, columnMap.get(QUESTION_CODE)))
+                    .answerRangeCode(options)
+                    .answerOptions(answerRangeCodeToAnswerOptionsMap.get(options))
+                    .index(i - 1)
+                    .questionImpacts(questionImpacts)
+                    .description(getCellString(row, columnMap.get(QUESTION_DESCRIPTION)))
+                    .mayNotBeApplicable(getCellInteger(row, columnMap.get(QUESTION_NOT_APPLICABLE)) == 1)
+                    .advisable(getCellInteger(row, columnMap.get(QUESTION_ADVISABLE)) == 1)
+                    .build();
+            })
+            .collect(Collectors.toList());
+    }
+}
