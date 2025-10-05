988dbc2af63182a49e0e84ab7f6d5ff43ceb11c6
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-10-05 14:36:59 +0330

    FAC-1248: Modify ConvertExcelToDslRestController to return zipFile

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/kitdsl/ConvertExcelToDslResponseDto.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/kitdsl/ConvertExcelToDslResponseDto.java
@@ -1,6 +1,4 @@
 package org.flickit.assessment.kit.adapter.in.rest.kitdsl;
 
-import org.flickit.assessment.kit.application.domain.dsl.AssessmentKitDslModel;
-
-public record ConvertExcelToDslResponseDto(AssessmentKitDslModel model) {
+public record ConvertExcelToDslResponseDto(byte[] zipFile) {
 }
--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/kitdsl/ConvertExcelToDslRestController.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/kitdsl/ConvertExcelToDslRestController.java
@@ -3,8 +3,7 @@ package org.flickit.assessment.kit.adapter.in.rest.kitdsl;
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.config.jwt.UserContext;
 import org.flickit.assessment.kit.application.port.in.kitdsl.ConvertExcelToDslUseCase;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,11 +20,26 @@ public class ConvertExcelToDslRestController {
     ResponseEntity<ConvertExcelToDslResponseDto> convertExcelToDsl(@RequestParam MultipartFile excelFile) {
         var currentUserId = userContext.getUser().id();
         var result = useCase.convertExcelToDsl(toParam(excelFile, currentUserId));
+        HttpHeaders headers = createHttpHeaders(result.fileName());
 
-        return new ResponseEntity<>(new ConvertExcelToDslResponseDto(result), HttpStatus.OK);
+        return new ResponseEntity<>(toResponse(result.file()), headers, HttpStatus.OK);
     }
 
     private ConvertExcelToDslUseCase.Param toParam(MultipartFile excelFile, UUID currentUserId) {
         return new ConvertExcelToDslUseCase.Param(excelFile, currentUserId);
+    }
+
+    private static HttpHeaders createHttpHeaders(String fileName) {
+        HttpHeaders headers = new HttpHeaders();
+        ContentDisposition contentDisposition = ContentDisposition
+            .attachment()
+            .filename(fileName)
+            .build();
+        headers.setContentDisposition(contentDisposition);
+        return headers;
+    }
+
+    private ConvertExcelToDslResponseDto toResponse(byte[] file) {
+        return new ConvertExcelToDslResponseDto(file);
     }
 }
