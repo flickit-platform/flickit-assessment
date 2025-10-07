636762f15f9d2e5decc19538c00adf808125b750
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-10-07 14:20:19 +0330

    FAC-1267: Add `attributes` to subjects of GetKitDetailUseCase

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/assessmentkit/GetKitDetailServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/assessmentkit/GetKitDetailServiceTest.java
@@ -2,6 +2,7 @@ package org.flickit.assessment.kit.application.service.assessmentkit;
 
 import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.kit.application.domain.Attribute;
 import org.flickit.assessment.kit.application.domain.MaturityLevel;
 import org.flickit.assessment.kit.application.domain.Questionnaire;
 import org.flickit.assessment.kit.application.domain.Subject;
@@ -13,10 +14,7 @@ import org.flickit.assessment.kit.application.port.out.expertgroupaccess.CheckEx
 import org.flickit.assessment.kit.application.port.out.maturitylevel.LoadMaturityLevelsPort;
 import org.flickit.assessment.kit.application.port.out.questionnaire.LoadQuestionnairesPort;
 import org.flickit.assessment.kit.application.port.out.subject.LoadSubjectsPort;
-import org.flickit.assessment.kit.test.fixture.application.ExpertGroupMother;
-import org.flickit.assessment.kit.test.fixture.application.MaturityLevelMother;
-import org.flickit.assessment.kit.test.fixture.application.QuestionnaireMother;
-import org.flickit.assessment.kit.test.fixture.application.SubjectMother;
+import org.flickit.assessment.kit.test.fixture.application.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -57,7 +55,7 @@ class GetKitDetailServiceTest {
     private LoadActiveKitVersionIdPort loadActiveKitVersionIdPort;
 
     @Test
-    void testGetKitDetail_WhenKitExist_shouldReturnKitDetails() {
+    void testGetKitDetail_whenKitExist_thenShouldReturnKitDetails() {
         var expertGroup = ExpertGroupMother.createExpertGroup();
         var currentUserId = expertGroup.getOwnerId();
         var kitVersionId = 1L;
@@ -66,7 +64,10 @@ class GetKitDetailServiceTest {
         List<MaturityLevel> maturityLevels = List.of(
             MaturityLevelMother.levelOne(),
             MaturityLevelMother.levelTwo());
-        List<Subject> subjects = List.of(SubjectMother.subjectWithTitle("subject1"));
+        Attribute attributeWithIndex1 = AttributeMother.attributeWithTitle("attributeWithIndex1");
+        Attribute attributeWithIndex2 = AttributeMother.attributeWithTitle("attributeWithIndex2");
+        List<Attribute> attributes = List.of(attributeWithIndex2, attributeWithIndex1);
+        List<Subject> subjects = List.of(SubjectMother.subjectWithAttributes("subject1", attributes));
         List<Questionnaire> questionnaires = List.of(QuestionnaireMother.questionnaireWithTitle("questionnaire1"));
 
         when(loadKitExpertGroupPort.loadKitExpertGroup(param.getKitId())).thenReturn(expertGroup);
@@ -83,10 +84,14 @@ class GetKitDetailServiceTest {
             result.maturityLevels().get(1).competences().size());
         assertEquals(subjects.size(), result.subjects().size());
         assertEquals(questionnaires.size(), result.questionnaires().size());
+        var resultAttributes = result.subjects().getFirst().attributes();
+        assertEquals(2, resultAttributes.size());
+        assertEquals(1, resultAttributes.getFirst().index());
+        assertEquals(2, resultAttributes.getLast().index());
     }
 
     @Test
-    void testGetKitDetail_WhenKitDoesNotExist_ThrowsException() {
+    void testGetKitDetail_whenKitDoesNotExist_thenThrowsException() {
         GetKitDetailUseCase.Param param = new GetKitDetailUseCase.Param(12L, UUID.randomUUID());
 
         when(loadKitExpertGroupPort.loadKitExpertGroup(param.getKitId()))
@@ -97,7 +102,7 @@ class GetKitDetailServiceTest {
     }
 
     @Test
-    void testGetKitDetail_WhenUserIsNotMember_ThrowsException() {
+    void testGetKitDetail_whenUserIsNotMember_thenThrowsException() {
         var expertGroup = ExpertGroupMother.createExpertGroup();
         GetKitDetailUseCase.Param param = new GetKitDetailUseCase.Param(12L, UUID.randomUUID());
 
