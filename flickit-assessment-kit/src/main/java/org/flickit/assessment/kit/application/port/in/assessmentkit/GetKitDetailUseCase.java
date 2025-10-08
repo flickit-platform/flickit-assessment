8a1282495db44eeb22b956fc979620e778425af7
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-10-08 12:28:28 +0330

    FAC-1271: Add KitDetailAnswerRange and KitDetailAnswer to GetKitDetailUseCase

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/in/assessmentkit/GetKitDetailUseCase.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/port/in/assessmentkit/GetKitDetailUseCase.java
@@ -4,9 +4,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.flickit.assessment.common.application.SelfValidating;
+import org.flickit.assessment.common.application.domain.kit.KitLanguage;
+import org.flickit.assessment.common.application.domain.kit.translation.AnswerOptionTranslation;
+import org.flickit.assessment.common.application.domain.kit.translation.AnswerRangeTranslation;
+import org.flickit.assessment.kit.application.domain.AnswerOption;
 import org.flickit.assessment.kit.application.domain.Attribute;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_ID_NOT_NULL;
@@ -37,7 +42,8 @@ public interface GetKitDetailUseCase {
         List<KitDetailMaturityLevel> maturityLevels,
         List<KitDetailSubject> subjects,
         List<KitDetailQuestionnaire> questionnaires,
-        List<KitDetailMeasure> measures) {
+        List<KitDetailMeasure> measures,
+        List<KitDetailAnswerRange> answerRanges) {
     }
 
     record KitDetailMaturityLevel(long id, String title, int index, List<Competences> competences) {
@@ -60,5 +66,15 @@ public interface GetKitDetailUseCase {
     }
 
     record KitDetailMeasure(long id, String title, int index) {
+    }
+
+    record KitDetailAnswerRange(long id, String title,  List<KitDetailAnswer> answers, Map<String, AnswerRangeTranslation> translations) {
+    }
+
+    record KitDetailAnswer(int index, String title, double value, Map<KitLanguage, AnswerOptionTranslation> translations) {
+
+        public static KitDetailAnswer of(AnswerOption answerOption) {
+            return new KitDetailAnswer(answerOption.getIndex(), answerOption.getTitle(), answerOption.getValue(), answerOption.getTranslations());
+        }
     }
 }
