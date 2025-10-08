29cc2fbf4ce00cd897ca9796ee1fbd23198dd1d9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-10-08 12:25:14 +0330

    FAC-1271: Add answerRanges to GetKitDetailResponseDto and Refactor GetKitDetailRestController

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/kitdsl/GetKitDetailResponseDto.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/kitdsl/GetKitDetailResponseDto.java
@@ -1,9 +1,6 @@
 package org.flickit.assessment.kit.adapter.in.rest.kitdsl;
 
-import org.flickit.assessment.kit.application.port.in.assessmentkit.GetKitDetailUseCase.KitDetailMaturityLevel;
-import org.flickit.assessment.kit.application.port.in.assessmentkit.GetKitDetailUseCase.KitDetailMeasure;
-import org.flickit.assessment.kit.application.port.in.assessmentkit.GetKitDetailUseCase.KitDetailQuestionnaire;
-import org.flickit.assessment.kit.application.port.in.assessmentkit.GetKitDetailUseCase.KitDetailSubject;
+import org.flickit.assessment.kit.application.port.in.assessmentkit.GetKitDetailUseCase.*;
 
 import java.util.List;
 
@@ -11,5 +8,6 @@ public record GetKitDetailResponseDto(
     List<KitDetailMaturityLevel> maturityLevels,
     List<KitDetailSubject> subjects,
     List<KitDetailQuestionnaire> questionnaires,
-    List<KitDetailMeasure> measures) {
+    List<KitDetailMeasure> measures,
+    List<KitDetailAnswerRange> answerRanges) {
 }
