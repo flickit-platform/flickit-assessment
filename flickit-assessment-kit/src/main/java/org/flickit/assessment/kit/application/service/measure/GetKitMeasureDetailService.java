ac50a54d15fc4168cdf2e4abc7ddeddb02c6310c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-10-12 16:22:35 +0330

    FAC-1276: Implement service completely

--- flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/measure/GetKitMeasureDetailService.java
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/application/service/measure/GetKitMeasureDetailService.java
@@ -2,18 +2,26 @@ package org.flickit.assessment.kit.application.service.measure;
 
 import lombok.RequiredArgsConstructor;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.kit.application.domain.ExpertGroup;
+import org.flickit.assessment.common.exception.ResourceNotFoundException;
+import org.flickit.assessment.kit.application.domain.*;
 import org.flickit.assessment.kit.application.port.in.measure.GetKitMeasureDetailUseCase;
+import org.flickit.assessment.kit.application.port.out.answeroption.LoadAnswerOptionsPort;
+import org.flickit.assessment.kit.application.port.out.answerrange.LoadAnswerRangesPort;
 import org.flickit.assessment.kit.application.port.out.assessmentkit.LoadActiveKitVersionIdPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadKitExpertGroupPort;
 import org.flickit.assessment.kit.application.port.out.expertgroupaccess.CheckExpertGroupAccessPort;
 import org.flickit.assessment.kit.application.port.out.measure.LoadMeasurePort;
-import org.flickit.assessment.kit.application.port.out.questionnaire.LoadKitQuestionnaireDetailPort;
+import org.flickit.assessment.kit.application.port.out.question.LoadQuestionsPort;
 import org.flickit.assessment.kit.application.port.out.questionnaire.LoadQuestionnairesPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
+import static org.flickit.assessment.kit.common.ErrorMessageKey.GET_KIT_MEASURE_DETAIL_MEASURE_ID_NOT_FOUND;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,14 +29,48 @@ import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT
 public class GetKitMeasureDetailService implements GetKitMeasureDetailUseCase {
 
     private final LoadKitExpertGroupPort loadKitExpertGroupPort;
-    private final LoadQuestionnairesPort loadQuestionnairesPort;
-    private final LoadActiveKitVersionIdPort loadActiveKitVersionIdPort;
     private final CheckExpertGroupAccessPort checkExpertGroupAccessPort;
+    private final LoadActiveKitVersionIdPort loadActiveKitVersionIdPort;
+    private final LoadQuestionsPort loadQuestionsPort;
+    private final LoadQuestionnairesPort loadQuestionnairesPort;
     private final LoadMeasurePort loadMeasurePort;
+    private final LoadAnswerRangesPort loadAnswerRangesPort;
+    private final LoadAnswerOptionsPort loadAnswerOptionsPort;
 
     @Override
     public Result getKitMeasureDetail(Param param) {
-        return null;
+        ExpertGroup expertGroup = loadKitExpertGroupPort.loadKitExpertGroup(param.getKitId());
+        if (!checkExpertGroupAccessPort.checkIsMember(expertGroup.getId(), param.getCurrentUserId()))
+            throw new AccessDeniedException(COMMON_CURRENT_USER_NOT_ALLOWED);
 
+        var kitVersionId = loadActiveKitVersionIdPort.loadKitVersionId(param.getKitId());
+        var measure = loadMeasurePort.load(param.getMeasureId(), kitVersionId)
+            .orElseThrow(() -> new ResourceNotFoundException(GET_KIT_MEASURE_DETAIL_MEASURE_ID_NOT_FOUND));
+        var questions = loadQuestionsPort.loadAllByMeasureIdAndKitVersionId(measure.getId(), kitVersionId);
+
+        return new Result(measure.getTitle(),
+            measure.getDescription(),
+            questions.size(),
+            toKitMeasureQuestions(kitVersionId, questions),
+            measure.getTranslations());
+    }
+
+    private List<MeasureDetailQuestion> toKitMeasureQuestions(long kitVersionId, List<Question> questions) {
+        var questionnaireIdToQuestionnaireMap = loadQuestionnairesPort.loadAllByKitVersionId(kitVersionId).stream()
+            .collect(Collectors.toMap(Questionnaire::getId, Function.identity()));
+        var answerRangeIdToAnswerRangeMap = loadAnswerRangesPort.loadAll(kitVersionId).stream()
+            .collect(Collectors.toMap(AnswerRange::getId, Function.identity()));
+        var answerRangeIdToAnswerOptionsMap = loadAnswerOptionsPort.loadByRangeIds(answerRangeIdToAnswerRangeMap.keySet(), kitVersionId).stream()
+            .collect(Collectors.groupingBy(AnswerOption::getAnswerRangeId));
+
+        return questions.stream()
+            .map(question -> new MeasureDetailQuestion(question.getTitle(),
+                MeasureDetailAnswerRange.of(answerRangeIdToAnswerRangeMap.get(question.getAnswerRangeId())),
+                MeasureDetailQuestionnaire.of(questionnaireIdToQuestionnaireMap.get(question.getQuestionnaireId())),
+                answerRangeIdToAnswerOptionsMap.get(question.getAnswerRangeId()).stream()
+                    .map(Option::of)
+                    .toList())
+            )
+            .toList();
     }
 }
