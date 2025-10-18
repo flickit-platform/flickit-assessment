5f2e1bb61fcf30ab19f2c89153bf8c3bd5e29196
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-10-18 06:42:26 +0330

    FAC-1283: Add Result to LoadAnswerHistoryListPort for returning suitable result instead of AnswerHistory

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/answerhistory/LoadAnswerHistoryListPort.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/port/out/answerhistory/LoadAnswerHistoryListPort.java
@@ -1,11 +1,20 @@
 package org.flickit.assessment.core.application.port.out.answerhistory;
 
 import org.flickit.assessment.common.application.domain.crud.PaginatedResponse;
-import org.flickit.assessment.core.application.domain.AnswerHistory;
+import org.flickit.assessment.core.application.domain.Answer;
+import org.flickit.assessment.core.application.domain.FullUser;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface LoadAnswerHistoryListPort {
 
-    PaginatedResponse<AnswerHistory> load(UUID assessmentId, long questionId, int page, int size);
+    PaginatedResponse<Result> load(UUID assessmentId, long questionId, int page, int size);
+
+    record Result(Answer Answer,
+                  FullUser createdBy,
+                  LocalDateTime creationTime,
+                  Long answerOptionId,
+                  Integer answerOptionIndex) {
+    }
 }
