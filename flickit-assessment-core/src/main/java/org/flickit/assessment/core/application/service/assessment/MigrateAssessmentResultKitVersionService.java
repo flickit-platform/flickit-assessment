e53197622ef718bd177ab45f4e54d5f7b7251c14
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-10-20 10:57:10 +0330

    FAC-1287: Add ability to delete Answer range - Refactor MigrateAssessmentResultKitVersionService to recognize deleted answer ranges in new `kitVersion` (active kit version - Refactor MigrateAssessmentResultKitVersionServiceTest to test deleting answer ranges work correctly

--- flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessment/MigrateAssessmentResultKitVersionService.java
+++ flickit-assessment-core/src/main/java/org/flickit/assessment/core/application/service/assessment/MigrateAssessmentResultKitVersionService.java
@@ -6,11 +6,16 @@ import org.flickit.assessment.common.exception.AccessDeniedException;
 import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.common.exception.ValidationException;
 import org.flickit.assessment.core.application.port.in.assessment.MigrateAssessmentResultKitVersionUseCase;
+import org.flickit.assessment.core.application.port.out.answer.LoadAnswerPort;
+import org.flickit.assessment.core.application.port.out.answerrange.LoadAnswerRangePort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.InvalidateAssessmentResultCalculatePort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.LoadAssessmentResultPort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.UpdateAssessmentResultPort;
+import org.flickit.assessment.core.application.port.out.answer.DeleteAnswerPort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.MIGRATE_KIT_VERSION;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
@@ -25,6 +30,9 @@ public class MigrateAssessmentResultKitVersionService implements MigrateAssessme
     private final LoadAssessmentResultPort loadAssessmentResultPort;
     private final InvalidateAssessmentResultCalculatePort loadAssessmentResultCalculatePort;
     private final UpdateAssessmentResultPort updateAssessmentResultPort;
+    private final LoadAnswerRangePort loadAnswerRangePort;
+    private final LoadAnswerPort loadAnswerPort;
+    private final DeleteAnswerPort deleteAnswerPort;
 
     @Override
     public void migrateKitVersion(Param param) {
@@ -37,6 +45,16 @@ public class MigrateAssessmentResultKitVersionService implements MigrateAssessme
         var activeKitVersionId = assessmentResult.getAssessment().getAssessmentKit().getKitVersion();
         if (activeKitVersionId == null)
             throw new ValidationException(MIGRATE_ASSESSMENT_RESULT_KIT_VERSION_ACTIVE_VERSION_NOT_FOUND);
+
+        var currentAnswerRangeIds = loadAnswerRangePort.loadIdsByKitVersionId(assessmentResult.getKitVersionId());
+        var activeAnswerRangeIds = loadAnswerRangePort.loadIdsByKitVersionId(activeKitVersionId);
+        var missingAnswerRangeIds = currentAnswerRangeIds.stream()
+            .filter(id -> !activeAnswerRangeIds.contains(id))
+            .collect(Collectors.toSet());
+
+        var answerIdsOfQuestionsWithMissingAnswerRange = loadAnswerPort.loadIdsByAnswerRangeIds(missingAnswerRangeIds);
+        if (!answerIdsOfQuestionsWithMissingAnswerRange.isEmpty())
+            deleteAnswerPort.deleteSelectedOptionFromAnswers(answerIdsOfQuestionsWithMissingAnswerRange);
 
         updateAssessmentResultPort.updateKitVersionId(assessmentResult.getId(), activeKitVersionId);
         loadAssessmentResultCalculatePort.invalidateCalculate(assessmentResult.getId());
--- flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/MigrateAssessmentResultKitVersionServiceTest.java
+++ flickit-assessment-core/src/test/java/org/flickit/assessment/core/application/service/assessment/MigrateAssessmentResultKitVersionServiceTest.java
@@ -6,6 +6,9 @@ import org.flickit.assessment.common.exception.ResourceNotFoundException;
 import org.flickit.assessment.common.exception.ValidationException;
 import org.flickit.assessment.core.application.port.in.assessment.MigrateAssessmentResultKitVersionUseCase;
 import org.flickit.assessment.core.application.port.in.assessment.MigrateAssessmentResultKitVersionUseCase.Param;
+import org.flickit.assessment.core.application.port.out.answer.DeleteAnswerPort;
+import org.flickit.assessment.core.application.port.out.answer.LoadAnswerPort;
+import org.flickit.assessment.core.application.port.out.answerrange.LoadAnswerRangePort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.InvalidateAssessmentResultCalculatePort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.LoadAssessmentResultPort;
 import org.flickit.assessment.core.application.port.out.assessmentresult.UpdateAssessmentResultPort;
@@ -15,16 +18,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Consumer;
 
 import static org.flickit.assessment.common.application.domain.assessment.AssessmentPermission.MIGRATE_KIT_VERSION;
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
 import static org.flickit.assessment.core.common.ErrorMessageKey.MIGRATE_ASSESSMENT_RESULT_KIT_VERSION_ACTIVE_VERSION_NOT_FOUND;
 import static org.flickit.assessment.core.common.ErrorMessageKey.MIGRATE_ASSESSMENT_RESULT_KIT_VERSION_ASSESSMENT_RESULT_ID_NOT_FOUND;
-import static org.flickit.assessment.core.test.fixture.application.AssessmentResultMother.validResult;
-import static org.flickit.assessment.core.test.fixture.application.AssessmentResultMother.validResultWithoutActiveVersion;
+import static org.flickit.assessment.core.test.fixture.application.AssessmentResultMother.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -46,6 +47,15 @@ class MigrateAssessmentResultKitVersionServiceTest {
 
     @Mock
     private UpdateAssessmentResultPort updateAssessmentResultPort;
+
+    @Mock
+    private LoadAnswerRangePort loadAnswerRangePort;
+
+    @Mock
+    private LoadAnswerPort loadAnswerPort;
+
+    @Mock
+    private DeleteAnswerPort deleteAnswerPort;
 
     @Test
     void testMigrateAssessmentResultKitVersionService_CurrentUserDoesNotHaveAccess_ShouldThrowAccessDeniedException() {
@@ -91,19 +101,27 @@ class MigrateAssessmentResultKitVersionServiceTest {
 
     @Test
     void testMigrateAssessmentResultKitVersionService_ValidParameters_SuccessfulUpdate() {
-        var assessmentResult = validResult();
+        var assessmentResult = resultWithDeprecatedKitVersion();
         var activeKitVersionId = assessmentResult.getAssessment().getAssessmentKit().getKitVersion();
+        var currentAnswerRangeIds = Set.of(1L, 2L, 3L, 4L, 5L);
+        var activeAnswerRangeIds = Set.of(1L, 2L, 4L, 6L);
+        var deletedAnswerRangeIds = Set.of(3L, 5L);
+        var answersWithMissingAnswerRangeIds = Set.of(UUID.randomUUID(), UUID.randomUUID());
 
         var param = createParam(b -> b.assessmentId(assessmentResult.getAssessment().getId()));
 
         when(assessmentAccessChecker.isAuthorized(param.getAssessmentId(), param.getCurrentUserId(), MIGRATE_KIT_VERSION))
             .thenReturn(true);
         when(loadAssessmentResultPort.loadByAssessmentId(assessmentResult.getAssessment().getId())).thenReturn(Optional.of(assessmentResult));
+        when(loadAnswerRangePort.loadIdsByKitVersionId(assessmentResult.getKitVersionId())).thenReturn(currentAnswerRangeIds);
+        when(loadAnswerRangePort.loadIdsByKitVersionId(assessmentResult.getAssessment().getAssessmentKit().getKitVersion())).thenReturn(activeAnswerRangeIds);
+        when(loadAnswerPort.loadIdsByAnswerRangeIds(deletedAnswerRangeIds)).thenReturn(answersWithMissingAnswerRangeIds);
 
         service.migrateKitVersion(param);
 
         verify(updateAssessmentResultPort, times(1)).updateKitVersionId(assessmentResult.getId(), activeKitVersionId);
         verify(invalidateAssessmentResultCalculatePort, times(1)).invalidateCalculate(assessmentResult.getId());
+        verify(deleteAnswerPort).deleteSelectedOptionFromAnswers(answersWithMissingAnswerRangeIds);
     }
 
     private MigrateAssessmentResultKitVersionUseCase.Param createParam(Consumer<Param.ParamBuilder> changer) {
