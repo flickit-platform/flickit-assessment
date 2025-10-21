695a0f65feba4b1d66c231045667f41d09073e6d
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-10-21 19:36:37 +0330

    FAC-1295: Add DeleteAnswerRangeRestController

---
+++ flickit-assessment-kit/src/main/java/org/flickit/assessment/kit/adapter/in/rest/answerrange/DeleteAnswerRangeRestController.java
@@ -0,0 +1,34 @@
+package org.flickit.assessment.kit.adapter.in.rest.answerrange;
+
+import lombok.RequiredArgsConstructor;
+import org.flickit.assessment.common.config.jwt.UserContext;
+import org.flickit.assessment.kit.application.port.in.answerrange.DeleteAnswerRangeUseCase;
+import org.flickit.assessment.kit.application.port.in.answerrange.DeleteAnswerRangeUseCase.Param;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class DeleteAnswerRangeRestController {
+
+    private final UserContext userContext;
+    private final DeleteAnswerRangeUseCase useCase;
+
+    @DeleteMapping("/kit-versions/{kitVersionId}/answer-ranges/{answerRangeId}")
+    public ResponseEntity<Void> deleteAnswerRange(@PathVariable("kitVersionId") Long kitVersionId,
+                                                  @PathVariable("answerRangeId") Long answerRangeId) {
+        UUID currentUserId = userContext.getUser().id();
+        useCase.deleteAnswerRange(toParam(kitVersionId, answerRangeId, currentUserId));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private static Param toParam(Long kitVersionId, Long answerRangeId, UUID currentUserId) {
+        return new Param(answerRangeId,
+            kitVersionId,
+            currentUserId);
+    }
+}
