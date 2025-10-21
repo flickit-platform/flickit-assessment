f9b26249ab311e5db2b72b340eea2c6c1829313e
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-10-21 18:03:27 +0330

    FAC-1288: Add answer range modification ability - Remove the constraint from service - Refactor Service test to test this ability

--- flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/question/UpdateQuestionServiceTest.java
+++ flickit-assessment-kit/src/test/java/org/flickit/assessment/kit/application/service/question/UpdateQuestionServiceTest.java
@@ -2,14 +2,10 @@ package org.flickit.assessment.kit.application.service.question;
 
 import org.flickit.assessment.common.application.domain.kit.translation.QuestionTranslation;
 import org.flickit.assessment.common.exception.AccessDeniedException;
-import org.flickit.assessment.common.exception.ValidationException;
 import org.flickit.assessment.kit.application.domain.KitVersion;
-import org.flickit.assessment.kit.application.domain.Question;
 import org.flickit.assessment.kit.application.port.in.question.UpdateQuestionUseCase;
-import org.flickit.assessment.kit.application.port.out.assessmentkit.CountKitAssessmentsPort;
 import org.flickit.assessment.kit.application.port.out.expertgroup.LoadExpertGroupOwnerPort;
 import org.flickit.assessment.kit.application.port.out.kitversion.LoadKitVersionPort;
-import org.flickit.assessment.kit.application.port.out.question.LoadQuestionPort;
 import org.flickit.assessment.kit.application.port.out.question.UpdateQuestionPort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,10 +20,8 @@ import java.util.UUID;
 import java.util.function.Consumer;
 
 import static org.flickit.assessment.common.error.ErrorMessageKey.COMMON_CURRENT_USER_NOT_ALLOWED;
-import static org.flickit.assessment.kit.common.ErrorMessageKey.UPDATE_QUESTION_ANSWER_RANGE_ID_NOT_UPDATABLE;
 import static org.flickit.assessment.kit.test.fixture.application.AssessmentKitMother.simpleKit;
 import static org.flickit.assessment.kit.test.fixture.application.KitVersionMother.createKitVersion;
-import static org.flickit.assessment.kit.test.fixture.application.QuestionMother.createQuestion;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -46,12 +40,6 @@ class UpdateQuestionServiceTest {
     @Mock
     private LoadExpertGroupOwnerPort loadExpertGroupOwnerPort;
 
-    @Mock
-    private LoadQuestionPort loadQuestionPort;
-
-    @Mock
-    private CountKitAssessmentsPort countKitAssessmentsPort;
-
     @Captor
     private ArgumentCaptor<UpdateQuestionPort.Param> outPortParam;
 
@@ -69,32 +57,15 @@ class UpdateQuestionServiceTest {
         var throwable = assertThrows(AccessDeniedException.class, () -> service.updateQuestion(param));
         assertEquals(COMMON_CURRENT_USER_NOT_ALLOWED, throwable.getMessage());
 
-        verifyNoInteractions(loadQuestionPort, updateQuestionPort, countKitAssessmentsPort);
-    }
-
-    @Test
-    void testUpdateQuestion_whenAnswerRangeIdUpdatedAndKitIsUsedInAssessments_thenThrowValidationException() {
-        Question question = createQuestion(param.getAnswerRangeId() + 1);
-
-        when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
-        when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
-        when(loadQuestionPort.load(param.getQuestionId(), param.getKitVersionId())).thenReturn(question);
-        when(countKitAssessmentsPort.count(kitVersion.getKit().getId())).thenReturn(1L);
-
-        var throwable = assertThrows(ValidationException.class, () -> service.updateQuestion(param));
-        assertEquals(UPDATE_QUESTION_ANSWER_RANGE_ID_NOT_UPDATABLE, throwable.getMessageKey());
-
         verifyNoInteractions(updateQuestionPort);
     }
 
     @Test
     void testUpdateQuestion_whenCurrentUserIsOwner_thenUpdateQuestion() {
         param = createParam(b -> b.measureId(12L));
-        Question question = createQuestion(param.getAnswerRangeId());
 
         when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
         when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
-        when(loadQuestionPort.load(param.getQuestionId(), param.getKitVersionId())).thenReturn(question);
 
         service.updateQuestion(param);
 
@@ -112,18 +83,12 @@ class UpdateQuestionServiceTest {
         assertEquals(param.getTranslations(), outPortParam.getValue().translations());
         assertEquals(param.getCurrentUserId(), outPortParam.getValue().lastModifiedBy());
         assertNotNull(outPortParam.getValue().lastModificationTime());
-
-        verifyNoInteractions(countKitAssessmentsPort);
     }
 
     @Test
     void testUpdateQuestion_whenAnswerRangeIdUpdatedAndKitIsNotUsedInAssessments_thenUpdateQuestion() {
-        Question question = createQuestion(param.getAnswerRangeId() + 1);
-
         when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
         when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
-        when(loadQuestionPort.load(param.getQuestionId(), param.getKitVersionId())).thenReturn(question);
-        when(countKitAssessmentsPort.count(kitVersion.getKit().getId())).thenReturn(0L);
 
         service.updateQuestion(param);
 
@@ -145,11 +110,8 @@ class UpdateQuestionServiceTest {
 
     @Test
     void testUpdateQuestion_whenAnswerRangeIdOfQuestionIsNUll_thenUpdateQuestion() {
-        Question question = createQuestion(null);
-
         when(loadKitVersionPort.load(param.getKitVersionId())).thenReturn(kitVersion);
         when(loadExpertGroupOwnerPort.loadOwnerId(kitVersion.getKit().getExpertGroupId())).thenReturn(ownerId);
-        when(loadQuestionPort.load(param.getQuestionId(), param.getKitVersionId())).thenReturn(question);
 
         service.updateQuestion(param);
 
